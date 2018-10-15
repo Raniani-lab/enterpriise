9af941428880af8e1810dfd9feef494d7a745e98
jem-odoo <jem@openerp.com>
2018-10-15 07:51:08 +0000

    [ADD] project_enterprise: rename settings labels when enterprise
    Timesheet feature has been moved in enterprise (naming
    differently). "Task logs" is the community label, but is
    still visible when enteprise is installed. This commit
    provides a module to rename this into "Timesheets", to
    avoid user confusion.
    
    Task-1893021

---
+++ project_enterprise/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "Project Enterprise",
+    'summary': """Bridge module for mail and enterprise""",
+    'description': """
+Bridge module for mail and enterprise
+    """,
+    'category': 'Project',
+    'version': '1.0',
+    'depends': ['project'],
+    'data': [
+        'views/res_config_settings_views.xml',
+    ],
+    'auto_intall': True,
+    'license': 'OEEL-1',
+}

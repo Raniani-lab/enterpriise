6ff3615a89b3bb417a80a0b7bf57232aeaa0a020
Pierre Masereel <pim@odoo.com>
2018-11-16 13:28:46 +0000

    [FIX] quality_control: only check measure_frequency_type when test_type is measure Measure_frequency_type is used when test_type is measure it is not use full for other types so check only if it's measure type and remove all other check_execute_now methods
    TaskId: #1891804
    Closes: https://github.com/odoo/enterprise/pull/2820

--- quality_control/models/quality.py
+++ quality_control/models/quality.py
@@ -65,23 +65,24 @@ class QualityPoint(models.Model):
     @api.multi
     def check_execute_now(self):
         self.ensure_one()
-        if self.measure_frequency_type == 'all':
-            return True
-        elif self.measure_frequency_type == 'random':
-            return (random.random() < self.measure_frequency_value / 100.0)
-        elif self.measure_frequency_type == 'periodical':
-            delta = False
-            if self.measure_frequency_unit == 'day':
-                delta = relativedelta(days=self.measure_frequency_unit_value)
-            elif self.measure_frequency_unit == 'week':
-                delta = relativedelta(weeks=self.measure_frequency_unit_value)
-            elif self.measure_frequency_unit == 'month':
-                delta = relativedelta(months=self.measure_frequency_unit_value)
-            date_previous = datetime.today() - delta
-            checks = self.env['quality.check'].search([
-                ('point_id', '=', self.id),
-                ('create_date', '>=', date_previous.strftime(DEFAULT_SERVER_DATETIME_FORMAT))], limit=1)
-            return not(bool(checks))
+        if self.test_type == 'measure':
+            if self.measure_frequency_type == 'all':
+                return True
+            elif self.measure_frequency_type == 'random':
+                return (random.random() < self.measure_frequency_value / 100.0)
+            elif self.measure_frequency_type == 'periodical':
+                delta = False
+                if self.measure_frequency_unit == 'day':
+                    delta = relativedelta(days=self.measure_frequency_unit_value)
+                elif self.measure_frequency_unit == 'week':
+                    delta = relativedelta(weeks=self.measure_frequency_unit_value)
+                elif self.measure_frequency_unit == 'month':
+                    delta = relativedelta(months=self.measure_frequency_unit_value)
+                date_previous = datetime.today() - delta
+                checks = self.env['quality.check'].search([
+                    ('point_id', '=', self.id),
+                    ('create_date', '>=', date_previous.strftime(DEFAULT_SERVER_DATETIME_FORMAT))], limit=1)
+                return not(bool(checks))
         return super(QualityPoint, self).check_execute_now()
 
     def _get_type_default_domain(self):

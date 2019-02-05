199312090a6c7af9df03dac35ad5719eabd64a77
Romeo Fragomeli <rfr@odoo.com>
2019-02-05 13:25:10 +0000

    [IMP] web_mobile: add mobile (native) barcode scanner field
    After this commit a new widget will be available FieldMany2OneBarcode.
    When user touch this button the native app will open the native barcode
    scanner.
    
    Task ID: 1924766
    
    closes odoo/enterprise#3588

---
+++ web_mobile/static/src/js/barcode_fields.js
@@ -0,0 +1,95 @@
+odoo.define('web_mobile.barcode_fields', function (require) {
+"use strict";
+
+var mobile = require('web_mobile.rpc');
+var field_registry = require('web.field_registry');
+var relational_fields = require('web.relational_fields');
+
+/**
+ * Override the Many2One to open a dialog in mobile.
+ */
+var FieldMany2OneBarcode = relational_fields.FieldMany2One.extend({
+    template: "FieldMany2OneBarcode",
+    events: _.extend({}, relational_fields.FieldMany2One.prototype.events, {
+        'click .o_barcode_mobile': '_onBarcodeButtonClick',
+    }),
+
+    /**
+     * @override
+     */
+    start: function () {
+        this._super.apply(this, arguments);
+        this._startBarcode();
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * External button is visible
+     *
+     * @return {boolean}
+     * @private
+     */
+    _isExternalButtonVisible: function () {
+        return this.$external_button.is(':visible');
+    },
+    /**
+     * @override
+     * @private
+     */
+    _renderEdit: function () {
+        this._super.apply(this, arguments);
+        // Hide button if a record is set or external button is visible
+        this.$barcode_button && this.$barcode_button.toggle(!this._isExternalButtonVisible());
+    },
+    /**
+     * Initialisation of barcode button
+     *
+     * @private
+     */
+    _startBarcode: function () {
+        this.$barcode_button = this.$('.o_barcode_mobile');
+        // Hide button if a record is set
+        this.$barcode_button.toggle(!this.isSet());
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * On click on button
+     *
+     * @private
+     */
+    _onBarcodeButtonClick: function () {
+        var self = this;
+        mobile.methods.scanBarcode().then(function (response){
+            var barcode = response.data;
+            if (barcode) {
+                self._onBarcodeScanned(barcode);
+                mobile.methods.vibrate({'duration': 100});
+            } else {
+                mobile.methods.showToast({'message':'Please, Scan again !!'});
+            }
+        });
+    },
+    /**
+     * When barcode is scanned
+     *
+     * @param barcode
+     * @private
+     */
+    _onBarcodeScanned: function (barcode) {
+        this._invokeMobileDialog(barcode);
+    },
+});
+
+if (mobile.methods.scanBarcode) {
+    field_registry.add('many2one_barcode', FieldMany2OneBarcode);
+}
+
+return FieldMany2OneBarcode;
+});
---
+++ web_mobile/static/tests/barcodes_mobile_tests.js
@@ -0,0 +1,133 @@
+odoo.define('web_mobile.barcode.tests', function (require) {
+    "use strict";
+
+    var field_registry = require('web.field_registry');
+    var FormView = require('web.FormView');
+    var relational_fields = require('web.relational_fields');
+    var testUtils = require('web.test_utils');
+
+    var barcode_fields = require('web_mobile.barcode_fields');
+    var mobile = require('web_mobile.rpc');
+
+    var createView = testUtils.createView;
+
+    var NAME_SEARCH = "name_search";
+    var PRODUCT_PRODUCT = 'product.product';
+    var SALE_ORDER_LINE = 'sale_order_line';
+    var PRODUCT_FIELD_NAME = 'product_id';
+
+    QUnit.module('web_mobile', {
+        beforeEach: function () {
+            this.data = {
+                [PRODUCT_PRODUCT]: {
+                    fields: {
+                        id: {type: 'integer'},
+                        name: {},
+                        barcode: {},
+                    },
+                    records: [{
+                        id: 111,
+                        name: 'product_cable_management_box',
+                        barcode: '601647855631',
+                    }]
+                },
+                [SALE_ORDER_LINE]: {
+                    fields: {
+                        id: {type: 'integer'},
+                        [PRODUCT_FIELD_NAME]: {
+                            string: PRODUCT_FIELD_NAME,
+                            type: 'many2one',
+                            relation: PRODUCT_PRODUCT
+                        },
+                        product_uom_qty: {type: 'integer'}
+                    }
+                },
+            };
+        },
+    }, function () {
+
+        QUnit.test("web_mobile: barcode button in a mobile environment", function (assert) {
+            var self = this;
+
+            assert.expect(2);
+
+            // simulate a mobile environment
+            field_registry.add('many2one_barcode', barcode_fields);
+            var __many2oneDialog = mobile.methods.many2oneDialog;
+            var __scanBarcode = mobile.methods.scanBarcode;
+            var __showToast = mobile.methods.showToast;
+            var __vibrate = mobile.methods.vibrate;
+
+            mobile.methods.many2oneDialog = function (args) {
+                // Check if we have one result minimum (1 result + create + create and edit)
+                if (args.records.length >= 3) {
+                    return $.when({'data': {'action': 'select', 'value': {'id': args.records[0].id}}});
+                } else {
+                    return $.when({'data': {'action': 'cancel'}});
+                }
+            };
+
+            mobile.methods.scanBarcode = function () {
+                return $.when({
+                    'data': self
+                        .data[PRODUCT_PRODUCT]
+                        .records[0]
+                        .barcode
+                });
+            };
+
+            mobile.methods.showToast = function (data) {};
+
+            mobile.methods.vibrate = function () {};
+
+            var form = createView({
+                View: FormView,
+                arch:
+                    '<form>' +
+                        '<sheet>' +
+                            '<field name="' + PRODUCT_FIELD_NAME + '" widget="many2one_barcode"/>' +
+                        '</sheet>' +
+                    '</form>',
+                data: this.data,
+                model: SALE_ORDER_LINE,
+                mockRPC: function (route, args) {
+                    if (args.method === NAME_SEARCH && args.model === PRODUCT_PRODUCT) {
+                        return this._super.apply(this, arguments).then(function (result) {
+                            var records = self
+                                .data[PRODUCT_PRODUCT]
+                                .records
+                                .filter(function (record) {
+                                    return record.barcode === args.kwargs.name;
+                                })
+                                .map(function (record) {
+                                    return [record.id, record.name];
+                                })
+                            ;
+                            return records.concat(result);
+                        });
+                    }
+                    return this._super.apply(this, arguments);
+                },
+            });
+
+            var $scanButton = form.$('.o_barcode_mobile');
+
+            assert.equal($scanButton.length, 1, "has scanner button");
+
+            testUtils.dom.click($scanButton);
+
+            var selectedId = form.renderer.state.data[PRODUCT_FIELD_NAME].res_id;
+            assert.equal(selectedId, self.data[PRODUCT_PRODUCT].records[0].id,
+                "product found and selected (" +
+                self.data[PRODUCT_PRODUCT].records[0].barcode + ")");
+
+            mobile.methods.vibrate = __vibrate;
+            mobile.methods.showToast = __showToast;
+            mobile.methods.scanBarcode = __scanBarcode;
+            mobile.methods.many2oneDialog = __many2oneDialog;
+            field_registry.add('many2one_barcode', relational_fields.FieldMany2One);
+
+            form.destroy();
+        });
+    });
+});

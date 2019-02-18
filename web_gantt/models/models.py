ba1ebe999395bbd2540bd06d2f22eff80191be81
David Monjoie <dmo@odoo.com>
2019-02-18 15:09:33 +0000

    [IMP] web_gantt: add default gantt view generator
    Inspired by calendar.

--- web_gantt/models/models.py
+++ web_gantt/models/models.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
-
 from odoo import _, api, models
-
+from lxml.builder import E
+from odoo.exceptions import UserError
 
 
 class Base(models.AbstractModel):
     _inherit = 'base'
+
+    _start_name = 'date_start'       # start field to use for default gantt view
+    _stop_name = 'date_stop'         # stop field to use for default gantt view
+
+    @api.model
+    def _get_default_gantt_view(self):
+        """ Generates a default gantt view by trying to infer
+        time-based fields from a number of pre-set attribute names
+
+        :returns: a gantt view
+        :rtype: etree._Element
+        """
+        view = E.gantt(string=self._description)
+
+        gantt_field_names = {
+            '_start_name': ['date_start', 'start_date', 'x_date_start', 'x_start_date'],
+            '_stop_name': ['date_stop', 'stop_date', 'date_end', 'end_date', 'x_date_stop', 'x_stop_date', 'x_date_end', 'x_end_date'],
+        }
+        for name in gantt_field_names.keys():
+            if getattr(self, name) not in self._fields:
+                for dt in gantt_field_names[name]:
+                    if dt in self._fields:
+                        setattr(self, name, dt)
+                        break
+                else:
+                    raise UserError(_("Insufficient fields for Gantt View!"))
+        view.set('date_start', self._start_name)
+        view.set('date_stop', self._stop_name)
+
+        return view
 
     @api.model
     def gantt_unavailability(self, start_date, end_date, scale, group_bys=None):

4b43ecdc788026a5bd8619fd1fa38428e6c92084
Geoffroy Larue <gla@odoo.com>
2019-02-18 15:07:26 +0000

    [IMP] industry_fsm: module description, icon
    Add a module description, and delete the wrong svg icon.
    
    Task-1942377 Closes #3670

--- industry_fsm/__manifest__.py
+++ industry_fsm/__manifest__.py
@@ -5,6 +5,17 @@
     'description': """
 Field Services Management
 =========================
+This module adds the features needed for a modern Field service management.
+It installs the following apps:
+- Project
+- Timesheet
+- Sales
+
+Adds the following options:
+- reports on tasks
+- FSM app with custom view for onsite worker
+- add products on tasks
+- create Sales order with timesheets and products from tasks
 
     """,
     'category': 'Industry',

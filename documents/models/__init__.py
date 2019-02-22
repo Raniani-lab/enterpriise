8f2383b1cd208d81f251e461fae3725f015a2704
Thanh Dodeur <tso@odoo.com>
2019-02-22 14:49:09 +0000

    [REF] documents: removes outdated code
    Following the refactor of documents (3e4ef5054822672e6b7f735556afe1efc8f229e4), it isn't necessary to
    explicitly redefine the activity icon for activities on documents.
    
    closes odoo/enterprise#3715
    
    Signed-off-by: =?utf-8?q?Xavier_Doll=C3=A9_=28xdo=29?= <xdo@odoo.com>

--- documents/models/__init__.py
+++ documents/models/__init__.py
@@ -5,7 +5,6 @@ from . import ir_attachment
 from . import document
 from . import folder
 from . import res_partner
-from . import res_users
 from . import share
 from . import tags
 from . import workflow

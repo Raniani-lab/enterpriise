6843838d8982163294b5e71b2b27d3a278a90503
Lucas Lefèvre <lul@odoo.com>
2019-03-20 10:35:53 +0100

    [IMP] hr_payroll*: Add work entry data
    This commit adds benefit types and leave types data.
    
    In particular, add belgian data.

--- l10n_be_hr_payroll/__manifest__.py
+++ l10n_be_hr_payroll/__manifest__.py
@@ -32,6 +32,7 @@ Belgian Payroll Rules.
         'views/report_termination_fees.xml',
         'views/report_termination_holidays.xml',
         'data/l10n_be_hr_payroll_data.xml',
+        'data/cp200/work_entry_data.xml',
         'data/cp200/employee_double_holidays_data.xml',
         'data/cp200/employee_salary_data.xml',
         'data/cp200/employee_thirteen_month_data.xml',

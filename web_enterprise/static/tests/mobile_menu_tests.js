52cf54e7f9dcfe164042555949d923e91569c857
Prakash Prajapati <ppr@odoo.com>
2019-04-11 05:20:37 +0000

    [IMP] web_enterprise: Hovering out of a systray menu item closes its submenus dropdown
    While hovering in the systray a menu dropdown can only be open if user
    currently on his parent menu.
    
    It should close when user hover on another systray menu.
    
    move createMenu common method in test_utils, introduced new namespace
    web_enterprise.test_utils where all common code resides, used createMenu
    of test_utils in menu_tests.js and mobile_menu_tests.js
    
    Task: #1959678
    
    [IMP]Imrprovement
    
    [IMP]Improve test
    
    [IMP]rename file name
    
    [IMP]
    
    closes odoo/enterprise#4108
    
    Signed-off-by: VincentSchippefilt <VincentSchippefilt@users.noreply.github.com>

--- web_enterprise/static/tests/mobile_menu_tests.js
+++ web_enterprise/static/tests/mobile_menu_tests.js
@@ -1,51 +1,8 @@
 odoo.define('web_enterprise.mobile_menu_tests', function (require) {
 "use strict";
 
-var Menu = require('web_enterprise.Menu');
 var testUtils = require('web.test_utils');
-var SystrayMenu = require('web.SystrayMenu');
-var UserMenu = require('web.UserMenu');
-
-/**
- * create a menu from given parameters.
- *
- * @param {Object} params This object will be given to addMockEnvironment, so
- *   any parameters from that method applies
- * @param {Object} params.menuData This object will define the menu's data
- *   structure to render
- * @param {Widget[]} [params.systrayMenuItems=[]] This array will define the systray
- *  items to use. Will at least contain and default to UserMenu
- * @returns {Menu}
- */
-async function createMenu(params) {
-    var parent = testUtils.createParent({});
-
-    var systrayMenuItems = params.systrayMenuItems || [];
-    if (params.systrayMenuItems) {
-        delete params.systrayMenuItems;
-    }
-
-    var initialSystrayMenuItems = _.clone(SystrayMenu.Items);
-    SystrayMenu.Items = _.union([UserMenu], systrayMenuItems);
-
-    var menuData = params.menuData || {};
-    if (params.menuData) {
-        delete params.menuData;
-    }
-
-    var menu = new Menu(parent, menuData);
-    testUtils.mock.addMockEnvironment(menu, params);
-    return menu.appendTo($('#qunit-fixture')).then(function(){
-        var menuDestroy = menu.destroy;
-        menu.destroy = function () {
-            SystrayMenu.Items = initialSystrayMenuItems;
-            menuDestroy.call(this);
-            parent.destroy();
-        };
-
-        return menu;
-    });
-}
+var testUtilsEnterprise = require('web_enterprise.test_utils');
 
 QUnit.module('web_enterprise mobile_menu_tests', {
     beforeEach: function () {
@@ -102,7 +59,7 @@ QUnit.module('web_enterprise mobile_menu_tests', {
     QUnit.test('Burger Menu on home menu', async function (assert) {
         assert.expect(3);
 
-        var mobileMenu = await createMenu({ menuData: this.data });
+        var mobileMenu = await testUtilsEnterprise.createMenu({ menuData: this.data });
 
         if (mobileMenu.$burgerMenu.length) {
             var menuInMobileMenu = mobileMenu.$burgerMenu[0].querySelector('.o_burger_menu_user');
@@ -124,7 +81,7 @@ QUnit.module('web_enterprise mobile_menu_tests', {
     QUnit.test('Burger Menu on an App', async function (assert) {
         assert.expect(4);
 
-        var mobileMenu = await createMenu({ menuData: this.data });
+        var mobileMenu = await testUtilsEnterprise.createMenu({ menuData: this.data });
 
         mobileMenu.change_menu_section(3);
         mobileMenu.toggle_mode(false);

48613ac661ff198333f0b8e3c33d6394f3ca03ba
David Monjoie <dmo@odoo.com>
2019-04-17 11:57:32 +0000

    [FIX] web_gantt: fix slot insertion with DST
    When we switch to DST, two unusual things happen:
    - One day is missing one hour (1pm then 3pm)
    - One day has the same hour twice (2pm then 2pm)
    
    Before this commit, pills were placed in wrong slots on those days
    because of the missing/duplicated slots.
    
    To fix this, I changed the way we insert pills into slots, which
    is now the same way we match pills into intervals for aggregation.
    
    Because the variable was named "pillsInThisSlot", I thought I could
    merge the two and only compute it once. However, one of them put
    pills into slots (the new one), while the other actually put pills
    into intervals (the existing one), so I changed the name of some
    existing variables to make it more obvious which are referring to
    slots and which are referring to intervals.

--- web_gantt/static/tests/gantt_tests.js
+++ web_gantt/static/tests/gantt_tests.js
@@ -13,6 +13,42 @@ initialDate = new Date(initialDate.getTime() - initialDate.getTimezoneOffset() *
 
 var createView = testUtils.createView;
 
+// The gantt view uses momentjs for all time computation, which bypasses
+// tzOffset, making it hard to test. We had two solutions to test this:
+//
+// 1. Change everywhere in gantt where we use momentjs to manual
+//    manipulations using tzOffset so that we can manipulate it tests.
+//    Pros:
+//      - Consistent with other mechanisms in Odoo.
+//      - Full coverage of the behavior since we can manipulate in tests
+//    Cons:
+//      - We need to change nearly everything everywhere in gantt
+//      - The code works, it is sad to have to change it, risking to
+//        introduce new bugs, just to be able to test this.
+//      - Just applying the tzOffset to the date is not as easy as it
+//        sounds. Moment is smart. It offsets the date with the offset
+//        that you would locally have AT THAT DATE. Meaning that it
+//        sometimes offsets of 1 hour or 2 hour in the same locale
+//        depending as if the particular datetime we are offseting would
+//        be in DST or not at that time. We would have to handle all
+//        the DST conversion shenanigans manually to be correct.
+//
+// 2. Use the same computation path as the production code to compute
+//    the expected value.
+//    Pros:
+//      - Very easy to implement
+//      - Momentjs is smart, see last Con above. It does all the heavy
+//        lifting for us and it is a well known, stable and maintained
+//        library, so we can trust it on these matters.
+//    Cons:
+//      - The test relies on the behavior of Momentjs. If the library
+//        has a bug, the gantt view will have an issue that this test
+//        will never be able to see.
+//
+// Considering the Cons of the first option are tremendous and the one
+// of the second option is offest by the fact that we consider Momentjs
+// to be a trustworthy library, we chose option 2. It was required in
+// only a few test but we think it was still interesting to mention it.
 
 function getPillItemWidth($el) {
     return $el.attr('style').split('width: ')[1].split(';')[0];
@@ -2006,6 +2042,100 @@ QUnit.module('Views', {
 
         gantt.destroy();
         testUtils.unpatch(GanttRenderer);
+    });
+
+    QUnit.test('dst spring forward', async function (assert) {
+        assert.expect(2);
+
+        // This is one of the few tests which have dynamic assertions, see
+        // our justification for it in the comment at the top of this file.
+
+        var firstStartDateUTCString = '2019-03-30 03:00:00';
+        var firstStartDateUTC = moment.utc(firstStartDateUTCString);
+        var firstStartDateLocalString = firstStartDateUTC.local().format('YYYY-MM-DD hh:mm:ss');
+        this.data.tasks.records.push({
+            id: 99,
+            name: 'DST Task 1',
+            start: firstStartDateUTCString,
+            stop: '2019-03-30 03:30:00',
+        });
+
+        var secondStartDateUTCString = '2019-03-31 03:00:00';
+        var secondStartDateUTC = moment.utc(secondStartDateUTCString);
+        var secondStartDateLocalString = secondStartDateUTC.local().format('YYYY-MM-DD hh:mm:ss');
+        this.data.tasks.records.push({
+            id: 99,
+            name: 'DST Task 2',
+            start: secondStartDateUTCString,
+            stop: '2019-03-31 03:30:00',
+        });
+
+        var gantt = await createView({
+            View: GanttView,
+            model: 'tasks',
+            data: this.data,
+            arch: '<gantt date_start="start" date_stop="stop" default_scale="day"/>',
+            viewOptions: {
+                initialDate: new Date(2019, 2, 30, 8, 0, 0),
+            },
+        });
+
+        assert.containsOnce(gantt, '.o_gantt_row_container .o_gantt_cell[data-date="' + firstStartDateLocalString + '"] .o_gantt_pill_wrapper:contains(DST Task 1)',
+            'should be in the right cell');
+
+        await testUtils.dom.click(gantt.$buttons.find('.o_gantt_button_next'));
+
+        assert.containsOnce(gantt, '.o_gantt_row_container .o_gantt_cell[data-date="' + secondStartDateLocalString + '"] .o_gantt_pill_wrapper:contains(DST Task 2)',
+            'should be in the right cell');
+
+        gantt.destroy();
+    });
+
+    QUnit.test('dst fall back', async function (assert) {
+        assert.expect(2);
+
+        // This is one of the few tests which have dynamic assertions, see
+        // our justification for it in the comment at the top of this file.
+
+        var firstStartDateUTCString = '2019-10-26 03:00:00';
+        var firstStartDateUTC = moment.utc(firstStartDateUTCString);
+        var firstStartDateLocalString = firstStartDateUTC.local().format('YYYY-MM-DD hh:mm:ss');
+        this.data.tasks.records.push({
+            id: 99,
+            name: 'DST Task 1',
+            start: firstStartDateUTCString,
+            stop: '2019-10-26 03:30:00',
+        });
+
+        var secondStartDateUTCString = '2019-10-27 03:00:00';
+        var secondStartDateUTC = moment.utc(secondStartDateUTCString);
+        var secondStartDateLocalString = secondStartDateUTC.local().format('YYYY-MM-DD hh:mm:ss');
+        this.data.tasks.records.push({
+            id: 99,
+            name: 'DST Task 2',
+            start: secondStartDateUTCString,
+            stop: '2019-10-27 03:30:00',
+        });
+
+        var gantt = await createView({
+            View: GanttView,
+            model: 'tasks',
+            data: this.data,
+            arch: '<gantt date_start="start" date_stop="stop" default_scale="day"/>',
+            viewOptions: {
+                initialDate: new Date(2019, 9, 26, 8, 0, 0),
+            },
+        });
+
+        assert.containsOnce(gantt, '.o_gantt_row_container .o_gantt_cell[data-date="' + firstStartDateLocalString + '"] .o_gantt_pill_wrapper:contains(DST Task 1)',
+            'should be in the right cell');
+
+        await testUtils.dom.click(gantt.$buttons.find('.o_gantt_button_next'));
+
+        assert.containsOnce(gantt, '.o_gantt_row_container .o_gantt_cell[data-date="' + secondStartDateLocalString + '"] .o_gantt_pill_wrapper:contains(DST Task 2)',
+            'should be in the right cell');
+
+        gantt.destroy();
     });
 
     // OTHER TESTS

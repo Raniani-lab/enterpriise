b00193d89b6dfe05c299406911d39cd0304ef41b
Geoffroy Larue <gla@odoo.com>
2019-04-29 14:36:07 +0000

    [IMP] helpdesk_sale: proper use of domain
    Improve generation of domain on sale order field on tickets by avoiding
    use of custom method.
    
    Task-1974083
    
    closes odoo/enterprise#4227
    
    Signed-off-by: Jérome Maes (jem) <jem@openerp.com>

--- helpdesk_sale/models/helpdesk.py
+++ helpdesk_sale/models/helpdesk.py
@@ -7,13 +7,13 @@ from odoo import api, fields, models
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    sale_order_ids = fields.Many2many('sale.order', compute="compute_sale_order_ids")
-    sale_order_id = fields.Many2one('sale.order', string='Sales Order', domain="[('id', 'in', sale_order_ids)]")
+    commercial_partner_id = fields.Many2one(related='partner_id.commercial_partner_id')
+    sale_order_id = fields.Many2one('sale.order', string='Sales Order', domain="[('partner_id', 'child_of', commercial_partner_id)]")
 
-    @api.depends('partner_id')
-    def compute_sale_order_ids(self):
-        for ticket in self:
-            domain = []
-            if ticket.partner_id.commercial_partner_id:
-                domain = [('partner_id', 'child_of', ticket.partner_id.commercial_partner_id.id)]
-            ticket.sale_order_ids = ticket.env['sale.order'].search(domain)
+    @api.onchange('partner_id')
+    def _onchange_partner_id_so_domain(self):
+        return {
+            'domain': {
+                'sale_order_id': [('partner_id', 'child_of', self.commercial_partner_id.id)] if self.partner_id else []
+            }
+        }

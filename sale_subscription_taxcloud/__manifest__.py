f4a2bb345ddad79eb19923333dc3cbff9c4ebdd0
Damien Bouvy <dbo@odoo.com>
2019-05-13 07:01:11 +0000

    [IMP] sale_subscription_taxcloud: proper flow
    This module was initially added as a bugfix; now it needs to be a proper
    module that handles flow properly by automatically updating taxes with
    taxcloud upon invoice generation.

--- sale_subscription_taxcloud/__manifest__.py
+++ sale_subscription_taxcloud/__manifest__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     "name": "TaxCloud and Subscriptions",
     "summary": """Compute taxes with TaxCloud after automatic invoice creation.""",
@@ -7,7 +9,8 @@
     "category": "Sales/Subscription",
     "depends": ["sale_subscription", "account_taxcloud"],
     "data": [
-        "views/subscription_portal_templates.xml"
+        "views/subscription_portal_templates.xml",
+        "views/sale_subscription_views.xml",
     ],
     "auto_install": True,
     "license": "OEEL-1",

5ec0e1fec5a6cd81385f93e8a43694b6c094d5db
svs-odoo <svs@odoo.com>
2019-05-16 08:50:36 +0000

    [REM] stock_barcode: barcode form handler on inv adj
    The barcode form handler relies on the onchange mechanism. Now that the
    inventory lines are not displayed in a one2many but in an editable list,
    onchanges aren't triggered on the parent object (the inv adj) thus
    making the barcode form handler useless.
    
    Task #1935921
    
    closes odoo/enterprise#4059
    
    Signed-off-by: Simon Lejeune (sle) <sle@openerp.com>

--- stock_barcode/models/stock_inventory.py
+++ stock_barcode/models/stock_inventory.py
@@ -7,7 +7,6 @@ from odoo.exceptions import UserError
 class StockInventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
-    product_barcode = fields.Char(related='product_id.barcode', readonly=False)
     dummy_id = fields.Char(compute='_compute_dummy_id', inverse='_inverse_dummy_id')
 
     def _compute_dummy_id(self):
@@ -18,10 +17,7 @@ class StockInventoryLine(models.Model):
 
 
 class StockInventory(models.Model):
-    _name = 'stock.inventory'
-    _inherit = ['stock.inventory', 'barcodes.barcode_events_mixin']
-
-    scan_location_id = fields.Many2one('stock.location', 'Scanned Location', store=False)
+    _inherit = 'stock.inventory'
 
     def action_client_action(self):
         """ Open the mobile view specialized in handling barcodes on mobile devices.
@@ -86,8 +82,6 @@ class StockInventory(models.Model):
 
     @api.model
     def open_new_inventory(self):
-        use_form_handler = self.env['ir.config_parameter'].sudo().get_param('stock_barcode.use_form_handler')
-
         company_user = self.env.user.company_id
         warehouse = self.env['stock.warehouse'].search([('company_id', '=', company_user.id)], limit=1)
         if warehouse:
@@ -95,70 +89,21 @@ class StockInventory(models.Model):
         else:
             raise UserError(_('You must define a warehouse for the company: %s.') % (company_user.name,))
 
-        if use_form_handler:
-            action = self.env.ref('stock_barcode.stock_inventory_action_new_inventory').read()[0]
-            if self.env.ref('stock.warehouse0', raise_if_not_found=False):
-                new_inv = self.env['stock.inventory'].create({
-                    'start_empty': True,
-                    'name': fields.Date.context_today(self),
-                    'location_ids': [(4, default_location_id.id, None)],
-                })
-                new_inv.action_start()
-                action['res_id'] = new_inv.id
-        else:
-            action = self.env.ref('stock_barcode.stock_barcode_inventory_client_action').read()[0]
-            if self.env.ref('stock.warehouse0', raise_if_not_found=False):
-                new_inv = self.env['stock.inventory'].create({
-                    'start_empty': True,
-                    'name': fields.Date.context_today(self),
-                    'location_ids': [(4, default_location_id.id, None)],
-                })
-                new_inv.action_start()
-                action['res_id'] = new_inv.id
+        action = self.env.ref('stock_barcode.stock_barcode_inventory_client_action').read()[0]
+        if self.env.ref('stock.warehouse0', raise_if_not_found=False):
+            new_inv = self.env['stock.inventory'].create({
+                'start_empty': True,
+                'name': fields.Date.context_today(self),
+                'location_ids': [(4, default_location_id.id, None)],
+            })
+            new_inv.action_start()
+            action['res_id'] = new_inv.id
 
-                params = {
-                    'model': 'stock.inventory',
-                    'inventory_id': new_inv.id,
-                }
-                action['context'] = {'active_id': new_inv.id}
-                action = dict(action, target='fullscreen', params=params)
+            params = {
+                'model': 'stock.inventory',
+                'inventory_id': new_inv.id,
+            }
+            action['context'] = {'active_id': new_inv.id}
+            action = dict(action, target='fullscreen', params=params)
 
         return action
-
-    def _add_product(self, product, qty=1.0):
-        corresponding_line = self.line_ids.filtered(lambda r: r.product_id.id == product.id and (self.scan_location_id.id == r.location_id.id or not self.scan_location_id))
-        if corresponding_line:
-            corresponding_line[0].product_qty += qty
-        else:
-            StockQuant = self.env['stock.quant']
-            company_id = self.company_id.id
-            if not company_id:
-                company_id = self._uid.company_id.id
-            dom = [('company_id', '=', company_id), ('location_id', '=', self.scan_location_id.id or self.location_id.id), ('lot_id', '=', False),
-                        ('product_id','=', product.id), ('owner_id', '=', False), ('package_id', '=', False)]
-            quants = StockQuant.search(dom)
-            th_qty = sum([x.quantity for x in quants])
-            self.line_ids += self.line_ids.new({
-                'location_id': self.scan_location_id.id or self.location_id.id,
-                'product_id': product.id,
-                'product_uom_id': product.uom_id.id,
-                'theoretical_qty': th_qty,
-                'product_qty': qty,
-            })
-        return True
-
-    def on_barcode_scanned(self, barcode):
-        product = self.env['product.product'].search([('barcode', '=', barcode)])
-        if product:
-            self._add_product(product)
-            return
-
-        product_packaging = self.env['product.packaging'].search([('barcode', '=', barcode)])
-        if product_packaging.product_id:
-            self._add_product(product_packaging.product_id, product_packaging.qty)
-            return
-
-        location = self.env['stock.location'].search([('barcode', '=', barcode)])
-        if location:
-            self.scan_location_id = location[0]
-            return

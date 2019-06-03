52228b9cfa16126e059c53fe436d3745fc71bdf1
MaxLarue <mla@odoo.com>
2019-06-03 11:04:37 +0000

    [IMP] project_forecast: display unavailability in gantt
    This is useful to visualize when an employee is able to work on tasks/projects.
    This takes into account ut the working time of the employee and the leaves he may have.
    
    related task #1929079

--- project_forecast/models/hr.py
+++ project_forecast/models/hr.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import itertools
 import pytz
 
 from odoo import api, fields, models
@@ -31,4 +32,24 @@ class Employee(models.Model):
                 employee_mapping[employee.id] = (work_intervals[0][0].astimezone(pytz.utc), work_intervals[-1][1].astimezone(pytz.utc))
             else:
                 employee_mapping[employee.id] = (None, None)
+        return employee_mapping
+
+    def _get_unavailable_intervals(self, start, end):
+        """Compute the intervals during which employee is unavailable with hour granularity between start and end
+        """
+        start_datetime = timezone_datetime(start)
+        end_datetime = timezone_datetime(end)
+        employee_mapping = {}
+        for employee in self:
+            calendar = employee.resource_calendar_id
+            resource = employee.resource_id
+            employee_work_intervals = calendar._work_intervals(start_datetime, end_datetime, resource)
+            employee_work_intervals = [(start, stop) for start, stop, meta in employee_work_intervals]
+            # start + flatten(intervals) + end
+            employee_work_intervals = [start_datetime] + list(itertools.chain.from_iterable(employee_work_intervals)) + [end_datetime]
+            # put it back to UTC
+            employee_work_intervals = list(map(lambda dt: dt.astimezone(pytz.utc), employee_work_intervals))
+            # pick groups of two
+            employee_work_intervals = list(zip(employee_work_intervals[0::2], employee_work_intervals[1::2]))
+            employee_mapping[employee.id] = employee_work_intervals
         return employee_mapping

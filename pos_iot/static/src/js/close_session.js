24c74b3c7faf4284a19854990d196db56edb228d
Antoine Prieels <anp@odoo.com>
2019-06-04 13:47:35 +0200

    [IMP] pos_iot: Send terminal balance from backend
    Send the terminal balance when pressing on "Validate Closing & Post
    Entries". The balance will be sent before the accounting entries are
    created.
    
    TaskId: 1932416

---
+++ pos_iot/static/src/js/close_session.js
@@ -0,0 +1,140 @@
+odoo.define('pos_iot.CloseSession', function (require) {
+"use static";
+
+var core = require('web.core');
+var rpc = require('web.rpc');
+var widgetRegistry = require('web.widget_registry');
+var Widget = require('web.Widget');
+var DeviceProxy = require('iot.widgets').DeviceProxy;
+var PrinterProxy = require('pos_iot.Printer');
+
+var _t = core._t;
+
+var CloseSession = Widget.extend({
+    template: 'CloseSession',
+    events: {
+        'click': '_onClickCloseSession',
+    },
+
+    /**
+     * @override
+     */
+    init: function (parent, record, options) {
+        var res = this._super.apply(this, arguments);
+        this.attrs = options.attrs;
+        this.data = record.data;
+        this.loaded = this.load(this.data.config_id.res_id)
+        return res;
+    },
+
+    load: function (config_id) {
+        var self = this;
+        return rpc.query({
+            model: 'pos.config',
+            method: 'read',
+            args: [[config_id], ['iotbox_id', 'proxy_ip', 'iface_payment_terminal', 'iface_printer_id']]
+        }).then(function (config) {
+            if (config[0].iface_payment_terminal) {
+                self.iot_ip = config[0].proxy_ip;
+                rpc.query({
+                    model: 'iot.device',
+                    method: 'search_read',
+                    args: [
+                        [['iot_id', '=', config[0].iotbox_id[0]]],
+                        ['id', 'type', 'identifier'],
+                    ],
+                }).then(function (devices) {
+                    devices.forEach(function (device) {
+                        if (config[0].iface_printer_id && device.id === config[0].iface_printer_id[0]) {
+                            self.printer = new PrinterProxy({
+                                iot_ip: self.iot_ip,
+                                identifier: device.identifier,
+                            });
+                        } else if (device.type === "payment") {
+                            self.terminal = new DeviceProxy({
+                                iot_ip: self.iot_ip,
+                                identifier: device.identifier,
+                            });
+                        }
+                    });
+                });
+            }
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Calls the method specified in this.action on the current pos.session
+     */
+    _performAction: function () {
+        return this._rpc({
+            model: 'pos.session',
+            method: this.attrs.action,
+            args: [this.data.id],
+        });
+
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Sends a Balance operation to the terminal if needed then performs the
+     * closing action.
+     */
+    _onClickCloseSession: function () {
+        var self = this;
+        this.loaded.then(function () {
+            if (self.terminal) {
+                self.terminal.add_listener(self._onValueChange.bind(self));
+                self.terminal.action({ messageType: 'Balance' })
+                    .then(self._onTerminalActionResult.bind(self));
+            } else {
+                self._performAction().then(self.trigger_up.bind(self, 'reload'));
+            }
+        });
+    },
+
+    /**
+     * Processes the return value of an action sent to the terminal
+     *
+     * @param {Object} data 
+     * @param {boolean} data.result
+     */
+    _onTerminalActionResult: function (data) {
+        if (data.result === false) {
+            this.do_warn(_t('Connection to terminal failed'), _t('Please check if the terminal is still connected.'));
+            this.terminal.remove_listener();
+        }
+    },
+
+    /**
+     * Listens for changes from the payment terminal, prints receipts destined
+     * to the merchant then performs the closing action.
+     *
+     * @param {Object} data
+     * @param {String} data.Error
+     * @param {String} data.TicketMerchant
+     */
+    _onValueChange: function (data) {
+        var self = this;
+
+        if (data.Error) {
+            this.do_warn(_t('Error performing balance'), data.Error);
+            return;
+        } else if (data.TicketMerchant && this.printer) {
+            this.printer.print_receipt("<div class='pos-receipt'><div class='pos-payment-terminal-receipt'>" + data.TicketMerchant.replace(/\n/g, "<br />") + "</div></div>");
+        }
+
+        this._performAction().then(self.trigger_up.bind(self, 'reload'));
+        this.terminal.remove_listener();
+    },
+});
+
+widgetRegistry.add('close_session', CloseSession);
+
+});

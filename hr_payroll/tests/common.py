1f490decaeb80b897fd6620b8dfe8917e76670cb
Christophe Simonis <chs@odoo.com>
2019-06-27 23:37:40 +0200

    [FIX] hr_payroll: do not force validity start date on test leave types
    Oversight of previous forward-port.

--- hr_payroll/tests/common.py
+++ hr_payroll/tests/common.py
@@ -69,6 +69,7 @@ class TestPayslipBase(TransactionCase):
             'name': 'Unpaid Leaves',
             'time_type': 'leave',
             'allocation_type': 'no',
+            'validity_start': False,
             'work_entry_type_id': self.work_entry_type_unpaid.id
         })
 

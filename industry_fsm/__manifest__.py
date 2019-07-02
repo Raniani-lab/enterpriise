6027dc3d7f310712b7306c8dc439dcef1e886edd
MaxLarue <mla@odoo.com>
2019-07-02 15:56:44 +0000

    [IMP] industry_fsm: add task analysis report for fsm
    This commit create a new reporting menu to fsm.
    This menu brings you on 'almost' the same screen as in project.
    The only difference being that the tasks selected to generate the report are restricted to one belonging to an fsm project.
    
    related task 2009563

--- industry_fsm/__manifest__.py
+++ industry_fsm/__manifest__.py
@@ -25,7 +25,9 @@ Adds the following options:
     'data': [
         'data/fsm_data.xml',
         'security/fsm_security.xml',
+        'security/ir.model.access.csv',
         'views/res_config_settings_views.xml',
+        'report/project_report_views.xml',
         'views/product_product_views.xml',
         'views/project_views.xml',
         'views/assets.xml',

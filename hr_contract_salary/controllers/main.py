d0ab81becf40d7231b8dc2c9022332b3821457f6
Yannick Tivisse <yti@odoo.com>
2019-07-18 11:18:24 +0200

    [IMP] hr_contract_salary: Split method to allow inheritence

--- hr_contract_salary/controllers/main.py
+++ hr_contract_salary/controllers/main.py
@@ -234,22 +234,8 @@ class website_hr_contract_salary(http.Controller):
             'countries': request.env['res.country'].search([]),
         }
 
-    def create_new_contract(self, contract, advantages, no_write=False, **kw):
-        # Generate a new contract with the current modifications
-        personal_info = advantages['personal_info']
-
-        if kw.get('employee'):
-            employee = kw.get('employee')
-        elif contract.employee_id:
-            employee = contract.employee_id
-        else:
-            employee = request.env['hr.employee'].sudo().create({
-                'name': 'Simulation Employee',
-                'active': False
-            })
-        if personal_info:
-            employee.with_context(lang=None).update_personal_info(personal_info, no_name_write=bool(kw.get('employee')))
-        new_contract = request.env['hr.contract'].sudo().new({
+    def _get_new_contract_values(self, contract, employee, advantages):
+        return {
             'active': False,
             'name': contract.name if contract.state == 'draft' else "Package Simulation",
             'job_id': contract.job_id.id,
@@ -280,7 +266,24 @@ class website_hr_contract_salary(http.Controller):
             'contract_type': advantages['contract_type'],
             'internet': advantages['internet'],
             'date_start': fields.Date.today().replace(day=1),
-        })
+        }
+
+    def create_new_contract(self, contract, advantages, no_write=False, **kw):
+        # Generate a new contract with the current modifications
+        personal_info = advantages['personal_info']
+
+        if kw.get('employee'):
+            employee = kw.get('employee')
+        elif contract.employee_id:
+            employee = contract.employee_id
+        else:
+            employee = request.env['hr.employee'].sudo().create({
+                'name': 'Simulation Employee',
+                'active': False
+            })
+        if personal_info:
+            employee.with_context(lang=None).update_personal_info(personal_info, no_name_write=bool(kw.get('employee')))
+        new_contract = request.env['hr.contract'].sudo().new(self._get_new_contract_values(contract, employee, advantages))
 
         if advantages['has_mobile']:
             new_contract.mobile = request.env['ir.default'].sudo().get('hr.contract', 'mobile')

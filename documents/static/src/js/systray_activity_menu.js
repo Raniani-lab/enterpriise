6fd2607779ac7454a510f67d304991278d7c9790
Parth Choksi <pch@odoo.com>
2019-07-19 11:56:25 +0000

    [IMP] documents: show "Request a Document" just for documents users
    Before this commit, everyone had "Request a Document" in the systray
    activity menu. This should only be visible for users of Documents.
    
    This commit displays this item only for users of Documents.
    
    Task-Id: 2034580
    
    closes odoo/enterprise#4953
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    
    
    Co-authored-by: Mohammed Shekha <msh@odoo.com>

--- documents/static/src/js/systray_activity_menu.js
+++ documents/static/src/js/systray_activity_menu.js
@@ -3,10 +3,20 @@ odoo.define('documents.systray.ActivityMenu', function (require) {
 
 var ActivityMenu = require('mail.systray.ActivityMenu');
 
+const session = require('web.session');
+
 ActivityMenu.include({
     events: _.extend({}, ActivityMenu.prototype.events, {
         'click .o_sys_documents_request': '_onRequestDocument',
     }),
+
+    /**
+     * @override
+     */
+    async willStart() {
+        await this._super(...arguments);
+        this.hasDocumentUserGroup = await session.user_has_group('documents.group_documents_user');
+    },
 
     //--------------------------------------------------------------------------
     // Handlers
--- documents/static/tests/documents_systray_activity_menu_tests.js
+++ documents/static/tests/documents_systray_activity_menu_tests.js
@@ -15,7 +15,7 @@ QUnit.module('mail', {}, function () {
     });
 
     QUnit.test('activity menu widget: documents request button', async function (assert) {
-        assert.expect(2);
+        assert.expect(4);
 
         var activityMenu = new ActivityMenu();
         testUtils.mock.addMockEnvironment(activityMenu, {
@@ -32,10 +32,20 @@ QUnit.module('mail', {}, function () {
                         "should open the document request form");
                 },
             },
+            session: {
+                async user_has_group(group) {
+                    if (group === 'documents.group_documents_user') {
+                        assert.step('user_has_group:documents.group_documents_user');
+                        return true;
+                    }
+                    return this._super(...arguments);
+                },
+            },
         });
         await activityMenu.appendTo($('#qunit-fixture'));
 
         await testUtils.dom.click(activityMenu.$('> .dropdown-toggle'));
+        assert.verifySteps(['user_has_group:documents.group_documents_user']);
         assert.containsOnce(activityMenu, '.o_sys_documents_request');
         await testUtils.dom.click(activityMenu.$('.o_sys_documents_request'));
 

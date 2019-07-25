814bbaa97978deaca0d5e996c789160ed62d3344
Thibault Delavallée <tde@odoo.com>
2019-07-25 14:47:17 +0000

    [ADD] marketing_automation_sms: allow to send SMS in marketing campaigns
    PURPOSE
    
    SMS are a powerful marketing tool. For instance it is perfect to announce a
    sale or to communicate a coupon code, to welcome a new customer in a fidelity
    program, ...
    
    Purpose of this task is to integrate SMS capabilities in mass mailing. It
    will use same mailing objects but sending SMS instead of emails. Some metrics
    and flows will have to be slightly updated at the same time.
    
    SPECIFICATIONS
    
    Integrate SMS activities inside marketing automation.
    
    In the activity modal
      * See https://drive.google.com/a/odoo.com/file/d/1lJdeoNtYsJtyFMWyrHJUZxAn3NFLZDSQ/view?usp=drivesdk
      * add value 'SMS Text Message' to field 'Activity Type' (in second position)
      * switch field 'Activity Type' widget to vertical radio
      * field 'Template' should display SMS or Mail template depending of the value
        of 'Activity Type'
    
    For SMS activities
      * in the marketing.campaign formview, the 'workflow card' of an SMS activity
        should have the number of texts sent at the bottom (the Clicked and Replied
        stats can be removed);
      * Rename "EMAIL DETAILS" to "SMS DETAILS"
      * Rename "Email" to "SMS Text Message"
      * The icon should be switched to fa-comment (https://nimb.ws/j48hp2)
    
    As we have no way to know whether the SMS was received, and since we do not
    handle answers, in the 'Add child activity' widget (at the bottom of the card)
    the only option available is 'add another activity' (like for server action
    activities), see https://drive.google.com/a/odoo.com/file/d/1dkAWsl520Nlg4q8XCmI-eMKbEfsAje6j/view?usp=drivesdk
    
    LINKS
    
    Task 1997464 (mass mailing SMS)
    Task 1922198 (marketing automation SMS)
    PR #4864
    Community PR https://github.com/odoo/odoo/pull/34424

---
+++ marketing_automation_sms/models/marketing_activity.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+import logging
+
+from odoo import fields, models, _
+from odoo.fields import Datetime
+from odoo.exceptions import AccessError
+
+_logger = logging.getLogger(__name__)
+
+
+class MarketingActivity(models.Model):
+    _inherit = ['marketing.activity']
+
+    activity_type = fields.Selection(selection_add=[('sms', 'SMS')])
+    sms_mailing_id = fields.Many2one(
+        'mailing.mailing', string='SMS Mailing', related='mass_mailing_id',
+        help='UX Field to choose an SMS mailing. Separate field to ease options / default / domain propagation.')
+
+    def _execute_sms(self, traces):
+        res_ids = [r for r in set(traces.mapped('res_id'))]
+
+        mailing = self.mass_mailing_id.with_context(
+            default_marketing_activity_id=self.ids[0],
+        )
+
+        # we only allow to continue if the user has sufficient rights, as a sudo() follows
+        if not self.env.is_superuser() and not self.user_has_groups('marketing_automation.group_marketing_automation_user'):
+            raise AccessError(_('To use this feature you should be an administrator or belong to the marketing automation group.'))
+        try:
+            mailing.sudo().action_send_sms(res_ids)
+        except Exception as e:
+            _logger.warning(_('Marketing Automation: activity <%s> encountered mass mailing issue %s'), self.id, str(e), exc_info=True)
+            traces.write({
+                'state': 'error',
+                'schedule_date': Datetime.now(),
+                'state_msg': _('Exception in mass SMS: %s') % str(e),
+            })
+        else:
+            failed_stats = self.env['mailing.trace'].sudo().search([
+                ('marketing_trace_id', 'in', traces.ids),
+                '|', ('exception', '!=', False), ('ignored', '!=', False)])
+            ignored_doc_ids = [stat.res_id for stat in failed_stats if stat.exception]
+            error_doc_ids = [stat.res_id for stat in failed_stats if stat.ignored]
+
+            processed_traces = traces
+            ignored_traces = traces.filtered(lambda trace: trace.res_id in ignored_doc_ids)
+            error_traces = traces.filtered(lambda trace: trace.res_id in error_doc_ids)
+
+            if ignored_traces:
+                ignored_traces.write({
+                    'state': 'canceled',
+                    'schedule_date': Datetime.now(),
+                    'state_msg': _('Email ignored')
+                })
+                processed_traces = processed_traces - ignored_traces
+            if error_traces:
+                error_traces.write({
+                    'state': 'error',
+                    'schedule_date': Datetime.now(),
+                    'state_msg': _('Email failed')
+                })
+                processed_traces = processed_traces - error_traces
+            if processed_traces:
+                processed_traces.write({
+                    'state': 'processed',
+                    'schedule_date': Datetime.now(),
+                })
+        return True

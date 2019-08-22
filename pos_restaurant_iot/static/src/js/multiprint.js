b6f852567c388024b9bf0c2f17b989fe81db872f
Pierre Masereel <pim@odoo.com>
2019-08-22 11:42:49 +0000

    [FIX] pos_epson_printer_restaurant: call to super
    The model PosModel is extended, and super is calaled as if it was
    included, we have fixed it by calling the parent prototype correctly.
    
    closes odoo/enterprise#5234
    
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- pos_restaurant_iot/static/src/js/multiprint.js
+++ pos_restaurant_iot/static/src/js/multiprint.js
@@ -6,9 +6,16 @@ var PrinterProxy = require('pos_iot.Printer');
 
 models.load_fields("restaurant.printer", 'device_identifier');
 
+var _super_posmodel = models.PosModel.prototype;
+
 models.PosModel = models.PosModel.extend({
     create_printer: function (config) {
-        return new PrinterProxy({ iot_ip: config.proxy_ip, identifier: config.device_identifier }, this);
+        if (config.device_identifier && config.printer_type === "iot"){
+            return new PrinterProxy({ iot_ip: config.proxy_ip, identifier: config.device_identifier }, this);
+        }
+        else {
+            return _super_posmodel.create_printer.apply(this, arguments);
+        }
     },
 });
 });

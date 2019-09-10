0432c45f155dfe9decb85337540e6786ad7b87e4
Lucas Lefèvre <lul@odoo.com>
2019-09-10 13:52:23 +0000

    [FIX] hr_appraisal: Avoid infinite employee creation loop
    If you have an empty database, without any employee:
    Try to create your first employee.
    
    But by default "Appraisal by managers" is checked,
    hence the appraisal managers field is required.
    You try to set a manager, but there are no employee!
    You then want to create an employee to be the manager.
    
    But by default "Appraisal by managers" is checked,
    hence the appraisal managers field is required.
    You try to set a manager, but there are no employee!
    You then want to create an employee to be the manager.
    
    But by default "Appraisal by managers" is checked,
    hence the appraisal managers field is required.
    You try to set a manager, but there are no employee!
    You then want to create an employee to be the manager.
    
    But by default "Appraisal by managers" is checked,
    hence the appraisal managers field is required.
    You try to set a manager, but there are no employee!
    You then want to create an employee to be the manager.
    
       ---->
      ^     |
      |     |
      |     v
       <----
    
    To solve this infinite employee creation loop,
    the appraisal managers field should not be checked by default.
    For consistency, the appraisal by employee should not be checked.

--- hr_appraisal/models/res_company.py
+++ hr_appraisal/models/res_company.py
@@ -24,8 +24,8 @@ class ResCompany(models.Model):
     appraisal_send_reminder = fields.Boolean(string='Send Automatic Appraisals Reminder', default=True)
     appraisal_reminder = fields.One2many('hr.appraisal.reminder', 'company_id', string='Appraisal Reminder',
         copy=True, groups="base.group_system")
-    appraisal_by_manager = fields.Boolean(string='Manager', default=True)
-    appraisal_by_employee = fields.Boolean(string='Employee', default=True)
+    appraisal_by_manager = fields.Boolean(string='Manager', default=False)
+    appraisal_by_employee = fields.Boolean(string='Employee', default=False)
     appraisal_by_collaborators = fields.Boolean(string='Collaborators', default=False)
     appraisal_by_colleagues = fields.Boolean(string='Colleagues', default=False)
     appraisal_by_manager_body_html = fields.Html('Manager Mail Content', default=_get_default_appraisal_body_html)

748d7553ca893bf9c48bdf19ea3aa1cecc3e4ffa
jbm-odoo <jbm@odoo.com>
2019-09-11 13:44:31 +0000

    [FIX] hr_referral: Fix write on job
    Fix write on open date when a job is set as recruiting.
    Add demo data to have something in report.
    
    closes odoo/enterprise#5529
    
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_referral/models/hr_applicant.py
+++ hr_referral/models/hr_applicant.py
@@ -11,8 +11,6 @@ from odoo.osv import expression
 
 class Applicant(models.Model):
     _inherit = ["hr.applicant"]
-    _friends_count = 5
-    _max_referral_level = 5
 
     ref_employee_id = fields.Many2one('hr.employee', string='Referred By Employee', tracking=True,
         compute='_compute_ref_employee_id', inverse='_inverse_ref_employee_id', store=True)
@@ -154,7 +152,7 @@ class Applicant(models.Model):
         if not self.env.user.employee_id:
             return
         if self_sudo.ref_employee_id == self.env.user.employee_id and not self_sudo.friend_id:
-            # Use sudo, employee has normaly not the right to write on referral
+            # Use sudo, employee has normaly not the right to write on applicant
             self_sudo.write({'friend_id': friend_id})
 
     def _get_onboarding_steps(self):
@@ -270,6 +268,7 @@ class Applicant(models.Model):
         ], order='points asc', limit=1)
         if next_referral_level:
             self.env.user.employee_id.write({'hr_referral_level_id': next_referral_level.id})
+
 
 class RecruitmentStage(models.Model):
     _inherit = "hr.recruitment.stage"

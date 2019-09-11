35d29052916ad1f34e1d57a30d1d9953b255c94f
Gert Pellin <gpe@odoo.com>
2019-09-11 11:46:27 +0000

    [FIX] pos_loyalty: selection fields not working
    Since the ORM refactoring, it is not possible anymore to declare a
    Selection field with a tuple containing the selection values, but
    we have to use a list.
    
    So we modified the declaration of fields 'rule_type' and
    'reward_type' to match the correct syntax for Selection fields.
    
    also changed the layout of the reward_rule to make the fields rule_type and reward_type
    readable.
    Variable label for point_cost field to give a better understanding of
    it's function.
    
    closes odoo/enterprise#5525
    
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>

--- pos_loyalty/models/pos_loyalty.py
+++ pos_loyalty/models/pos_loyalty.py
@@ -24,7 +24,7 @@ class LoyaltyRule(models.Model):
 
     name = fields.Char(index=True, required=True, help="An internal identification for this loyalty program rule")
     loyalty_program_id = fields.Many2one('loyalty.program', string='Loyalty Program', help='The Loyalty Program this exception belongs to')
-    rule_type = fields.Selection((('product', 'Product'), ('category', 'Category')), old_name='type', required=True, default='product', help='Does this rule affects products, or a category of products ?')
+    rule_type = fields.Selection([('product', 'Product'), ('category', 'Category')], old_name='type', required=True, default='product', help='Does this rule affects products, or a category of products ?')
     product_id = fields.Many2one('product.product', string='Target Product', help='The product affected by the rule')
     category_id = fields.Many2one('pos.category', string='Target Category', help='The category affected by the rule')
     cumulative = fields.Boolean(help='The points won from this rule will be won in addition to other rules')
@@ -39,7 +39,7 @@ class LoyaltyReward(models.Model):
     name = fields.Char(index=True, required=True, help='An internal identification for this loyalty reward')
     loyalty_program_id = fields.Many2one('loyalty.program', string='Loyalty Program', help='The Loyalty Program this reward belongs to')
     minimum_points = fields.Float(help='The minimum amount of points the customer must have to qualify for this reward')
-    reward_type = fields.Selection((('gift', 'Gift'), ('discount', 'Discount'), ('resale', 'Resale')), old_name='type', required=True, help='The type of the reward')
+    reward_type = fields.Selection([('gift', 'Gift'), ('discount', 'Discount'), ('resale', 'Resale')], old_name='type', required=True, help='The type of the reward')
     gift_product_id = fields.Many2one('product.product', string='Gift Product', help='The product given as a reward')
     point_cost = fields.Float(string='Points per Currency', help='The cost of the reward')
     discount_product_id = fields.Many2one('product.product', string='Discount Product', help='The product used to apply discounts')

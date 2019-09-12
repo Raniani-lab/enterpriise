5e29f0e9c2a4f800de586223788098d67bdf17cf
Geoffroy Larue <gla@odoo.com>
2019-09-12 11:19:47 +0000

    [IMP] project_enterprise: unavailability
    Add the display of unavailabilities of users on the gantt view on
    tasks, to facilitate creation and planning of new tasks.
    
    Task-2069861
    
    closes odoo/enterprise#5559
    
    Signed-off-by: Jérome Maes (jem) <jem@openerp.com>

--- project_enterprise/models/project.py
+++ project_enterprise/models/project.py
@@ -25,3 +25,61 @@ class Task(models.Model):
     _sql_constraints = [
         ('planned_dates_check', "CHECK ((planned_date_begin <= planned_date_end))", "The planned start date must be prior to the planned end date."),
     ]
+
+    # ----------------------------------------------------
+    # Gantt view
+    # ----------------------------------------------------
+
+    @api.model
+    def gantt_unavailability(self, start_date, end_date, scale, group_bys=None, rows=None):
+        start_datetime = fields.Datetime.from_string(start_date)
+        end_datetime = fields.Datetime.from_string(end_date)
+        user_ids = set()
+
+        # function to "mark" top level rows concerning users
+        # the propagation of that user_id to subrows is taken care of in the traverse function below
+        def tag_user_rows(rows):
+            for row in rows:
+                group_bys = row.get('groupedBy')
+                res_id = row.get('resId')
+                if group_bys:
+                    # if user_id is the first grouping attribute
+                    if group_bys[0] == 'user_id' and res_id:
+                        user_id = res_id
+                        user_ids.add(user_id)
+                        row['user_id'] = user_id
+                    # else we recursively traverse the rows
+                    elif 'user_id' in group_bys:
+                        tag_user_rows(row.get('rows'))
+
+        tag_user_rows(rows)
+        users = self.env['res.users'].browse(user_ids)
+        resources = users.mapped('resource_ids')
+        leaves_mapping = resources._get_unavailable_intervals(start_datetime, end_datetime)
+
+        # function to recursively replace subrows with the ones returned by func
+        def traverse(func, row):
+            new_row = dict(row)
+            if new_row.get('user_id'):
+                for sub_row in new_row.get('rows'):
+                    sub_row['user_id'] = new_row['user_id']
+            new_row['rows'] = [traverse(func, row) for row in new_row.get('rows')]
+            return func(new_row)
+
+        cell_dt = timedelta(hours=1) if scale == 'day' else timedelta(days=1)
+
+        # for a single row, inject unavailability data
+        def inject_unavailability(row):
+            new_row = dict(row)
+            # if (not row.get('groupedBy') or row.get('groupedBy')[0] == 'user_id'):
+            if row.get('user_id'):
+                resource_ids = self.env['res.users'].browse(row.get('user_id')).resource_ids
+                if resource_ids:
+                    # remove intervals smaller than a cell, as they will cause half a cell to turn grey
+                    # ie: when looking at a week, a employee start everyday at 8, so there is a unavailability
+                    # like: 2019-05-22 20:00 -> 2019-05-23 08:00 which will make the first half of the 23's cell grey
+                    notable_intervals = filter(lambda interval: interval[1] - interval[0] >= cell_dt, leaves_mapping[resource_ids.id])
+                    new_row['unavailabilities'] = [{'start': interval[0], 'stop': interval[1]} for interval in notable_intervals]
+            return new_row
+
+        return [traverse(inject_unavailability, row) for row in rows]

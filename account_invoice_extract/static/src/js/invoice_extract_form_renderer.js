2db2a59aa9d9a7c2567f7ab0555ca796434f4f6c
fda-odoo <fda@odoo.com>
2019-09-23 08:32:45 +0000

    [FIX] account_invoice_extract: change typo of the vendor button
    Show 'Customer' instead of 'Vendor' on customer invoice
    
    closes odoo/enterprise#5677
    
    Signed-off-by: Florian Daloze (fda) <fda@odoo.com>

--- account_invoice_extract/static/src/js/invoice_extract_form_renderer.js
+++ account_invoice_extract/static/src/js/invoice_extract_form_renderer.js
@@ -30,7 +30,10 @@ var InvoiceExtractFormRenderer = FormRenderer.extend({
         this._super.apply(this, arguments);
 
         this._invoiceExtractBoxLayers = [];
-        this._invoiceExtractFields = new InvoiceExtractFields(this);
+        var is_customer_invoice = false;
+        if (this.state.context.default_type == 'out_invoice' || this.state.context.default_type == 'out_refund')
+            is_customer_invoice = true;
+        this._invoiceExtractFields = new InvoiceExtractFields(this, is_customer_invoice);
         this._$invoiceExtractButtons = [];
     },
 

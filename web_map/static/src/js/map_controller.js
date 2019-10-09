c3ac92f1f1092d6a66b49e47e926bb92cd631634
Julien Mougenot <jum@odoo.com>
2019-10-09 09:35:44 +0000

    [REF] web_map: code cleaning, linting and standardizing
    - Fixed linter-related errors
    - Cleaned the map view code
    - Refactored several functions and strings for readability's sake
    - Cleaned the map tests and set the correct assertions

--- web_map/static/src/js/map_controller.js
+++ web_map/static/src/js/map_controller.js
@@ -1,142 +1,139 @@
 odoo.define('web_map.MapController', function (require) {
-    'use strict';
-    var AbstractController = require('web.AbstractController');
-    var core = require('web.core');
-    var qweb = core.qweb;
-    var Pager = require('web.Pager');
-    var MapController = AbstractController.extend({
+"use strict";
 
-        custom_events: _.extend({}, AbstractController.prototype.custom_events, {
-            'pin_clicked': '_onPinClick',
-            'get_itinerary_clicked': '_onGetItineraryClicked',
-            'open_clicked': '_onOpenClicked',
-        }),
+const AbstractController = require('web.AbstractController');
+const core = require('web.core');
+const qweb = core.qweb;
+const Pager = require('web.Pager');
 
-        /**
-         * @constructor
-         */
-        init: function (parent, model, renderer, params) {
-            this._super.apply(this, arguments);
-            this.actionName = params.actionName;
-        },
+const MapController = AbstractController.extend({
+    custom_events: _.extend({}, AbstractController.prototype.custom_events, {
+        'pin_clicked': '_onPinClick',
+        'get_itinerary_clicked': '_onGetItineraryClicked',
+        'open_clicked': '_onOpenClicked',
+    }),
 
-        //---------------------------------------------------------------------------------
-        //Public
-        //-----------------------------------------------------------------------------
+    /**
+     * @constructor
+     */
+    init: function (parent, model, renderer, params) {
+        this._super.apply(this, arguments);
+        this.actionName = params.actionName;
+    },
 
-        /**
-         * @override
-         * @param {JqueryElement} $node
-         */
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
 
-        renderButtons: function ($node) {
-            var url = 'https://www.google.com/maps/dir/?api=1';
-            if (this.model.data.records.length) {
-                url += '&waypoints=';
-                this.model.data.records
-                    .filter((record) => record.partner && record.partner.partner_latitude && record.partner.partner_longitude)
-                    .forEach((record) => {
-                        url += record.partner.partner_latitude + ',' + record.partner.partner_longitude + '|';
-                    });
-                url = url.slice(0, -1);
-            }
-            var $buttons = $(qweb.render("MapView.buttons"), { widget: this });
-            $buttons.find('a').attr('href', url);
-            $buttons.appendTo($node);
-        },
+    /**
+     * @override
+     * @param {JqueryElement} $node
+     */
 
-        /**
-         * @override
-         * @param {JqueryElement} $node
-         */
-        renderPager: function ($node) {
-            const params = this._getPagerParams();
-            this.pager = new Pager(this, params.size, params.current_min, params.limit);
-            this.pager.on('pager_changed', this, newState => {
-                this.pager.disable();
-                this.reload({ limit: newState.limit, offset: newState.current_min - 1 })
-                    .then(this.pager.enable.bind(this.pager));
-            });
-            return this.pager.appendTo($node);
-        },
-        /**
-         * @override
-         */
-        update: function () {
-            return this._super.apply(this, arguments).then(() => {
-                this._updatePager();
-            });
-        },
-
-        //--------------------------------------------------------------------------
-        // Private
-        //--------------------------------------------------------------------------
-
-        /**
-         * Return the params (current_min, limit and size) to pass to the pager,
-         * according to the current state.
-         *
-         * @private
-         * @returns {Object}
-         */
-        _getPagerParams: function () {
-            const state = this.model.get();
-            return {
-                current_min: state.offset + 1,
-                limit: state.limit,
-                size: state.count,
-            };
-        },
-        /**
-         * Update the pager with the current state.
-         *
-         * @private
-         */
-        _updatePager: function () {
-            if (this.pager) {
-                this.pager.updateState(this._getPagerParams());
-            }
-        },
-
-        //-------------------------------------------------------------------------------------
-        //Handler
-        //------------------------------------------------------------------------------------
-
-        /**
-         *
-         * @param {MouseEvent} ev
-         * @private
-         * redirects to google maps with all the records' coordinates
-         */
-        _onGetItineraryClicked: function (ev) {
-            window.open('https://www.google.com/maps/dir/?api=1&destination=' + ev.data.lat + ',' + ev.data.lon);
-        },
-
-        /**
-         *
-         * @param {MouseEvent} ev
-         * @private
-         *
-         * Redirects to views when clicked on open button in marker popup
-         */
-        _onOpenClicked: function (ev) {
-            if (ev.data.ids.length > 1) {
-                this.do_action({
-                    type: 'ir.actions.act_window',
-                    name: this.actionName,
-                    views: [[false, 'list'], [false, 'form']],
-                    res_model: this.modelName,
-                    domain: [['id', 'in', ev.data.ids]],
-                });
-            } else {
-                this.trigger_up('switch_view', {
-                    view_type: 'form',
-                    res_id: ev.data.ids[0],
-                    mode: 'readonly',
-                    model: this.modelName
-                });
-            }
+    renderButtons: function ($node) {
+        let url = 'https://www.google.com/maps/dir/?api=1';
+        if (this.model.data.records.length) {
+            const coordinates = this.model.data.records
+                .filter(record => record.partner && record.partner.partner_latitude && record.partner.partner_longitude)
+                .map(record => record.partner.partner_latitude + ',' + record.partner.partner_longitude);
+            url += `&waypoints=${coordinates.join('|')}`;
         }
-    });
-    return MapController;
+        const $buttons = $(qweb.render("MapView.buttons"), { widget: this });
+        $buttons.find('a').attr('href', url);
+        $buttons.appendTo($node);
+    },
+
+    /**
+     * @override
+     * @param {JqueryElement} $node
+     */
+    renderPager: function ($node) {
+        const params = this._getPagerParams();
+        this.pager = new Pager(this, params.size, params.current_min, params.limit);
+        this.pager.on('pager_changed', this, newState => {
+            this.pager.disable();
+            this.reload({ limit: newState.limit, offset: newState.current_min - 1 })
+                .then(this.pager.enable.bind(this.pager));
+        });
+        return this.pager.appendTo($node);
+    },
+    /**
+     * @override
+     */
+    update: function () {
+        return this._super.apply(this, arguments).then(() => {
+            this._updatePager();
+        });
+    },
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+
+    /**
+     * Return the params (current_min, limit and size) to pass to the pager,
+     * according to the current state.
+     *
+     * @private
+     * @returns {Object}
+     */
+    _getPagerParams: function () {
+        const state = this.model.get();
+        return {
+            current_min: state.offset + 1,
+            limit: state.limit,
+            size: state.count,
+        };
+    },
+    /**
+     * Update the pager with the current state.
+     *
+     * @private
+     */
+    _updatePager: function () {
+        if (this.pager) {
+            this.pager.updateState(this._getPagerParams());
+        }
+    },
+
+    //--------------------------------------------------------------------------
+    // Handlers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Redirects to google maps with all the records' coordinates.
+     *
+     * @private
+     * @param {MouseEvent} ev
+     */
+    _onGetItineraryClicked: function (ev) {
+        window.open(`https://www.google.com/maps/dir/?api=1&destination=${ev.data.lat},${ev.data.lon}`);
+    },
+    /**
+     * Redirects to views when clicked on open button in marker popup.
+     *
+     * @private
+     * @param {MouseEvent} ev
+     */
+    _onOpenClicked: function (ev) {
+        if (ev.data.ids.length > 1) {
+            this.do_action({
+                type: 'ir.actions.act_window',
+                name: this.actionName,
+                views: [[false, 'list'], [false, 'form']],
+                res_model: this.modelName,
+                domain: [['id', 'in', ev.data.ids]],
+            });
+        } else {
+            this.trigger_up('switch_view', {
+                view_type: 'form',
+                res_id: ev.data.ids[0],
+                mode: 'readonly',
+                model: this.modelName
+            });
+        }
+    }
+});
+
+return MapController;
 });
--- web_map/static/src/js/map_view.js
+++ web_map/static/src/js/map_view.js
@@ -1,74 +1,74 @@
 odoo.define('web_map.MapView', function (require) {
-    "use strict";
+"use strict";
 
-    var MapModel = require('web_map.MapModel');
-    var MapController = require('web_map.MapController');
-    var MapRenderer = require('web_map.MapRenderer');
-    var AbstractView = require('web.AbstractView');
-    var utils = require('web.utils');
-    var viewRegistry = require('web.view_registry');
-    var _t = require('web.core')._t;
+const MapModel = require('web_map.MapModel');
+const MapController = require('web_map.MapController');
+const MapRenderer = require('web_map.MapRenderer');
+const AbstractView = require('web.AbstractView');
+const utils = require('web.utils');
+const viewRegistry = require('web.view_registry');
+const _t = require('web.core')._t;
 
-    var MapView = AbstractView.extend({
-        jsLibs: [
-            '/web_map/static/lib/leaflet/leaflet.js',
-        ],
-        config: _.extend({}, AbstractView.prototype.config, {
-            Model: MapModel,
-            Controller: MapController,
-            Renderer: MapRenderer,
-        }),
-        icon: 'fa-map-marker',
-        display_name: 'Map',
-        viewType: 'map',
-        mobile_friendly: true,
-        searchMenuTypes: ['filter', 'favorite'],
+const MapView = AbstractView.extend({
+    jsLibs: [
+        '/web_map/static/lib/leaflet/leaflet.js',
+    ],
+    config: _.extend({}, AbstractView.prototype.config, {
+        Model: MapModel,
+        Controller: MapController,
+        Renderer: MapRenderer,
+    }),
+    icon: 'fa-map-marker',
+    display_name: 'Map',
+    viewType: 'map',
+    mobile_friendly: true,
+    searchMenuTypes: ['filter', 'favorite'],
 
-        init: function (viewInfo, params) {
-            this._super.apply(this, arguments);
+    init: function (viewInfo, params) {
+        this._super.apply(this, arguments);
 
-            var fieldNames = [];
-            var fieldNamesMarkerPopup = [];
+        const fieldNames = [];
+        const fieldNamesMarkerPopup = [];
 
-            this.loadParams.resPartnerField = this.arch.attrs.res_partner;
-            fieldNames.push(this.arch.attrs.res_partner);
+        this.loadParams.resPartnerField = this.arch.attrs.res_partner;
+        fieldNames.push(this.arch.attrs.res_partner);
+        fieldNames.push('display_name');
+
+        if (this.arch.attrs.default_order) {
+            this.loadParams.orderBy = [{ name: this.arch.attrs.default_order || 'display_name', asc: true }];
+        }
+
+        const routing = ["true", "True", "1"].includes(this.arch.attrs.routing);
+
+        this.loadParams.routing = routing;
+        this.rendererParams.routing = routing;
+        this.rendererParams.numbering = this.arch.attrs.routing ? true : false;
+        this.rendererParams.defaultOrder = this.arch.attrs.default_order;
+        this.rendererParams.panelTitle = this.arch.attrs.panel_title || params.displayName || _t('Items');
+
+        const hideName = utils.toBoolElse(this.arch.attrs.hide_name || '', false);
+        this.rendererParams.hideName = hideName;
+        if (!hideName) {
             fieldNames.push('display_name');
+        }
+        this.rendererParams.hideAddress = utils.toBoolElse(this.arch.attrs.hide_address || '', false);
 
-            if (this.arch.attrs.default_order) {
-                this.loadParams.orderBy = [{ name: this.arch.attrs.default_order || 'display_name', asc: true }];
+        this.arch.children.forEach(node => {
+            if (node.tag === 'field') {
+                fieldNames.push(node.attrs.name);
+                fieldNamesMarkerPopup.push({ fieldName: node.attrs.name, string: node.attrs.string });
             }
+        });
+        this.loadParams.fieldNames = _.uniq(fieldNames);
+        this.rendererParams.fieldNamesMarkerPopup = fieldNamesMarkerPopup;
 
-            const routing = ["true", "True", "1"].includes(this.arch.attrs.routing);
+        this.rendererParams.hasFormView = params.actionViews.find(view => view.type === "form");
 
-            this.loadParams.routing = routing;
-            this.rendererParams.routing = routing;
-            this.rendererParams.numbering = this.arch.attrs.routing ? true: false;
-            this.rendererParams.defaultOrder = this.arch.attrs.default_order;
-            this.rendererParams.panelTitle = this.arch.attrs.panel_title || params.displayName || _t('Items');
+        this.controllerParams.actionName = params.action ? params.action.name : _t("Untitled");
+    },
+});
 
-            const hideName = utils.toBoolElse(this.arch.attrs.hide_name || '', false);
-            this.rendererParams.hideName = hideName;
-            if (!hideName) {
-                fieldNames.push('display_name');
-            }
-            this.rendererParams.hideAddress = utils.toBoolElse(this.arch.attrs.hide_address || '', false);
+viewRegistry.add('map', MapView);
 
-            this.arch.children.forEach(function (node) {
-                if (node.tag === 'field') {
-                    fieldNames.push(node.attrs.name);
-                    fieldNamesMarkerPopup.push({ fieldName: node.attrs.name, string: node.attrs.string });
-                }
-            });
-            this.loadParams.fieldNames = _.uniq(fieldNames);
-            this.rendererParams.fieldNamesMarkerPopup = fieldNamesMarkerPopup;
-
-            this.rendererParams.hasFormView = params.actionViews.find(function (view) {
-                return view.type === "form";
-            });
-
-            this.controllerParams.actionName = params.action ? params.action.name : _t("Untitled");
-        },
-    });
-    viewRegistry.add('map', MapView);
-    return MapView;
+return MapView;
 });

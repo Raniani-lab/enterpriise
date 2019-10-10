a568e871a8d369535243e66988becf618c3129b1
Laurent Smet <las@odoo.com>
2019-10-10 12:47:07 +0000

    [IMP] account_reports: Rewrite get_html
    - Add _get_table method returning (headers, lines) that will be useful
    on the financial report with group by ir.filters.
    - Remove management of analytic accounts in the get_html that was there
    for bug fixes purpose.
    - Don't recompute headers when unfolding a line but store them inside the options
    instead.

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -716,6 +716,10 @@ class AccountReport(models.AbstractModel):
         return []
 
     #TO BE OVERWRITTEN
+    def _get_table(self, options):
+        return self.get_header(options), self._get_lines(options)
+
+    #TO BE OVERWRITTEN
     def _get_templates(self):
         return {
                 'main_template': 'account_reports.main_template',
@@ -1137,16 +1141,36 @@ class AccountReport(models.AbstractModel):
 
         templates = self._get_templates()
         report_manager = self._get_report_manager(options)
-        report = {'name': self._get_report_name(),
-                'summary': report_manager.summary,
-                'company_name': self.env.company.name,}
-        lines = self._get_lines(options, line_id=line_id)
 
+        render_values = {
+            'report': {
+                'name': self._get_report_name(),
+                'summary': report_manager.summary,
+                'company_name': self.env.company.name,
+            },
+            'options': options,
+            'context': self.env.context,
+            'model': self,
+        }
+        if additional_context:
+            render_values.update(additional_context)
+
+        # Create lines/headers.
+        if line_id:
+            headers = options['headers']
+            lines = self._get_lines(options, line_id=line_id)
+            template = templates['line_template']
+        else:
+            headers, lines = self._get_table(options)
+            options['headers'] = headers
+            template = templates['main_template']
         if options.get('hierarchy'):
             lines = self._create_hierarchy(lines, options)
         if options.get('selected_column'):
             lines = self._sort_lines(lines, options)
+        render_values['lines'] = {'columns_header': headers, 'lines': lines}
 
+        # Manage footnotes.
         footnotes_to_render = []
         if self.env.context.get('print_mode', False):
             # we are in print mode, so compute footnote number and include them in lines values, otherwise, let the js compute the number correctly as
@@ -1160,26 +1184,8 @@ class AccountReport(models.AbstractModel):
                     line['footnote'] = str(number)
                     footnotes_to_render.append({'id': f.id, 'number': number, 'text': f.text})
 
-        rcontext = {'report': report,
-                    'lines': {'columns_header': self.get_header(options), 'lines': lines},
-                    'options': options,
-                    'context': self.env.context,
-                    'model': self,
-                }
-        if additional_context and type(additional_context) == dict:
-            rcontext.update(additional_context)
-        if self.env.context.get('analytic_account_ids'):
-            rcontext['options']['analytic_account_ids'] = [
-                {'id': acc.id, 'name': acc.name} for acc in self.env.context['analytic_account_ids']
-            ]
-
-        render_template = templates.get('main_template', 'account_reports.main_template')
-        if line_id is not None:
-            render_template = templates.get('line_template', 'account_reports.line_template')
-        html = self.env['ir.ui.view'].render_template(
-            render_template,
-            values=dict(rcontext),
-        )
+        # Render.
+        html = self.env.ref(template).render(render_values)
         if self.env.context.get('print_mode', False):
             for k,v in self._replace_class().items():
                 html = html.replace(k, v)

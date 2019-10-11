3cbd357bfe67327835bf28eeb5bb34c21597e48a
Romeo Fragomeli <rfr@odoo.com>
2019-10-11 13:31:57 +0000

    [IMP] web_mobile: open app mobile from mail link
    Before this commit, when a mail was send, there was any possibility to
    open it with the mobile app for all instance of Odoo.
    
    After this commit, all link related to a model will be prefixed with
    a custom link. This link is a "FireBase Dynamic Links" site.
    
    Using this allow us, to be able to open the app mobile when it's installed
    and redirect to the normal link when the user don't have the app.
    
    Note we force to have absolute url and not relative url to avoid
    dynamic-links with relative link.
    
    Ref:
    * https://firebase.google.com/docs/dynamic-links/#how_does_it_work
    
    Task ID: 1895451
    
    closes odoo/enterprise#6414
    
    Related: odoo/odoo#39958
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- test_mail_enterprise/tests/__init__.py
+++ test_mail_enterprise/tests/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
+from . import test_discuss
 from . import test_sms_performance
--- web_mobile/models/res_config_settings.py
+++ web_mobile/models/res_config_settings.py
@@ -8,3 +8,9 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_ocn_client = fields.Boolean('Push Notifications')
+    # We want default=True but because of odoo/addons/base/models/ir_config_parameter.py:L91
+    # when we do a change in False it's not saved in the database. So we have inverted the condition
+    disable_redirect_firebase_dynamic_link = fields.Boolean(
+        "Allows to open mail's links in the native mobile app Android and iOS if possible (e.g. \"View Task\")",
+        config_parameter='web_mobile.disable_redirect_firebase_dynamic_link'
+    )

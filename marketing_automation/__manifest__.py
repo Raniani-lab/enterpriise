1f5a2f06d040a96099a7c5cb451face4d068e002
Florent Lejoly <fle@odoo.com>
2019-10-17 08:12:57 +0000

    [IMP] marketing_automation: improve server action creation usability
    If applied this commit will improve the usability of the server action for
    non-technical users in market automation as well as cleaning the view a bit
    (because so far we display the standard server action form view).
    this is done by:
    -hiding the header containing the buttons
    -hiding the security tab
    -setting the model field to read-only
    
    closes odoo/enterprise#6200
    
    Task-id: 2088171
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- marketing_automation/__manifest__.py
+++ marketing_automation/__manifest__.py
@@ -11,6 +11,7 @@
         'security/marketing_automation_security.xml',
         'security/ir.model.access.csv',
         'views/assets.xml',
+        'views/ir_actions_views.xml',
         'views/ir_model_views.xml',
         'views/marketing_automation_menus.xml',
         'wizard/marketing_campaign_test_views.xml',

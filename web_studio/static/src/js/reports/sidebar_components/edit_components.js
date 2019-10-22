57ec938d32a5103cddee77758a3fa4f1c93eac6a
fja-odoo <fja@odoo.com>
2019-10-22 14:27:49 +0000

    [REF] web_studio: adapt color picker
    Some changes have been done in the web_editor color picker.
    These broke the studio color picker, it is now fixed.
    Also an adaptation of the studio color picker to reflect these changes.
    
    See https://github.com/odoo/odoo/pull/38959
    
    maintask-2066614
    task-2087383
    
    closes odoo/enterprise#6464
    
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- web_studio/static/src/js/reports/sidebar_components/edit_components.js
+++ web_studio/static/src/js/reports/sidebar_components/edit_components.js
@@ -1,6 +1,7 @@
 odoo.define('web_studio.reportEditComponents', function (require) {
 "use strict";
 
+var ColorPickerDialog = require('web.ColorpickerDialog');
 var config = require('web.config');
 var core = require('web.core');
 var utils = require('web.utils');
@@ -520,29 +521,31 @@ var LayoutEditable = AbstractEditComponent.extend({
         var groupColors = [];
         var $clpicker = $(qweb.render('web_editor.colorpicker'));
         $clpicker.children('.o_colorpicker_section').each(function () {
-            groupColors.push($(this).attr('data-display'));
+            if (this.dataset.display) {
+                groupColors.push(this.dataset.display);
+            }
             var colors = [];
-            $(this.children).each(function () {
-                if ($(this).hasClass('clearfix')) {
-                    groupColors.push(colors);
-                    colors = [];
-                } else {
-                    colors.push($(this).attr('data-color') || '');
+            $(this).children('button').each(function () {
+                var $el = $(this);
+                if (!$el.hasClass('d-none')) {
+                    colors.push($el.attr('data-color') || '');
                 }
             });
-            groupColors.push(colors);
+            if (colors.length) {
+                groupColors.push(colors);
+            }
+            if (this.dataset.name === 'common') {
+                groupColors = groupColors.concat([
+                    ['#FF0000', '#FF9C00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#9C00FF', '#FF00FF'],
+                    ['#F7C6CE', '#FFE7CE', '#FFEFC6', '#D6EFD6', '#CEDEE7', '#CEE7F7', '#D6D6E7', '#E7D6DE'],
+                    ['#E79C9C', '#FFC69C', '#FFE79C', '#B5D6A5', '#A5C6CE', '#9CC6EF', '#B5A5D6', '#D6A5BD'],
+                    ['#E76363', '#F7AD6B', '#FFD663', '#94BD7B', '#73A5AD', '#6BADDE', '#8C7BC6', '#C67BA5'],
+                    ['#CE0000', '#E79439', '#EFC631', '#6BA54A', '#4A7B8C', '#3984C6', '#634AA5', '#A54A7B'],
+                    ['#9C0000', '#B56308', '#BD9400', '#397B21', '#104A5A', '#085294', '#311873', '#731842'],
+                    ['#630000', '#7B3900', '#846300', '#295218', '#083139', '#003163', '#21104A', '#4A1031']
+                ]);
+            }
         });
-
-        groupColors = groupColors.concat([
-            ['#FF0000', '#FF9C00', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#9C00FF', '#FF00FF'],
-            ['#F7C6CE', '#FFE7CE', '#FFEFC6', '#D6EFD6', '#CEDEE7', '#CEE7F7', '#D6D6E7', '#E7D6DE'],
-            ['#E79C9C', '#FFC69C', '#FFE79C', '#B5D6A5', '#A5C6CE', '#9CC6EF', '#B5A5D6', '#D6A5BD'],
-            ['#E76363', '#F7AD6B', '#FFD663', '#94BD7B', '#73A5AD', '#6BADDE', '#8C7BC6', '#C67BA5'],
-            ['#CE0000', '#E79439', '#EFC631', '#6BA54A', '#4A7B8C', '#3984C6', '#634AA5', '#A54A7B'],
-            ['#9C0000', '#B56308', '#BD9400', '#397B21', '#104A5A', '#085294', '#311873', '#731842'],
-            ['#630000', '#7B3900', '#846300', '#295218', '#083139', '#003163', '#21104A', '#4A1031']
-        ]);
-
         return groupColors;
     },
 
@@ -585,17 +588,19 @@ var LayoutEditable = AbstractEditComponent.extend({
      */
     _onCustomColor: function (e) {
         e.preventDefault();
-        core.bus.trigger('color_picker_dialog_demand', {
-            color: 'rgb(255, 0, 0)',
-            onSave: function (color) {
-                var $button = $('<button/>');
-                $button.attr('data-color', color);
-                $button.attr('data-value', color);
-                $button.css('background-color', color);
-                $(e.target).closest('.dropdown-item').find('.o_web_studio_custom_colors').append($button);
-                $button.mousedown();
-            }
+        const colorpicker = new ColorPickerDialog(this, {
+            defaultColor: 'rgb(255, 0, 0)',
         });
+        colorpicker.on('colorpicker:saved', this, (ev) => {
+            var color = ev.data.cssColor;
+            var $button = $('<button/>');
+            $button.attr('data-color', color);
+            $button.attr('data-value', color);
+            $button.css('background-color', color);
+            $(e.target).closest('.dropdown-item').find('.o_web_studio_custom_colors').append($button);
+            $button.mousedown();
+        });
+        colorpicker.open();
     },
     /**
      * @private

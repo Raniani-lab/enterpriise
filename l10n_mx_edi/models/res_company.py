aef5d1aaac575a1b114af6c0ba97666ec3722c84
Luis Torres <luis_t@vauxoo.com>
2019-10-22 23:49:39 +0000

    [FIX] l10n_mx_edi: Ensure module installation on sh
    The hook that download the XSD files for the CFDI was moved to a cron,
    this to ensure the module installation on sh instances.
    
    Now not take time to download the XSD files in the module installation.
    Was moved for a cron that is executed one time, and could be executed
    manually with the same server action that already exists.
    
    closes odoo/enterprise#11056
    
    X-original-commit: cf8fd41c7b2a3d91a7b34fa268bd01b1b44c189c
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- l10n_mx_edi/models/res_company.py
+++ l10n_mx_edi/models/res_company.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
-from odoo.addons.l10n_mx_edi.hooks import _load_xsd_files, _load_xsd_complements
+import base64
+import logging
+import requests
+
+from lxml import etree, objectify
+from werkzeug import url_quote
+from os.path import join
+
+from odoo import api, fields, models, tools, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
 
 
 class ResCompany(models.Model):
@@ -113,4 +122,123 @@ class ResCompany(models.Model):
         xsd_files = ['%s.%s' % (x.module, x.name) for x in xml_ids]
         for xsd in xsd_files:
             self.env.ref(xsd).unlink()
-        _load_xsd_files(self._cr, None, url)
+        self._load_xsd_files(url)
+
+    @api.model
+    def _load_xsd_files(self, url):
+        fname = url.split('/')[-1]
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            logging.getLogger(__name__).info(
+                'I cannot connect with the given URL.')
+            return ''
+        try:
+            res = objectify.fromstring(response.content)
+        except etree.XMLSyntaxError as e:
+            logging.getLogger(__name__).info(
+                'You are trying to load an invalid xsd file.\n%s', e)
+            return ''
+        namespace = {'xs': 'http://www.w3.org/2001/XMLSchema'}
+        if fname == 'cfdv33.xsd':
+            # This is the xsd root
+            res = self._load_xsd_complements(res)
+        sub_urls = res.xpath('//xs:import', namespaces=namespace)
+        for s_url in sub_urls:
+            s_url_catch = self._load_xsd_files(s_url.get('schemaLocation'))
+            s_url.attrib['schemaLocation'] = url_quote(s_url_catch)
+        try:
+            xsd_string = etree.tostring(res, pretty_print=True)
+        except etree.XMLSyntaxError:
+            logging.getLogger(__name__).info('XSD file downloaded is not valid')
+            return ''
+        if not xsd_string:
+            logging.getLogger(__name__).info('XSD file downloaded is empty')
+            return ''
+        env = api.Environment(self._cr, SUPERUSER_ID, {})
+        xsd_fname = 'xsd_cached_%s' % fname.replace('.', '_')
+        attachment = env.ref('l10n_mx_edi.%s' % xsd_fname, False)
+        filestore = tools.config.filestore(self._cr.dbname)
+        if attachment:
+            return join(filestore, attachment.store_fname)
+        attachment = env['ir.attachment'].create({
+            'name': xsd_fname,
+            'datas': base64.encodebytes(xsd_string),
+        })
+        # Forcing the triggering of the store_fname
+        attachment._inverse_datas()
+        self._cr.execute(
+            """INSERT INTO ir_model_data
+            (name, res_id, module, model, noupdate)
+            VALUES (%s, %s, 'l10n_mx_edi', 'ir.attachment', true)""", (
+                xsd_fname, attachment.id))
+        return join(filestore, attachment.store_fname)
+
+    @api.model
+    def _load_xsd_complements(self, content):
+        complements = [
+            ['http://www.sat.gob.mx/servicioparcialconstruccion',
+             'http://www.sat.gob.mx/sitio_internet/cfd/servicioparcialconstruccion/servicioparcialconstruccion.xsd'],
+            ['http://www.sat.gob.mx/EstadoDeCuentaCombustible',
+             'http://www.sat.gob.mx/sitio_internet/cfd/EstadoDeCuentaCombustible/ecc11.xsd'],
+            ['http://www.sat.gob.mx/donat',
+             'http://www.sat.gob.mx/sitio_internet/cfd/donat/donat.xsd'],
+            ['http://www.sat.gob.mx/divisas',
+             'http://www.sat.gob.mx/sitio_internet/cfd/divisas/Divisas.xsd'],
+            ['http://www.sat.gob.mx/implocal',
+             'http://www.sat.gob.mx/sitio_internet/cfd/implocal/implocal.xsd'],
+            ['http://www.sat.gob.mx/leyendasFiscales',
+             'http://www.sat.gob.mx/sitio_internet/cfd/leyendasFiscales/leyendasFisc.xsd'],
+            ['http://www.sat.gob.mx/pfic',
+             'http://www.sat.gob.mx/sitio_internet/cfd/pfic/pfic.xsd'],
+            ['http://www.sat.gob.mx/TuristaPasajeroExtranjero',
+             'http://www.sat.gob.mx/sitio_internet/cfd/TuristaPasajeroExtranjero/TuristaPasajeroExtranjero.xsd'],
+            ['http://www.sat.gob.mx/detallista',
+             'http://www.sat.gob.mx/sitio_internet/cfd/detallista/detallista.xsd'],
+            ['http://www.sat.gob.mx/registrofiscal',
+             'http://www.sat.gob.mx/sitio_internet/cfd/cfdiregistrofiscal/cfdiregistrofiscal.xsd'],
+            ['http://www.sat.gob.mx/nomina12',
+             'http://www.sat.gob.mx/sitio_internet/cfd/nomina/nomina12.xsd'],
+            ['http://www.sat.gob.mx/pagoenespecie',
+             'http://www.sat.gob.mx/sitio_internet/cfd/pagoenespecie/pagoenespecie.xsd'],
+            ['http://www.sat.gob.mx/valesdedespensa',
+             'http://www.sat.gob.mx/sitio_internet/cfd/valesdedespensa/valesdedespensa.xsd'],
+            ['http://www.sat.gob.mx/consumodecombustibles',
+             'http://www.sat.gob.mx/sitio_internet/cfd/consumodecombustibles/consumodecombustibles.xsd'],
+            ['http://www.sat.gob.mx/aerolineas',
+             'http://www.sat.gob.mx/sitio_internet/cfd/aerolineas/aerolineas.xsd'],
+            ['http://www.sat.gob.mx/notariospublicos',
+             'http://www.sat.gob.mx/sitio_internet/cfd/notariospublicos/notariospublicos.xsd'],
+            ['http://www.sat.gob.mx/vehiculousado',
+             'http://www.sat.gob.mx/sitio_internet/cfd/vehiculousado/vehiculousado.xsd'],
+            ['http://www.sat.gob.mx/renovacionysustitucionvehiculos',
+             'http://www.sat.gob.mx/sitio_internet/cfd/renovacionysustitucionvehiculos/renovacionysustitucionvehiculos.xsd'],
+            ['http://www.sat.gob.mx/certificadodestruccion',
+             'http://www.sat.gob.mx/sitio_internet/cfd/certificadodestruccion/certificadodedestruccion.xsd'],
+            ['http://www.sat.gob.mx/arteantiguedades',
+             'http://www.sat.gob.mx/sitio_internet/cfd/arteantiguedades/obrasarteantiguedades.xsd'],
+            ['http://www.sat.gob.mx/ComercioExterior11',
+             'http://www.sat.gob.mx/sitio_internet/cfd/ComercioExterior11/ComercioExterior11.xsd'],
+            ['http://www.sat.gob.mx/Pagos',
+             'http://www.sat.gob.mx/sitio_internet/cfd/Pagos/Pagos10.xsd'],
+            ['http://www.sat.gob.mx/iedu',
+             'http://www.sat.gob.mx/sitio_internet/cfd/iedu/iedu.xsd'],
+            ['http://www.sat.gob.mx/ventavehiculos',
+             'http://www.sat.gob.mx/sitio_internet/cfd/ventavehiculos/ventavehiculos11.xsd'],
+            ['http://www.sat.gob.mx/terceros',
+             'http://www.sat.gob.mx/sitio_internet/cfd/terceros/terceros11.xsd'],
+            ['http://www.sat.gob.mx/spei',
+             'http://www.sat.gob.mx/sitio_internet/cfd/spei/spei.xsd'],
+            ['http://www.sat.gob.mx/ine',
+             'http://www.sat.gob.mx/sitio_internet/cfd/ine/INE11.xsd'],
+            ['http://www.sat.gob.mx/acreditamiento',
+             'http://www.sat.gob.mx/sitio_internet/cfd/acreditamiento/AcreditamientoIEPS10.xsd'],
+            ['http://www.sat.gob.mx/TimbreFiscalDigital',
+             'http://www.sat.gob.mx/sitio_internet/cfd/TimbreFiscalDigital/TimbreFiscalDigitalv11.xsd'],
+        ]
+        for complement in complements:
+            xsd = {'namespace': complement[0], 'schemaLocation': complement[1]}
+            node = etree.Element('{http://www.w3.org/2001/XMLSchema}import', xsd)
+            content.insert(0, node)
+        return content

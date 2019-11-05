4203df7e2c3fe1d01df4708eda6d1d2d356dc3f9
Yannick Tivisse <yti@odoo.com>
2019-11-05 09:41:33 +0100

    [IMP] hr_payroll_account_sepa: Adapt tests to work with/without demo data

--- hr_payroll_account_sepa/tests/test_payroll_sepa.py
+++ hr_payroll_account_sepa/tests/test_payroll_sepa.py
@@ -8,114 +8,95 @@ from lxml import etree
 
 from odoo.fields import Date
 from odoo.modules.module import get_module_resource
+from odoo.addons.hr_payroll_account.tests.test_hr_payroll_account import TestHrPayrollAccount
 from odoo.tests import common, tagged
 
 
 @tagged('post_install', '-at_install')
-class TestPayrollSEPACreditTransfer(common.TransactionCase):
+class TestPayrollSEPACreditTransfer(TestHrPayrollAccount):
 
-    def setUp(self):
-        super(TestPayrollSEPACreditTransfer, self).setUp()
+    # YTI TODO: test executed multiple times, to put into a isolated common class
+    @classmethod
+    def setUpClass(cls):
+        super(TestPayrollSEPACreditTransfer, cls).setUpClass()
 
-        self.res_partner_bank = self.env['res.partner.bank'].create({
-            'acc_number': 'BE32707171912447',
-            'partner_id': self.ref('base.res_partner_12'),
-            'acc_type': 'bank',
-            'bank_id': self.ref('base.res_bank_1'),
-        })
-
-        self.ir_sequence = self.env['ir.sequence'].create({
-            'name' : 'SEQ',
-            'padding' : 4,
-            'number_increment' : 1,
-        })
-
-        self.bank_partner = self.env['res.partner.bank'].create({
-            'acc_number' : 'BE84567968814145',
-            'acc_type': 'iban',
-            'partner_id': self.env.ref('base.main_company').partner_id.id,
-        })
-
-        self.bank = self.env['res.bank'].create({
+        cls.bank = cls.env['res.bank'].create({
             'name':'BNP',
             'bic': 'GEBABEBB',
         })
 
-        self.hr_employee_john = self.env['hr.employee'].create({
-            'address_home_id': self.ref('base.res_partner_address_2'),
-            'address_id': self.ref('base.res_partner_address_27'),
-            'birthday': '1984-05-01',
-            'children': 0.0,
-            'country_id': self.ref('base.in'),
-            'department_id': self.ref('hr.dep_rd'),
-            'gender': 'male',
-            'marital': 'single',
-            'name': 'John',
-            'bank_account_id': self.res_partner_bank.bank_id.id,
+        cls.res_partner_bank = cls.env['res.partner.bank'].create({
+            'acc_number': 'BE32707171912447',
+            'partner_id': cls.private_address.id,
+            'acc_type': 'bank',
+            'bank_id': cls.bank.id,
         })
 
-        self.account_journal = self.env['account.journal'].create({
-            'name' : 'MISC',
-            'code' : 'MSC',
-            'type' : 'general',
-            'sequence_id' : self.ir_sequence.id,
+        # cls.ir_sequence = cls.env['ir.sequence'].create({
+        #     'name' : 'SEQ',
+        #     'padding' : 4,
+        #     'number_increment' : 1,
+        # })
+
+        cls.bank_partner = cls.env['res.partner.bank'].create({
+            'acc_number' : 'BE84567968814145',
+            'acc_type': 'iban',
+            'partner_id': cls.env.ref('base.main_company').partner_id.id,
         })
 
-        self.bank_journal = self.env['account.journal'].create({
+
+        cls.hr_employee_john.bank_account_id = cls.res_partner_bank
+
+        # cls.hr_employee_john = cls.env['hr.employee'].create({
+        #     'address_home_id': cls.ref('base.res_partner_address_2'),
+        #     'address_id': cls.ref('base.res_partner_address_27'),
+        #     'birthday': '1984-05-01',
+        #     'children': 0.0,
+        #     'country_id': cls.ref('base.in'),
+        #     'department_id': cls.ref('hr.dep_rd'),
+        #     'gender': 'male',
+        #     'marital': 'single',
+        #     'name': 'John',
+        #     'bank_account_id': cls.res_partner_bank.bank_id.id,
+        # })
+
+        # cls.account_journal = cls.env['account.journal'].create({
+        #     'name' : 'MISC',
+        #     'code' : 'MSC',
+        #     'type' : 'general',
+        #     'sequence_id' : cls.ir_sequence.id,
+        # })
+
+        cls.bank_journal = cls.env['account.journal'].create({
             'name' : 'Bank',
             'code' : 'BNK',
             'type' : 'bank',
-            'sequence_id' : self.ir_sequence.id,
-            'bank_id' : self.bank.id,
-            'bank_account_id': self.bank_partner.id,
+            'sequence_id' : cls.ir_sequence.id,
+            'bank_id' : cls.bank.id,
+            'bank_account_id': cls.bank_partner.id,
         })
 
-        self.hr_structure_softwaredeveloper = self.env['hr.payroll.structure'].create({
-            'name': 'Salary Structure for Software Developer',
-            'rule_ids': [(6, 0, [
-                self.ref('hr_payroll.hr_salary_rule_houserentallowance1'),
-                self.ref('hr_payroll.hr_salary_rule_convanceallowance1'),
-                self.ref('hr_payroll.hr_salary_rule_professionaltax1'),
-                self.ref('hr_payroll.hr_salary_rule_providentfund1'),
-                self.ref('hr_payroll.hr_salary_rule_meal_voucher'),
-            ])],
-            'journal_id' : self.account_journal.id,
-            'type_id': self.env.ref('hr_payroll.structure_type_employee').id,
-        })
+        # cls.hr_structure_softwaredeveloper = cls.env['hr.payroll.structure'].create({
+        #     'name': 'Salary Structure for Software Developer',
+        #     'rule_ids': [(6, 0, [
+        #         cls.ref('hr_payroll.hr_salary_rule_houserentallowance1'),
+        #         cls.ref('hr_payroll.hr_salary_rule_convanceallowance1'),
+        #         cls.ref('hr_payroll.hr_salary_rule_professionaltax1'),
+        #         cls.ref('hr_payroll.hr_salary_rule_providentfund1'),
+        #         cls.ref('hr_payroll.hr_salary_rule_meal_voucher'),
+        #     ])],
+        #     'journal_id' : cls.account_journal.id,
+        #     'type_id': cls.env.ref('hr_payroll.structure_type_employee').id,
+        # })
 
-        self.hr_structure_type = self.env['hr.payroll.structure.type'].create({
-            'name': 'Salary Structure Type',
-            'struct_ids': [(4, self.hr_structure_softwaredeveloper.id)],
-        })
-
-        self.hr_contract_john = self.env['hr.contract'].create({
-            'date_end': Date.to_string(datetime.now() + timedelta(days=365)),
-            'date_start': Date.today(),
-            'name': 'Contract for John',
-            'wage': 5000.0,
-            'employee_id': self.hr_employee_john.id,
-            'structure_type_id': self.hr_structure_type.id,
-            'state': 'open',
-        })
-
-        self.hr_payslip_john = self.env['hr.payslip'].create({
-            'employee_id': self.hr_employee_john.id,
-            'struct_id' : self.hr_structure_softwaredeveloper.id,
-            'contract_id': self.hr_contract_john.id,
-            'journal_id': self.account_journal.id,
-            'name': 'Test Payslip John',
-        })
-        self.hr_payslip_john.compute_sheet()
-
-        self.payslip_run = self.env['hr.payslip.run'].create({
-            'date_end': '2011-09-30',
-            'date_start': '2011-09-01',
-            'name': 'Payslip for Employee'
-        })
+        # cls.hr_structure_type = cls.env['hr.payroll.structure.type'].create({
+        #     'name': 'Salary Structure Type',
+        #     'struct_ids': [(4, cls.hr_structure_softwaredeveloper.id)],
+        # })
 
         # Get a pain.001.001.03 schema validator
         schema_file_path = get_module_resource('account_sepa', 'schemas', 'pain.001.001.03.xsd')
-        self.xmlschema = etree.XMLSchema(etree.parse(open(schema_file_path)))
+        cls.xmlschema = etree.XMLSchema(etree.parse(open(schema_file_path)))
 
     def test_00_hr_payroll_account_sepa(self):
         """ Checking the process of payslip when you create a SEPA payment. """

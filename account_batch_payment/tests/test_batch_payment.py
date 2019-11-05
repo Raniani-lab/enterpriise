192fce8d01be1f6a17491e9a15250d74e9917b9f
Yannick Tivisse <yti@odoo.com>
2019-11-05 09:38:28 +0100

    [IMP] account_batch_payment: Adapt tests to work with/without demo data

--- account_batch_payment/tests/test_batch_payment.py
+++ account_batch_payment/tests/test_batch_payment.py
@@ -11,40 +11,42 @@ from odoo.exceptions import ValidationError
 @tagged('post_install', '-at_install')
 class TestBatchPayment(AccountingTestCase):
 
-    def setUp(self):
-        super(TestBatchPayment, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super(TestBatchPayment, cls).setUpClass()
 
         # Get some records
-        self.customers = self.env['res.partner'].search([])
-        self.batch_deposit = self.env.ref('account_batch_payment.account_payment_method_batch_deposit')
+        cls.customers = cls.env['res.partner'].search([])
+        cls.batch_deposit = cls.env.ref('account_batch_payment.account_payment_method_batch_deposit')
 
         # Create a bank journal
-        journal_account = self.env['account.account'].create({
+        journal_account = cls.env['account.account'].create({
             'code': 'BNKT',
             'name': 'Bank Test',
-            'user_type_id': self.ref('account.data_account_type_liquidity'),
+            'user_type_id': cls.env.ref('account.data_account_type_liquidity').id,
         })
-        self.journal = self.env['account.journal'].create({
+        cls.journal = cls.env['account.journal'].create({
             'name': 'Bank Test',
             'code': 'BNKT',
             'type': 'bank',
             'default_debit_account_id': journal_account.id,
             'default_credit_account_id': journal_account.id,
-            'company_id': self.env.ref('base.main_company').id
+            'company_id': cls.env.ref('base.main_company').id
         })
 
         # Create some payments
-        self.payments = [
-            self.createPayment(self.customers[0], 100),
-            self.createPayment(self.customers[1], 200),
-            self.createPayment(self.customers[2], 500),
+        cls.payments = [
+            cls.createPayment(cls.customers[0], 100),
+            cls.createPayment(cls.customers[1], 200),
+            cls.createPayment(cls.customers[2], 500),
         ]
 
-    def createPayment(self, partner, amount):
+    @classmethod
+    def createPayment(cls, partner, amount):
         """ Create a batch deposit payment """
-        return self.env['account.payment'].create({
-            'journal_id': self.journal.id,
-            'payment_method_id': self.batch_deposit.id,
+        return cls.env['account.payment'].create({
+            'journal_id': cls.journal.id,
+            'payment_method_id': cls.batch_deposit.id,
             'payment_type': 'inbound',
             'payment_date': time.strftime('%Y') + '-07-15',
             'amount': amount,

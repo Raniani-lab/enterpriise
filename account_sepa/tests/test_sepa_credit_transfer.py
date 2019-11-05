0319a309d0ac8cea620cd3724a84ed6ac3b5cf8b
Yannick Tivisse <yti@odoo.com>
2019-11-05 09:39:32 +0100

    [IMP] account_sepa: Adapt tests to work with/without demo data

--- account_sepa/tests/test_sepa_credit_transfer.py
+++ account_sepa/tests/test_sepa_credit_transfer.py
@@ -9,74 +9,84 @@ from odoo.tests import tagged
 from odoo.tests.common import Form
 
 
-@tagged('post_install','-at_install')
+@tagged('post_install', '-at_install')
 class TestSEPACreditTransfer(AccountingTestCase):
 
-    def setUp(self):
-        super(TestSEPACreditTransfer, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super(TestSEPACreditTransfer, cls).setUpClass()
 
-        self.env.user.company_id.country_id = self.env.ref('base.be')
+        cls.env.user.company_id.country_id = cls.env.ref('base.be')
 
         # Get some records
-        self.asustek_sup = self.env['res.partner'].search([('name', 'ilike', 'Wood Corner')])
-        self.suppliers = self.env['res.partner'].search([('name', 'not ilike', 'Wood Corner')])
-        self.sepa_ct = self.env.ref('account_sepa.account_payment_method_sepa_ct')
+        cls.asustek_sup = cls.env['res.partner'].create({'name': 'Wood Corner'})
+        cls.supplier = cls.env['res.partner'].create({'name': 'Lambda Supplier'})
+        cls.sepa_ct = cls.env.ref('account_sepa.account_payment_method_sepa_ct')
 
         # Create an IBAN bank account and its journal
-        bank = self.env['res.bank'].create({'name': 'ING', 'bic': 'BBRUBEBB'})
-        self.bank_journal = self.env['account.journal'].create({
+        bank = cls.env['res.bank'].create({'name': 'ING', 'bic': 'BBRUBEBB'})
+        cls.bank_journal = cls.env['account.journal'].create({
             'name': 'BE48363523682327',
             'type': 'bank',
             'bank_acc_number': 'BE48363523682327',
             'bank_id': bank.id,
         })
-        if self.bank_journal.company_id.currency_id != self.env.ref("base.EUR"):
-            self.bank_journal.default_credit_account_id.write({'currency_id': self.env.ref("base.EUR").id})
-            self.bank_journal.default_debit_account_id.write({'currency_id': self.env.ref("base.EUR").id})
-            self.bank_journal.write({'currency_id': self.env.ref("base.EUR").id})
+        if cls.bank_journal.company_id.currency_id != cls.env.ref("base.EUR"):
+            cls.bank_journal.default_credit_account_id.write({'currency_id': cls.env.ref("base.EUR").id})
+            cls.bank_journal.default_debit_account_id.write({'currency_id': cls.env.ref("base.EUR").id})
+            cls.bank_journal.write({'currency_id': cls.env.ref("base.EUR").id})
+
+        cls.bank_bnp = cls.env['res.bank'].create({'name': 'BNP Paribas', 'bic': 'GEBABEBB'})
 
         # Make sure all suppliers have exactly one bank account
-        self.setSingleBankAccountToPartner(self.asustek_sup, {
+        cls.setSingleBankAccountToPartner(cls.asustek_sup, {
             'acc_type': 'iban',
-            'partner_id': self.asustek_sup[0].id,
-            'acc_number': 'BE39103123456719',
-            'bank_id': self.env.ref('base.bank_bnp').id,
-            'currency_id': self.env.ref('base.USD').id,
+            'partner_id': cls.asustek_sup[0].id,
+            'acc_number': 'BE08429863697813',
+            'bank_id': cls.bank_bnp.id,
+            'currency_id': cls.env.ref('base.USD').id,
         })
-        self.setSingleBankAccountToPartner(self.suppliers[0], {
+        cls.setSingleBankAccountToPartner(cls.supplier, {
             'acc_type': 'bank',
-            'partner_id': self.suppliers[0].id,
-            'acc_number': '123456789',
+            'partner_id': cls.supplier.id,
+            'acc_number': '1234567890',
             'bank_name': 'Mock & Co',
         })
 
         # Create 1 payment per supplier
-        self.payment_1 = self.createPayment(self.asustek_sup, 500)
-        self.payment_1.post()
-        self.payment_2 = self.createPayment(self.asustek_sup, 600)
-        self.payment_2.post()
-        self.payment_3 = self.createPayment(self.suppliers[0], 700)
-        self.payment_3.post()
+        cls.payment_1 = cls.createPayment(cls.asustek_sup, 500)
+        cls.payment_1.post()
+        cls.payment_2 = cls.createPayment(cls.asustek_sup, 600)
+        cls.payment_2.post()
+        cls.payment_3 = cls.createPayment(cls.supplier, 700)
+        cls.payment_3.post()
 
         # Get a pain.001.001.03 schema validator
         schema_file_path = get_module_resource('account_sepa', 'schemas', 'pain.001.001.03.xsd')
-        self.xmlschema = etree.XMLSchema(etree.parse(open(schema_file_path)))
+        cls.xmlschema = etree.XMLSchema(etree.parse(open(schema_file_path)))
 
-    def setSingleBankAccountToPartner(self, partner_id, bank_account_vals):
+        # <record model="res.bank" id="bank_bnp">
+        #     <field name="name">BNP Paribas</field>
+        #     <field name="bic">GEBABEBB</field>
+        # </record>
+
+    @classmethod
+    def setSingleBankAccountToPartner(cls, partner_id, bank_account_vals):
         """ Make sure a partner has exactly one bank account """
         partner_id.bank_ids.unlink()
-        return self.env['res.partner.bank'].create(bank_account_vals)
+        return cls.env['res.partner.bank'].create(bank_account_vals)
 
-    def createPayment(self, partner, amount):
+    @classmethod
+    def createPayment(cls, partner, amount):
         """ Create a SEPA credit transfer payment """
-        return self.env['account.payment'].create({
-            'journal_id': self.bank_journal.id,
+        return cls.env['account.payment'].create({
+            'journal_id': cls.bank_journal.id,
             'partner_bank_account_id': partner.bank_ids[0].id,
-            'payment_method_id': self.sepa_ct.id,
+            'payment_method_id': cls.sepa_ct.id,
             'payment_type': 'outbound',
             'payment_date': '2015-04-28',
             'amount': amount,
-            'currency_id': self.env.ref("base.EUR").id,
+            'currency_id': cls.env.ref("base.EUR").id,
             'partner_id': partner.id,
             'partner_type': 'supplier',
         })
@@ -126,7 +136,7 @@ class TestSEPACreditTransfer(AccountingTestCase):
         self.assertEqual(form.display_qr_code, False)
         form.payment_method_code == 'manual'
         self.assertEqual(form.display_qr_code, False)
-        form.partner_id = self.suppliers[0]
+        form.partner_id = self.supplier
         self.assertEqual(form.display_qr_code, True)
         self.assertIn('The SEPA QR Code information is not set correctly', form.qr_code_url, 'A warning should be displayed')
         form.partner_id = self.asustek_sup

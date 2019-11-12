ab08541d8a63b648e1b7cac7061fa949607740b3
Arnaud Joset <arj@odoo.com>
2019-11-12 14:08:42 +0000

    [IMP] subscription: allow selecting tokens of children partners
    On a subscription, the payment token of a partner could not be set to the company of a partner when it existed.
    
    taskid: 2119417

--- sale_subscription/models/sale_subscription.py
+++ sale_subscription/models/sale_subscription.py
@@ -77,10 +77,11 @@ class SaleSubscription(models.Model):
     # customer portal
     uuid = fields.Char('Account UUID', default=lambda self: str(uuid4()), copy=False, required=True)
     website_url = fields.Char('Website URL', compute='_website_url', help='The full URL to access the document through the website.')
+    commercial_partner_id = fields.Many2one('res.partner', related='partner_id.commercial_partner_id', store=False)
     payment_token_id = fields.Many2one(
         'payment.token', 'Payment Token', check_company=True,
         help='If not set, the default payment token of the partner will be used.',
-        domain="[('partner_id', '=', partner_id), ('company_id', '=', company_id)]")
+        domain="[('partner_id', 'child_of', commercial_partner_id), ('company_id', '=', company_id)]")
     recurring_rule_boundary = fields.Selection(related="template_id.recurring_rule_boundary", readonly=False)
     starred_user_ids = fields.Many2many(
         'res.users', 'subscription_starred_user_rel', 'subscription_id', 'user_id',

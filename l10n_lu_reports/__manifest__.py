e28b066d34da040b324e51d9fe08879fa3ab0410
Adrien Horgnies <aho@odoo.com>
2019-11-13 07:33:06 +0000

    [FIX] l10n_lu: fine tune translations, action_id, 142000 and special_date_changer
    closes odoo/enterprise#6733
    
    X-original-commit: d56bc1d612cfa53212c4b77383b28e5a031c50ec
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- l10n_lu_reports/__manifest__.py
+++ l10n_lu_reports/__manifest__.py
@@ -10,10 +10,10 @@ Accounting reports for Luxembourg
     'category': 'Accounting',
     'depends': ['l10n_lu', 'account_reports'],
     'data': [
-        'data/account_financial_html_report_bs.xml',
-        'data/account_financial_html_report_bs_abr.xml',
         'data/account_financial_html_report_pl.xml',
         'data/account_financial_html_report_pl_abr.xml',
+        'data/account_financial_html_report_bs.xml',
+        'data/account_financial_html_report_bs_abr.xml',
     ],
     'auto_install': True,
     'license': 'OEEL-1',

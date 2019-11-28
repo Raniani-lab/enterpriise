77cd4b9e5d373a5a0f3bf0c4a587f6798da170b7
Goffin Simon <sig@odoo.com>
2019-11-28 13:38:47 +0000

    [FIX] sale_subscription: Invoicing from subscription ignore fiscal position
    Steps to reproduce the bug:
    
    - Let's consider a customer C with a fiscal position FP called 'Import/Export'
    - Let's consider a subscription template ST with payment_mode = 'manually'
    - Let's consider a subscription product P with a 21% tax with template ST
    - Create a SO for C with P
    - 21% tax is automatically mapped by 0% tax thanks to FP
    - Confirm the SO and a subscription is automatically created
    - Click on 'Create invoice'
    
    Bug:
    
    The 21% tax was not mapped by FP in the created invoice
    
    opw:2129516
    
    closes odoo/enterprise#6980
    
    X-original-commit: b431441f76558d71344f2f817baf165d7555eb70
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- sale_subscription/models/sale_subscription.py
+++ sale_subscription/models/sale_subscription.py
@@ -543,6 +543,9 @@ class SaleSubscription(models.Model):
         return recurring_next_date
 
     def _prepare_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
+        tax_ids = line.product_id.taxes_id.filtered(lambda t: t.company_id == line.analytic_account_id.company_id)
+        if fiscal_position:
+            tax_ids = self.env['account.fiscal.position'].browse(fiscal_position).map_tax(tax_ids)
         return {
             'name': line.name,
             'subscription_id': line.analytic_account_id.id,
@@ -551,7 +554,7 @@ class SaleSubscription(models.Model):
             'quantity': line.quantity,
             'product_uom_id': line.uom_id.id,
             'product_id': line.product_id.id,
-            'tax_ids': [(6, 0, line.product_id.taxes_id.filtered(lambda t: t.company_id == line.analytic_account_id.company_id).ids)],
+            'tax_ids': [(6, 0, tax_ids.ids)],
             'analytic_account_id': line.analytic_account_id.analytic_account_id.id,
             'subscription_start_date': date_start,
             'subscription_end_date': date_stop,

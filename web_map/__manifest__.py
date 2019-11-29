cd444b8dd9324e5202e56e0e450f4fa61a3a26a5
mightyjol (jhk) <jhk@odoo.com>
2019-11-29 12:31:55 +0000

    [FIX]web_map:contact map view content is translatable
    steps to reproduce:
    -install contacts
    -go to settings>translations>languages and activate a second language,
    for example Ukrainian as it has been fully translated in 13.0
    -switch to the Ukrainian language, go to contacts and open the map view
    you can see the fields are not translated
    they do not appear in the web_map.pot either
    
    translation seem to not happen because the templates.xml file was not
    living in the static/src folder
    
    the fields "Name" and "Address" are part of the contacts_enterprise
    module (so they will appear in contacts_enterprise.pot, not web_map.pot)
    the pot file of the contacts_enterprise module was not named properly
    
    previous behavior:
    most fields inside the map view and the buttons are not translatable
    
    current behavior:
    all text inside the map view is translatable except for the "zoom in/out"
    labels (they are part of a third party library)
    
    opw-2144401
    
    closes odoo/enterprise#7060
    
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- web_map/__manifest__.py
+++ web_map/__manifest__.py
@@ -12,7 +12,7 @@
         "views/res_partner_views.xml",
     ],
     'qweb':[
-        "static/xml/templates.xml"
+        "static/src/xml/templates.xml"
     ],
     'auto_install': True,
     'license': 'OEEL-1',

643b3d12935fda9333bce91d910cde5f5df967e3
jbm-odoo <jbm@odoo.com>
2019-12-02 10:07:48 +0000

    [FIX] helpdesk_sale_timesheet: Compute field and view
    closes odoo/enterprise#6984
    
    Taskid: 2148606
    Related: odoo/odoo#41229
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- helpdesk_sale_timesheet/models/helpdesk.py
+++ helpdesk_sale_timesheet/models/helpdesk.py
@@ -14,9 +14,12 @@ class HelpdeskTicket(models.Model):
 
     @api.depends('task_id.sale_line_id')
     def _compute_is_overdue(self):
-        for ticket in self.filtered('task_id.sale_line_id'):
-            sale_line_id = ticket.task_id.sale_line_id
-            ticket.is_overdue = sale_line_id.qty_delivered >= sale_line_id.product_uom_qty
+        for ticket in self:
+            if ticket.task_id.sale_line_id:
+                sale_line_id = ticket.task_id.sale_line_id
+                ticket.is_overdue = sale_line_id.qty_delivered >= sale_line_id.product_uom_qty
+            else:
+                ticket.is_overdue = False
 
     @api.onchange('partner_id', 'project_id')
     def _onchange_partner_project(self):

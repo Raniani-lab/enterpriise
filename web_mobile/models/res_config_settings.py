f4d4a59ff2471d453ec7b481abae402c233cb86a
Adrien Dieudonne <adr@odoo.com>
2019-12-02 12:19:01 +0000

    [FIX] web_mobile: better explanation for Firebase URL field
    Related to https://github.com/odoo/enterprise/pull/6414
    
    closes odoo/enterprise#6987
    
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>

--- web_mobile/models/res_config_settings.py
+++ web_mobile/models/res_config_settings.py
@@ -11,6 +11,10 @@ class ResConfigSettings(models.TransientModel):
     # We want default=True but because of odoo/addons/base/models/ir_config_parameter.py:L91
     # when we do a change in False it's not saved in the database. So we have inverted the condition
     disable_redirect_firebase_dynamic_link = fields.Boolean(
-        "Allows to open mail's links in the native mobile app Android and iOS if possible (e.g. \"View Task\")",
+        "Disable link redirection to mobile app",
+        help="Check this if dynamic mobile-app detection links cause problems "
+            "for your installation. This will stop the automatic wrapping of "
+            "links inside outbound emails. The links will always open in a "
+            "normal browser, even for users who have the Android/iOS app installed.",
         config_parameter='web_mobile.disable_redirect_firebase_dynamic_link'
     )

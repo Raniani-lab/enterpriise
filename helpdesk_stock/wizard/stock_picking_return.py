3ce1e68cbd0784a0af88a46f7df70c0dcdb82929
Xavier Morel <xmo@odoo.com>
2019-12-05 13:59:51 +0000

    [REM] helpdesk_stock: dynamic domain
    Not really suitable for a regular conditional as the dynamic domain
    would use a sequence of conditional. And it doesn't seem this can be
    simplified without a change in behavior as pickings don't really
    relate the sale_id and the partner_id.
    
    Ticket 2115472
    
    closes odoo/enterprise#7083
    
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- helpdesk_stock/wizard/stock_picking_return.py
+++ helpdesk_stock/wizard/stock_picking_return.py
@@ -10,15 +10,18 @@ class ReturnPicking(models.TransientModel):
     partner_id = fields.Many2one('res.partner', related="ticket_id.partner_id", string="Customer")
     ticket_id = fields.Many2one('helpdesk.ticket')
     sale_order_id = fields.Many2one('sale.order', related="ticket_id.sale_order_id", string='Sales Order')
+    picking_id = fields.Many2one(domain="[('id', 'in', suitable_picking_ids)]")
+    suitable_picking_ids = fields.Many2many('stock.picking', compute='_compute_suitable_picking_ids')
 
-    @api.onchange('sale_order_id', 'partner_id')
-    def _onchange_picking_id_domain(self):
-        domain = [('state', '=', 'done'), ('picking_type_code', '=', 'outgoing')]
-        if self.sale_order_id:
-            domain += [('id', 'in', self.sale_order_id.picking_ids.ids)]
-        elif self.partner_id:
-            domain += [('partner_id', 'child_of', self.partner_id.commercial_partner_id.id)]
-        return {'domain': {'picking_id': domain}}
+    @api.depends('ticket_id.sale_order_id.picking_ids', 'ticket_id.partner_id.commercial_partner_id')
+    def _compute_suitable_picking_ids(self):
+        for r in self:
+            domain = [('state', '=', 'done'), ('picking_type_code', '=', 'outgoing')]
+            if r.sale_order_id:
+                domain += [('id', 'in', r.sale_order_id.picking_ids._origin.ids)]
+            elif r.partner_id:
+                domain += [('partner_id', 'child_of', r.partner_id.commercial_partner_id._origin.id)]
+            r.suitable_picking_ids = self.env['stock.picking'].search(domain)
 
     def create_returns(self):
         res = super(ReturnPicking, self).create_returns()

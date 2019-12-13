db8c13306d368b28a51194436cc035e366693440
Xavier Bol (xbo) <xbo@odoo.com>
2019-12-13 10:01:59 +0100

    [IMP] timesheet_grid: add condition to validate timesheets
    TaskID: 2088586
    
    We must check that the user only validate the timesheets where the timer
    is not running. Because if we validate a timesheet when its timer is
    running, then the user may not stop the timer of his own timesheet,
    because he may not edit a validated timesheet.

--- timesheet_grid/wizard/timesheet_validation.py
+++ timesheet_grid/wizard/timesheet_validation.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from odoo.osv import expression
 
 
 class ValidationWizard(models.TransientModel):
@@ -12,7 +13,7 @@ class ValidationWizard(models.TransientModel):
     validation_line_ids = fields.One2many('timesheet.validation.line', 'validation_id')
 
     def action_validate(self):
-        domain = self.env['account.analytic.line']._get_domain_for_validation_timesheets()
+        domain = expression.AND([[('timer_start', '=', False)], self.env['account.analytic.line']._get_domain_for_validation_timesheets()])
 
         # sudo needed because timesheet approver may not have access on account.analytic.line
         self.validation_line_ids.filtered('validate').mapped('timesheet_ids').sudo().filtered_domain(domain).write({'validated': True})

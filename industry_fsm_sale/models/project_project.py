89936d701f5e0b86f21e92a8f75281662a7d2bde
jdoutreloux <jud@odoo.com>
2019-12-24 12:38:53 +0100

    [IMP] industry_fsm_sale : change sql constraint in api constrains and move timesheet_product_id to community
    This change avoids errors when installing this module when projects
    with allow_timesheets=True and allow_billable=True are already
    created.
    
    Timesheet_product_id
    ====================
    
    This field was being added in industry_fsm_sale and was required
    if allow_billable and allow_timesheets were true. But those 2
    other fields are already available once sale_timesheet is
    installed.
    
    The field timesheet_product_id is thus being moved to
    sale_timesheet for more coherence.
    
    Task-2157571

--- industry_fsm_sale/models/project_project.py
+++ industry_fsm_sale/models/project_project.py
@@ -7,52 +7,19 @@ from odoo import api, fields, models
 class Project(models.Model):
     _inherit = "project.project"
 
-    allow_billable = fields.Boolean("Bill from Tasks", compute='_compute_allow_billable', store=True, readonly=False)
     allow_material = fields.Boolean("Products on Tasks", compute="_compute_allow_material", store=True, readonly=False)
     allow_quotations = fields.Boolean("Extra Quotations", compute='_compute_allow_quotations')
-    timesheet_product_id = fields.Many2one(
-        'product.product', string='Timesheet Product',
-        compute='_compute_timesheet_product_id', store=True, readonly=False,
-        domain="""[
-            ('type', '=', 'service'),
-            ('invoice_policy', '=', 'delivery'),
-            ('service_type', '=', 'timesheet'),
-            '|', ('company_id', '=', False), ('company_id', '=', company_id)]""",
-        help='Select a Service product with which you would like to bill your time spent on tasks.')
-    allow_timesheets = fields.Boolean(default=True)
-    allow_timesheet_timer = fields.Boolean(default=True)
 
     _sql_constraints = [
         ('material_imply_billable', "CHECK((allow_material = 't' AND allow_billable = 't') OR (allow_material = 'f'))", 'The material can be allowed only when the task can be billed.'),
-        ('timesheet_product_required_if_billable_and_timesheets', "CHECK((allow_billable = 't' AND allow_timesheets = 't' AND timesheet_product_id IS NOT NULL) OR (allow_billable = 'f') OR (allow_timesheets = 'f') OR (is_fsm = 'f'))", 'The timesheet product is required when the task can be billed and timesheets are allowed.'),
         ('fsm_imply_task_rate', "CHECK((is_fsm = 't' AND sale_line_id IS NULL) OR (is_fsm = 'f'))", 'An FSM project must be billed at task rate.'),
     ]
 
-    @api.depends('allow_timesheets')
-    def _compute_allow_timesheet_timer(self):
-        for project in self:
-            project.allow_timesheet_timer = project.allow_timesheets
-
-    @api.depends('is_fsm')
-    def _compute_allow_timesheets(self):
-        for project in self:
-            if not project._origin:
-                project.allow_timesheets = True
-
-    @api.depends('allow_timesheets', 'allow_billable')
-    def _compute_timesheet_product_id(self):
-        default_product = self.env.ref('sale_timesheet_enterprise.fsm_time_product', False)
-        for project in self:
-            if project.allow_timesheets and project.allow_billable and not project.timesheet_product_id:
-                self.timesheet_product_id = default_product
-            else:
-                self.timesheet_product_id = False
-
-    @api.depends('is_fsm')
+    @api.depends('allow_billable')
     def _compute_allow_billable(self):
+        super()._compute_allow_billable()
         for project in self:
-            if not project._origin:
-                project.allow_billable = project.is_fsm
+            project.allow_material = project.allow_billable
 
     @api.depends('allow_billable', 'is_fsm')
     def _compute_allow_material(self):

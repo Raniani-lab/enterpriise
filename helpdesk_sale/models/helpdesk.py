3b7d0daf525aae5809462b738d5e3511e4ce7410
jdoutreloux <jud@odoo.com>
2019-12-24 10:08:08 +0100

    [IMP] helpdesk_sale : change the sale_order_id field into a computed field.
    This change is made so that we can set the sale_order_id when the ticket
    is linked to a project and/or a task that has a sale_order_id.
    
    Task-2157571

--- helpdesk_sale/models/helpdesk.py
+++ helpdesk_sale/models/helpdesk.py
@@ -8,6 +8,9 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     commercial_partner_id = fields.Many2one(related='partner_id.commercial_partner_id')
-    sale_order_id = fields.Many2one('sale.order', string='Sales Order', domain="['|', (not commercial_partner_id, '=', 1), ('partner_id', 'child_of', commercial_partner_id or []), ('company_id', '=', company_id)]",
+    sale_order_id = fields.Many2one('sale.order', string='Ref. Sales Order',
+        domain="""[
+            '|', (not commercial_partner_id, '=', 1), ('partner_id', 'child_of', commercial_partner_id or []),
+            ('company_id', '=', company_id)]""",
         groups="sales_team.group_sale_salesman,account.group_account_invoice",
         help="Reference of the Sales Order to which this ticket refers. Setting this information aims at easing your After Sales process and only serves indicative purposes.")

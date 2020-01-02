73cf87525535d90e49fb5587195fb1c9018ea603
Jason Van Malder <jvm@odoo.com>
2020-01-02 14:29:58 +0000

    [FIX] inter_company_rules: fix "assign to" required
    Issue
    
        - Have 2 company
        - Install Sales
        - General settings
    
        The field "Assign to" is required in the company form but
        not in the general settings.
    
    Cause
    
        Missing `required=True`
    
    Solution
    
        Adding it
    
    OPW-2162496
    
    closes odoo/enterprise#7501
    
    X-original-commit: e3cf627cd87ba5f63bfd5b68c111fb90552fcbf4
    Signed-off-by: Jason Van Malder <jvm-odoo@users.noreply.github.com>

--- inter_company_rules/models/res_config_settings.py
+++ inter_company_rules/models/res_config_settings.py
@@ -8,7 +8,7 @@ class ResConfigSettings(models.TransientModel):
     rule_type = fields.Selection(related='company_id.rule_type', readonly=False)
     applicable_on = fields.Selection(related='company_id.applicable_on', readonly=False)
     auto_validation = fields.Boolean(related='company_id.auto_validation', readonly=False)
-    intercompany_user_id = fields.Many2one(related='company_id.intercompany_user_id', readonly=False)
+    intercompany_user_id = fields.Many2one(related='company_id.intercompany_user_id', readonly=False, required=True)
     rules_company_id = fields.Many2one(related='company_id', string='Select Company', readonly=True)
     warehouse_id = fields.Many2one(related='company_id.warehouse_id', string='Warehouse For Purchase Orders', readonly=False, domain=lambda self: [('company_id', '=', self.env.company.id)])
     intercompany_transaction_message = fields.Char()

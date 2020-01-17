Raniani-lab/enterpriise

commit b967916cd2ebba319c25e09f7ee8e2512465d591

[FIX] account_bank_statement_import_csv: missing adaptation from main It seems that this module was forgotten at the refactoring of https://github.com/odoo/odoo/commit/97c3f0f0257774af0e064b0085e2b6c5b65d9efe
closes odoo/enterprise#5605

Signed-off-by: Quentin De Paoli (qdp) <qdp@openerp.com>

account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
@@ -14,13 +14,14 @@ class AccountBankStatementImport(models.TransientModel):
         return filename and filename.lower().strip().endswith('.csv')
 
     def import_file(self):
-        if not self._check_csv(self.filename):
+        self.attachment_ids.ensure_one()
+        if not self._check_csv(self.attachment_ids.name):
             return super(AccountBankStatementImport, self).import_file()
         ctx = dict(self.env.context)
         import_wizard = self.env['base_import.import'].create({
             'res_model': 'account.bank.statement.line',
-            'file': base64.b64decode(self.data_file),
-            'file_name': self.filename,
+            'file': base64.b64decode(self.attachment_ids.datas),
+            'file_name': self.attachment_ids.name,
             'file_type': 'text/csv'
         })
         ctx['wizard_id'] = import_wizard.id
@@ -30,7 +31,7 @@ class AccountBankStatementImport(models.TransientModel):
             'params': {
                 'model': 'account.bank.statement.line',
                 'context': ctx,
-                'filename': self.filename,
+                'filename': self.attachment_ids.name,
             }
         }
 

account_bank_statement_import_ofx/tests/test_import_bank_statement.py
@@ -23,10 +23,9 @@ class TestOfxFile(TransactionCase):
         # Use an import wizard to process the file
         import_wizard = self.env['account.bank.statement.import'].with_context(journal_id=bank_journal.id).create({
             'attachment_ids': [(0, 0, {
-                'name': 'test file',
+                'name': 'test_ofx.ofx',
                 'datas': ofx_file,
             })],
-            'filename': 'test_ofx.ofx',
         })
         import_wizard.import_file()
 

commit 3e283339c71b962f042df52e2114fdc57ce96103

[FIX] account_bank_statement_import_csv: import multiple files
- Open the bank statement import wizard
- Select more than one file

Boom, singleton error.

The feature developed in https://github.com/odoo/odoo/commit/97c3f0f0257774af0e064b00
has been promptly broken by b967916cd2ebba319c25. Interestingly, using
`ensure_one` when more than one attachment is allowed doesn't work very
well.

We raise meaningful errors instead.

opw-2172693

closes odoo/enterprise#7781

X-original-commit: e8c6f12c63aa35862a2e791d43cab98fabb1a5ef
Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

account_bank_statement_import_csv/i18n/account_bank_statement_import_csv.pot
@@ -39,6 +39,18 @@ msgid "Journal"
 msgstr ""
 
 #. module: account_bank_statement_import_csv
+#: code:addons/account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py:0
+#, python-format
+msgid "Mixing CSV files with other file types is not allowed."
+msgstr ""
+
+#. module: account_bank_statement_import_csv
+#: code:addons/account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py:0
+#, python-format
+msgid "Only one CSV file can be selected."
+msgstr ""
+
+#. module: account_bank_statement_import_csv
 #. openerp-web
 #: code:addons/account_bank_statement_import_csv/static/src/js/import_bank_stmt.js:0
 #, python-format

account_bank_statement_import_csv/wizard/account_bank_statement_import_csv.py
@@ -4,7 +4,8 @@
 import base64
 import psycopg2
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 
 class AccountBankStatementImport(models.TransientModel):
@@ -14,7 +15,15 @@ class AccountBankStatementImport(models.TransientModel):
         return filename and filename.lower().strip().endswith('.csv')
 
     def import_file(self):
-        self.attachment_ids.ensure_one()
+        # In case of CSV files, only one file can be imported at a time.
+        if len(self.attachment_ids) > 1:
+            csv = [bool(self._check_csv(att.name)) for att in self.attachment_ids]
+            if True in csv and False in csv:
+                raise UserError(_('Mixing CSV files with other file types is not allowed.'))
+            if csv.count(True) > 1:
+                raise UserError(_('Only one CSV file can be selected.'))
+            return super(AccountBankStatementImport, self).import_file()
+
         if not self._check_csv(self.attachment_ids.name):
             return super(AccountBankStatementImport, self).import_file()
         ctx = dict(self.env.context)
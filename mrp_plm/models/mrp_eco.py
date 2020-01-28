58272a81f51de41db5d2e5582b597a4f9fc0e5fa
Nicolas Martinelli <nim@odoo.com>
2020-01-28 17:02:35 +0000

    [FIX] mrp_plm: BoM and routing changes
    Improved version of bbf0afe23a87892502992825e50e29fa67e03002 to make it
    work on new records.
    
    closes odoo/enterprise#7999
    
    X-original-commit: d1c322bde2ee6713a33ea0cf9d0f3ded960a194e
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- mrp_plm/models/mrp_eco.py
+++ mrp_plm/models/mrp_eco.py
@@ -274,7 +274,7 @@ class MrpEco(models.Model):
 
     def _get_difference_bom_lines(self, old_bom, new_bom):
         # Return difference lines from two bill of material.
-        new_bom_commands = []
+        new_bom_commands = [(5,)]
         old_bom_lines = dict(((line.product_id, tuple(line.bom_product_template_attribute_value_ids.ids)), line) for line in old_bom.bom_line_ids)
         if self.new_bom_id:
             for line in new_bom.bom_line_ids:
@@ -362,13 +362,11 @@ class MrpEco(models.Model):
     @api.depends('bom_id.bom_line_ids', 'new_bom_id.bom_line_ids', 'new_bom_id.bom_line_ids.product_qty', 'new_bom_id.bom_line_ids.product_uom_id', 'new_bom_id.bom_line_ids.operation_id')
     def _compute_bom_change_ids(self):
         # Compute difference between old bom and new bom revision.
-        self.mapped('bom_change_ids').unlink()
         for eco in self:
             eco.bom_change_ids = eco._get_difference_bom_lines(eco.bom_id, eco.new_bom_id)
 
     @api.depends('bom_id.bom_line_ids', 'current_bom_id.bom_line_ids', 'current_bom_id.bom_line_ids.product_qty', 'current_bom_id.bom_line_ids.product_uom_id', 'current_bom_id.bom_line_ids.operation_id')
     def _compute_previous_bom_change(self):
-        self.mapped('previous_change_ids').unlink()
         for eco in self:
             if eco.current_bom_id:
                 # Compute difference between old bom and newly activated bom.
@@ -378,10 +376,9 @@ class MrpEco(models.Model):
 
     @api.depends('routing_id.operation_ids', 'new_routing_id.operation_ids')
     def _compute_routing_change_ids(self):
-        self.mapped('routing_change_ids').unlink()
         for rec in self:
             # TDE TODO: should we add workcenter logic ?
-            new_routing_commands = []
+            new_routing_commands = [(5,)]
             old_routing_lines = dict(((op.workcenter_id,), op) for op in rec.routing_id.operation_ids)
             if rec.new_routing_id and rec.routing_id:
                 for operation in rec.new_routing_id.operation_ids:

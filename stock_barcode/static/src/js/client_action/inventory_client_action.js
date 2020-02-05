fb981fc487b3e9348243724b6d116867a1c41764
Nicolas Martinelli <nim@odoo.com>
2020-02-05 12:05:30 +0000

    [FIX] stock_barcode: inventory with lot
    - Create a product tracked by unique S/N, set a barcode
    - Create some stock
    - Create an inventory adjustment, set all quantities to zero
    - Open the barcode view
    - Scan the product barcode
    - Scan the lot
    
    An error prevents from registering the product: ''The scanned serial
    number is already used.'
    
    We should check the quantity to know if a line is used of not, as done
    in a40f51ed1f59bdd69f4a1c4b47679d26f70a618e
    
    opw-2189812
    
    closes odoo/enterprise#8254
    
    X-original-commit: 8a915b0bb1f5d7385929959576c660ee41ed2eeb
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- stock_barcode/static/src/js/client_action/inventory_client_action.js
+++ stock_barcode/static/src/js/client_action/inventory_client_action.js
@@ -137,7 +137,7 @@ var InventoryClientAction = ClientAction.extend({
         var lines = this._getLines(this.currentState);
         for (var i = 0; i < lines.length; i++) {
             var line = lines[i];
-            if (line.product_id.id === product.id &&
+            if (line.product_qty !== 0 && line.product_id.id === product.id &&
                 line.prod_lot_id && line.prod_lot_id[1] === lot_name) {
                 return true;
             }

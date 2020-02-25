d8321475d92215dd8a98375d6fc960f31249a225
Thibault Delavallée <tde@odoo.com>
2020-02-25 12:36:11 +0000

    [REF] various: update to "move rendering code to mail render mixin"
    Some methods notably _render_template is now locate on mail.render.mixin
    instead of mail.template. This commit updates enterprise accordingly.
    
    Task ID 1963529
    Community PR odoo/odoo#32397
    Enterprise PR odoo/enterprise#8634

--- hr_appraisal/wizard/request_appraisal.py
+++ hr_appraisal/wizard/request_appraisal.py
@@ -90,8 +90,8 @@ class RequestAppraisal(models.TransientModel):
                     'author_name': wizard.author_id.name,
                     'url': "${ctx['url']}",
                 }
-                wizard.subject = self.env['mail.template'].with_context(ctx)._render_template(wizard.template_id.subject, 'res.users', self.env.user.ids, post_process=True)[self.env.user.id]
-                wizard.body = self.env['mail.template'].with_context(ctx)._render_template(wizard.template_id.body_html, 'res.users', self.env.user.ids, post_process=False)[self.env.user.id]
+                wizard.subject = self.env['mail.render.mixin'].with_context(ctx)._render_template(wizard.template_id.subject, 'res.users', self.env.user.ids, post_process=True)[self.env.user.id]
+                wizard.body = self.env['mail.render.mixin'].with_context(ctx)._render_template(wizard.template_id.body_html, 'res.users', self.env.user.ids, post_process=False)[self.env.user.id]
             elif not wizard.body:
                 wizard.body = ''
 
@@ -111,7 +111,7 @@ class RequestAppraisal(models.TransientModel):
         appraisal.colleagues_ids |= self.recipient_ids.user_ids.employee_ids - involved_employees
 
         ctx = {'url': '/mail/view?model=%s&res_id=%s' % ('hr.appraisal', appraisal.id)}
-        body = self.env['mail.template'].with_context(ctx)._render_template(self.body, 'hr.appraisal', appraisal.ids, post_process=True)[appraisal.id]
+        body = self.env['mail.render.mixin'].with_context(ctx)._render_template(self.body, 'hr.appraisal', appraisal.ids, post_process=True)[appraisal.id]
 
         for user in self.recipient_ids.user_ids or self.env.user:
             appraisal.with_context(mail_activity_quick_update=True).activity_schedule(

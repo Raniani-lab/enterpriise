67cd444c704359cf9877c4480e8d04e02aaf8fce
jbm-odoo <jbm@odoo.com>
2020-03-02 09:06:52 +0000

    [FIX] planning: No start_date in domain
    Before this commit:
    If we got to view Schedule by Role, and we show list view, there
    are a traceback.
    
    After this commit:
    No more traceback
    
    task ID 2206454
    
    closes odoo/enterprise#8959
    
    X-original-commit: ae44da4860e7eb570281164801ec9c96e503fd5f
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- planning/models/planning.py
+++ planning/models/planning.py
@@ -668,7 +668,8 @@ class Planning(models.Model):
 
     def _read_group_employee_id(self, employees, domain, order):
         if self._context.get('planning_expand_employee'):
-            start_date = datetime.strptime([dom[2] for dom in domain if dom[0] == 'start_datetime'][-1], '%Y-%m-%d %H:%M:%S') or datetime.now()
+            start_date_list = [dom[2] for dom in domain if dom[0] == 'start_datetime']
+            start_date = datetime.strptime(start_date_list[-1], '%Y-%m-%d %H:%M:%S') if start_date_list else datetime.now()
             min_date = start_date - timedelta(days=30)
             max_date = start_date + timedelta(days=30)
             return self.env['planning.slot'].search([('start_datetime', '>=', min_date), ('start_datetime', '<=', max_date)]).mapped('employee_id')

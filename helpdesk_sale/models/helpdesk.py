738fb8bb54c59228da405e5a73e92870ae9e122e
Goffin Simon <sig@odoo.com>
2020-03-04 12:12:07 +0000

    [FIX] helpdesk_sale: Impossible to duplicate an helpdesk ticket
    Steps to reproduce the bug:
    
    - Let's consider a user U with no rights in Sales and Accounting
    - Log with U and open an Helpdesk ticket T
    - Try to duplicate T
    
    Bug:
    
    An access error was raised.
    
    opw:2201306
    
    closes odoo/enterprise#9100
    
    X-original-commit: 8dfb397d46b971e30dd25515e65e69cbe0f548e2
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- helpdesk_sale/models/helpdesk.py
+++ helpdesk_sale/models/helpdesk.py
@@ -14,3 +14,11 @@ class HelpdeskTicket(models.Model):
             ('company_id', '=', company_id)]""",
         groups="sales_team.group_sale_salesman,account.group_account_invoice",
         help="Reference of the Sales Order to which this ticket refers. Setting this information aims at easing your After Sales process and only serves indicative purposes.")
+
+    def copy(self, default=None):
+        if not self.env.user.has_group('sales_team.group_sale_salesman') and not self.env.user.has_group('account.group_account_invoice'):
+            if default is None:
+                default = {'sale_order_id': False}
+            else:
+                default.update({'sale_order_id': False})
+        return super(HelpdeskTicket, self).copy(default=default)

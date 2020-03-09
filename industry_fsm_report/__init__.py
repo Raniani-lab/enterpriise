42922944c721da4ea994312180b022bc3bbabd0c
Nicolas Martinelli <nim@odoo.com>
2020-03-09 13:35:49 +0000

    [FIX] industry_fsm_report: missing record
    - Install industry_fsm
    - Delete product variant "Field Service"
    - Install industry_fsm_report
    
    An error is raised because of a missing record.
    
    Complement of b8ee437232f0506abd0c877c39f40f15046c6c60
    
    opw-2180900
    
    closes odoo/enterprise#9135
    
    X-original-commit: 8f14dfbeeeaf2b4d41e5221f36746fb855a4aa5c
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- industry_fsm_report/__init__.py
+++ industry_fsm_report/__init__.py
@@ -17,3 +17,9 @@ def post_init(cr, registry):
                 "worksheet_template_id": env.ref("industry_fsm_report.fsm_worksheet_template").id,
             }
         )
+
+    fsm_product = env.ref("industry_fsm.field_service_product", raise_if_not_found=False)
+    if fsm_product:
+        fsm_product.write(
+            {"worksheet_template_id": env.ref("industry_fsm_report.fsm_worksheet_template").id,}
+        )

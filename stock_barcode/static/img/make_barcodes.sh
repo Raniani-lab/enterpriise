669886fb1a0b3eaad81fbeddfde205ca81dc7ae1
svs-odoo <svs@odoo.com>
2020-03-12 09:14:16 +0000

    [IMP] stock_barcode: rearrange demo barcode pdf
    Rearranges the disposition of barcodes to make demonstration flux easier
    to accomplish. Also adds a label for each flux.
    
    task-2199747
    
    X-original-commit: 3004a5dea736efd183de1b77fa57b97205224e24

--- stock_barcode/static/img/make_barcodes.sh
+++ stock_barcode/static/img/make_barcodes.sh
@@ -37,47 +37,58 @@ HEADER
 cat barcodes_actions_header.ps barcodes_actions_barcode.ps | ps2pdf - - > barcodes_actions.pdf
 rm barcodes_actions_header.ps barcodes_actions_barcode.ps
 
-barcode -t 3x10+20+20 -m 25x15 -p "210x297mm" -e code128b -n > barcodes_demo_barcode.ps  << BARCODES
-601647855631
-601647855640
-601647855644
-601647855638
-WH-STOCK
-2601892
-2601985
-WH-SHELF-REF
-PACK0000001
-WH/OUT/00005
+barcode -t 3x7+20+35 -m 25x30 -p "210x297mm" -e code128b -n > barcodes_demo_barcode.ps  << BARCODES
 WH/IN/00003
-LOT-000001
+601647855638
+O-BTN.validate
+WH/OUT/00005
+601647855644
+O-BTN.validate
+WH-RECEIPTS
+601647855640
+601647855631
 LOT-000002
+LOT-000003
+O-BTN.validate
 WH-DELIVERY
 WH-RECEIPTS
 WH-INTERNAL
 WH-PICK
 WH-PACK
+PACK0000001
+WH-STOCK
+2601892
+2601985
 BARCODES
 
 cat > barcodes_demo_header.ps << HEADER
+/showLabel { /Helvetica findfont 14 scalefont setfont moveto show } def
 /showTitle { /Helvetica findfont 11 scalefont setfont moveto show } def
-(Cable Management Box) 45 807 showTitle
-(Corner Desk Black) 230 807 showTitle
-(Desk Combination) 415 807 showTitle
-(Desk Stand with Screen) 45 727 showTitle
-(WH/Stock) 230 727 showTitle
-(WH/Stock/Shelf 1) 415 727 showTitle
-(WH/Stock/Shelf 2) 45 647 showTitle
-(WH/Stock/Shelf 2/Small Refrigerator) 230 647 showTitle
-(PACK0000001) 415 647 showTitle
-(WH/OUT/00005) 45 567 showTitle
-(WH/IN/00003) 230 567 showTitle
-(LOT-000001) 415 567 showTitle
-(LOT-000002) 45 487 showTitle
-(YourCompany delivery) 230 487 showTitle
-(YourCompany receipts) 415 487 showTitle
-(YourCompany internal) 45 407 showTitle
-(YourCompany pick) 230 407 showTitle
-(YourCompany pack) 415 407 showTitle
+(Receive products in stock) 45 797 showLabel
+(WH/IN/00003) 45 777 showTitle
+(Desk Stand with Screen) 230 777 showTitle
+(Validate) 415 777 showTitle
+(Deliver products to your customers) 45 687 showLabel
+(WH/OUT/00005) 45 667 showTitle
+(Desk Combination) 230 667 showTitle
+(Validate) 415 667 showTitle
+(Receive products tracked by lot number (activate Lots & Serial Numbers)) 45 577 showLabel
+(YourCompany Receipts) 45 557 showTitle
+(Corner Desk Black) 230 557 showTitle
+(Cable Management Box) 415 557 showTitle
+(LOT-000002) 45 447 showTitle
+(LOT-000003) 230 447 showTitle
+(Validate) 415 447 showTitle
+(Operation types, locations and packs for YourCompany) 45 357 showLabel
+(YourCompany Delivery) 45 337 showTitle
+(YourCompany Receipts) 230 337 showTitle
+(YourCompany Internal) 415 337 showTitle
+(YourCompany Pick) 45 227 showTitle
+(YourCompany Pack) 230 227 showTitle
+(PACK0000001) 415 227 showTitle
+(WH/Stock) 45 117 showTitle
+(WH/Stock/Shelf 1) 230 117 showTitle
+(WH/Stock/Shelf 2) 415 117 showTitle
 HEADER
 
 barcode -t 3x10+20+20 -m 25x15 -p "210x297mm" -e code128b -n > barcodes_demo_barcode_chicago.ps  << BARCODES

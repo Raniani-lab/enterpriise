18a1fac31baa5d3fb243fa926e530bf88aac7024
Nasreddin Boulif <bon@odoo.com>
2020-03-12 11:18:55 +0000

    [IMP] event_barcode: support Registration Desk by improve barcode interface
    PURPOSE
    
    Introduce a clearer way of handling registrations for single / multiple events
    occurring at the same time and place by
    
      * improve attendees kanban view, notably by better supporting mono and multi
        event (hide unnecessary information);
      * introducing a Registration Desk menu item to manage multi-event mode;
    
    SPECIFICATIONS
    
    Previously to this commit, ticket validation team had to switch event before
    launching barcode interface. This process was verious tedious in multi-event
    mode.
    
    In this commit we add a Registration Desk menu opening the event barcode
    interface in multi event mode. It allows to scan tickets whatever their
    event, having therefore one interface for any ongoing events.
    
    LINKS
    
    Task ID #2214290
    Community PR #47511
    Enterprise PR odoo/enterprise#9190
    
    Co-Authored-By: Nasreddin Boulif <bon@odoo.com>
    Co-Authored-By: Thibault Delavallée <tde@odoo.com>

--- event_barcode/controllers/main.py
+++ event_barcode/controllers/main.py
@@ -1,39 +1,28 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, http, _
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http, _
 from odoo.http import request
 
 
 class EventBarcode(http.Controller):
 
-    @http.route('/event_barcode/register_attendee', type='json', auth="user")
-    def register_attendee(self, barcode, event_id, **kw):
-        Registration = request.env['event.registration']
-        attendee = Registration.search([('barcode', '=', barcode)], limit=1)
-        if not attendee:
-            return {'error': 'invalid_ticket'}
-        is_different_event = attendee.event_id.id != event_id
-        need_confirmation = attendee.state not in ['done', 'cancel']
-        res = attendee._get_registration_summary()
-        if need_confirmation:
-            if not is_different_event:
-                attendee.action_set_done()
-                status = 'confirmed_registration'
-            else:
-                status = 'need_manual_confirmation'
-        elif attendee.state == 'cancel':
-            status = 'canceled_registration'
+    @http.route(['/event/init_barcode_interface'], type='json', auth="user")
+    def init_barcode_interface(self, event_id):
+        event = request.env['event.event'].browse(event_id).exists() if event_id else False
+        if event:
+            return {
+                'name': event.name,
+                'country': event.address_id.country_id.name,
+                'city': event.address_id.city,
+                'company_name': event.company_id.name,
+                'company_id': event.company_id.id
+            }
         else:
-            status = 'already_registered'
-        res.update({'is_different_event': is_different_event,
-                    'status': status})
-        return res
-
-    @http.route(['/event_barcode/event'], type='json', auth="user")
-    def get_event_data(self, event_id):
-        event = request.env['event.event'].browse(event_id)
-        return {
-            'name': event.name,
-            'country': event.address_id.country_id.name,
-            'city': event.address_id.city,
-            'company_name': event.company_id.name
-        }
+            return {
+                'name': _('Registration Desk'),
+                'country': False,
+                'city': False,
+                'company_name': request.env.company.name,
+                'company_id': request.env.company.id
+            }

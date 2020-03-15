ac920e554c62fa2add176230f65c04e88a0cf987
Romain Estievenart <res@odoo.com>
2020-03-15 12:33:52 +0000

    [REF] barcodes_mobile, event_barcode_mobile: create mixin
    This commit extracts some code from event_barcode_mobile to
    a new mixin in barcodes_mobiles to be able to reuse it in the
    future.
    
    Task ID: 2210396

--- event_barcode_mobile/__manifest__.py
+++ event_barcode_mobile/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': 'Event Barcode scan in Mobile',
     'version': '1.0',
     'description': """ """,
-    'depends': ['event_barcode', 'web_mobile'],
+    'depends': ['event_barcode', 'barcodes_mobile'],
     'qweb': ['static/src/xml/event_barcode_mobile.xml'],
     'data': ['views/event_barcode_mobile_template.xml'],
     'installable': True,

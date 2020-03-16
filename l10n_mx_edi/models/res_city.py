180aed0b92fcbed7ad4ac3eaff3b4a65925b7b15
Ravi Gohil <rgo@odoo.com>
2020-03-16 09:23:12 +0000

    [FIX] l10n_mx_edi: too generic field lable to be duplicated
    Label 'Code' is too generic to be duplicated for city model
    as other localizations may also add a field having label Code.
    Same is the case here as l10n_pe module in community has same
    Label for one of it's field. This doesn't allow tests run with
    l10nall tag(--test-tag=l10nall) to be excuted successfully.
    
    This PR fixes this and will successfully execute all tests when
    run with l10nall tag.
    
    closes odoo/enterprise#9345
    
    Task: 2212790
    X-original-commit: 4075999f8b3b4fd983be803089748ccd90b26181
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- l10n_mx_edi/models/res_city.py
+++ l10n_mx_edi/models/res_city.py
@@ -7,5 +7,5 @@ class City(models.Model):
     _inherit = 'res.city'
 
     l10n_mx_edi_code = fields.Char(
-        'Code', help='Code to use in the CFDI with external trade complement. '
+        'Code MX', help='Code to use in the CFDI with external trade complement. '
         'It is based on the SAT catalog.')

5fbc214e361910005473bf826147a61c50331857
fja-odoo <fja@odoo.com>
2020-04-03 09:17:45 +0000

    [IMP] website_helpdesk_form: adapt form to recaptcha
    Adapt website_form related controllers to recaptcha changes
    
    Related to: https://github.com/odoo/odoo/pull/48466
    task-2217980
    
    closes odoo/enterprise#9649
    
    Related: odoo/odoo#48466
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- website_helpdesk_form/controller/main.py
+++ website_helpdesk_form/controller/main.py
@@ -8,8 +8,7 @@ from odoo.addons.website_form.controllers.main import WebsiteForm
 
 class WebsiteForm(WebsiteForm):
 
-    @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True)
-    def website_form(self, model_name, **kwargs):
+    def _handle_website_form(self, model_name, **kwargs):
         email = request.params.get('partner_email')
         if email:
             partner = request.env['res.partner'].sudo().search([('email', '=', email)], limit=1)
@@ -20,4 +19,4 @@ class WebsiteForm(WebsiteForm):
                 })
             request.params['partner_id'] = partner.id
 
-        return super(WebsiteForm, self).website_form(model_name, **kwargs)
+        return super(WebsiteForm, self)._handle_website_form(model_name, **kwargs)

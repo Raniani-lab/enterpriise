Raniani-lab/enterpriise

commit 172e8a4f3df759ad6261563222f5b65eb5e12149

[REF] account_reports: Improve performances of the financial reports
====================
Performance analysis
====================

Analysis made on the balance sheet with 5000 journal entries.

                        | time (s)   | # queries
--------------------------------------------------
Before the refactoring:

Load report             | 0.68       | 244
Unfolding line          | 0.19       | 55

After the refactoring:

Load report             | 0.33       | 145
Unfolding line          | 0.16       | 55
--------------------------------------------------

====================
ADDITIONAL POINTS
====================

- Fix the groupby feature allowing some horizontal groupby & make it usable by the user.
This feature is current hidden in the ir.filters. Not really user friendly and leads
to some bugs when unfolding the lines.

- Remove the debit_credit filter.
This feature was used by 2-3 reports. However, since the formulas was simply copy-pasted
and the amounts was only based on the balance sign, we decided to remove this feature.

- Simplify the formulas.
No longer allow a left operand on formula since the debit_credit feature has been removed.
"sum" is now directly the balance, not an object.
E.g:
"balance = sum.balance" is now "sum"

- Allow having multiple headers lines in a more generic way(adapt the trial balance) + remove _get_super_column method.
In some reports, we want multiple headers lines like the financial reports with the additional groupby.
Then, this commit removes such custom stuff allowing this to bring a more generic way to do so.

--task: 2151051

l10n_mx_reports/models/trial_balance.py
@@ -44,11 +44,32 @@ class MxReportAccountTrial(models.AbstractModel):
         buttons += [{'name': _('Export For SAT (XML)'), 'sequence': 3, 'action': 'print_xml', 'file_export_type': _('SAT XML')}]
         return buttons
 
-    def _get_templates(self):
-        """Get this template for better fit of columns"""
-        templates = super(MxReportAccountTrial, self)._get_templates()
-        templates['main_table_header_template'] = 'l10n_mx_reports.template_coa_table_header'
-        return templates
+    @api.model
+    def _get_columns(self, options):
+        header1 = [
+            {'name': '', 'colspan': 2},
+        ] + [
+            {'name': period['string'], 'class': 'text-center', 'colspan': 2}
+            for period in options['comparison'].get('periods', [])
+        ] + [
+            {'name': options['date']['string'], 'class': 'text-center', 'colspan': 2},
+            {'name': ''},
+        ]
+        header2 = [
+            {'name': ''},
+            {'name': _('Initial Balance'), 'class': 'number'},
+        ]
+        if options.get('comparison') and options['comparison'].get('periods'):
+            header2 += [
+                {'name': _('Debit'), 'class': 'number'},
+                {'name': _('Credit'), 'class': 'number'},
+            ] * len(options['comparison']['periods'])
+        header2 += [
+            {'name': _('Debit'), 'class': 'number'},
+            {'name': _('Credit'), 'class': 'number'},
+            {'name': _('Total'), 'class': 'number'},
+        ]
+        return [header1, header2]
 
     def _get_columns_name(self, options):
         """Get more specific columns to use in SAT report"""

account_reports/models/__init__.py
@@ -18,3 +18,4 @@ from . import account_partner_ledger
 from . import res_config_settings
 from . import chart_template
 from . import account_activity
+from . import ir_module_module

account_reports/models/account_report.py
@@ -694,18 +694,16 @@ class AccountReport(models.AbstractModel):
     ####################################################
 
     def get_header(self, options):
-        if not options.get('groups', {}).get('ids'):
-            columns = self._get_columns_name(options)
-            if 'selected_column' in options and self.order_selected_column:
-                selected_column = columns[abs(options['selected_column']) - 1]
-                if 'sortable' in selected_column.get('class', ''):
-                    selected_column['class'] = (options['selected_column'] > 0 and 'up ' or 'down ') + selected_column['class']
-            return [columns]
-        return self._get_columns_name_hierarchy(options)
+        columns = self._get_columns(options)
+        if 'selected_column' in options and self.order_selected_column:
+            selected_column = columns[0][abs(options['selected_column']) - 1]
+            if 'sortable' in selected_column.get('class', ''):
+                selected_column['class'] = (options['selected_column'] > 0 and 'up ' or 'down ') + selected_column['class']
+        return columns
 
-    #TO BE OVERWRITTEN
-    def _get_columns_name_hierarchy(self, options):
-        return []
+    # TO BE OVERWRITTEN
+    def _get_columns(self, options):
+        return [self._get_columns_name(options)]
 
     # TO BE OVERWRITTEN
     def _get_columns_name(self, options):
@@ -1411,28 +1409,6 @@ class AccountReport(models.AbstractModel):
                          }
                 }
 
-    def _get_super_columns(self, options):
-        """
-        Essentially used when getting the xlsx of a report
-        Some reports may need super title cells on top of regular
-        columns title, This methods retrieve the formers.
-        e.g. in Trial Balance, you can compare periods (super cells)
-            and each have debit/credit columns
-
-
-        @params {dict} options: options for computing the report
-        @return {dict}:
-            {list(dict)} columns: the dict of the super columns of the xlsx report,
-                the columns' string is contained into the 'string' key
-            {int} merge: optional parameter. Indicates to xlsxwriter
-                that it should put the contents of each column into the resulting
-                cell of the merge of this [merge] number of cells
-                -- only merging on one line is supported
-            {int} x_offset: tells xlsxwriter it should start writing the columns from
-                [x_offset] cells on the left
-        """
-        return {}
-
     def get_xlsx(self, options, response=None):
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
@@ -1443,7 +1419,6 @@ class AccountReport(models.AbstractModel):
         default_col1_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666', 'indent': 2})
         default_style = workbook.add_format({'font_name': 'Arial', 'font_size': 12, 'font_color': '#666666'})
         title_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'bottom': 2})
-        super_col_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'align': 'center'})
         level_0_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 6, 'font_color': '#666666'})
         level_1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 13, 'bottom': 1, 'font_color': '#666666'})
         level_2_col1_style = workbook.add_format({'font_name': 'Arial', 'bold': True, 'font_size': 12, 'font_color': '#666666', 'indent': 1})
@@ -1456,44 +1431,23 @@ class AccountReport(models.AbstractModel):
         #Set the first column width to 50
         sheet.set_column(0, 0, 50)
 
-        super_columns = self._get_super_columns(options)
-        y_offset = bool(super_columns.get('columns')) and 1 or 0
+        y_offset = 0
+        headers, lines = self.with_context(no_format=True, print_mode=True, prefetch_fields=False)._get_table(options)
 
-        sheet.write(y_offset, 0, '', title_style)
-
-        # Todo in master: Try to put this logic elsewhere
-        x = super_columns.get('x_offset', 0)
-        for super_col in super_columns.get('columns', []):
-            cell_content = super_col.get('string', '').replace('<br/>', ' ').replace('&nbsp;', ' ')
-            x_merge = super_columns.get('merge')
-            if x_merge and x_merge > 1:
-                sheet.merge_range(0, x, 0, x + (x_merge - 1), cell_content, super_col_style)
-                x += x_merge
-            else:
-                sheet.write(0, x, cell_content, super_col_style)
-                x += 1
-        for row in self.get_header(options):
-            x = 0
-            for column in row:
+        # Add headers.
+        for header in headers:
+            x_offset = 0
+            for column in header:
+                column_name_formated = column.get('name', '').replace('<br/>', ' ').replace('&nbsp;', ' ')
                 colspan = column.get('colspan', 1)
-                header_label = column.get('name', '').replace('<br/>', ' ').replace('&nbsp;', ' ')
                 if colspan == 1:
-                    sheet.write(y_offset, x, header_label, title_style)
+                    sheet.write(y_offset, x_offset, column_name_formated, title_style)
                 else:
-                    sheet.merge_range(y_offset, x, y_offset, x + colspan - 1, header_label, title_style)
-                x += colspan
+                    sheet.merge_range(y_offset, x_offset, y_offset, x_offset + colspan - 1, column_name_formated, title_style)
+                x_offset += colspan
             y_offset += 1
-        ctx = self._set_context(options)
-        ctx.update({'no_format':True, 'print_mode':True, 'prefetch_fields': False})
-        # deactivating the prefetching saves ~35% on get_lines running time
-        lines = self.with_context(ctx)._get_lines(options)
 
-        if options.get('hierarchy'):
-            lines = self._create_hierarchy(lines, options)
-        if options.get('selected_column'):
-            lines = self._sort_lines(lines, options)
-
-        #write all data rows
+        # Add lines.
         for y in range(0, len(lines)):
             level = lines[y].get('level')
             if lines[y].get('caret_options'):

account_reports/models/account_report_coa.py
@@ -21,40 +21,34 @@ class AccountChartOfAccountReport(models.AbstractModel):
     MAX_LINES = None
 
     @api.model
-    def _get_templates(self):
-        templates = super(AccountChartOfAccountReport, self)._get_templates()
-        templates['main_table_header_template'] = 'account_reports.template_coa_table_header'
-        return templates
-
-    @api.model
-    def _get_columns_name(self, options):
-        columns = [
+    def _get_columns(self, options):
+        header1 = [
+            {'name': '', 'style': 'width:40%'},
+            {'name': _('Initial Balance'), 'class': 'number', 'colspan': 2},
+        ] + [
+            {'name': period['string'], 'class': 'number', 'colspan': 2}
+            for period in options['comparison'].get('periods', [])
+        ] + [
+            {'name': options['date']['string'], 'class': 'number', 'colspan': 2},
+            {'name': _('Total'), 'class': 'number', 'colspan': 2},
+        ]
+        header2 = [
             {'name': '', 'style': 'width:40%'},
             {'name': _('Debit'), 'class': 'number'},
             {'name': _('Credit'), 'class': 'number'},
         ]
         if options.get('comparison') and options['comparison'].get('periods'):
-            columns += [
-                {'name': _('Debit'), 'class': 'number '},
+            header2 += [
+                {'name': _('Debit'), 'class': 'number'},
                 {'name': _('Credit'), 'class': 'number'},
             ] * len(options['comparison']['periods'])
-        return columns + [
-            {'name': _('Debit'), 'class': 'number '},
+        header2 += [
+            {'name': _('Debit'), 'class': 'number'},
             {'name': _('Credit'), 'class': 'number'},
-            {'name': _('Debit'), 'class': 'number '},
+            {'name': _('Debit'), 'class': 'number'},
             {'name': _('Credit'), 'class': 'number'},
         ]
-
-    @api.model
-    def _get_super_columns(self, options):
-        date_cols = options.get('date') and [options['date']] or []
-        date_cols += (options.get('comparison') or {}).get('periods', [])
-
-        columns = [{'string': _('Initial Balance')}]
-        columns += reversed(date_cols)
-        columns += [{'string': _('Total')}]
-
-        return {'columns': columns, 'x_offset': 1, 'merge': 2}
+        return [header1, header2]
 
     @api.model
     def _get_lines(self, options, line_id=None):

account_reports/models/formula.py
@@ -1,0 +1,283 @@
+# -*- coding: utf-8 -*-
+from odoo.tools.safe_eval import safe_eval
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+
+from datetime import datetime
+import re
+import ast
+
+
+PROTECTED_KEYWORDS = (
+    'sum', 'sum_if_pos', 'sum_if_neg',
+    'debit', 'credit', 'balance',
+    'count_rows', 'from_context', 'NDays',
+    '__builtins__'
+)
+
+
+class FormulaLocals(dict):
+    ''' Class to set as "locals" when evaluating the formula to compute all formula.
+    The evaluation must be done for each key so this class takes a key as parameter.
+    '''
+
+    def __init__(self, solver, financial_line, key):
+        super().__init__()
+        self.solver = solver
+        self.financial_line = financial_line
+        self.key = key
+
+    def __getitem__(self, item):
+        if item == 'NDays':
+            return self.solver._get_number_of_days(self.key[0])
+        elif item == 'from_context':
+            return self.solver._get_balance_from_context(self.financial_line)
+        elif item == 'count_rows':
+            return self.solver._get_amls_results(self.financial_line)[item].get(self.key[0], 0)
+        elif item in ('sum', 'sum_if_pos', 'sum_if_neg'):
+            return self.solver._get_amls_results(self.financial_line)[item].get(self.key, 0.0)
+        else:
+            financial_line = self.solver._get_line_by_code(item)
+            if not financial_line:
+                return super().__getitem__(item)
+            return self.solver._get_formula_results(financial_line).get(self.key, 0.0)
+
+
+class FormulaSolver:
+    def __init__(self, options_list, financial_report):
+        self.options_list = options_list
+        self.financial_report = financial_report
+        self.env = financial_report.env
+
+        # A mapping of financial.report.line's code => account.financial.html.report.line record
+        # to avoid redundant search.
+        self.cache_line_by_code = {}
+
+        # A mapping of financial.report.line's id with a dictionary containing all gathered data during the evaluation
+        # of the formulas. Such dictionaries looks like:
+        # {
+        #     'formula': {
+        #         key1: <balance>,
+        #         key2: <balance>,
+        #         ...
+        #     },
+        #     'amls': {
+        #         'sum':          {key: <balance>...},
+        #         'sum_if_pos':   {key: <balance>...},
+        #         'sum_if_neg':   {key: <balance>...},
+        #         'count_rows':   {period_index: <number_of_rows_in_period>...},
+        #         'from_context': <balance>,
+        #         'sign':         <1 or -1>,
+        #     }
+        #     'sub_codes':    {code_a, code_b, ...},
+        # }
+        # where...
+        # * key1, key2, ... are tuple where the first element is the period index and others are the additional
+        #   group by fields.
+        #   E.g. if there is a group by on 'partner_id', the key (1, 2) will be the balance for the first
+        #   comparison only when 'partner_id' = 2.
+        # * 'sub_codes' that are the formula codes used by the current formula.
+        #   E.g. a line having 'A' as code and 'B + C' as formula will have ['B', 'C'] in this map.
+        # where key1, key2, ... are tuple where the first element is the period index and others are the additional
+        # group by fields.
+        # E.g. if there is a group by on 'partner_id', the key (1, 2) will be the balance for the first
+        # comparison only when 'partner_id' = 2.
+        self.cache_results_by_id = {}
+
+        # A set of all encountered keys when computing the leaves.
+        # E.g. Suppose a group by on 'partner_id' and one period comparison. The involved partners on the current
+        # period are the one having 1, 2 or 3 as ids and 2, 3 or 4 in the comparison. In that case, this set will
+        # contains (0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (1, 4).
+        self.encountered_keys = set()
+
+    # -------------------------------------------------------------------------
+    # PRIVATE METHODS
+    # -------------------------------------------------------------------------
+
+    def _eval_formula(self, financial_line, key):
+        ''' Evaluate the current formula using the custom object passed as parameter as locals.
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        :param key:             A tuple being the concatenation of the period index plus the additional group-by keys.
+                                Suppose you are evaluating the formula for 'partner_id'=3 for the first comparison, the
+                                key will be (1, 3).
+        '''
+        if not financial_line.formulas:
+            return 0.0
+
+        try:
+            return safe_eval(financial_line.formulas, globals_dict=FormulaLocals(self, financial_line, key), nocopy=True)
+        except ZeroDivisionError:
+            return 0.0
+
+    def _get_line_by_code(self, line_code):
+        ''' Retrieve an account.financial.html.report.line record from its code.
+        If the financial line is not already known, a search is made and its formula is directly evaluated to collect
+        all involved keys by this newly added line.
+        :param line_code:   The code that could be owned by the account.financial.html.report.line record.
+        :return:            An account.financial.html.report.line recordset having 0 or 1 as arity.
+        '''
+        if line_code in self.cache_line_by_code:
+            # Line is already cached.
+            return self.cache_line_by_code[line_code]
+        else:
+            # Fetch line.
+            financial_line = self.env['account.financial.html.report.line'].search([('code', '=', line_code)], limit=1)
+
+            if financial_line:
+                self._prefetch_line(financial_line)
+
+            return financial_line
+
+    def _get_formula_results(self, financial_line):
+        ''' Get or compute the 'formula' results of a financial report line (see 'cache_results_by_id').
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        :return: see 'cache_results_by_id', 'formula' key.
+        '''
+        self.cache_results_by_id.setdefault(financial_line.id, {})
+
+        if 'formula' not in self.cache_results_by_id[financial_line.id]:
+            results = {}
+            if financial_line.formulas:
+                for key in self.encountered_keys:
+                    # Compute formula for each key.
+                    results[key] = self._eval_formula(financial_line, key)
+
+            self.cache_results_by_id[financial_line.id]['formula'] = results
+
+        return self.cache_results_by_id[financial_line.id]['formula']
+
+    def _get_amls_results(self, financial_line):
+        ''' Get or compute the 'amls' results of a financial report line (see 'cache_results_by_id').
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        :return: see 'cache_results_by_id', 'amls' key.
+        '''
+        self.cache_results_by_id.setdefault(financial_line.id, {})
+
+        if 'amls' not in self.cache_results_by_id[financial_line.id]:
+
+            # The current financial line is a leaf using at least one "sum" in its formulas.
+            # If this line is visited for the first time, trigger the computation of '_compute_sum' and
+            # cache the results.
+
+            results = financial_line._compute_sum(self.options_list)
+            for key in results['sum']:
+                self.encountered_keys.add(key)
+
+            # Detect the sign of the 'sum' formula. The sign is only negative when the formula is '-sum'.
+            # In this specific case, the balance of unfolded lines will be negate.
+
+            if financial_line.formulas and re.search(r'-\s*sum', financial_line.formulas):
+                results['sign'] = -1
+            else:
+                results['sign'] = 1
+
+            self.cache_results_by_id[financial_line.id]['amls'] = results
+
+        return self.cache_results_by_id[financial_line.id]['amls']
+
+    def _prefetch_line(self, financial_line):
+        ''' Ensure all leaves that depends of this line are evaluated.
+        E.g. if the formula is 'A + B', make sure 'A' and 'B' are also fetch.
+        If 'A' is a leaf, its formula will be evaluated directly.
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        '''
+        # 'code' is not a required field.
+        if financial_line.code:
+            self.cache_line_by_code[financial_line.code] = financial_line
+
+        self.cache_results_by_id.setdefault(financial_line.id, {})
+
+        if not financial_line.formulas:
+            return
+
+        class LeafResolver(ast.NodeTransformer):
+            # Helper class to iterate through the AST without evaluating the formulas, only leaves.
+            #
+            # Suppose you are dealing with a group by on the 'partner_id' field and the formula is "COS + OPINC" for
+            # the current line and "sum" for the COS & OPINC lines.
+            # Before evaluating "COS + OPINC", you need to know all the involved 'partner_id' by the formula
+            # recursively.
+            #
+            # E.g. "COS" involves partner_id=1 & partner=2, "OPINC" involves partner_id=3 & partner_id=4.
+            # It means you need to evaluate "COS" & "OPINC" before evaluating "COS + OPINC" to know the formulas will
+            # involve partner_id = 1,2,3,4.
+
+            def __init__(self, solver, financial_line):
+                self.solver = solver
+                self.financial_line = financial_line
+
+            def visit_Name(self, node):
+                if node.id in ('sum', 'sum_if_pos', 'sum_if_neg'):
+                    # The current line contains a 'sum' and then, must be evaluate directly.
+                    self.solver._get_amls_results(self.financial_line)
+                else:
+                    # Iterate sub-formula recursively. If the line is not already computed, it will trigger a new
+                    # '_prefetch_line'.
+                    self.solver._get_line_by_code(node.id)
+                return node
+
+        LeafResolver(self, financial_line).visit(ast.parse(financial_line.formulas))
+
+    def _get_number_of_days(self, period_index):
+        ''' Helper to compute the NDays value that could be used inside formulas. This key returns the number of days
+        inside the current period.
+        :param period_index:    The period number, 0 being the current one.
+        :return:                The number of days inside the period.
+        '''
+        options = self.options_list[period_index]
+        date_from = datetime.strptime(options['date']['date_from'], DEFAULT_SERVER_DATE_FORMAT).date()
+        date_to = datetime.strptime(options['date']['date_to'], DEFAULT_SERVER_DATE_FORMAT).date()
+        return (date_to - date_from).days
+
+    def _get_balance_from_context(self, financial_line):
+        ''' Retrieve the balance from context.
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        :return:                The balance found in the context or 0.0.
+        '''
+        financial_report_line_values = self.options_list[0].get('financial_report_line_values', {})
+        return financial_report_line_values.get(financial_line.code, 0.0)
+
+    # -------------------------------------------------------------------------
+    # PUBLIC METHODS
+    # -------------------------------------------------------------------------
+
+    def get_results(self, financial_line):
+        ''' Get results for the given financial report line.
+        :param financial_line:  A record of the account.financial.html.report.line model.
+        :return: see 'cache_results_by_id' for more details.
+        '''
+        if financial_line.id not in self.cache_results_by_id:
+            # The financial line has not pre-computed using '_prefetch_line'. Then, it could lead to some
+            # wrong values.
+            return {}
+
+        # Ensure values are computed. This part is done lazily.
+        self._get_formula_results(financial_line)
+
+        return self.cache_results_by_id[financial_line.id]
+
+    def fetch_lines(self, financial_lines):
+        ''' Prefetch lines passed as parameter.
+        The lines involved through a formula will also be prefetched.
+        :param financial_lines: An account.financial.html.report.line recordset.
+        '''
+        children_financial_lines = self.env['account.financial.html.report.line']
+        for financial_line in financial_lines:
+            self._prefetch_line(financial_line)
+            children_financial_lines += financial_line.children_ids
+        if children_financial_lines:
+            self.fetch_lines(children_financial_lines)
+
+    def get_keys(self):
+        ''' Get all involved keys found in the solver. '''
+        return self.encountered_keys
+
+    def is_leaf(self, financial_line):
+        ''' Helper telling if the financial line passed as parameter is a leaf. '''
+        return 'amls' in self.cache_results_by_id.get(financial_line.id, {})
+
+    def has_move_lines(self, financial_line):
+        ''' Helper telling if the financial line passed as parameter has some move lines in its domain. '''
+        if not self.is_leaf(financial_line):
+            return False
+        total_count_rows = sum(self.cache_results_by_id[financial_line.id]['amls']['count_rows'].values())
+        return bool(total_count_rows)

commit e492919029f05ddce35665217c5e75e2402618bf

[FIX] l10n_mx_reports: fixed broken COA report
Since 172e8a4f, Mexican Trial Balance Report gets its
header columns through _get_columns() and makes call
to _get_columns_name() ineffective. This, in turn,
makes same call in its child report, that is,
Mexican Chart of Account Report, ineffective, and makes
it necessary to override _get_columns() for it.

Also, we can reduced one extra call to _get_templates() from
COA report as same method overridden in parent report uses
bit advance version of main template which has one input box
to search account on UI, which will be provided to COA report
as well.

This commit fixes/improves those things.

task 2227490

closes odoo/enterprise#9723

X-original-commit: 04f437c9c6eb15efb2eb9de41646eaec2b96b136
Signed-off-by: Josse Colpaert <jco@openerp.com>

l10n_mx_reports/models/coa.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _, fields, tools
+from odoo import models, _, api, fields, tools
 from odoo.exceptions import ValidationError
 from odoo.tools.xml_utils import _check_with_xsd
 
@@ -28,15 +28,9 @@ class MXReportAccountCoa(models.AbstractModel):
     filter_hierarchy = None
     filter_journals = None
 
-    def _get_templates(self):
-        templates = super(MXReportAccountCoa, self)._get_templates()
-        #use the main template instead of the trial balance with 2 header lines
-        templates['main_table_header_template'] = 'account_reports.main_table_header'
-        templates['main_template'] = 'account_reports.main_template'
-        return templates
-
-    def _get_columns_name(self, options):
-        return [{'name': ''}, {'name': _('Nature')}]
+    @api.model
+    def _get_columns(self, options):
+        return [[{'name': ''}, {'name': _('Nature')}]]
 
     def _get_lines(self, options, line_id=None):
         options['coa_only'] = True

l10n_mx_reports/models/trial_balance.py
@@ -71,21 +71,6 @@ class MxReportAccountTrial(models.AbstractModel):
         ]
         return [header1, header2]
 
-    def _get_columns_name(self, options):
-        """Get more specific columns to use in SAT report"""
-        columns = [{'name': ''}, {'name': _('Initial Balance'), 'class': 'number'}]
-        if options.get('comparison') and options['comparison'].get('periods'):
-            for period in options['comparison']['periods']:
-                columns += [
-                    {'name': _('Debit'), 'class': 'number'},
-                    {'name': _('Credit'), 'class': 'number'},
-                    ]
-        return columns + [
-            {'name': _('Debit'), 'class': 'number'},
-            {'name': _('Credit'), 'class': 'number'},
-            {'name': _('Total'), 'class': 'number'},
-        ]
-
     def _post_process(self, grouped_accounts, initial_balances, options, comparison_table):
         afrl_obj = self.env['account.financial.html.report.line']
         lines = []
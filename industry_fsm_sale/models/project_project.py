98af80e930cbd71964a8616b70a79aa003aaedc3
Lucas Lefèvre <lul@odoo.com>
2020-04-28 08:52:22 +0200

    [FIX] industry_fsm_sale: Allow extra quotations configuration per project
    Enabling "Extra quotation" is done through the general settings
    but it should be possible to disable/enable it on specific projects.
    The current implementation does not allow that since the field
    enabling the feature is a readonly, user dependent field.
    
    Task 2192763

--- industry_fsm_sale/models/project_project.py
+++ industry_fsm_sale/models/project_project.py
@@ -8,12 +8,19 @@ class Project(models.Model):
     _inherit = "project.project"
 
     allow_material = fields.Boolean("Products on Tasks", compute="_compute_allow_material", store=True, readonly=False)
-    allow_quotations = fields.Boolean("Extra Quotations", compute='_compute_allow_quotations')
+    allow_quotations = fields.Boolean("Extra Quotations")
 
     _sql_constraints = [
         ('material_imply_billable', "CHECK((allow_material = 't' AND allow_billable = 't') OR (allow_material = 'f'))", 'The material can be allowed only when the task can be billed.'),
         ('fsm_imply_task_rate', "CHECK((is_fsm = 't' AND sale_line_id IS NULL) OR (is_fsm = 'f'))", 'An FSM project must be billed at task rate.'),
     ]
+
+    @api.model
+    def default_get(self, *args, **kwargs):
+        defaults = super().default_get(*args, **kwargs)
+        if 'allow_quotations' not in defaults and defaults.get('is_fsm'):
+            defaults['allow_quotations'] = self.env.user.has_group('industry_fsm_sale.group_fsm_quotation_from_task')
+        return defaults
 
     @api.depends('allow_billable')
     def _compute_allow_billable(self):
@@ -28,12 +35,6 @@ class Project(models.Model):
                 project.allow_material = project.is_fsm
             else:
                 project.allow_material = project.allow_billable
-
-    @api.depends('is_fsm')
-    def _compute_allow_quotations(self):
-        has_group = self.env.user.has_group('industry_fsm_sale.group_fsm_quotation_from_task')
-        for record in self.filtered(lambda p: not p.allow_quotations):
-            record.allow_quotations = record.is_fsm and has_group
 
     def flush(self, fnames=None, records=None):
         if fnames is not None:

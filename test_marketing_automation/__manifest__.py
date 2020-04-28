383573473faec174189e9f009e280ffcfec87c59
Thibault Delavallée <tde@odoo.com>
2020-04-28 15:58:11 +0000

    [MOV] test_marketing_automation: reorganize module according to guidelines
    PURPOSE
    
    Improve marketing automation tests: improve their reading, add more tests
    and notably SMS tests.
    
    SPECIFICATIONS
    
    Reorganize module before working on tests: move models in their right place,
    split tests in main files (flow and internal details).
    
    Add dependency on marketing_automation_sms to write SMS related tests as
    well as on test_mail_full to benefits from all tools available in those
    test modules.
    
    LINKS
    
    Task ID 2247037
    Community PR odoo/odoo#50384
    Enterprise PR odoo/enterprise#10266
    Upgrade PR odoo/upgrade#1122

--- test_marketing_automation/__manifest__.py
+++ test_marketing_automation/__manifest__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': "Marketing Automation Tests",
-    'version': "1.0",
-    'summary': "Test Suits for Automated Marketing Campaigns",
+    'version': "1.1",
+    'summary': "Test Suite for Automated Marketing Campaigns",
     'category': "Hidden",
     'depends': [
         'marketing_automation',

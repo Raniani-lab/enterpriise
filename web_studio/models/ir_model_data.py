f42b93f832ae00e11725c7bbda68a6ca1b2c4ae9
Anh Thao Pham (pta) <pta@odoo.com>
2020-05-13 06:54:03 +0000

    [FIX] web_studio: fix import of studio customizations - Activate Studio from dashboard of database A - Export studio customizations - Activate Studio from dashboard of database B - Import studio customizations previously exported The imported data don't have the boolean studio field set to True.
    Since v12, when loading a module, a query is executed to create or update XML ids.
    The create and write methods are not called anymore.
    Therefore the overridden create and write methods of "ir.model.data" defined in Studio module
    are not called and thus cannot set the studio field to True.
    
    The generation of the query creating XML ids has been moved to a private method
    allowing another module like Studio to override it.
    
    opw-2245578
    
    closes odoo/enterprise#10586
    
    X-original-commit: 4d75777446883de92eeecd4c9ec00f5f4bf50f23
    Related: odoo/odoo#51203
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- web_studio/models/ir_model_data.py
+++ web_studio/models/ir_model_data.py
@@ -23,3 +23,19 @@ class IrModelData(models.Model):
             vals['noupdate'] = True
             vals['studio'] = True
         return super(IrModelData, self).write(vals)
+
+    def _build_update_xmlids_query(self, sub_rows, update):
+        '''Override of the base method to include the `studio` attribute for studio module imports.'''
+        if self._context.get('studio'):
+            rowf = "(%s, %s, %s, %s, %s, now() at time zone 'UTC', now() at time zone 'UTC', 't')"
+            return """
+                INSERT INTO ir_model_data (module, name, model, res_id, noupdate, date_init, date_update, studio)
+                VALUES {rows}
+                ON CONFLICT (module, name)
+                DO UPDATE SET date_update=(now() at time zone 'UTC'), noupdate='t' {where}
+            """.format(
+                rows=", ".join([rowf] * len(sub_rows)),
+                where="WHERE NOT ir_model_data.noupdate" if update else "",
+            )
+        else:
+            return super(IrModelData, self)._build_update_xmlids_query(sub_rows, update)

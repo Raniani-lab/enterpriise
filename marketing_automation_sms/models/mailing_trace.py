8dc5d6ece94cc486edc61c83275b27ae54556ce7
Thibault Delavallée <tde@odoo.com>
2020-05-13 15:42:10 +0000

    [FIX] marketing_automation(_sms): fix forward port of traces management
    Somehow PR odoo/enterprise#10422 was not correctly forward ported.
    This commit fixes the commit by correctly calling process_event
    on a marketing_trace, not a mailing_trace.
    
    closes odoo/enterprise#10587
    
    X-original-commit: db4dec3822e062eaa24244313beb477bd5dddddd
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- marketing_automation_sms/models/mailing_trace.py
+++ marketing_automation_sms/models/mailing_trace.py
@@ -21,13 +21,13 @@ class MailingTrace(models.Model):
     def set_clicked(self, mail_mail_ids=None, mail_message_ids=None):
         traces = super(MailingTrace, self).set_clicked(mail_mail_ids=mail_mail_ids, mail_message_ids=mail_message_ids)
         marketing_sms_traces = traces.filtered(lambda trace: trace.marketing_trace_id and trace.marketing_trace_id.activity_type == 'sms')
-        for marketing_trace in marketing_sms_traces:
+        for marketing_trace in marketing_sms_traces.marketing_trace_id:
             marketing_trace.process_event('sms_click')
         return traces
 
     def set_bounced(self, mail_mail_ids=None, mail_message_ids=None):
         traces = super(MailingTrace, self).set_bounced(mail_mail_ids=mail_mail_ids, mail_message_ids=mail_message_ids)
         marketing_sms_traces = traces.filtered(lambda trace: trace.marketing_trace_id and trace.marketing_trace_id.activity_type == 'sms')
-        for marketing_trace in marketing_sms_traces:
+        for marketing_trace in marketing_sms_traces.marketing_trace_id:
             marketing_trace.process_event('sms_bounce')
         return traces

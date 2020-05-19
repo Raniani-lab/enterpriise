aab5e737b2ce07380794ffe39c132e81cd79b3f0
oco-odoo <oco@odoo.com>
2020-05-19 07:51:10 +0000

    [FIX] account_reports: correct sign for cash basis taxes
    The report directly considered the journal in which each move was done in order to influence the sign modificator to apply to the move lines. While correct for other cases, this was wrong for cash basis moves, generated by taxes based on payment (aka cash basis taxes). For them, the journal type to be considered is the one in which their original invoice had been done.
    
    closes odoo/enterprise#10857
    
    X-original-commit: dd0a19af1cfe1e8f8278a4aee8f1272bc5b974cf
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: oco-odoo <oco-odoo@users.noreply.github.com>

--- account_reports/models/account_generic_tax_report.py
+++ account_reports/models/account_generic_tax_report.py
@@ -287,18 +287,18 @@ class generic_tax_report(models.AbstractModel):
 
     def _sql_net_amt_regular_taxes(self):
         return '''
-            SELECT 
+            SELECT
                 tax.id,
                  COALESCE(SUM(account_move_line.balance))
             FROM %s
             JOIN account_move_line_account_tax_rel rel ON rel.account_move_line_id = account_move_line.id
             JOIN account_tax tax ON tax.id = rel.account_tax_id
-            WHERE %s AND tax.tax_exigibility = 'on_invoice' 
+            WHERE %s AND tax.tax_exigibility = 'on_invoice'
             GROUP BY tax.id
 
             UNION ALL
 
-            SELECT 
+            SELECT
                 child_tax.id,
                  COALESCE(SUM(account_move_line.balance))
             FROM %s
@@ -306,9 +306,9 @@ class generic_tax_report(models.AbstractModel):
             JOIN account_tax tax ON tax.id = rel.account_tax_id
             JOIN account_tax_filiation_rel child_rel ON child_rel.parent_tax = tax.id
             JOIN account_tax child_tax ON child_tax.id = child_rel.child_tax
-            WHERE %s 
-                AND child_tax.tax_exigibility = 'on_invoice' 
-                AND tax.amount_type = 'group' 
+            WHERE %s
+                AND child_tax.tax_exigibility = 'on_invoice'
+                AND tax.amount_type = 'group'
                 AND child_tax.amount_type != 'group'
             GROUP BY child_tax.id
         '''
@@ -328,8 +328,8 @@ class generic_tax_report(models.AbstractModel):
         tables, where_clause, where_params = self._query_get(options)
         sql = """SELECT account_tax_report_line_tags_rel.account_tax_report_line_id,
                         SUM(coalesce(account_move_line.balance, 0) * CASE WHEN acc_tag.tax_negate THEN -1 ELSE 1 END
-                                                 * CASE WHEN account_journal.type = 'sale' THEN -1 ELSE 1 END
-                                                 * CASE WHEN account_move.move_type in ('in_refund', 'out_refund') THEN -1 ELSE 1 END)
+                                                 * CASE WHEN COALESCE(caba_origin_journal.type, account_journal.type) = 'sale' THEN -1 ELSE 1 END
+                                                 * CASE WHEN COALESCE(caba_origin_move.move_type, account_move.move_type) in ('in_refund', 'out_refund') THEN -1 ELSE 1 END)
                         AS balance
                  FROM """ + tables + """
                  JOIN account_move
@@ -344,10 +344,19 @@ class generic_tax_report(models.AbstractModel):
                  ON acc_tag.id = account_tax_report_line_tags_rel.account_account_tag_id
                  JOIN account_tax_report_line report_line
                  ON account_tax_report_line_tags_rel.account_tax_report_line_id = report_line.id
+                 LEFT JOIN account_partial_reconcile caba_partial_rec
+                 ON caba_partial_rec.id = account_move.tax_cash_basis_rec_id
+                 LEFT JOIN account_move_line caba_origin_aml
+                 ON caba_origin_aml.id != account_move_line.id and caba_origin_aml.id IN (caba_partial_rec.credit_move_id, caba_partial_rec.debit_move_id)
+                 LEFT JOIN account_journal caba_origin_journal
+                 ON caba_origin_journal.id = caba_origin_aml.journal_id
+                 LEFT JOIN account_move caba_origin_move
+                 ON caba_origin_move.id = caba_origin_aml.move_id
                  WHERE """ + where_clause + """
                  AND report_line.report_id = %s
                  AND account_move_line.tax_exigible
                  AND account_journal.id = account_move_line.journal_id
+                 AND (caba_origin_journal.type IS NULL OR caba_origin_journal.type != 'bank')
                  GROUP BY account_tax_report_line_tags_rel.account_tax_report_line_id
         """
 

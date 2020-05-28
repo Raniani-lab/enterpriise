b843684be22637031fafb260b1a3e7c3b03927d2
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2020-05-28 11:47:32 +0000

    [FIX] sale_coupon_taxcloud: prevent modification of invoiced sol
    using Tax Cloud, create the invoice from the
    SO and once the invoice is paid, add a free product on the SO.
    
    When the user wants to create the second invoice (in order to make the SO 'fully
    invoiced') an error message raise.
    
    To avoid such situations we prevent the user to add products
    when using Tax Cloud and promotion program lines with a UserError message.
    The user should refund all existing invoices for this SO
    (so that any 'invoiced' line get back to being uninvoiced),
    then re-issue an invoice for the whole order.
    
    opw-2261069
    
    closes odoo/enterprise#10955
    
    X-original-commit: 3651e06d1bece09f1a9e5572a4a5f693de367c8b
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- sale_coupon_taxcloud/models/sale_order.py
+++ sale_coupon_taxcloud/models/sale_order.py
@@ -66,6 +66,48 @@ class SaleOrderLine(models.Model):
     price_taxcloud = fields.Float('Taxcloud Price', default=0,
                                   help='Technical fields to hold prices for TaxCloud.')
 
+    def _check_taxcloud_promo(self, vals):
+        """Ensure that users cannot modify sale order lines of a Taxcloud order
+           with promotions if there is already a valid invoice"""
+
+        blocked_fields = (
+            'product_id',
+            'price_unit',
+            'price_subtotal',
+            'price_tax',
+            'price_total',
+            'tax_id',
+            'discount',
+            'product_id',
+            'product_uom_qty',
+            'product_qty'
+        )
+        for line in self:
+            if (
+                line.order_id.is_taxcloud
+                and not line.display_type
+                and any(field in vals for field in blocked_fields)
+                and any(line.order_id.order_line.mapped(lambda sol: sol.invoice_status not in ('no','to invoice')))
+                and any(line.order_id.order_line.mapped('is_reward_line'))
+            ):
+                raise UserError(
+                    _(
+                    'Orders with coupons or promotions programs that use TaxCloud for '
+                    'automatic tax computation cannot be modified after having been invoiced.\n'
+                    'To modify this order, you must first cancel or refund all existing invoices.'
+                    )
+                )
+
+    def write(self, vals):
+        self._check_taxcloud_promo(vals)
+        return super(SaleOrderLine, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        line = super(SaleOrderLine, self).create(vals)
+        line._check_taxcloud_promo(vals)
+        return line
+
     def _get_taxcloud_price(self):
         self.ensure_one()
         return self.price_taxcloud

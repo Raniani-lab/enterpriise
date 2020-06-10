db27bf31afa0c1764de6552a40aefa1b7084502d
Thibault Delavallée <tde@odoo.com>
2020-06-10 13:26:40 +0000

    [MOV] event_barcode: reorganize module according to guidelines
    This module was still a bit fancy in its organization. Mock it
    a bit to make it more standard.
    
    Task ID #2214290
    Enterprise PR odoo/enterprise#9190

--- event_barcode/__manifest__.py
+++ event_barcode/__manifest__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': "Event Barcode Scanning",
     'summary': "Add barcode scanning feature to event management.",
+    'version': '1.0',
     'description': """
 This module adds support for barcodes scanning to the Event management system.
 A barcode is generated for each attendee and printed on the badge. When scanned,
@@ -11,9 +13,10 @@ the registration is confirmed.
     'category': 'Marketing/Events',
     'depends': ['barcodes', 'event'],
     'data': [
-        'views/event_barcode_template.xml',
-        'views/event_barcode.xml',
-        'views/event_report_template_badge.xml',
+        'views/assets.xml',
+        'views/event_event_views.xml',
+        'views/event_registration_views.xml',
+        'views/event_report_templates.xml',
         'views/res_config_settings_views.xml',
     ],
     'qweb': [

119ce82d727004ae85556e0a222f911fe6af911e
William Henrotin <whe@odoo.com>
2020-06-24 09:12:29 +0000

    [FIX] mrp_workorder: production validation with quality checks
    This commit block the production validation if there are still quality
    check to do. Moreover, the state stay in 'progress' until every QC are done.
    
    Task : 2278147
    
    X-original-commit: fa336b57b8135fa6f858104aca7afa03bb58e906

--- mrp_workorder/models/mrp_production.py
+++ mrp_workorder/models/mrp_production.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import UserError
 
 
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
     check_ids = fields.One2many('quality.check', 'production_id', string="Checks")
+
+    @api.depends('workorder_ids.check_ids')
+    def _compute_state(self):
+        super()._compute_state()
+        for production in self:
+            if production.state == 'to_close' and any(x.quality_state == 'none' for x in production.workorder_ids.check_ids):
+                production.state = 'progress'
 
     def action_assign(self):
         res = super().action_assign()
@@ -27,3 +35,8 @@ class MrpProduction(models.Model):
             if wo.component_id:
                 wo._update_component_quantity()
         return backorders
+
+    def _button_mark_done_sanity_checks(self):
+        if any(x.quality_state == 'none' for x in self.workorder_ids.check_ids):
+            raise UserError(_('You still need to do the quality checks!'))
+        return super()._button_mark_done_sanity_checks()

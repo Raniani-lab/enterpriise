877a7c038c29f1ce178f84616abf6da04b7d070f
Luis Torres <luis_t@vauxoo.com>
2020-06-29 23:51:52 +0000

    [FIX] l10n_mx_reports_closing: Avoid errors on trial balance
    In the next case:
    A company starts operations in 2018, in December makes the close
    entry, and mark the entry with the option "MARK AS CLOSING ENTRY FOR THE
    FISCAL YEAR".
    Get the trial balance and is ok.
    In 2019 makes the close entry with the same option.
    
    But when in 2019, try to get the "Trial Balance Report", ignore the
    close entry for 2018 moves.
    
    That is incorrect because the entry in 2018 must be considered in the
    new years.
    
    OPW #2276153
    
    closes odoo/enterprise#11646
    
    X-original-commit: e9d5855e0e5035d8c491cbf7fa71d8a2276efbc9
    Signed-off-by: Jorge Pinna Puissant (jpp) <jpp@odoo.com>

--- l10n_mx_reports_closing/models/account_move.py
+++ l10n_mx_reports_closing/models/account_move.py
@@ -41,10 +41,8 @@ class AccountMoveLine(models.Model):
         """
         if self._context.get('model') != 'l10n_mx.trial.report':
             return super(AccountMoveLine, self)._query_get(domain=domain)
-        closing_moves = self.search([
-            ('move_id.l10n_mx_closing_move', '=', 'True'),
-            ('move_id.state', '=', 'posted')])
+        closing_moves = self.env['account.move']._get_closing_move(self._context.get('date_to'))
         if domain is None:
             domain = []
-        domain.append(('id', 'not in', closing_moves.ids))
+        domain.append(('move_id', 'not in', closing_moves.ids))
         return super(AccountMoveLine, self)._query_get(domain=domain)

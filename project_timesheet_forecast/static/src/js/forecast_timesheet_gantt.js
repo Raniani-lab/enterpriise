d92e3428e90023eb54d0e6f2382b00f093fbf7c1
Bruno Boi <boi@odoo.com>
2020-07-09 14:29:54 +0000

    [IMP] project_timesheet_forecast: add employee avatar widget support
    Allow the user to chat with employee by clicking on their avatar from the gantt views.
    
    closes odoo/enterprise#11412
    
    Taskid: 2272544
    Taskid: 2272582
    Related: odoo/odoo#54230
    Signed-off-by: Mathieu Duckerts-Antoine <Polymorphe57@users.noreply.github.com>

--- project_timesheet_forecast/static/src/js/forecast_timesheet_gantt.js
+++ project_timesheet_forecast/static/src/js/forecast_timesheet_gantt.js
@@ -1,46 +1,46 @@
 odoo.define('forecast_timesheet.ForecastTimesheetGanttView', function (require) {
     'use strict';
 
-const PlanningGanttView = require('forecast.ForecastGanttView');
-const GanttRenderer = require('planning.PlanningGanttRenderer');
-const viewRegistry = require('web.view_registry');
-const GanttRow = require('web_gantt.GanttRow');
+    const viewRegistry = require('web.view_registry');
+    const ForecastGanttView = require('forecast.ForecastGanttView');
+    const PlanningGanttRenderer = require('planning.PlanningGanttRenderer');
+    const PlanningGanttRow = require('planning.PlanningGanttRow');
 
+    const ForecastTimesheetGanttRow = PlanningGanttRow.extend({
+        template: 'PlanningTimesheetGanttView.Row',
 
-const PlanningGanttRow = GanttRow.extend({
-    template: 'PlanningTimesheetGanttView.Row',
-});
+        /**
+         * @override
+         */
+        init(parent, pillsInfo, viewInfo, options) {
+            this._super(...arguments);
+            // here find slots with pills and task_id
+            if (this.pills) {
+                // there are slots.
+                this.pills.forEach((pill) => {
+                    if (pill.task_id && pill.allocated_hours) {
+                        // the slot have a task_id, we need to display the cell as a progress bar
+                        pill.progress = Math.round(pill.effective_hours / pill.allocated_hours * 100);
+                    } else {
+                        pill.progress = 0;
+                    }
+                });
+            }
+        },
+    });
 
-const ForecastGanttRenderer = GanttRenderer.extend({
-    /*
-     Override the renderer to use custom ForecastGanttRow
-    */
-    _renderRow: function (pillsInfo, params) {
-        const ganttRow = new PlanningGanttRow(this, pillsInfo, this.viewInfo, params);
-        this.rowWidgets[ganttRow.rowId] = ganttRow;
-        this.proms.push(ganttRow._widgetRenderAndInsert(function () {}));
-        // here find slots with pills and task_id
-        if (ganttRow.pills) {
-            // there are slots.
-            ganttRow.pills.forEach((pill) => {
-                if (pill.task_id && pill.allocated_hours) {
-                    // the slot have a task_id, we need to display the cell as a progress bar
-                    pill.progress = Math.round(pill.effective_hours / pill.allocated_hours * 100);
-                } else {
-                   pill.progress = 0;
-                }
-            });
-        }
-        return ganttRow;
-    },
-});
+    const ForecastTimesheetGanttRenderer = PlanningGanttRenderer.extend({
+        config: Object.assign({}, PlanningGanttRenderer.prototype.config, {
+            GanttRow: ForecastTimesheetGanttRow,
+        }),
+    });
 
-const ForecastTimesheetGanttView = PlanningGanttView.extend({
-    config: _.extend({}, PlanningGanttView.prototype.config, {
-        Renderer: ForecastGanttRenderer,
-    }),
-});
+    const ForecastTimesheetGanttView = ForecastGanttView.extend({
+        config: Object.assign({}, ForecastGanttView.prototype.config, {
+            Renderer: ForecastTimesheetGanttRenderer,
+        }),
+    });
 
-viewRegistry.add('forecast_timesheet', ForecastTimesheetGanttView);
-return ForecastTimesheetGanttView;
+    viewRegistry.add('forecast_timesheet', ForecastTimesheetGanttView);
+    return ForecastTimesheetGanttView;
 });

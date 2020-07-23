970e27b6a719e8a6fe41ff246e970c3c955f3fb2
Luis Torres <luis_t@vauxoo.com>
2020-07-23 00:01:22 +0000

    [FIX] l10n_mx_edi: Update xsd version to Donatarias Complement
    Now the complement for ``donatarias`` need the version 1.1, was updated
    the URL to get XSD files for that version
    
    This version was introduced on 74dec3a83c75366cc9e6a1e61e72a7f0c12c4e34,
    but by mistake was removed on 47f1ad82f7a40157496618fb9ccf64365af37b8b
    
    closes odoo/enterprise#11988
    
    X-original-commit: 007b33d5ac52d2016b8d823e64ca5d1e61ebb27d
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- l10n_mx_edi/models/res_company.py
+++ l10n_mx_edi/models/res_company.py
@@ -183,7 +183,7 @@ class ResCompany(models.Model):
             ['http://www.sat.gob.mx/EstadoDeCuentaCombustible',
              'http://www.sat.gob.mx/sitio_internet/cfd/EstadoDeCuentaCombustible/ecc11.xsd'],
             ['http://www.sat.gob.mx/donat',
-             'http://www.sat.gob.mx/sitio_internet/cfd/donat/donat.xsd'],
+             'http://www.sat.gob.mx/sitio_internet/cfd/donat/donat11.xsd'],
             ['http://www.sat.gob.mx/divisas',
              'http://www.sat.gob.mx/sitio_internet/cfd/divisas/Divisas.xsd'],
             ['http://www.sat.gob.mx/implocal',

d73672d6f01229cd605b57d785adeb6d78b4a81c
Tiffany Chang (tic) <tic@odoo.com>
2020-07-23 14:02:11 +0000

    [IMP] stock_barcode: add "Print barcode demo sheet" to settings
    Adds a new "Print barcode demo sheet" download link in the Inventory >
    Barcode Scanner. This link will only show up when demo data is installed
    and handles both module being installed with demo data and demo data
    being added later via General Settings > Developer Tools. The hope is to
    prevent people from not having access to these demo barcodes after they
    have closed the "message_demo_barcodes" in the barcode app.
    
    Part of overall barcode improvements task.
    
    Task: 1884520
    Related COM PR: odoo/odoo#55096
    Closes: odoo/enterprise#12086

--- stock_barcode/models/res_config_settings.py
+++ stock_barcode/models/res_config_settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
@@ -8,3 +8,9 @@ class ResConfigSettings(models.TransientModel):
     barcode_nomenclature_id = fields.Many2one('barcode.nomenclature', related='company_id.nomenclature_id', readonly=False)
     group_barcode_keyboard_shortcuts = fields.Boolean("Keyboard Shortcuts", implied_group='stock_barcode.group_barcode_keyboard_shortcuts')
     keyboard_layout = fields.Selection(related="company_id.keyboard_layout", default='qwerty', string="Keyboard Layout", readonly=False)
+    stock_barcode_demo_active = fields.Boolean("Demo Data Active", compute='_compute_stock_barcode_demo_active')
+
+    @api.depends('company_id')
+    def _compute_stock_barcode_demo_active(self):
+        for rec in self:
+            rec.stock_barcode_demo_active = bool(self.env['ir.module.module'].search([('name', '=', 'stock_barcode'), ('demo', '=', True)]))

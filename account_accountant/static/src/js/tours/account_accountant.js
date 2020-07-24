d1709d53aaf72d3747a8229b2e49ce78753f1e4e
Nicolas (vin) <vin@odoo.com>
2020-07-24 12:45:54 +0000

    [IMP] account_accountant : update the account_accountant tour
    The current accounting tour is broken, this change will fix it.
    
    Task id #2289610
    
    closes odoo/enterprise#12389
    
    Related: odoo/odoo#55850
    Related: odoo/upgrade#1643
    Signed-off-by: Cedric Snauwaert (csn) <csn@openerp.com>

--- account_accountant/static/src/js/tours/account_accountant.js
+++ account_accountant/static/src/js/tours/account_accountant.js
@@ -1,31 +1,57 @@
 odoo.define('account_accountant.tour', function (require) {
-"use strict";
+    "use strict";
 
-var core = require('web.core');
-var tour = require('web_tour.tour');
+    const core = require('web.core');
+    const tour = require('web_tour.tour');
 
-var _t = core._t;
+    const _t = core._t;
 
-tour.register('account_accountant_tour', {
-    'skip_enabled': true,
-}, [{
-    trigger: '.o_app[data-menu-xmlid="account_accountant.menu_accounting"]',
-    content: _t('Ready to discover your new favorite <b>accounting app</b>? Get started by clicking here.'),
-    position: 'bottom',
-}, {
-    trigger: ".o_invoice_new",
-    extra_trigger: 'div.o_view_controller:not(.o_has_banner) .o_account_kanban',
-    content: _t("Let's start with a new customer invoice."),
-    position: "bottom"
-}, {
-    trigger: ".breadcrumb-item:not(.active):last",
-    extra_trigger: "[data-id='open'].btn-primary, [data-id='open'].oe_active",
-    content: _t("Use the path to quickly click back to <b>previous screens</b>, without reloading the page."),
-    position: "bottom"
-}, {
-    trigger: 'li a[data-menu-xmlid="account.menu_finance_reports"], div[data-menu-xmlid="account.menu_finance_reports"]',
-    content: _t("Your reports are available in real time. <i>No need to close a fiscal year to get a Profit &amp; Loss statement or view the Balance Sheet.</i>"),
-    position: "bottom"
-}]);
+    tour.register('account_accountant_tour_upload', 
+        {
+            skip_enabled: true,
+            url: "/web",
+            rainbowManMessage: _t("Congratulation, your first bill is validated !\n" +
+                "Odoo’s Artificial Intelligence saved your precious time."),
+        }, 
+        [
+            ...tour.stepUtils.goToAppSteps('account_accountant.menu_accounting', _t('Let’s automate your bills, bank transactions and accounting processes..')),
+            {
+                trigger: 'a[data-method="setting_upload_bill_wizard"].o_onboarding_step_action',
+                content: _t('Create your first vendor bill.<br/><br/><i>Tip: If you don’t have one on hand, use our sample bill.</i>'),
+                position: 'bottom',
+            }, {
+                trigger: 'button[name="apply"]',
+                content: _t('Great! Let’s continue.'),
+                position: 'top',
+            }, {
+                trigger: '.o_data_cell',
+                extra_trigger: 'tr:not(.o_sample_data_disabled)>td:has(span[name="payment_state"])',
+                content: _t('Let’s see how a bill look like in form view.'),
+                position: 'top',
+            }, {
+                trigger: 'button.btn-primary[name="action_post"]',
+                extra_trigger: 'body:has(div[name="waiting_extraction"].o_invisible_modifier)',
+                content: _t('Check & validate the bill. If no vendor has been found, add one before validating.'),
+                position: 'bottom',
+            }, {
+                trigger: '.breadcrumb-item:not(.active):first',
+                extra_trigger: 'button[data-value="posted"].btn-primary',
+                content: _t('Let’s go back to the dashboard using your previous path…'),
+                position: 'bottom',
+            }
+        ]
+    );
 
+    tour.register('account_accountant_tour_upload_ocr_step', {
+            skip_enabled: true,
+            rainbowMan: false,
+        },
+        [
+            {
+                trigger: 'button.btn-primary[name="check_status"]',
+                content: _t('Let’s use AI to fill in the form<br/><br/><i>Tip: If the OCR is not done yet, wait a few more seconds and try again.</i>'),
+                position: 'bottom',
+            }
+        ]
+    )
 });

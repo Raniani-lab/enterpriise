45b837214fa975afa09f1529eda88276ce664c10
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2020-07-28 12:49:57 +0000

    [FIX] account_accountant: allow billing admin to set lock date
    Otherwise, only the ones with permission to write on the company could
    
    closes odoo/enterprise#12254
    
    X-original-commit: b6ac731d33df045a1e948a1424dd11744002b5cc
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- account_accountant/wizard/account_change_lock_date.py
+++ account_accountant/wizard/account_change_lock_date.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, _
+from odoo.exceptions import UserError
 
 
 class AccountChangeLockDate(models.TransientModel):
@@ -23,8 +24,12 @@ class AccountChangeLockDate(models.TransientModel):
 
 
     def change_lock_date(self):
-        self.env.company.write({'period_lock_date': self.period_lock_date,
-                                        'fiscalyear_lock_date': self.fiscalyear_lock_date,
-                                        'tax_lock_date': self.tax_lock_date,
-                                        })
+        if self.user_has_groups('account.group_account_manager'):
+            self.env.company.sudo().write({
+                'period_lock_date': self.period_lock_date,
+                'fiscalyear_lock_date': self.fiscalyear_lock_date,
+                'tax_lock_date': self.tax_lock_date,
+            })
+        else:
+            raise UserError(_('Only Billing Administrators are allowed to change lock dates!'))
         return {'type': 'ir.actions.act_window_close'}

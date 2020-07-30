0ad23db5776eeaba7e55a573089e7a7431c7469b
Antoine Prieels <anp@odoo.com>
2020-07-30 10:18:41 +0000

    [FIX] pos_iot: Do not override execute in res.config.settings
    Some errors occur when modules are uninstalled after unchecking some
    group in the settings. Excecute method should not be overriden in
    res.config.settings.
    
    closes odoo/enterprise#12120
    
    Taskid: 2282427
    X-original-commit: 6c40f289b05c89dbf884d93dc3be7ca2628c1ab7
    Signed-off-by: pimodoo <pimodoo@users.noreply.github.com>
    Signed-off-by: Antoine Prieëls <aprieels@users.noreply.github.com>

--- pos_iot/models/res_config_settings.py
+++ pos_iot/models/res_config_settings.py
@@ -8,10 +8,9 @@ class ResConfigSettings(models.TransientModel):
 
     ingenico_payment_terminal = fields.Boolean(string="Ingenico Payment Terminal", config_parameter='pos_iot.ingenico_payment_terminal')
 
-    def execute(self):
-        res = super(ResConfigSettings, self).execute()
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
         payment_methods = self.env['pos.payment.method']
         if not self.env['ir.config_parameter'].sudo().get_param('pos_iot.ingenico_payment_terminal'):
             payment_methods |= payment_methods.search([('use_payment_terminal', '=', 'ingenico')])
         payment_methods.write({'use_payment_terminal': False, 'iot_device_id': False})
-        return res

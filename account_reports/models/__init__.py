c9c9b446f7af41c0d770ac1854515fd1576edb5e
william <wan@odoo.com>
2020-07-31 12:48:21 +0000

    [IMP] account_reports: add generic accounting report builder
    Basically all you need to know is in the new class's docstring.
    
    This class helps the creating of reports based on account.move.line by
    creating a models.Model based on a VIEW and doing successive read_group
    on the VIEW.
    In most cases, this means we don't have to do custom optimisation an
    custom layouts for the report. It also shrinks the size of each
    implementation down by keeping only one SQL query and declaration of the
    layout.

--- account_reports/models/__init__.py
+++ account_reports/models/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from . import res_partner
+from . import res_company
+from . import account
 from . import account_report
+from . import account_accounting_report
 from . import account_bank_reconciliation_report
 from . import account_financial_report
 from . import account_general_ledger
@@ -10,8 +14,6 @@ from . import account_analytic_report
 from . import account_consolidated_journals
 from . import account_cash_flow_report
 from . import account_multicurrency_revaluation_report
-from . import res_partner
-from . import res_company
 from . import account_move_line
 from . import account_report_coa
 from . import account_aged_partner_balance
@@ -20,5 +22,4 @@ from . import res_config_settings
 from . import chart_template
 from . import account_activity
 from . import ir_module_module
-from . import account
 from . import account_journal_dashboard

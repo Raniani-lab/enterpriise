9e0ff09d2be56f22d5b62ca8934f729b5e4628ab
Nisha Patel <nit@odoo.com>
2020-08-10 10:37:45 +0000

    [FIX] industry_fsm_sale: Traceback when editing product quantity
    Currently, in the field services module when we edit the quantity
    of a product by clicking on it and set blank then it produces a traceback.
    Step to reproduce:
     -Create an FSM task
     -Products
     -edit the quantity of a product by clicking on it
    
    So in this commit, if there is quantity in the product then it
    it should check the condition. if quantity is blank then make
    it 0.
    
    closes odoo/enterprise#12789
    
    Taskid: 2315103
    Closes: #12330
    X-original-commit: 1daa82bda82b4811334e1381657be53c09c50518
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- industry_fsm_sale/models/product_product.py
+++ industry_fsm_sale/models/product_product.py
@@ -70,7 +70,7 @@ class ProductProduct(models.Model):
     def set_fsm_quantity(self, quantity):
         task = self._get_contextual_fsm_task()
         # project user with no sale rights should be able to change material quantities
-        if not task or quantity < 0 or not self.user_has_groups('project.group_project_user'):
+        if not task or quantity and quantity < 0 or not self.user_has_groups('project.group_project_user'):
             return
         self = self.sudo()
         # don't add material on confirmed/locked SO to avoid inconsistence with the stock picking

558318271940e2c38bdc99decd51fa1db120775c
Arnold Moyaux <arm@odoo.com>
2020-08-25 10:38:12 +0000

    [FIX] mrp_mps: forecast with kit
    Usecase to reproduce:
    - Product A and Product B with route buy and a Vendor
    - A kit with Product A and Product B as components
    - Add Procut A and Kit in MPS
    - Go to MPS and set the demand forecast to 10 for both kit and product A
    - Product A needs to replenish 20 units (10 from demand + 10 from kit)
    - Click on replenish in the header
    
    The PO generated contains 2 lines:
    - Line 1 with Product A and qty 10 (due to kit)
    - Line 2 with Product B and qty 30
    
    The 30 units come from the 2 procurements:
    - Procurement for Product B order 20 units
    - Proucrement for Kit order 10 prouct A and 10 product B
    However it should not order the quantity for B since it's
    already in the indirect demand.
    
    Fix the issue by only order the kit's components that are not
    present in a production schedule.
    
    opw-2301582
    
    closes odoo/enterprise#12672
    
    X-original-commit: 823e22bacb807ab84e1fcd84478b947b4fdf0cd6
    Signed-off-by: Arnold Moyaux <amoyaux@users.noreply.github.com>

--- mrp_mps/models/mrp_mps.py
+++ mrp_mps/models/mrp_mps.py
@@ -115,6 +115,25 @@ class MrpProductionSchedule(models.Model):
         forecasts_to_set_as_launched = self.env['mrp.product.forecast']
         for production_schedule in self:
             production_schedule_state = production_schedule_states[production_schedule.id]
+            # Check for kit. If a kit and its component are both in the MPS we want to skip the
+            # the kit procurement but instead only refill the components not in MPS
+            bom = self.env['mrp.bom']._bom_find(
+                product=production_schedule.product_id, company_id=production_schedule.company_id.id,
+                bom_type='phantom')
+            product_ratio = []
+            if bom:
+                dummy, bom_lines = bom.explode(production_schedule.product_id, 1)
+                product_ids = [l[0].product_id.id for l in bom_lines]
+                product_ids_with_forecast = self.env['mrp.production.schedule'].search([
+                    ('company_id', '=', production_schedule.company_id.id),
+                    ('warehouse_id', '=', production_schedule.warehouse_id.id),
+                    ('product_id', 'in', product_ids)
+                ]).product_id.ids
+                product_ratio += [
+                    (l[0], l[0].product_qty * l[1]['qty'])
+                    for l in bom_lines if l[0].product_id.id not in product_ids_with_forecast
+                ]
+
             # Cells with values 'to_replenish' means that they are based on
             # lead times. There is at maximum one forecast by schedule with
             # 'forced_replenish', it's the cell that need a modification with
@@ -126,14 +145,26 @@ class MrpProductionSchedule(models.Model):
                     p.date >= forecast['date_start'] and p.date <= forecast['date_stop']
                 )
                 extra_values = production_schedule._get_procurement_extra_values(forecast)
-                procurements.append(self.env['procurement.group'].Procurement(
-                    production_schedule.product_id,
-                    forecast['replenish_qty'] - forecast['incoming_qty'],
-                    production_schedule.product_uom_id,
-                    production_schedule.warehouse_id.lot_stock_id,
-                    production_schedule.product_id.name,
-                    'MPS', production_schedule.company_id, extra_values
-                ))
+                quantity = forecast['replenish_qty'] - forecast['incoming_qty']
+                if not bom or not product_ratio:
+                    procurements.append(self.env['procurement.group'].Procurement(
+                        production_schedule.product_id,
+                        quantity,
+                        production_schedule.product_uom_id,
+                        production_schedule.warehouse_id.lot_stock_id,
+                        production_schedule.product_id.name,
+                        'MPS', production_schedule.company_id, extra_values
+                    ))
+                else:
+                    for bom_line, qty_ratio in product_ratio:
+                        procurements.append(self.env['procurement.group'].Procurement(
+                            bom_line.product_id,
+                            quantity * qty_ratio,
+                            bom_line.product_uom_id,
+                            production_schedule.warehouse_id.lot_stock_id,
+                            bom_line.product_id.name,
+                            'MPS', production_schedule.company_id, extra_values
+                        ))
 
                 if existing_forecasts:
                     forecasts_to_set_as_launched |= existing_forecasts

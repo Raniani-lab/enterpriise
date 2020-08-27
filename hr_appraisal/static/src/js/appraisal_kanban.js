d5ea98b84119fdb7edc418327860002c0489274d
Sébastien Theys <seb@odoo.com>
2020-08-27 18:20:23 +0000

    [FIX] hr_appraisal: fix opens and redirects
    Enterprise counter-part of https://github.com/odoo/odoo/pull/53143
    
    task-2244279
    task-2262862
    task-2274993
    task-2277537
    task-2282238
    
    closes odoo/enterprise#12769
    
    X-original-commit: 9fd9c38d8baaaad168b03386bc97731931c97d78
    Related: odoo/odoo#56755
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- hr_appraisal/static/src/js/appraisal_kanban.js
+++ hr_appraisal/static/src/js/appraisal_kanban.js
@@ -6,8 +6,6 @@ const KanbanController = require('web.KanbanController');
 const KanbanView = require('web.KanbanView');
 const KanbanRenderer = require('web.KanbanRenderer');
 const KanbanRecord = require('web.KanbanRecord');
-const session = require('web.session');
-const { _t } = require('web.core');
 
 const { Component } = owl;
 
@@ -23,9 +21,8 @@ const AppraisalKanbanRecord = KanbanRecord.extend({
     _onOpenChat: function(ev) {
         ev.preventDefault();
         ev.stopImmediatePropagation();
-        this.trigger_up('open_chat', {
-            employee_id: $(ev.target).data('id')
-        });
+        const env = Component.env;
+        env.messaging.openChat({ employeeId: $(ev.target).data('id') });
     },
 });
 
@@ -35,43 +32,9 @@ const AppraisalKanbanRenderer = KanbanRenderer.extend({
     }),
 });
 
-const AppraisalKanbanController = KanbanController.extend({
-    custom_events: _.extend({}, KanbanController.prototype.custom_events, {
-        open_chat: '_onOpenChat'
-    }),
-
-    _onOpenChat: async function (ev) {
-        const partner_data = await this._rpc({
-            model: 'hr.employee.public',
-            method: 'read',
-            args: [[ev.data.employee_id], ['user_partner_id']],
-        });
-        const partnerId = partner_data[0].user_partner_id[0];
-        if (partnerId && partnerId !== session.partner_id) {
-            const env = Component.env;
-            const partner = env.models['mail.partner'].insert({
-                id: partner_data[0].id,
-            });
-            partner.openChat();
-        } else if (partnerId !== session.partner_id) {
-            // this is not ourself, so if we get here it means that the
-            // employee is not associated with any user
-            this.displayNotification({
-                message: _t('You can only chat with employees that have a dedicated user'),
-                type: 'info',
-            });
-        } else {
-            this.displayNotification({
-                message: _t('You cannot chat with yourself'),
-                type: 'info',
-            });
-        }
-    },
-});
-
 const AppraisalKanbanView = KanbanView.extend({
     config: _.extend({}, KanbanView.prototype.config, {
-        Controller: AppraisalKanbanController,
+        Controller: KanbanController,
         Renderer: AppraisalKanbanRenderer,
     }),
 });
@@ -79,7 +42,6 @@ const AppraisalKanbanView = KanbanView.extend({
 viewRegistry.add('appraisal_kanban', AppraisalKanbanView);
 return {
     AppraisalKanbanView: AppraisalKanbanView,
-    AppraisalKanbanController: AppraisalKanbanController,
     AppraisalKanbanRecord: AppraisalKanbanRecord,
     AppraisalKanbanRenderer: AppraisalKanbanRenderer,
 };

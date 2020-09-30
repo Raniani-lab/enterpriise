4220dee772266c3f15eaead95fab89e1a1a3fe59
Xavier BOL (xbo) <xbo@odoo.com>
2020-09-30 19:44:56 +0200

    [IMP] l10n_be_hr_payroll: reevaluate the right to time off
    reevaluate the time off when a credit time is made for an employee
    ------------------------------------------------------------------
    
    When the employee needs to a credit to change his working schedule, we
    need to reevaluate the right to time off at each change of working
    schedule.
    
    reevaluate the time off when exit credit time
    ---------------------------------------------
    
    When the user removes the credit time for an employee to create a new
    contract at full time, we need to reevaluate the paid time off to
    allocate.
    
    compute the old work time rate
    ------------------------------
    
    We need to compute the old work time rate of the employee because we
    must recompute the correct work time rate to compute correctly the paid
    time off.
    
    task-2314223

--- l10n_be_hr_payroll/wizard/l10n_be_hr_payroll_credit_time_wizard.py
+++ l10n_be_hr_payroll/wizard/l10n_be_hr_payroll_credit_time_wizard.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
-WORK_RATE = [('0', 'Set To Full Time'), ('0.5', '1/2'), ('0.8', '4/5'), ('0.9', '9/10')]
+WORK_RATE = [('0', 'Full Time Credit Time'), ('0.5', '1/2'), ('0.8', '4/5'), ('0.9', '9/10')]
 
 class L10nBeHrPayrollCreditTime(models.TransientModel):
     _name = 'l10n_be.hr.payroll.credit.time.wizard'
@@ -20,8 +20,9 @@ class L10nBeHrPayrollCreditTime(models.TransientModel):
         return super().default_get(field_list)
 
     contract_id = fields.Many2one('hr.contract', string='Contract', default=lambda self: self.env.context.get('active_id'))
-    date_start = fields.Date('Start Date Credit Time', help="Start date of the credit time contract.", required=True)
-    date_end = fields.Date('End Date Credit Time', required=True,
+    employee_id = fields.Many2one(related='contract_id.employee_id')
+    date_start = fields.Date('Start Date', help="Start date of the credit time contract.", required=True)
+    date_end = fields.Date('End Date', required=True,
         help="Last day included of the credit time contract.")
 
     resource_calendar_id = fields.Many2one(
@@ -35,11 +36,49 @@ class L10nBeHrPayrollCreditTime(models.TransientModel):
 
     work_time = fields.Selection(WORK_RATE, string='Work Time Rate',
         required=True, help='Work time rate versus full time working schedule.')
+    time_off_allocation = fields.Float(string='New Time Off Allocation', compute='_compute_paid_time_off', store=True, readonly=False)
+    remaining_allocated_time_off = fields.Float(string='Current Remaining Leaves', compute='_compute_paid_time_off', store=True, readonly=True)
+    holiday_status_id = fields.Many2one(
+        "hr.leave.type", string="Time Off Type", required=True,
+        domain=[('valid', '=', True), ('allocation_type', '!=', 'no')])
+    leave_allocation_id = fields.Many2one('hr.leave.allocation')
 
     @api.depends('work_time')
     def _compute_wage(self):
         for wizard in self:
             wizard.wage = wizard.contract_id._get_contract_wage() * float(wizard.work_time)
+
+    @api.depends('holiday_status_id', 'work_time', 'resource_calendar_id')
+    def _compute_paid_time_off(self):
+        for wizard in self:
+            if not wizard.holiday_status_id.id or not wizard.work_time:
+                continue
+
+            # Normally, it should only have an allocation for each employee per year
+            leave_allocation = self.env['hr.leave.allocation'].search([
+                ('holiday_status_id', '=', wizard.holiday_status_id.id),
+                ('employee_id', '=', wizard.contract_id.employee_id.id),
+                ('state', 'in', ('validate', 'validate1'))], limit=1)
+            if not leave_allocation.id:
+                continue
+
+            remaining_allocated_time_off = leave_allocation.number_of_days - leave_allocation.leaves_taken
+            work_time_rate = wizard.resource_calendar_id.work_time_rate / 100 * (float(wizard.work_time) if wizard.work_time else 1.0)
+
+            # Compute old work time rate
+            old_work_time_rate = wizard.contract_id.resource_calendar_id.work_time_rate / 100 * (float(wizard.contract_id.work_time_rate) if wizard.contract_id.work_time_rate else 1.0)
+
+            if 0 < old_work_time_rate < 1:  # Then we need to remove the older to compute correctly the paid time off to allocate
+                work_time_rate /= old_work_time_rate
+
+            time_off_allocation = remaining_allocated_time_off * work_time_rate
+            time_off_allocation = round(time_off_allocation * 2) / 2
+
+            wizard.write({
+                'time_off_allocation': leave_allocation.max_leaves_allocated - leave_allocation.leaves_taken if time_off_allocation + leave_allocation.leaves_taken > leave_allocation.max_leaves_allocated else time_off_allocation,
+                'remaining_allocated_time_off': remaining_allocated_time_off,
+                'leave_allocation_id': leave_allocation.id
+            })
 
     def validate_credit_time(self):
         if self.date_start > self.date_end:
@@ -62,6 +101,10 @@ class L10nBeHrPayrollCreditTime(models.TransientModel):
             'state': 'draft',
         })
 
+        if self.leave_allocation_id:
+            # Update the number of days to allocate for the current year
+            self.leave_allocation_id.update({'number_of_days': self.time_off_allocation + self.leave_allocation_id.leaves_taken})
+
         self.contract_id.date_end = self.date_start + timedelta(days=-1)
 
         return {
@@ -72,6 +115,7 @@ class L10nBeHrPayrollCreditTime(models.TransientModel):
             'view_mode': 'tree,form',
             'type': 'ir.actions.act_window',
         }
+
 
 class L10nBeHrPayrollExitCreditTime(models.TransientModel):
     _name = 'l10n_be.hr.payroll.exit.credit.time.wizard'
@@ -91,6 +135,7 @@ class L10nBeHrPayrollExitCreditTime(models.TransientModel):
 
     credit_time_contract_id = fields.Many2one('hr.contract', string='Credit Time Contract', default=lambda self: self.env.context.get('active_id'))
     contract_id = fields.Many2one('hr.contract', string='Contract')
+    employee_id = fields.Many2one(related='credit_time_contract_id.employee_id')
     date_start = fields.Date('Start Date', help="Start date of the normal time contract.", required=True)
     date_end = fields.Date('End Date', help="End date of the normal time contract (if it's a fixed-term contract).")
 
@@ -98,6 +143,45 @@ class L10nBeHrPayrollExitCreditTime(models.TransientModel):
         'resource.calendar', 'New Working Schedule', required=True)
     wage = fields.Monetary('New Wage', required=True, help="Employee's monthly gross wage.")
     currency_id = fields.Many2one(string="Currency", related='contract_id.company_id.currency_id', readonly=True)
+
+    holiday_status_id = fields.Many2one(
+        "hr.leave.type", string="Time Off Type", required=True,
+        domain=[('valid', '=', True), ('allocation_type', '!=', 'no')])
+    time_off_allocation = fields.Float('New Time Off Allocation', compute='_compute_paid_time_off', store=True, readonly=False)
+    remaining_allocated_time_off = fields.Float('Current Remaining Leaves', compute='_compute_paid_time_off', store=True, readonly=True)
+    holiday_status_id = fields.Many2one(
+        "hr.leave.type", string="Time Off Type", required=True,
+        domain=[('valid', '=', True), ('allocation_type', '!=', 'no')])
+    leave_allocation_id = fields.Many2one('hr.leave.allocation')
+
+    @api.depends('holiday_status_id', 'resource_calendar_id')
+    def _compute_paid_time_off(self):
+        for wizard in self:
+            leave_allocation = wizard.env['hr.leave.allocation'].search([
+                ('holiday_status_id', '=', wizard.holiday_status_id.id),
+                ('employee_id', '=', wizard.contract_id.employee_id.id),
+                ('state', 'in', ('validate', 'validate1'))], limit=1)
+            if not leave_allocation.id:
+                continue
+
+            remaining_allocated_time_off = leave_allocation.number_of_days - leave_allocation.leaves_taken
+
+            work_time_rate = wizard.resource_calendar_id.work_time_rate / 100
+
+            # Compute old work time rate
+            old_work_time_rate = wizard.credit_time_contract_id.resource_calendar_id.work_time_rate / 100 * (float(wizard.credit_time_contract_id.work_time_rate) if wizard.credit_time_contract_id.work_time_rate else 1.0)
+
+            if 0 < old_work_time_rate < 1:  # Then we need to remove the older to compute correctly the paid time off to allocate
+                work_time_rate /= old_work_time_rate
+
+            time_off_allocation = remaining_allocated_time_off * work_time_rate
+            time_off_allocation = round(time_off_allocation * 2) / 2
+
+            wizard.write({
+                'time_off_allocation': leave_allocation.max_leaves_allocated - leave_allocation.leaves_taken if time_off_allocation + leave_allocation.leaves_taken > leave_allocation.max_leaves_allocated else time_off_allocation,
+                'remaining_allocated_time_off': remaining_allocated_time_off,
+                'leave_allocation_id': leave_allocation.id
+            })
 
     def validate_full_time(self):
         if self.date_end and self.date_start > self.date_end:
@@ -113,6 +197,10 @@ class L10nBeHrPayrollExitCreditTime(models.TransientModel):
             'time_credit' : False,
             'state': 'draft',
         })
+
+        if self.leave_allocation_id.id:
+            # Update the number of days to allocate for the current year
+            self.leave_allocation_id.update({'number_of_days': self.time_off_allocation + self.leave_allocation_id.leaves_taken})
 
         return {
             'name': _('Full time contract'),

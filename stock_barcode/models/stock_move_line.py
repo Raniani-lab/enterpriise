5a4d13e79dc23883f8e4d4ccccd87915b188e793
yhu-odoo <yhu@odoo.com>
2020-10-16 06:55:05 +0000

    [IMP] stock_barcode: show product quant in edit form
    Show the locations and quantities in the barcode stock.move.line edit
    form.
    
    Task 2344342

--- stock_barcode/models/stock_move_line.py
+++ stock_barcode/models/stock_move_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMoveLine(models.Model):
@@ -8,3 +8,9 @@ class StockMoveLine(models.Model):
 
     picking_location_id = fields.Many2one(related='picking_id.location_id')
     picking_location_dest_id = fields.Many2one(related='picking_id.location_dest_id')
+    product_stock_quant_ids = fields.One2many('stock.quant', compute='_compute_product_stock_quant_ids')
+
+    @api.depends('product_id', 'product_id.stock_quant_ids')
+    def _compute_product_stock_quant_ids(self):
+        for line in self:
+            line.product_stock_quant_ids = line.product_id.stock_quant_ids.filtered(lambda q: q.company_id in self.env.companies and q.location_id.usage == 'internal')

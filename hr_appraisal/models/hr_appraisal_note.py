78194a8943385da28e69946b651819aa44ba4373
Nicolas Martinelli <nim@odoo.com>
2020-11-09 12:41:52 +0000

    [FIX] hr_appraisal: use sequence for ordering
    Since the `sequence` field is used with a `widget="handle"`, we need to
    sort by sequence, otherwise the reordering has no effect visually.
    
    opw-2378255
    
    closes odoo/enterprise#14662
    
    X-original-commit: e4cd50768fbd25de34f3f52fe3b5d9d6b94670b9
    Signed-off-by: Nicolas Martinelli (nim) <nim@odoo.com>

--- hr_appraisal/models/hr_appraisal_note.py
+++ hr_appraisal/models/hr_appraisal_note.py
@@ -7,6 +7,7 @@ from odoo import api, fields, models
 class HrAppraisalNote(models.Model):
     _name = "hr.appraisal.note"
     _description = "Appraisal Assessment Note"
+    _order = "sequence, id"
 
     name = fields.Char(required=True)
     sequence = fields.Integer(default=10)

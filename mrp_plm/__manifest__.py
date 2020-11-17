40944148f7cbe1cf7295e6e072fcfe7ec2b2f6bf
Thibault Delavallée <tde@odoo.com>
2020-11-17 08:38:51 +0000

    [MOV][IMP] mrp_plm: reorganize qweb / jinja templates used for mailing
    PURPOSE
    
    Clean organization of templates in odoo apps: mail.template records in data,
    qweb templates (views) used directly in code, notably using post with view.
    Purpose is to ease future improvements in posting based on templates.
    
    SPECIFICATIONS
    
      * move those templates in their own file to ease their discovering and
        maintenance;
      * put them into data (as those are not views even if it contains qweb)
      * guidelines are now :
    
        -> Qweb templates should be in data/mail_templates.xml;
        -> mail.template records should be in data/mail_template_data.xml;
    
      * put their declaration in no update when not done if template has no
        technical code or complex dependency on underlying code;
      * move found mail data (mail.message.subtype or mail.activity.type) records
        in a mail_data file that should contain only "core" records linked to mail;
    
    LINKS
    
    Task ID-2375767
    COM PR odoo/odoo#61814
    ENT PR odoo/enterprise#14775
    UPG PR odoo/upgrade#1936

--- mrp_plm/__manifest__.py
+++ mrp_plm/__manifest__.py
@@ -20,6 +20,7 @@ Product Life Management
     'data': [
         'security/mrp_plm.xml',
         'security/ir.model.access.csv',
+        'data/mail_templates.xml',
         'data/mrp_data.xml',
         'views/mrp_bom_views.xml',
         'views/mrp_document_views.xml',

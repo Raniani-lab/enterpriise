9d79ed1128e76e8553a7cc39e790de4369345eec
Nicolas Galler <ngl@odoo.com>
2020-12-04 16:54:46 +0000

    [FIX] l10n_es_reports: exclude EU customers from mod 347 report
    Behavior prior to this commit:
    
    - all operations are included on the mod347 report
    - the "Mod347 Invoice Type" field is available only for intracommunitary
    invoices
    
    Behavior after this commit:
    
    - operations within the EU are excluded by default
    - operations to third countries outside the EU are included by default
    - the "Mod347 Invoice Type" field is available to select for any
    operation involving a Spain-based company, in case a specific invoice
    needs to be excluded
    
    It should be noted that the logic for determining whether an invoice
    line is included / excluded on the mod347 is a bit more complicated than
    is implemented in this module - ideally this should be done on a line by
    line basis instead.
    
    - http://www.supercontable.com/envios/articulos/BOLETIN_SUPERCONTABLE_08_2016_Contenido_General_6.htm
    - https://www.agenciatributaria.es/AEAT.internet/Inicio/Ayuda/Manuales__Folletos_y_Videos/Manuales_practicos/_Ayuda_Folleto_Actividades_economicas/5__Impuesto_sobre_el_Valor_Anadido/5_8_Operaciones_intracomunitarias/5_8_Operaciones_intracomunitarias.html
    - https://github.com/OCA/l10n-spain/issues/1272
    
    opw-2282305
    
    closes odoo/enterprise#21964
    
    X-original-commit: b3ce71c305aa00c407b8e1490aaf8953f7605b0c
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- l10n_es_reports/models/account_move.py
+++ l10n_es_reports/models/account_move.py
@@ -44,8 +44,17 @@ class AccountMove(models.Model):
         return [('A', _("A - Acquisition")), ('E', _("E - Supply")), ('T', _("T - Triangular Operation")), ('S', _("S - Services sale")), ('I', _("I - Services acquisition")), ('M', _("M - Supply without taxes")), ('H', _("H - Supply without taxes delivered by a legal representative"))]
 
     l10n_es_reports_mod347_invoice_type = fields.Selection(string="Type for mod 347", selection=[('regular', "Regular operation"), ('insurance', "Insurance operation")], default='regular', help="Defines the category into which this invoice falls for mod 347 report.")
+    l10n_es_reports_mod347_available = fields.Boolean(string="Available for Mod347", compute="_compute_l10n_es_reports_mod347_available", help="True if and only if the invoice MIGHT need to be reported on mod 347, i.e. it concerns an operation from a Spanish headquarter.")
     l10n_es_reports_mod349_invoice_type = fields.Selection(string="Type for mod 349", selection="_mod_349_selection_values", help="Defines the category into which this invoice falls for mod 349 report", default=_default_mod_349_invoice_type)
     l10n_es_reports_mod349_available = fields.Boolean(string="Available for Mod349", store=True, compute="_compute_l10n_es_reports_mod349_available", help="True if and only if the invoice must be reported on mod 349 report, i.e. it concerns an intracommunitary operation.")
+
+
+    @api.depends('company_id')
+    def _compute_l10n_es_reports_mod347_available(self):
+        for record in self:
+            # Even though Mod 347 is normally not required for EU operations, it is sometimes required, so the users
+            # should be able to select it
+            record.l10n_es_reports_mod347_available = record.company_id.country_id.code == "ES"
 
     @api.depends('partner_id.country_id')
     def _compute_l10n_es_reports_mod349_available(self):
@@ -53,6 +62,17 @@ class AccountMove(models.Model):
         mod349_countries = self.env.ref('base.europe').country_ids.filtered_domain([('code', '!=', 'ES')])
         for record in self:
             record.l10n_es_reports_mod349_available = record.partner_id.country_id in mod349_countries
+
+    def _get_refund_copy_fields(self):
+        rslt = super(AccountMove, self)._get_refund_copy_fields()
+        return rslt + ['l10n_es_reports_mod347_invoice_type', 'l10n_es_reports_mod349_invoice_type']
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id_set_347_invoice_type(self):
+        eu_countries = self.env.ref('base.europe').country_ids - self.env.ref('base.es')
+        for record in self:
+            # Mod 347 is normally not required for imports / exports within the EU, since those are already on the 349
+            record.l10n_es_reports_mod347_invoice_type = False if record.partner_id.country_id in eu_countries else 'regular'
 
     def _post(self, soft=True):
         """ Overridden to require Spanish invoice type to be set if the company

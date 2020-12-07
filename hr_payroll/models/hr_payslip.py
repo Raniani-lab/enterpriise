531ec652c7805e0298452c89f52ffadd22332ab9
Yannick Tivisse <yti@odoo.com>
2020-12-07 11:01:29 +0000

    [MOV] hr_payroll_edit_lines: Move to hr_payroll
    closes odoo/enterprise#15161
    
    Related: odoo/upgrade#2008
    Related: odoo/odoo#62902
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll/models/hr_payslip.py
+++ hr_payroll/models/hr_payslip.py
@@ -84,6 +84,7 @@ class HrPayslip(models.Model):
     negative_net_to_report_message = fields.Char(compute='_compute_negative_net_to_report_display')
     negative_net_to_report_amount = fields.Float(compute='_compute_negative_net_to_report_display')
     is_superuser = fields.Boolean(compute="_compute_is_superuser")
+    edited = fields.Boolean()
 
     @api.depends('employee_id', 'state')
     def _compute_negative_net_to_report_display(self):
@@ -530,6 +531,48 @@ class HrPayslip(models.Model):
     def _get_unpaid_amount(self):
         self.ensure_one()
         return self._get_contract_wage() - self._get_paid_amount()
+
+    def action_edit_payslip_lines(self):
+        self.ensure_one()
+        if not self.user_has_groups('hr_payroll.group_hr_payroll_manager'):
+            raise UserError(_('This action is restricted to payroll managers only.'))
+        if self.state == 'done':
+            raise UserError(_('This action is forbidden on validated payslips.'))
+        wizard = self.env['hr.payroll.edit.payslip.lines.wizard'].create({
+            'payslip_id': self.id,
+            'line_ids': [(0, 0, {
+                'sequence': line.sequence,
+                'code': line.code,
+                'name': line.name,
+                'note': line.note,
+                'salary_rule_id': line.salary_rule_id.id,
+                'contract_id': line.contract_id.id,
+                'employee_id': line.employee_id.id,
+                'amount': line.amount,
+                'quantity': line.quantity,
+                'rate': line.rate,
+                'slip_id': self.id}) for line in self.line_ids],
+            'worked_days_line_ids': [(0, 0, {
+                'name': line.name,
+                'sequence': line.sequence,
+                'code': line.code,
+                'work_entry_type_id': line.work_entry_type_id.id,
+                'number_of_days': line.number_of_days,
+                'number_of_hours': line.number_of_hours,
+                'amount': line.amount,
+                'slip_id': self.id}) for line in self.worked_days_line_ids]
+        })
+
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Edit Payslip Lines'),
+            'res_model': 'hr.payroll.edit.payslip.lines.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'binding_model_id': self.env['ir.model.data'].xmlid_to_res_id('hr_payroll.model_hr_payslip'),
+            'binding_view_types': 'form',
+            'res_id': wizard.id
+        }
 
 
 class HrPayslipLine(models.Model):

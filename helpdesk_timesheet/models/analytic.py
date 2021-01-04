de102aeb0bbc2d13fea1f8cc14933bb290f7f563
Xavier BOL (xbo) <xbo@odoo.com>
2021-01-04 16:31:57 +0100

    [IMP] helpdesk_timesheet: check timesheet not linked to task and ticket
    Now, we don't want to have a timesheet linked to a task and a ticket at
    the same time. To do this, we add a constraint to check if it's not the
    case.
    
    task-2424382

--- helpdesk_timesheet/models/analytic.py
+++ helpdesk_timesheet/models/analytic.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.osv import expression
+from odoo.exceptions import ValidationError
 
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     helpdesk_ticket_id = fields.Many2one('helpdesk.ticket', 'Helpdesk Ticket')
+
+    @api.constrains('task_id', 'helpdesk_ticket_id')
+    def _check_no_link_task_and_ticket(self):
+        # Check if any timesheets are not linked to a ticket and a task at the same time
+        if any(timesheet.task_id and timesheet.helpdesk_ticket_id for timesheet in self):
+            raise ValidationError(_("A timesheet cannot be linked to a task and a ticket at the same time."))
 
     def _timesheet_preprocess(self, vals):
         helpdesk_ticket_id = vals.get('helpdesk_ticket_id')

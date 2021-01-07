a1d5fc19157c88508315e176aac914138ecd2aa7
Goffin Simon <sig@odoo.com>
2021-01-07 14:20:31 +0000

    [FIX] sale_account_taxcloud: Customer ID not sent
    Steps to reproduce the bug:
    - Let's consider a company C with TaxCloud setup
    - Create a sale order SO with a customer C
    - Click on update taxes
    
    Bug:
    
    Odoo sent the information to TaxCloud without the customer ID.
    This was a problem, because, if the customer C had an exemption certificate,
    TaxCloud ignored that, as it couldn't match the ID, it charged
    the full tax and fees as well.
    
    opw:2422962
    
    closes odoo/enterprise#15637
    
    X-original-commit: 628ff63d131fe2f28dcdb6c47895766361db3398
    Signed-off-by: Simon Goffin (sig) <sig@openerp.com>

--- sale_account_taxcloud/models/taxcloud_request.py
+++ sale_account_taxcloud/models/taxcloud_request.py
@@ -5,5 +5,6 @@ from odoo.addons.account_taxcloud.models import taxcloud_request
 class TaxCloudRequest(taxcloud_request.TaxCloudRequest):
 
     def set_order_items_detail(self, order):
+        self.customer_id = order.partner_id.id
         self.cart_items = self.factory.ArrayOfCartItem()
         self.cart_items.CartItem = self._process_lines(order.order_line)

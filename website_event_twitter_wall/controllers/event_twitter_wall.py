ec2d29cdb162bed17da6c6edb0a44929f3be0c4a
Romain Derie <rde@odoo.com>
2021-01-11 19:44:56 +0000

    [IMP] website_event_twitter_wall: method is now low level generic
    See https://github.com/odoo/odoo/pull/64313, can_access_from_current_website is now
    automatically added at a lowel level on methods with a model converter.
    
    closes odoo/enterprise#15688
    
    Related: odoo/odoo#64313
    Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

--- website_event_twitter_wall/controllers/event_twitter_wall.py
+++ website_event_twitter_wall/controllers/event_twitter_wall.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from werkzeug.exceptions import NotFound
-
 from odoo import http
 from odoo.http import request
 from odoo.addons.http_routing.models.ir_http import slug
@@ -15,9 +13,6 @@ class WebsiteEventTwitterWallController(WebsiteEventController):
         '/event/<model("event.event"):event>/social/page/<int:page>'
     ], type='http', auth="public", website=True, sitemap=False)
     def event_social(self, event, page=1):
-        if not event.can_access_from_current_website():
-            raise NotFound()
-
         tweets_count = 15
         wall = event.twitter_wall_id
         pager = request.website.pager(url='/event/%s/social' % slug(event), total=wall.total_tweets, page=page, step=tweets_count, scope=tweets_count)

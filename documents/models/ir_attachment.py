bfa5c942de2fa3b5ff457502fcb4586fec7d6348
Ivan Yelizariev <iel@odoo.com>
2021-01-11 13:42:42 +0000

    [FIX] documents: sudo on creating documents.document
    * install project and documents
    * open menu ``Documents > Configuration > Settings``
    * under **Files Centralization** activate Project
    * Login in with a user that doesn't have Document access right
    * Go to the project > Task and add an attachment
    
    BEFORE: error
    AFTER: file is attached
    
    ---
    
    opw-2427768
    
    closes odoo/enterprise#15689
    
    X-original-commit: 104c0a0d8e6c244c3d534bd3796e066d23cacae9
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>
    Signed-off-by: Ivan Yelizariev // IEL <yelizariev@users.noreply.github.com>

--- documents/models/ir_attachment.py
+++ documents/models/ir_attachment.py
@@ -77,12 +77,12 @@ class IrAttachment(models.Model):
         # the context can indicate that this new attachment is created from documents, and therefore
         # doesn't need a new document to contain it.
         if not self._context.get('no_document') and not attachment.res_field:
-            attachment._create_document(dict(vals, res_model=attachment.res_model, res_id=attachment.res_id))
+            attachment.sudo()._create_document(dict(vals, res_model=attachment.res_model, res_id=attachment.res_id))
         return attachment
 
     def write(self, vals):
         if not self._context.get('no_document'):
-            self.filtered(lambda a: not (vals.get('res_field') or a.res_field))._create_document(vals)
+            self.filtered(lambda a: not (vals.get('res_field') or a.res_field)).sudo()._create_document(vals)
         return super(IrAttachment, self).write(vals)
 
 

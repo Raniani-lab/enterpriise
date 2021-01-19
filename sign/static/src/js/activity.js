89f18717cbaac92b3beeafcf036b8727840f663d
Xavier Dubuc <xdu@odoo.com>
2021-01-19 11:37:55 +0000

    [FIX] sign: fix request signature button in list view
    task-2429670
    
    closes odoo/enterprise#15893
    
    X-original-commit: 4a454aada196d502e9d8c22b6dc13e6195f6a950
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- sign/static/src/js/activity.js
+++ sign/static/src/js/activity.js
@@ -4,30 +4,36 @@ odoo.define('sign.Activity', function (require) {
 const field_registry = require('web.field_registry');
 require('mail.Activity');
 const KanbanActivity = field_registry.get('kanban_activity');
+const ListActivity = field_registry.get('list_activity');
 
-KanbanActivity.include({
-    events: Object.assign({}, KanbanActivity.prototype.events, {
-        'click .o_mark_as_done_request_sign': '_onClickRequestSign',
-    }),
+function applyInclude(Activity) {
+    Activity.include({
+        events: Object.assign({}, Activity.prototype.events, {
+            'click .o_mark_as_done_request_sign': '_onClickRequestSign',
+        }),
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
+        //--------------------------------------------------------------------------
+        // Handlers
+        //--------------------------------------------------------------------------
 
-    /**
-     * @private
-     * @param {MouseEvent} ev
-     */
-    _onClickRequestSign(ev) {
-        ev.preventDefault();
-        this.do_action('sign.action_sign_send_request', {
-            additional_context: {
-                'sign_directly_without_mail': false,
-                'default_activity_id': $(ev.currentTarget).data('activity-id'),
-            },
-            on_close: this._reload.bind(this, {activity: true, thread: true}),
-        });
-    }
-});
+        /**
+         * @private
+         * @param {MouseEvent} ev
+         */
+        _onClickRequestSign(ev) {
+            ev.preventDefault();
+            this.do_action('sign.action_sign_send_request', {
+                additional_context: {
+                    'sign_directly_without_mail': false,
+                    'default_activity_id': $(ev.currentTarget).data('activity-id'),
+                },
+                on_close: this._reload.bind(this, {activity: true, thread: true}),
+            });
+        }
+    });
+}
+
+applyInclude(KanbanActivity);
+applyInclude(ListActivity);
 
 });

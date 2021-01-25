0b1620b2638ec308b1dfd57104f06191d0c18367
Arthur Jaques <ajs@odoo.com>
2021-01-25 08:32:51 +0000

    [FIX] l10n_lu_reports_electronic: do not add report line that are at 0, have no reported child line, and are not mandatory
    This way, the generated XML reports are cleaner and contain only the mandatory and meaningful data.
    
    X-original-commit: 9c611bf17f87724d5113f7418cedfc3098cd8a05

--- l10n_lu_reports_electronic/models/account_financial_report.py
+++ l10n_lu_reports_electronic/models/account_financial_report.py
@@ -2,8 +2,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import re
-from odoo import fields, models, tools
-
+from odoo import fields, models
+from odoo.tools.float_utils import float_compare, float_repr
 
 class ReportAccountFinancialReport(models.Model):
     _inherit = "account.financial.html.report"
@@ -22,7 +22,18 @@ class ReportAccountFinancialReport(models.Model):
     def _get_lu_electronic_report_values(self, options):
 
         def _format_amount(amount):
-            return "{:.2f}".format(amount).replace('.', ',') if amount else '0,00'
+            return float_repr(amount, 2).replace('.', ',') if amount else '0,00'
+
+        values = {}
+
+        def _report_useful_fields(amount, field, parent_field, required):
+            """Only reports fields containing values or that are required."""
+            # All required fields are always reported; all others reported only if different from 0.00.
+            if float_compare(amount, 0.0, 2) != 0 or required:
+                values.update({field: {'value': _format_amount(amount), 'field_type': 'number'}})
+                # The parent needs to be added even if at 0, if some child lines are filled in
+                if parent_code and not values.get(parent_code):
+                    values.update({parent_field: {'value': '0,00', 'field_type': 'number'}})
 
         lu_template_values = super()._get_lu_electronic_report_values(options)
 
@@ -37,11 +48,11 @@ class ReportAccountFinancialReport(models.Model):
         ReportLine = self.env['account.financial.html.report.line']
         date_from = fields.Date.from_string(options['date'].get('date_from'))
         date_to = fields.Date.from_string(options['date'].get('date_to'))
-        values = {
+        values.update({
             '01': {'value': date_from.strftime("%d/%m/%Y"), 'field_type': 'char'},
             '02': {'value': date_to.strftime("%d/%m/%Y"), 'field_type': 'char'},
             '03': {'value': self.env.company.currency_id.name, 'field_type': 'char'}
-        }
+        })
 
         # we only need `account.financial.html.report.line` record's IDs and line['id'] could hold account.account
         # record's IDs as well. Such can be identified by `financial_group_line_id` in line dictionary's key. So,
@@ -54,11 +65,19 @@ class ReportAccountFinancialReport(models.Model):
             columns = line['columns']
             # since we have enabled comparison by default, `columns` element will atleast have two dictionary items.
             # First dict will be holding current year's balance and second one will be holding previous year's balance.
-            if len(split_line_code) > 1:
-                values.update({
-                    split_line_code[2]: {'value': _format_amount(columns[0]['no_format']), 'field_type': 'number'}, # current year balance
-                    str(int(split_line_code[2]) + 1): {'value': _format_amount(columns[1]['no_format']), 'field_type': 'number'} # previous year balance
-                })
+            if len(split_line_code) > 2:
+                parent_code = None
+                parent_id = ReportLine.browse(line['id']).parent_id
+                if parent_id and parent_id.code:
+                    parent_split_code = parent_id.code.split('_')
+                    if len(parent_split_code) > 2:
+                        parent_code = parent_split_code[2]
+
+                required = line['level'] == 0  # Required fields all have level 0
+                # current year balance
+                _report_useful_fields(columns[0]['no_format'], split_line_code[2], parent_code, required)
+                # previous year balance
+                _report_useful_fields(columns[1]['no_format'], str(int(split_line_code[2]) + 1), parent_code and str(int(parent_code) + 1), required)
 
         lu_template_values.update({
             'forms': [{

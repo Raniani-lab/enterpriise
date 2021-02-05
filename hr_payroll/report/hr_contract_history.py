26025909488dde5ec9a1992740f9ebf336e63c48
Yannick Tivisse <yti@odoo.com>
2021-02-05 13:49:07 +0000

    [IMP] hr_payroll: Add stat button to access payslips from contract history
    closes odoo/enterprise#16157
    
    Related: odoo/odoo#65621
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll/report/hr_contract_history.py
+++ hr_payroll/report/hr_contract_history.py
@@ -7,3 +7,14 @@ class ContractHistory(models.Model):
     _inherit = 'hr.contract.history'
 
     wage_type = fields.Selection(related='structure_type_id.wage_type', readonly=True)
+    payslips_count = fields.Integer("# Payslips", compute='_compute_payslips_count', groups="hr_payroll.group_hr_payroll_user")
+
+    def _compute_payslips_count(self):
+        for history in self:
+            history.payslips_count = sum(contract.payslips_count for contract in history.contract_ids)
+
+    def action_open_payslips(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id("hr_payroll.action_view_hr_payslip_month_form")
+        action.update({'domain': [('contract_id', 'in', self.contract_ids.ids)]})
+        return action

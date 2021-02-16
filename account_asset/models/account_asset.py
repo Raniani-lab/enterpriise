799efc8f58f5b403e68f4a6b47bd5cc0094c87cc
Julien CHEVREAU (JCU) <jcu@odoo.com>
2021-02-16 09:17:50 +0000

    [FIX] account_asset: Generate Disposal Move for fully Depreciated Assets
    Beforehand, when disposing an asset, a disposal move was generated only
    if the asset still had draft depreciation moves, that is to say if the
    asset was not fully depreciated yet.
    If the asset was fully depreciated, then no disposal move was generated,
    and the asset was set to close without really being disposed of.
    
    Now, a disposal move is generated even for fully depreciated assets.
    
    opw-2391594
    
    closes odoo/enterprise#16422
    
    X-original-commit: d20e5000d6d29e0f9f77aac1ad9ee5bb875adc83
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_asset/models/account_asset.py
+++ account_asset/models/account_asset.py
@@ -563,46 +563,46 @@ class AccountAsset(models.Model):
             current_currency = asset.currency_id
             prec = company_currency.decimal_places
             unposted_depreciation_move_ids = asset.depreciation_move_ids.filtered(lambda x: x.state == 'draft')
-            if unposted_depreciation_move_ids:
-                old_values = {
-                    'method_number': asset.method_number,
-                }
 
-                # Remove all unposted depr. lines
-                commands = [(2, line_id.id, False) for line_id in unposted_depreciation_move_ids]
+            old_values = {
+                'method_number': asset.method_number,
+            }
 
-                # Create a new depr. line with the residual amount and post it
-                asset_sequence = len(asset.depreciation_move_ids) - len(unposted_depreciation_move_ids) + 1
+            # Remove all unposted depr. lines
+            commands = [(2, line_id.id, False) for line_id in unposted_depreciation_move_ids]
 
-                initial_amount = asset.original_value
-                initial_account = asset.original_move_line_ids.account_id if len(asset.original_move_line_ids.account_id) == 1 else asset.account_asset_id
-                depreciation_moves = asset.depreciation_move_ids.filtered(lambda r: r.state == 'posted' and not (r.reversal_move_id and r.reversal_move_id[0].state == 'posted'))
-                depreciated_amount = copysign(sum(depreciation_moves.mapped('amount_total')), -initial_amount)
-                depreciation_account = asset.account_depreciation_id
-                invoice_amount = copysign(invoice_line_id.price_subtotal, -initial_amount)
-                invoice_account = invoice_line_id.account_id
-                difference = -initial_amount - depreciated_amount - invoice_amount
-                difference_account = asset.company_id.gain_account_id if difference > 0 else asset.company_id.loss_account_id
-                line_datas = [(initial_amount, initial_account), (depreciated_amount, depreciation_account), (invoice_amount, invoice_account), (difference, difference_account)]
-                if not invoice_line_id:
-                    del line_datas[2]
-                vals = {
-                    'amount_total': current_currency._convert(asset.value_residual, company_currency, asset.company_id, disposal_date),
-                    'asset_id': asset.id,
-                    'ref': asset.name + ': ' + (_('Disposal') if not invoice_line_id else _('Sale')),
-                    'asset_remaining_value': 0,
-                    'asset_depreciated_value': max(asset.depreciation_move_ids.filtered(lambda x: x.state == 'posted'), key=lambda x: x.date, default=self.env['account.move']).asset_depreciated_value,
-                    'date': disposal_date,
-                    'journal_id': asset.journal_id.id,
-                    'line_ids': [get_line(asset, amount, account) for amount, account in line_datas if account],
-                }
-                commands.append((0, 0, vals))
-                asset.write({'depreciation_move_ids': commands, 'method_number': asset_sequence})
-                tracked_fields = self.env['account.asset'].fields_get(['method_number'])
-                changes, tracking_value_ids = asset._mail_track(tracked_fields, old_values)
-                if changes:
-                    asset.message_post(body=_('Asset sold or disposed. Accounting entry awaiting for validation.'), tracking_value_ids=tracking_value_ids)
-                move_ids += self.env['account.move'].search([('asset_id', '=', asset.id), ('state', '=', 'draft')]).ids
+            # Create a new depr. line with the residual amount and post it
+            asset_sequence = len(asset.depreciation_move_ids) - len(unposted_depreciation_move_ids) + 1
+
+            initial_amount = asset.original_value
+            initial_account = asset.original_move_line_ids.account_id if len(asset.original_move_line_ids.account_id) == 1 else asset.account_asset_id
+            depreciation_moves = asset.depreciation_move_ids.filtered(lambda r: r.state == 'posted' and not (r.reversal_move_id and r.reversal_move_id[0].state == 'posted'))
+            depreciated_amount = copysign(sum(depreciation_moves.mapped('amount_total')), -initial_amount)
+            depreciation_account = asset.account_depreciation_id
+            invoice_amount = copysign(invoice_line_id.price_subtotal, -initial_amount)
+            invoice_account = invoice_line_id.account_id
+            difference = -initial_amount - depreciated_amount - invoice_amount
+            difference_account = asset.company_id.gain_account_id if difference > 0 else asset.company_id.loss_account_id
+            line_datas = [(initial_amount, initial_account), (depreciated_amount, depreciation_account), (invoice_amount, invoice_account), (difference, difference_account)]
+            if not invoice_line_id:
+                del line_datas[2]
+            vals = {
+                'amount_total': current_currency._convert(asset.value_residual, company_currency, asset.company_id, disposal_date),
+                'asset_id': asset.id,
+                'ref': asset.name + ': ' + (_('Disposal') if not invoice_line_id else _('Sale')),
+                'asset_remaining_value': 0,
+                'asset_depreciated_value': max(asset.depreciation_move_ids.filtered(lambda x: x.state == 'posted'), key=lambda x: x.date, default=self.env['account.move']).asset_depreciated_value,
+                'date': disposal_date,
+                'journal_id': asset.journal_id.id,
+                'line_ids': [get_line(asset, amount, account) for amount, account in line_datas if account],
+            }
+            commands.append((0, 0, vals))
+            asset.write({'depreciation_move_ids': commands, 'method_number': asset_sequence})
+            tracked_fields = self.env['account.asset'].fields_get(['method_number'])
+            changes, tracking_value_ids = asset._mail_track(tracked_fields, old_values)
+            if changes:
+                asset.message_post(body=_('Asset sold or disposed. Accounting entry awaiting for validation.'), tracking_value_ids=tracking_value_ids)
+            move_ids += self.env['account.move'].search([('asset_id', '=', asset.id), ('state', '=', 'draft')]).ids
 
         return move_ids
 

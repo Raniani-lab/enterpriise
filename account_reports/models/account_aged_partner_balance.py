c4d76d9002ad1cdf1b4c2f2b59b4ede4970f6523
Paul Morelle <pmo@odoo.com>
2021-03-09 14:30:06 +0000

    [FIX] account_reports: limit aged receivable lines
    Exclude empty lines from the results of the Aged Receivable report
    query. Including them could lead to MemoryError if the amount of lines
    is too consequent, when Unfold All is pressed.
    
    OPW-2467527
    
    closes odoo/enterprise#17103
    
    X-original-commit: 5277e13f19a3a4dfcb643da49469763b54eb6601
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_reports/models/account_aged_partner_balance.py
+++ account_reports/models/account_aged_partner_balance.py
@@ -144,6 +144,7 @@ class ReportAccountAgedPartner(models.AbstractModel):
             WHERE account.internal_type = %(account_type)s
             GROUP BY account_move_line.id, partner.id, trust_property.id, journal.id, move.id, account.id,
                      period_table.period_index, currency_table.rate, currency_table.precision
+            HAVING ROUND(account_move_line.balance - COALESCE(SUM(part_debit.amount), 0) + COALESCE(SUM(part_credit.amount), 0), currency_table.precision) != 0
         """).format(
             move_line_fields=self._get_move_line_fields('account_move_line'),
             currency_table=self.env['res.currency']._get_query_currency_table(options),

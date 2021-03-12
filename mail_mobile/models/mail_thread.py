d1dc751dd0926906eadf5cdbea3e0806cfdab797
Martin Trigaux <mat@odoo.com>
2021-03-12 14:24:28 +0000

    [FIX] mail_mobile: do not crash on approval of message
    When approving a message in the moderation queue, the notification
    crashes in mail_mobile due to msg_value being False
    
      msg_type = msg_vals.get('message_type') or msg_sudo.message_type
      AttributeError: 'bool' object has no attribute 'get'
    
    This is because the method _moderate_accept from mail module calls
    _notify_thread without any value in msg_value
    
    _notify_record_by_ocn from mail_mobile assumes msg_value is a dict
    while False is a possible value
    
    Introduced at 201ad00df4bb868d7ea8e2ea238
    
    closes odoo/enterprise#17041
    
    X-original-commit: 64310590a0f86994ecb29fbef46ae90eb2e3b7e3
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- mail_mobile/models/mail_thread.py
+++ mail_mobile/models/mail_thread.py
@@ -32,7 +32,7 @@ class MailThread(models.AbstractModel):
         and every direct message. We have to take into account the risk of
         duplicated notifications in case of a mention in a channel of `chat` type.
         """
-
+        msg_vals = dict(msg_vals or {})
         icp_sudo = self.env['ir.config_parameter'].sudo()
         # Avoid to send notification if this feature is disabled or if no user use the mobile app.
         if not icp_sudo.get_param('odoo_ocn.project_id') or not icp_sudo.get_param('mail_mobile.enable_ocn'):

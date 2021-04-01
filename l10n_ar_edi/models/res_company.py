77187e357908e74a9b628c5703dde2b8c78e6b01
Thomas Beckers <tbs@odoo.com>
2021-04-01 14:26:57 +0000

    [FIX] l10n_ar_edi:Set the context to return the datas when getting the AFIP private key and certificate
    Getting AFIP private key and the certificate can be called with a context containing
    "bin_size" at True which will return the size of the attachment and not his datas
    (eg. try to open a debit note that call a "_compute_highest_name") The system will
    try to decode the size returned as if it was the datas and result to an error.
    
    So bin_size=False is added to the context to always get the datas needed.
    
    opw-2453951
    
    closes odoo/enterprise#17486
    
    X-original-commit: 472fad2b58303ac5ac9252117ada095872d2cbbe
    Signed-off-by: Josse Colpaert <jco@openerp.com>

--- l10n_ar_edi/models/res_company.py
+++ l10n_ar_edi/models/res_company.py
@@ -151,8 +151,8 @@ class ResCompany(models.Model):
     def _get_key_and_certificate(self):
         """ Return the pkey and certificate string representations in order to be used. Also raise exception if any key or certificate is not defined """
         self.ensure_one()
-        pkey = base64.decodebytes(self.l10n_ar_afip_ws_key) if self.l10n_ar_afip_ws_key else ''
-        cert = base64.decodebytes(self.l10n_ar_afip_ws_crt) if self.l10n_ar_afip_ws_crt else ''
+        pkey = base64.decodebytes(self.with_context(bin_size=False).l10n_ar_afip_ws_key) if self.l10n_ar_afip_ws_key else ''
+        cert = base64.decodebytes(self.with_context(bin_size=False).l10n_ar_afip_ws_crt) if self.l10n_ar_afip_ws_crt else ''
         res = (pkey, cert)
         if not all(res):
             error = '\n * ' + _(' Missing private key.') if not pkey else ''

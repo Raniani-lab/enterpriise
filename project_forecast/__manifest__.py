ff45a1d5a3068e4f762cd3ebd4ad7e2c244beb7b
Xavier BOL (xbo) <xbo@odoo.com>
2021-04-08 17:13:31 +0200

    [REV] project_forecast: remove the 'allocated time percentage' feature
    task-2463722
    
    Upgrade PR: odoo/upgrade#2426
    
    closes #17958

--- project_forecast/__manifest__.py
+++ project_forecast/__manifest__.py
@@ -10,13 +10,11 @@
     'version': '1.0',
     'depends': ['project', 'planning', 'web_grid'],
     'data': [
-        'security/forecast_security.xml',
         'views/planning_template_views.xml',
         'views/planning_templates.xml',
         'views/planning_views.xml',
         'views/project_forecast_views.xml',
         'views/project_views.xml',
-        'views/res_config_settings_views.xml',
     ],
     'application': False,
     'license': 'OEEL-1',

ba0388b8c638e89b969f80070b5c01c0af6ee87e
Yannick Tivisse <yti@odoo.com>
2021-04-15 17:20:48 +0200

    [FIX] l10n_be_hr_payroll: Store and fix 281.10 declaration
    Purpose
    =======
    
    - Improve the traceability
    - Remove the document posting in the document application, this
      is not supposed to be available from the employee portal, and
      it's easier to find the document on the declaration itself.
    - Compute 1 xml for all the employees instead of 1 xml for each employee
    - Update the xml/pdf file up to 2020
    - Validate the xml file with the xsd file
    - Move all the logic on the wizard instead of defining everything on the
      employee model.
    - Use a template to generate the XML file instead of bidouilling it
      with etree.

--- documents_l10n_be_hr_payroll/models/__init__.py
+++ documents_l10n_be_hr_payroll/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import models
-from . import wizard
+from . import l10n_be_281_10
+from . import l10n_be_281_45
--- documents_l10n_be_hr_payroll/wizard/__init__.py
+++ documents_l10n_be_hr_payroll/wizard/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import hr_payroll_281_base_wizard
 from . import l10n_be_social_balance_sheet
 from . import l10n_be_social_security_certificate
-from . import l10n_be_hr_payroll_273S_wizard
-from . import l10n_be_hr_payroll_274_XX_wizard

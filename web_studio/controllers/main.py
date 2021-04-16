b50157a24755c2e830ccfdc192afe3a823d90a2e
Nicolas Lempereur <nle@odoo.com>
2021-04-16 17:40:27 +0000

    [FIX] web_studio: can add chatter without activity_ids
    When adding a chatter in studio on a base model, if the model does not
    extends mail.activity.mixin this fails because it try to add
    activity_ids in the view.
    
    With this changeset, we only try to add activity_ids if it is available.
    
    opw-2508112
    
    closes odoo/enterprise#19323
    
    X-original-commit: 460fa4d09a3d69bc7efb2018090da2ffaf8af2fb
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- web_studio/controllers/main.py
+++ web_studio/controllers/main.py
@@ -1129,10 +1129,11 @@ Are you sure you want to remove the selection values of those records?""") % len
         })
         chatter_node = etree.Element('div', {'class': 'oe_chatter'})
         follower_node = etree.Element('field', {'name': 'message_follower_ids'})
-        activity_node = etree.Element('field', {'name': 'activity_ids'})
-        thread_node = etree.Element('field', {'name': 'message_ids'})
         chatter_node.append(follower_node)
-        chatter_node.append(activity_node)
+        if model.is_mail_activity:
+            activity_node = etree.Element('field', {'name': 'activity_ids'})
+            chatter_node.append(activity_node)
+        thread_node = etree.Element('field', {'name': 'message_ids'})
         chatter_node.append(thread_node)
         xpath_node.append(chatter_node)
 

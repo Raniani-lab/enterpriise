6882b13582aad8185def406d245d49f67396d328
lejeune quentin <qle@odoo.com>
2021-04-29 13:07:11 +0000

    [FIX] iot: Fix port 9000 used by Ingenico in IoT
    Now we try to open a socket every 3 secondes
    It generate some issue for connection of Ingenico
    Terminal.
    
    Now we open a socket in the initialization of the socket
    interface and listen all devices that send request to
    Iot.
    
    opw-2432864
    
    X-original-commit: 9d500f9c19dd541f809d6f30d3c47e823a990d92

--- iot/iot_handlers/interfaces/SocketInterface.py
+++ iot/iot_handlers/interfaces/SocketInterface.py
@@ -11,15 +11,21 @@ socket_devices = {}
 class SocketInterface(Interface):
     connection_type = 'socket'
 
+    def __init__(self):
+        super().__init__()
+        self.open_socket(9000)
+
+    def open_socket(self, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(('', port))
+        self.sock.listen()
+
     def get_devices(self):
         try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(('', 9000))
-            sock.listen(1)
-            dev, addr = sock.accept()
+            dev, addr = self.sock.accept()
             if addr and addr[0] not in socket_devices:
                 socket_devices[addr[0]] = type('', (), {'dev': dev})
             return socket_devices
-        except OSError as e:
-            _logger.error('Error in SocketInterface: %s', e.strerror)
+        except OSError:
+            pass

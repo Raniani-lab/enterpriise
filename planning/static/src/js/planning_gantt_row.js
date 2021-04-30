71dca4c887fce612a8ea0f49bc1dfe386f388eaa
Thibault Libioulle <tle@odoo.com>
2021-04-30 08:33:49 +0000

    [FIX] planning: format allocated hours in popover
    Previously, the allocated hours was formatted with Math.round for the
    minutes, so if a slot had a duration of 7:59:31, the result would be
    7:60 which is not an hour.
    
    Reminder : In gantt popover we are not able to use widgets so we have to
    format the values before the qweb rendering.
    
    This commit uses the float_time formatter to format allocated_hours and
    then pass it to renderer.
    
    PR : #15921
    
    task-2215216
    
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>

--- planning/static/src/js/planning_gantt_row.js
+++ planning/static/src/js/planning_gantt_row.js
@@ -1,10 +1,21 @@
-odoo.define('planning.PlanningGanttRow', function (require) {
-    'use strict';
-    const HrGanttRow = require('hr_gantt.GanttRow');
+/** @odoo-module alias=planning.PlanningGanttRow **/
 
-    const PlanningGanttRow = HrGanttRow.extend({
-        template: 'PlanningGanttView.Row'
-    });
+import HrGanttRow from 'hr_gantt.GanttRow';
+import fieldUtils from 'web.field_utils';
 
-    return PlanningGanttRow;
+const PlanningGanttRow = HrGanttRow.extend({
+    template: 'PlanningGanttView.Row',
+    /**
+     * Add allocated hours formatted to context
+     *
+     * @private
+     * @override
+     */
+    _getPopoverContext: function () {
+        const data = this._super.apply(this, arguments);
+        data.allocatedHoursFormatted = fieldUtils.format.float_time(data.allocated_hours);
+        return data;
+    },
 });
+
+export default PlanningGanttRow;

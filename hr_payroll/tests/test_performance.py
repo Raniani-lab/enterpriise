cbdce550883d88e4f3bffaabca1321e5add6361a
Thibault Delavallée <tde@odoo.com>
2021-05-06 14:16:09 +0000

    [IMP] various: update query counters
    Hr Payroll: seems somehow we gained 1 or 2 queries
    
    Test Mail Enterprise: seems SMS was optimized
    
    closes odoo/enterprise#18194
    
    X-original-commit: ca9cb4e84ddc8f81b5b6dd5556b353cb52d9ab69
    Related: odoo/odoo#70504
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- hr_payroll/tests/test_performance.py
+++ hr_payroll/tests/test_performance.py
@@ -35,7 +35,7 @@ class TestPayrollPerformance(TestPayslipBase):
     @warmup
     def test_performance_work_entry_generation(self):
         """ Work entry generation """
-        with self.assertQueryCount(__system__=23, admin=25):
+        with self.assertQueryCount(__system__=22, admin=24):
             self.employees.generate_work_entries(date(2018, 1, 1), date(2018, 1, 2))
         self.reset_work_entries()
 
@@ -55,7 +55,7 @@ class TestPayrollPerformance(TestPayslipBase):
         work_entry = self.create_work_entry(datetime(2018, 1, 1, 3, 0), datetime(2018, 1, 1, 4, 0))
         self.create_work_entry(datetime(2018, 1, 1, 11, 0), datetime(2018, 1, 1, 17, 0))
 
-        with self.assertQueryCount(__system__=7, admin=9):
+        with self.assertQueryCount(__system__=7, admin=8):
             work_entry.write({'date_stop': datetime(2018, 1, 1, 13, 0)})
 
     @users('__system__', 'admin')
@@ -65,7 +65,7 @@ class TestPayrollPerformance(TestPayslipBase):
         work_entry_2 = self.create_work_entry(datetime(2018, 1, 1, 7, 0), datetime(2018, 1, 1, 11, 0))
         self.create_work_entry(datetime(2018, 1, 1, 11, 0), datetime(2018, 1, 1, 17, 0))
 
-        with self.assertQueryCount(__system__=8, admin=10):
+        with self.assertQueryCount(__system__=8, admin=9):
             (work_entry_1 | work_entry_2).write({'date_stop': datetime(2018, 1, 1, 13, 0)})
 
     @users('__system__', 'admin')

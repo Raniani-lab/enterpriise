Raniani-lab/enterpriise

commit 75fab554980146c2c0c8681b3ddd69d029106a91

[IMP] account_invoice_extract: use webhooks to update the requests
Previously, we were only using a cron that ran every 30 minutes to
automatically update the extraction requests.

The client will now send a webhook URL as part of the request that
the extraction server will call once the results are ready.

The cron still remain with a much lower frequency. It acts as a
backup in case something went wrong during the webhook call.

Task: 2416741

Related server-side task: 2452019

closes odoo/enterprise#15663

Signed-off-by: Florian Daloze (fda) <fda@odoo.com>

account_invoice_extract/models/account_invoice.py
@@ -178,6 +178,7 @@ class AccountMove(models.Model):
 
     def retry_ocr(self):
         """Retry to contact iap to submit the first attachment in the chatter"""
+        self.ensure_one()
         if not self.company_id.extract_show_ocr_option_selection or self.company_id.extract_show_ocr_option_selection == 'no_send':
             return False
         attachments = self.message_main_attachment_id
@@ -192,12 +193,15 @@ class AccountMove(models.Model):
             }
             #this line contact iap to create account if this is the first request. This allow iap to give free credits if the database is elligible
             self.env['iap.account'].get_credits('invoice_ocr')
+            baseurl = self.env['ir.config_parameter'].get_param('web.base.url')
+            webhook_url = f"{baseurl}/account_invoice_extract/request_done"
             params = {
                 'account_token': account_token.account_token,
                 'dbuuid': self.env['ir.config_parameter'].sudo().get_param('database.uuid'),
                 'documents': [x.datas.decode('utf-8') for x in attachments],
                 'file_names': [x.name for x in attachments],
                 'user_infos': user_infos,
+                'webhook_url': webhook_url,
             }
             try:
                 result = self._contact_iap_extract('/iap/invoice_extract/parse', params)

account_invoice_extract/controllers/__init__.py
@@ -1,4 +1,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import controllers
-from . import models
+from . import main

account_invoice_extract/controllers/main.py
@@ -1,0 +1,16 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+
+
+class AccountInvoiceExtractController(http.Controller):
+    @http.route('/account_invoice_extract/request_done/<int:extract_remote_id>', type='http', auth='public', csrf=False)
+    def request_done(self, extract_remote_id):
+        """ This webhook is called when the extraction server is done processing a request."""
+        move_to_update = request.env['account.move'].sudo().search([('extract_remote_id', '=', extract_remote_id),
+                                                                    ('extract_state', 'in', ['waiting_extraction', 'extract_not_ready']),
+                                                                    ('state', '=', 'draft')])
+        for move in move_to_update:
+            move._check_status()
+        return 'OK'

account_invoice_extract/data/update_status_cron.xml
@@ -6,8 +6,8 @@
             <field name='model_id' ref='model_account_move'/>
             <field name='state'>code</field>
             <field name='code'>model.check_all_status()</field>
-            <field name='interval_number'>30</field>
-            <field name='interval_type'>minutes</field>
+            <field name='interval_number'>1</field>
+            <field name='interval_type'>days</field>
             <field name='numbercall'>-1</field>
             <field name="doall" eval="False"/>
         </record>

commit 00736677d0c62f6221968dc4220b3ce065d767a9

[FIX] account_invoice_extract: fix ocr access to ir.config_parameter
Add a missing `sudo()` in `retry_ocr`. Bug introduced in 75fab554980.

closes odoo/enterprise#18425

X-original-commit: b4f2e2a2e2004f60a4b10b68a35c12f15ff7d5fe
Signed-off-by: William André (wan) <wan@odoo.com>

account_invoice_extract/models/account_invoice.py
@@ -197,7 +197,7 @@ class AccountMove(models.Model):
             user_infos = self.get_user_infos()
             #this line contact iap to create account if this is the first request. This allow iap to give free credits if the database is elligible
             self.env['iap.account'].get_credits('invoice_ocr')
-            baseurl = self.env['ir.config_parameter'].get_param('web.base.url')
+            baseurl = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
             webhook_url = f"{baseurl}/account_invoice_extract/request_done"
             params = {
                 'account_token': account_token.account_token,
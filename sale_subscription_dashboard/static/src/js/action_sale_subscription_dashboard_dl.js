360433fa1e15ee4f47f27b804fe833c4f061a006
Samuel Degueldre <sad@odoo.com>
2021-05-31 06:54:46 +0200

    [FIX] sale_subscription_dashboard: adapt download action

--- sale_subscription_dashboard/static/src/js/action_sale_subscription_dashboard_dl.js
+++ sale_subscription_dashboard/static/src/js/action_sale_subscription_dashboard_dl.js
@@ -1,56 +1,18 @@
-odoo.define('sale_subscription_dashboard.ActionManager', function (require) {
-"use strict";
+/** @odoo-module */
+import { registry } from "@web/core/registry";
+import { download } from "@web/core/network/download";
 
-/**
- * The purpose of this file is to add the support of Odoo actions of type
- * 'ir_actions_sale_subscription_dashboard_download' to the ActionManager.
- */
+async function executeSubscriptionDashboardDownload({ env, action, options }) {
+    env.services.ui.block();
+    const url = "/salesman_subscription_reports";
+    const data = action.data;
+    try {
+        await download({ url, data });
+    } finally {
+        env.services.ui.unblock();
+    }
+}
 
-var ActionManager = require('web.ActionManager');
-var framework = require('web.framework');
-var session = require('web.session');
-
-ActionManager.include({
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Executes actions of type 'ir_actions_sale_subscription_dashboard_download'.
-     *
-     * @private
-     * @param {Object} action the description of the action to execute
-     * @returns {Promise} resolved when the report has been downloaded ;
-     *   rejected if an error occurred during the report generation
-     */
-    _executeSubscriptionDashboardDownloadAction: function (action) {
-        var self = this;
-        framework.blockUI();
-        return new Promise(function (resolve, reject) {
-            session.get_file({
-                url: '/salesman_subscription_reports',
-                data: action.data,
-                success: resolve,
-                error: (error) => {
-                    self.call('crash_manager', 'rpc_error', error);
-                    reject();
-                },
-                complete: framework.unblockUI,
-            });
-        });
-    },
-    /**
-     * Overrides to handle the 'ir_actions_sale_subscription_dashboard_download' actions.
-     *
-     * @override
-     * @private
-     */
-    _handleAction: function (action, options) {
-        if (action.type === 'ir_actions_sale_subscription_dashboard_download') {
-            return this._executeSubscriptionDashboardDownloadAction(action, options);
-        }
-        return this._super.apply(this, arguments);
-    },
-});
-
-});
+registry
+    .category("action_handlers")
+    .add("ir_actions_sale_subscription_dashboard_download", executeSubscriptionDashboardDownload);
--- sale_subscription_dashboard/static/tests/action_sale_subscription_dashboard_dl_test.js
+++ sale_subscription_dashboard/static/tests/action_sale_subscription_dashboard_dl_test.js
@@ -1,47 +1,42 @@
-odoo.define('sale_subscription_dashboard.sale_subscription_report', function (require) {
-"use strict";
+/** @odoo-module */
+import { mockDownload } from "@web/../tests/helpers/utils";
+import { createWebClient, getActionManagerTestConfig, doAction } from "@web/../tests/webclient/actions/helpers";
 
-var testUtils = require('web.test_utils');
-var createActionManager = testUtils.createActionManager;
-
-QUnit.module('Sale Subscription Dashboard Download Reports', {
+let testConfig;
+QUnit.module("Sale Subscription Dashboard Download Reports", {
     beforeEach: function () {
-        this.actions = [{
-            id: 1,
-            data: {
-                model: 'sale.subscription',
-                output_format: 'pdf',
-            },
-            type: 'ir_actions_sale_subscription_dashboard_download',
-        }];
+        testConfig = getActionManagerTestConfig();
     },
 }, function () {
-
-    QUnit.test('can execute sale subscription dashboard report download actions', async function (assert) {
-        assert.expect(4);
-
-        var actionManager = await createActionManager({
-            actions: this.actions,
+    QUnit.test("can execute sale subscription dashboard report download actions", async function (assert) {
+        assert.expect(5);
+        testConfig.serverData.actions[1] = {
+            id: 1,
+            data: {
+                model: "sale.subscription",
+                output_format: "pdf",
+            },
+            type: "ir_actions_sale_subscription_dashboard_download",
+        };
+        mockDownload((options) => {
+            assert.step(options.url);
+            assert.deepEqual(options.data, {
+                model: "sale.subscription",
+                output_format: "pdf",
+            }, "should give the correct data");
+            return Promise.resolve();
+        });
+        const webClient = await createWebClient({
+            testConfig,
             mockRPC: function (route, args) {
                 assert.step(args.method || route);
-                return this._super.apply(this, arguments);
-            },
-            session: {
-                get_file: function (params) {
-                    assert.step(params.url);
-                    assert.deepEqual(params.data, {
-                        model: 'sale.subscription',
-                        output_format: 'pdf',
-                    }, "should give the correct data");
-                    params.success();
-                    params.complete();
-                },
             },
         });
-        await actionManager.doAction(1);
-        assert.verifySteps(['/web/action/load', '/salesman_subscription_reports'],
-        'Error while trying to execute download action sale subscription dashboard report download.');
-        actionManager.destroy();
+        await doAction(webClient, 1);
+        assert.verifySteps([
+            "/web/webclient/load_menus",
+            "/web/action/load",
+            "/salesman_subscription_reports",
+        ]);
     });
-});
 });

aca6f29cfb2062d7857595bdbae00ad138fbd3b9
Aaron Bohy <aab@odoo.com>
2021-06-01 09:37:00 +0000

    [FIX] web_studio: prevent infinite loop if studio bundle crashes
    Before this commit, if there was a syntax error in the studio lazy
    loaded bundle, that bundle was loaded over and over until the browser
    died. The reason is that the studio client action is initially a
    function that loads the whole bundle. In that bundle, we override the
    client action in the registry with the real one. The initial function
    returns the same action, s.t. when it is done (when the bundle is
    loaded), we execute again the studio action (which is supposed to be
    the real action now). If there is a crash in the lazy loaded bundle,
    the action could not be overriden in the registry, meaning that when
    the action is executed again, it is still the function to lazy load
    the bundle, and so on.
    
    This commit ensures that we directly replace the action in the
    registry by another one, s.t. whatever happens, the bundles are
    only loaded once.

--- web_studio/static/src/client_action/studio_action_loader.js
+++ web_studio/static/src/client_action/studio_action_loader.js
@@ -1,17 +1,38 @@
 /** @odoo-module **/
 
+import { memoize } from "@web/core/utils/functions";
 import { registry } from "@web/core/registry";
 import { loadPublicAsset } from "@web/core/assets";
 
-async function loadStudioAction(env) {
-    // Some parts of the studio client action depend on the wysiwyg widgets, so load them first
+const actionRegistry = registry.category("actions");
+
+const loadStudioAction = memoize(async (env) => {
+    // some parts of the studio client action depend on the wysiwyg widgets, so load them first
     await loadPublicAsset("web_editor.compiled_assets_wysiwyg", env.services.orm);
     await loadPublicAsset("web_studio.compiled_assets_studio", env.services.orm);
+
+    if (actionRegistry.get("studio") === loadStudioAction) {
+        // At this point, the real studio client action should be loaded and have
+        // replaced this function in the action registry. If it's not the case,
+        // it probably means that there was a crash in the bundle (e.g. syntax
+        // error). In this case, this action will remain in the registry, which
+        // will lead to an infinite loop. To prevent that, we push another action
+        // in the registry.
+        actionRegistry.add(
+            "studio",
+            () => {
+                const msg = env._t("Studio couldn't be loaded");
+                env.services.notification.create(msg, { type: "danger" });
+            },
+            { force: true }
+        );
+    }
+
     return {
         target: "current",
         tag: "studio",
         type: "ir.actions.client",
     };
-}
+});
 
-registry.category("actions").add("studio", loadStudioAction);
+actionRegistry.add("studio", loadStudioAction);

157f1fa2348d1c0f9dd4db1ba9a0b4333988a4e3
Nicolas Lempereur <nle@odoo.com>
2021-06-07 14:19:55 +0000

    [FIX] l10n_mx_reports: typo "tal" instead of Total
    When adapting 302c7d67d that transforms `_("hello %s")` % "world to
    `_("hello %s", "world")` there was a small typo that would show eg.
    "tal 1 Activos" instead of "Total Activos".
    
    opw-2558644
    
    closes odoo/enterprise#18859
    
    X-original-commit: 16cdc7c40a7b7ec6d4bae8f23d8a3a3a02df4613
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>

--- l10n_mx_reports/models/trial_balance.py
+++ l10n_mx_reports/models/trial_balance.py
@@ -106,7 +106,7 @@ class MxReportAccountTrial(models.AbstractModel):
             if not options.get('coa_only'):
                 lines.append({
                     'id': self._get_generic_line_id(None, line.code, markup='total'),
-                    'name': _('Total %s', line.name)[2:],
+                    'name': _('Total %s', line.name[2:]),
                     'level': 0,
                     'class': 'hierarchy_total',
                     'columns': [{'name': self.format_value(v)} for v in total_line],

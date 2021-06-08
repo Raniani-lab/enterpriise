dd08030f5d859fc74efc11dba96227b1c1794920
Thibault Libioulle <tle@odoo.com>
2021-06-08 13:58:33 +0000

    [IMP] {project,sale_timesheet}_enterprise: improve mobile views
    Prior to this commit the project right side panel was shown in the
    bottom of the project update kanban/list view.
    
    With this the right panel is displayed at the top of the controller,
    with only section header displayed and section datas hidden.
    
    PR community : #71873
    
    task-2555323
    
    closes odoo/enterprise#18851
    
    Related: odoo/odoo#71873
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- sale_timesheet_enterprise/__manifest__.py
+++ sale_timesheet_enterprise/__manifest__.py
@@ -24,4 +24,12 @@
     ],
     'auto_install': True,
     'license': 'OEEL-1',
+    'assets': {
+        'web.assets_backend': [
+            'sale_timesheet_enterprise/static/src/js/**/*',
+        ],
+        'web.assets_qweb': [
+            'sale_timesheet_enterprise/static/src/xml/**/*',
+        ],
+    }
 }

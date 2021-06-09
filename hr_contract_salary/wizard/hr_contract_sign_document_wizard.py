fef1ba22667afbe305b863a268445735ad24361f
William Braeckman <wbr@odoo.com>
2021-06-09 10:12:39 +0000

    [IMP] hr_contract_sign: allow requesting multiple signatures
    Allows a hr officer to request multiple signatures at once for the same
    contract. Both employee only templates and employee+manager templates.
    
    Task ID: 2563227
    
    closes odoo/enterprise#18899
    
    Related: odoo/upgrade#2564
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_contract_salary/wizard/hr_contract_sign_document_wizard.py
+++ hr_contract_salary/wizard/hr_contract_sign_document_wizard.py
@@ -7,15 +7,15 @@ from odoo import api, fields, models
 class HrContractSignDocumentWizard(models.TransientModel):
     _inherit = 'hr.contract.sign.document.wizard'
 
-    sign_template_id = fields.Many2one(compute='_compute_sign_template_id', store=True, readonly=False)
+    sign_template_ids = fields.Many2many(compute='_compute_sign_template_ids', store=True, readonly=False)
 
     @api.depends('contract_id')
-    def _compute_sign_template_id(self):
+    def _compute_sign_template_ids(self):
         for wizard in self:
             contract = wizard.contract_id
             if not contract:
                 continue
             if contract.state == 'draft' and contract.sign_template_id:
-                wizard.sign_template_id = contract.sign_template_id
+                wizard.sign_template_ids |= contract.sign_template_id
             elif contract.state in ['open', 'close'] and contract.contract_update_template_id:
-                wizard.sign_template_id = contract.contract_update_template_id
+                wizard.sign_template_ids |= contract.contract_update_template_id

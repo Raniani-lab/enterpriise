75eb8724b38e3f0c2f0456214ea7b2ef93b5c1cf
Shahnawaz Idariya <sid@odoo.com>
2021-06-23 06:49:04 +0000

    [IMP] planning, project_forecast: generic improvements for the planning
    *- = project_timesheet_forecast
    
    Purpose of the commit is to do the generic improvements for
    planning app.
    
    So in this commit, done the following changes:
     - Changed the helper message in schedule and my planning menus from
       "Let's start your planning by adding a new shift." to
       "Shifts are periods allocated to your employees for a specific role.
        Together, they constitute your employees' planning."
     - In send planning by email form view:
       - Changed the arrow icon for Period field. And improve the arrow
         spacing between daterange widget.
       - add many2many_avatar_employee widget for the employee
     - In shift form view:
       - Improve the arrow spacing between daterange widget.
     - reduce the nb of days taken into account by the group_expand from
       30 days to 7 days.
    
    closes odoo/enterprise#19190
    
    Taskid: 2531446
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>

--- planning/models/planning.py
+++ planning/models/planning.py
@@ -920,12 +920,12 @@ class Planning(models.Model):
             if len(dom) == 3 and dom[0] == 'start_datetime' and dom[1] == '<=':
                 max_date = dom[2] if dom[2] else datetime.now()
                 max_date = max_date if isinstance(max_date, date) else datetime.strptime(max_date, '%Y-%m-%d %H:%M:%S')
-                max_date = max_date + timedelta(days=30)
+                max_date = max_date + timedelta(days=7)
                 filters.append((dom[0], dom[1], max_date))
             elif len(dom) == 3 and dom[0] == 'end_datetime' and dom[1] == '>=':
                 min_date = dom[2] if dom[2] else datetime.now()
                 min_date = min_date if isinstance(min_date, date) else datetime.strptime(min_date, '%Y-%m-%d %H:%M:%S')
-                min_date = min_date - timedelta(days=30)
+                min_date = min_date - timedelta(days=7)
                 filters.append((dom[0], dom[1], min_date))
             else:
                 filters.append(dom)

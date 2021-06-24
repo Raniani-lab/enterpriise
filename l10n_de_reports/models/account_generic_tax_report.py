91b3f236c1fc6c9d688f62d424d8a0af908c2f05
Touati Djamel (otd) <otd@odoo.com>
2021-06-24 15:20:25 +0000

    [FIX] l10n_de_reports: correction of the Elster tax report
    These corrections are made on the basis of the "ElsterBasis (EBA)" documentation,
    which can be found in this link: https://drive.google.com/file/d/1045jtYMpRtR30qXCjcgU--lpo0rpnCdw/view
    
    Changes made:
    1:/ Period must be with two digits: See documentation point 4.3.4.2
    2:/ The letter “K” in "Kz" must be uppercase
    3:/ see documentation 4.3.4.5.2:
        - "Kz09" and "Kz83" are the only ones that can be with a decimal digit and must be supplied with 0.00 if they don't have balance
        - "Kz39" must be a positive number only
        - All others "Kz" can be postive or negative, but without decimals
    
    opw-2560974
    
    closes odoo/enterprise#19621
    
    X-original-commit: 7f5b77ae2ef403bacf496fd0e0548e9fbdc6f16d
    Signed-off-by: Josse Colpaert <jco@openerp.com>
    Signed-off-by: Djamel Touati <DjamelTouati@users.noreply.github.com>

--- l10n_de_reports/models/account_generic_tax_report.py
+++ l10n_de_reports/models/account_generic_tax_report.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, _
+from odoo.tools import float_repr
 from lxml import etree
 from datetime import date, datetime
 
@@ -23,7 +24,7 @@ class AccountGenericTaxReport(models.AbstractModel):
         date_to = datetime.strptime(options['date']['date_to'], '%Y-%m-%d')
         template_context['year'] = date_to.year
         if options['date']['period_type'] == 'month':
-            template_context['period'] = date_to.month
+            template_context['period'] = date_to.strftime("%m")
         elif options['date']['period_type'] == 'quarter':
             month_end = int(date_to.month)
             if month_end % 3 != 0:
@@ -41,9 +42,21 @@ class AccountGenericTaxReport(models.AbstractModel):
         taxes = tree.xpath('//Umsatzsteuervoranmeldung')[0]
         # Add the values dynamically. We do it here because the tag is generated from the code and
         # Qweb doesn't allow dynamically generated tags.
+        elem = etree.SubElement(taxes, "Kz09")
+        elem.text = "0.00" #please keep "0.00" until Odoo has "Kz09"
         for line in report_lines:
-            if line['line_code'] and line['columns'][0]['balance']:
-                elem = etree.SubElement(taxes, "kz" + line['line_code'])
-                elem.text = str(line['columns'][0]['balance'])
+            if line['line_code']:
+                #all "Kz" may be supplied as negative, except "Kz39"
+                if line['columns'][0]['balance'] and (line['line_code'] != "39" or line['columns'][0]['balance'] > 0):
+                    elem = etree.SubElement(taxes, "Kz" + line['line_code'])
+                    #only "kz09" and "kz83" can be supplied with decimals
+                    if line['line_code'] in ("09", "83"):
+                        elem.text = float_repr(line['columns'][0]['balance'], self.env.company.currency_id.decimal_places)
+                    else:
+                        elem.text = float_repr(int(line['columns'][0]['balance']), 0)
+                #"Kz09" and "kz83" must be supplied with 0.00 if they don't have balance
+                elif line['line_code'] in ["09", "83"]:
+                    elem = etree.SubElement(taxes, "Kz" + line['line_code'])
+                    elem.text = "0.00"
 
         return etree.tostring(tree, pretty_print=True, standalone=False, encoding='ISO-8859-1',)

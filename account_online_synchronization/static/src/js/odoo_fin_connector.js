b4aa0439b7ed72445fbbb8df0f10e0a652826ac4
Florian Gilbert <flg@odoo.com>
2021-06-24 12:59:43 +0000

    [FIX] online_synchronization: Adapt the javascript code
    closes odoo/enterprise#19448
    
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

--- account_online_synchronization/static/src/js/odoo_fin_connector.js
+++ account_online_synchronization/static/src/js/odoo_fin_connector.js
@@ -1,71 +1,76 @@
-/* global OdooFin */
-odoo.define('account_online_synchronization.odoo_fin_connector', function(require) {
+/** @odoo-module alias=account_online_synchronization.odoo_fin_connector **/
 "use strict";
 
-    var core = require('web.core');
-    var ajax = require('web.ajax');
+import { registry } from "@web/core/registry";
 
-    function OdooFinConnector (parent, action) {
-        const id = action.id;
-        let mode = action.params.mode || 'link';
-        // Ensure that the proxyMode is valid
-        const modeRegexp = /^[a-z0-9-_]+$/i;
-        if (!modeRegexp.test(action.params.proxyMode)) { return; }
-        let url = 'https://' + action.params.proxyMode + '.odoofin.com/proxy/v1/odoofin_link';
-        
-        ajax.loadJS(url)
+const actionRegistry = registry.category('actions');
+const { loadJS } = owl.utils;
+/* global OdooFin */
+
+function OdooFinConnector(parent, action) {
+    const id = action.id;
+    let mode = action.params.mode || 'link';
+    // Ensure that the proxyMode is valid
+    const modeRegexp = /^[a-z0-9-_]+$/i;
+    if (!modeRegexp.test(action.params.proxyMode)) {
+        return;
+    }
+    let url = 'https://' + action.params.proxyMode + '.odoofin.com/proxy/v1/odoofin_link';
+
+    loadJS(url)
         .then(function () {
             // Create and open the iframe
             let params = {
-                data: action.params, 
-                proxyMode: action.params.proxyMode, 
-                onEvent: function(event, data){
-                    switch(event) {
+                data: action.params,
+                proxyMode: action.params.proxyMode,
+                onEvent: function (event, data) {
+                    let rpcUrl = '/web/dataset/call_kw/account.online.link'
+                    switch (event) {
                         case 'close':
                             return;
                         case 'reload':
-                            return parent.do_action({type: 'ir.actions.client', tag: 'reload'});
+                            return parent.services.action.doAction({type: 'ir.actions.client', tag: 'reload'});
                         case 'notification':
-                            parent.displayNotification(data);
+                            parent.services.notification.add(data.message, data);
                             break;
                         case 'exchange_token':
-                            parent._rpc({
+                            parent.services.rpc(rpcUrl + '/exchange_token', {
                                 model: 'account.online.link',
                                 method: 'exchange_token',
-                                args: [[id], data]
+                                args: [[id], data],
+                                kwargs: {}
                             })
                             break;
                         case 'success':
                             mode = data.mode || mode;
-                            return parent._rpc({
+                            return parent.services.rpc(rpcUrl + '/success', {
                                 model: 'account.online.link',
                                 method: 'success',
-                                args: [[id], mode, data]
+                                args: [[id], mode, data],
+                                kwargs: {}
                             })
-                            .then(action => parent.do_action(action));
+                            .then(action => parent.services.action.doAction(action));
                         default:
                             return;
                     }
                 },
-                onAddBank: function() {
-                    // If the user doesn't find his bank.
-                    return parent._rpc({
+                onAddBank: function () {
+                    // If the user doesn't find his bank
+                    return parent.services.rpc("/web/dataset/call_kw/account.online.link/create_new_bank_account_action", {
                         model: 'account.online.link',
                         method: 'create_new_bank_account_action',
+                        args: [],
+                        kwargs: {}
                     })
-                    .then(function(action) {
-                        return parent.do_action(action, {replace_last_action: true});
-                    });
+                    .then(action => parent.services.action.doAction(action, {replace_last_action: true}));
                 }
             }
             OdooFin.create(params);
             OdooFin.open();
-            // This is needed in case iframe is opened from a modal, close the modal when opening the iframe
-            if (parent.currentDialogController) {
-                parent._closeDialog();
-            }
         });
-        return;
-    }
-    core.action_registry.add('odoo_fin_connector', OdooFinConnector);
-});
+    return;
+}
+
+actionRegistry.add('odoo_fin_connector', OdooFinConnector);
+
+export default OdooFinConnector;

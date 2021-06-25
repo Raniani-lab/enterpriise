0511e5588efe709a7e4e7e9568934a2970dad673
Yannick Tivisse <yti@odoo.com>
2021-06-25 12:26:20 +0000

    [MOV] hr_payroll: Split files according to guidelines
    closes odoo/enterprise#19124
    
    Related: odoo/odoo#72402
    Related: odoo/upgrade#2583
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- hr_payroll/report/__init__.py
+++ hr_payroll/report/__init__.py
@@ -5,3 +5,4 @@ from . import hr_payroll_report
 from . import hr_work_entry_report
 from . import hr_contract_history
 from . import hr_salary_attachment_report
+from . import hr_payroll_contribution_register

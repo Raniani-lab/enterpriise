50eb0f3adb6d8ead14e0090060ddd14106c66227
Nasreddin Boulif (bon) <bon@odoo.com>
2021-07-12 12:50:34 +0000

    [FIX] documents_*: use company id in domain instead of model
    Impacted modules: documents_hr, documents_hr_payroll,
    documents_hr_recruitment, documents_product, documents_project
    
    Steps to reproduce:
    
      - Have a standard V14 with documents_account (not tested in other versions)
      - Create a new company
      - Go to Documents / Configuration / Workspaces
      - Set a company on each workspace
      - Go to Settings / Documents
      - Check "Products"
      - Click on the "Workspaces" dropdown
    
    Issue:
    
      You can be on any company, only the workspace without
      any company set will be shown.
      The ones from the current company are hidden.
    
    Cause:
    
      Using company model instead of id in domain.
    
    Updating all similar domain with issue.
    
    similar fix : https://github.com/odoo/enterprise/pull/16484
    
    opw-2588315
    
    closes odoo/enterprise#19624
    
    X-original-commit: a22aef86472c36c03d09ef5a3e66b0b5fb8e28ff
    Signed-off-by: Nicolas Lempereur (nle) <nle@odoo.com>
    Signed-off-by: bon-odoo <nboulif@users.noreply.github.com>

--- documents_hr_payroll/models/res_company.py
+++ documents_hr_payroll/models/res_company.py
@@ -9,7 +9,7 @@ class ResCompany(models.Model):
 
     def _domain_company(self):
         company = self.env.company
-        return ['|', ('company_id', '=', False), ('company_id', '=', company)]
+        return ['|', ('company_id', '=', False), ('company_id', '=', company.id)]
 
     documents_hr_payslips_tags = fields.Many2many('documents.tag', 'payslip_tags_table')
     documents_payroll_folder_id = fields.Many2one('documents.folder', domain=_domain_company,

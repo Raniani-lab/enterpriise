9e60263b15b00eca8a3b91597f531c6f5c9cd0c5
wan <wan@odoo.com>
2021-07-22 08:41:12 +0000

    [FIX] account: fix domain for analytic fields during bank reconcilition
    closes odoo/enterprise#19810
    
    Forward-port-of: https://github.com/odoo/odoo/pull/73405
    X-original-commit: 3319200ab142849a1115a5f4290d4e8951d7920e
    Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_accountant/static/src/js/reconciliation/reconciliation_renderer.js
+++ account_accountant/static/src/js/reconciliation/reconciliation_renderer.js
@@ -522,10 +522,12 @@ var LineRenderer = Widget.extend(FieldManagerMixin, {
             relation: 'account.analytic.account',
             type: 'many2one',
             name: 'analytic_account_id',
+            domain: ["|", ['company_id', '=', state.st_line.company_id], ['company_id', '=', false]],
         }, {
             relation: 'account.analytic.tag',
             type: 'many2many',
             name: 'analytic_tag_ids',
+            domain: ["|", ['company_id', '=', state.st_line.company_id], ['company_id', '=', false]],
         }, {
             type: 'boolean',
             name: 'force_tax_included',

acdf88627606048fddbd22e9e6adc237f3189b94
Prakash Prajapati <ppr@odoo.com>
2021-08-04 05:41:06 +0000

    [IMP] helpdesk_account, (_*): improve for the helpdesk return and refund
    _* = helpdesk_stock, helpdesk_stock_account
    
    Generic UX improvements to ease the creation of returns and refunds
    from tickets.
    
    Purpose of helpdesk_stock_account module-
       If the product is set on the ticket then the last sale order of that product is
       to be set in the invoice refund wizard.
    
    Ticket:
         -If there is a customer set on the ticket then the product of his
          confirmed sale order or outgoing picking will appear. Otherwise all.
    Refund and Return:
         - sale_order_id field should be editable.
         - restricted to the SO from the partner set on the ticket else, all sale_order_id
          should be visible.
    Return:
        - the selectable picking_id should be restricted to the ones containing the
          product_id set on the ticket else, all picking_id should be visible .
    Refund:
         - the Invoice to Refund  (move_ids field )should be restricted to the product
           selected on the ticket.
    
    closes odoo/enterprise#20230
    task-2496333
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- helpdesk_stock/wizard/stock_picking_return.py
+++ helpdesk_stock/wizard/stock_picking_return.py
@@ -9,11 +9,32 @@ class ReturnPicking(models.TransientModel):
 
     partner_id = fields.Many2one('res.partner', related="ticket_id.partner_id", string="Customer")
     ticket_id = fields.Many2one('helpdesk.ticket')
-    sale_order_id = fields.Many2one('sale.order', related="ticket_id.sale_order_id", string='Sales Order')
-    picking_id = fields.Many2one(domain="[('id', 'in', suitable_picking_ids)]")
+    sale_order_id = fields.Many2one('sale.order', string='Sales Order',
+        domain="[('order_line.product_id.type', '!=', 'service'), ('picking_ids.state', '=', 'done'), ('id', 'in', suitable_sale_order_ids)]",
+        compute='_compute_sale_order_id', readonly=False)
+    picking_id = fields.Many2one(domain="[('id', 'in', suitable_picking_ids)]", compute='_compute_picking_id', readonly=False, store=True)
     suitable_picking_ids = fields.Many2many('stock.picking', compute='_compute_suitable_picking_ids')
+    suitable_sale_order_ids = fields.Many2many('sale.order', compute='_compute_suitable_sale_orders')
 
-    @api.depends('ticket_id.sale_order_id.picking_ids', 'ticket_id.partner_id.commercial_partner_id')
+    @api.depends('picking_id')
+    def _compute_sale_order_id(self):
+        for r in self:
+            r.sale_order_id = r.picking_id.sale_id
+
+    @api.depends('sale_order_id')
+    def _compute_picking_id(self):
+        for r in self:
+            if not r.picking_id:
+                domain = [('state', '=', 'done'), ('partner_id', '=', r.ticket_id.partner_id.id)]
+                if r.ticket_id.product_id:
+                    domain += [('move_line_ids.product_id', '=', r.ticket_id.product_id.id)]
+                picking = self.env['stock.picking'].search(domain, limit=1, order='id desc')
+                if picking:
+                    r.picking_id = picking
+            if r.sale_order_id:
+                r.picking_id = r.sale_order_id.picking_ids[0] if r.sale_order_id.picking_ids else False
+
+    @api.depends('ticket_id.partner_id.commercial_partner_id', 'sale_order_id')
     def _compute_suitable_picking_ids(self):
         for r in self:
             domain = [('state', '=', 'done')]
@@ -21,7 +42,19 @@ class ReturnPicking(models.TransientModel):
                 domain += [('id', 'in', r.sale_order_id.picking_ids._origin.ids)]
             elif r.partner_id:
                 domain += [('partner_id', 'child_of', r.partner_id.commercial_partner_id._origin.id)]
+            if r.ticket_id.product_id:
+                domain += [('move_line_ids.product_id', '=', r.ticket_id.product_id._origin.id)]
             r.suitable_picking_ids = self.env['stock.picking'].search(domain)
+
+    @api.depends('ticket_id.partner_id.commercial_partner_id')
+    def _compute_suitable_sale_orders(self):
+        for r in self:
+            domain = [('state', '=', 'sale')]
+            if r.ticket_id.product_id:
+                domain += [('order_line.product_id', '=', r.ticket_id.product_id._origin.id)]
+            if r.ticket_id.partner_id:
+                domain += [('partner_id', 'child_of', r.ticket_id.partner_id.commercial_partner_id.id)]
+            r.suitable_sale_order_ids = self.env['sale.order'].search(domain)
 
     def create_returns(self):
         res = super(ReturnPicking, self).create_returns()

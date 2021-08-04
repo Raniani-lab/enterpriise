8f33a7c5a700e906a4047d55754ef918e42872cb
Kamlesh Pathekar <kpt@odoo.com>
2021-08-04 06:43:38 +0000

    [IMP] planning, project_forecast: disable the sample data on planning
    Disable sample data when using the "View planning" button on the employee tree view,
    since we sometimes want to display an empty table to create the planning.
    
    closes odoo/enterprise#20204
    
    Taskid: 2608855
    Related: odoo/upgrade#2738
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- planning/tests/test_hr.py
+++ planning/tests/test_hr.py
@@ -34,3 +34,16 @@ class TestPlanningHr(TestCommonPlanning):
         self.employee_joseph.default_planning_role_id = role_b
         self.assertTrue(role_a in self.employee_joseph.planning_role_ids, "role a should still be in planning roles")
         self.assertTrue(role_b in self.employee_joseph.planning_role_ids, "role b should be added to planning roles")
+
+    def test_hr_employee_view_planning(self):
+        self.env['planning.slot'].create({
+            'employee_id': self.employee_bert.id,
+            'start_datetime': datetime(2021, 6, 4, 8, 0),
+            'end_datetime': datetime(2021, 6, 5, 17, 0),
+        }).copy()
+        action = self.employee_bert.action_view_planning()
+        # just returns action
+        slots = self.env['planning.slot'].search(action['domain'])
+        self.assertEqual(action['res_model'], 'planning.slot')
+        self.assertEqual(len(slots), 2, 'Bert has 2 planning slots')
+        self.assertEqual(action['context']['default_employee_id'], self.employee_bert.id)

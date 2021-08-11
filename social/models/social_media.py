Raniani-lab/enterpriise

commit 9f4b0a05149e1e0d1b11513c75801736779eceae

[FIX] *: de-t-raw-ify kanban views

social/views/social_media_views.xml
@@ -30,7 +30,7 @@
                             </div>
                             <div>
                                 <h3 class="mt4"><t t-esc="record.name.value"/></h3>
-                                <t t-if="record.description.raw_value" t-raw="record.description.raw_value"/>
+                                <t t-if="record.description.raw_value" t-out="record.description.value"/>
                             </div>
                             <div>
                                 <button t-if="record.can_link_accounts.raw_value" type="object" class="btn btn-primary float-right" name="action_add_account">Link account</button>

hr_referral/views/hr_job_views.xml
@@ -42,7 +42,7 @@
                             </div>
                             <div class="o_kanban_card_content container">
                                 <div class="pb-2">
-                                    <t t-raw="record.description.raw_value" class="text-muted"/>
+                                    <t t-out="record.description.value" class="text-muted"/>
                                 </div>
                                 <div>
                                     <div class="o_button_referral">

hr_referral/views/hr_referral_reward_views.xml
@@ -63,7 +63,7 @@
                                     </div>
                                 </div>
                                 <div>
-                                    <t t-raw="record.description.raw_value"/>
+                                    <t t-out="record.description.value"/>
                                 </div>
                                 <div groups="base.group_multi_company">
                                     <i><field name="company_id"/></i>

social/static/src/xml/social_templates.xml
@@ -76,7 +76,7 @@
                             <t t-set="originalMessage" t-value="widget._htmlEscape(comment.message)"/>
                             <div t-attf-class="o_social_comment_text flex-grow-1 #{bubble_design? 'bg-200 py-2 px-3 mr-2 o_social_bubble_radius' : ''}"
                                 t-att-data-original-message="originalMessage"
-                                t-raw="widget._formatCommentStreamPost(comment.message)"/>
+                                t-out="widget._formatCommentStreamPost(comment.message)"/>
 
                             <div class="o_social_icon_dropdown o_social_manage_comment dropdown">
                                 <button t-attf-class="btn btn-light dropdown-toggle #{bubble_design? 'bg-200 px-3 rounded-pill' : ''}"
@@ -219,7 +219,7 @@
             </div>
             <div name="o_social_original_post_message_body" t-attf-class="o_social_original_post_message_body">
                 <div t-if="originalPost.postMessage"
-                    t-raw="widget._formatCommentStreamPost(originalPost.postMessage)"
+                    t-out="widget._formatCommentStreamPost(originalPost.postMessage)"
                     class="o_social_original_post_message_text overflow-auto mb-2"/>
                 <div t-if="originalPost.postImages.length > 0"
                     class="o_social_stream_post_image mt-3 d-flex overflow-hidde">

social/views/social_stream_post_views.xml
@@ -62,12 +62,12 @@
                                             class="o_social_stream_post_author d-flex align-items-center"
                                             t-att-href="record.author_link.value"
                                             t-att-title="record.author_name.value or 'Unknown'"
-                                            t-raw="author_info"
+                                            t-out="author_info"
                                             target="_blank"/>
 
                                         <div t-else=""
                                             class="o_social_stream_post_author d-flex align-items-center"
-                                            t-raw="author_info"/>
+                                            t-out="author_info"/>
                                     </div>
 
                                     <a t-att-href="record.post_link.value" target="_blank">

social/views/utm_campaign_views.xml
@@ -54,7 +54,7 @@
             <xpath expr="//ul[@id='o_utm_actions']">
                 <a name="action_redirect_to_social_media_posts" type="object"
                     t-attf-class="#{record.social_posts_count.raw_value === 0 ? 'text-muted' : ''}">
-                    <t t-raw="record.social_posts_count.raw_value"/> Posts
+                    <t t-out="record.social_posts_count.raw_value"/> Posts
                 </a>
             </xpath>
         </field>

social_push_notifications/views/utm_campaign_views.xml
@@ -37,7 +37,7 @@
             <xpath expr="//ul[@id='o_utm_actions']">
                 <a name="action_redirect_to_push_notifications" type="object"
                     t-attf-class="#{record.social_push_notifications_count.raw_value === 0 ? 'text-muted' : ''}">
-                    <t t-raw="record.social_push_notifications_count.raw_value"/> Notifications
+                    <t t-out="record.social_push_notifications_count.raw_value"/> Notifications
                 </a>
             </xpath>
         </field>

commit c89c00c6acc37f7109f3ee716c9592c3bb036bb5

[IMP] social: simplify the "description" field on social.media
The description field on a social media currently stores HTML content.
Which feels completely unnecessary since these records are only meant to be
created as data and the content of this field is very simple (one regular
sentence).

This commit simplifies the description by storing regular text instead.

(This also allows to solve an issue of rendering introduced by
9f4b0a05149e1e0d1b11513c75801736779eceae ).

We also took this opportunity to rename it to "media_description" to ease
greping.

Task-2613240
UPG PR odoo/upgrade#2695

closes odoo/enterprise#19985

Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

social/models/social_media.py
@@ -21,7 +21,7 @@ class SocialMedia(models.Model):
     _DEFAULT_SOCIAL_IAP_ENDPOINT = 'https://social.api.odoo.com'
 
     name = fields.Char('Name', readonly=True, required=True, translate=True)
-    description = fields.Char('Description', readonly=True)
+    media_description = fields.Char('Description', readonly=True)
     image = fields.Binary('Image', readonly=True)
     media_type = fields.Selection([], readonly=True,
         help="Used to make comparisons when we need to restrict some features to a specific media ('facebook', 'twitter', ...).")

social/views/social_media_views.xml
@@ -19,7 +19,7 @@
             <kanban create="false" class="o_kanban_social_media">
                 <field name="id"/>
                 <field name="name"/>
-                <field name="description"/>
+                <field name="media_description"/>
                 <field name="media_type"/>
                 <field name="can_link_accounts"/>
                 <templates>
@@ -28,9 +28,9 @@
                             <div class="o_kanban_image">
                                 <img type="open" t-att-src="kanban_image('social.media', 'image', record.id.raw_value)" alt="Social Media"/>
                             </div>
-                            <div>
+                            <div class="mb-2">
                                 <h3 class="mt4"><t t-esc="record.name.value"/></h3>
-                                <t t-if="record.description.raw_value" t-out="record.description.value"/>
+                                <t t-esc="record.media_description.value"/>
                             </div>
                             <div>
                                 <button t-if="record.can_link_accounts.raw_value" type="object" class="btn btn-primary float-right" name="action_add_account">Link account</button>

social_facebook/data/social_media_data.xml
@@ -4,7 +4,7 @@
         <record id="social_media_facebook" model="social.media">
             <field name="name">Facebook</field>
             <field name="media_type">facebook</field>
-            <field name="description" type="html"><p>Manage your Facebook pages and schedule posts</p></field>
+            <field name="media_description">Manage your Facebook pages and schedule posts</field>
             <field name="image" type="base64" file="social_facebook/static/src/img/facebook.svg"/>
         </record>
 

social_instagram/data/social_media_data.xml
@@ -4,9 +4,7 @@
         <record id="social_media_instagram" model="social.media">
             <field name="name">Instagram</field>
             <field name="media_type">instagram</field>
-            <field name="description" type="html">
-<p>Manage your Instagram Business account and schedule posts</p>
-            </field>
+            <field name="media_description">Manage your Instagram Business account and schedule posts</field>
             <field name="image" type="base64" file="social_instagram/static/src/img/instagram.svg"/>
         </record>
 

social_linkedin/data/social_media_data.xml
@@ -4,7 +4,7 @@
         <record id="social_media_linkedin" model="social.media">
             <field name="name">LinkedIn</field>
             <field name="media_type">linkedin</field>
-            <field name="description" type="html"><p>Manage your LinkedIn account and schedule posts</p></field>
+            <field name="media_description">Manage your LinkedIn account and schedule posts</field>
             <field name="image" type="base64" file="social_linkedin/static/src/img/linkedin.svg"/>
         </record>
 

social_push_notifications/data/social_media_data.xml
@@ -6,7 +6,7 @@
             <field name="media_type">push_notifications</field>
             <field name="has_streams" eval="False" />
             <field name="can_link_accounts" eval="False"/>
-            <field name="description" type="html"><p>Send live notifications to your web visitors</p></field>
+            <field name="media_description">Send live notifications to your web visitors</field>
             <field name="image" type="base64" file="social_push_notifications/static/src/img/push_notifications.png"/>
         </record>
     </data>

social_twitter/data/social_media_data.xml
@@ -4,7 +4,7 @@
         <record id="social_media_twitter" model="social.media">
             <field name="name">Twitter</field>
             <field name="media_type">twitter</field>
-            <field name="description" type="html"><p>Manage your Twitter accounts and schedule tweets</p></field>
+            <field name="media_description">Manage your Twitter accounts and schedule tweets</field>
             <field name="image" type="base64" file="social_twitter/static/src/img/twitter.svg"/>
         </record>
 

social_youtube/data/social_media_data.xml
@@ -4,7 +4,7 @@
         <record id="social_media_youtube" model="social.media">
             <field name="name">YouTube</field>
             <field name="media_type">youtube</field>
-            <field name="description" type="html"><p>Manage your YouTube videos and schedule video uploads</p></field>
+            <field name="media_description">Manage your YouTube videos and schedule video uploads</field>
             <field name="image" type="base64" file="social_youtube/static/src/img/youtube.svg"/>
         </record>
 
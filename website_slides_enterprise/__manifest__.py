7123d5c3d36f08744905747510e5dd0fc8fc6c37
Thomas Josse <thjo@odoo.com>
2021-08-27 12:17:04 +0000

    [IMP] website_slides_enterprise: adding dash view to website_slides
    This module contains the dashboard view for the website_slides.
    
    This view is used for the reporting of courses as the first view when
    entering eLearning > Reporting > Courses.
    
    COM
    PR: odoo/odoo#75646
    
    UPGRADE
    PR: odoo/upgrade#2784
    task-2597345
    
    closes odoo/enterprise#20480
    
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

---
+++ website_slides_enterprise/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'eLearning enterprise',
+    'version': '1.0',
+    'category': 'Website/eLearning',
+    'description': """
+    Contains advanced features for eLeaning such as new views
+    """,
+    'depends': ['website_slides', 'web_dashboard'],
+    'installable': True,
+    'application': False,
+    'auto_install': ['website_slides'],
+    'data': [
+        'views/slide_channel_views.xml'
+    ],
+    'license': 'OEEL-1',
+}

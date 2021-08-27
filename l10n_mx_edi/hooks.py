d935fc50d33a343c60aab88a0bee957c79aae3a7
Alvaro Fuentes <afu@odoo.com>
2021-08-27 06:44:24 +0000

    [REF] l10n_mx_edi: properly open file and minor refs
    Opening a file should be done through `tools.file_open`, better also
    with a context manager.
    
    closes odoo/enterprise#20523
    
    X-original-commit: b6515e206ece05a810b62692759d092ae13f64a9
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- l10n_mx_edi/hooks.py
+++ l10n_mx_edi/hooks.py
@@ -1,8 +1,7 @@
 # coding: utf-8
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from os.path import join, dirname, realpath
-from odoo import api, SUPERUSER_ID
+from odoo import api, tools, SUPERUSER_ID
 import csv
 
 
@@ -10,9 +9,8 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     mx_country = env["res.country"].search([("code", "=", "MX")])
 
-    csv_path = join(dirname(realpath(__file__)), 'data', 'res.city.csv')
     res_city_vals_list = []
-    with open(csv_path, 'r') as csv_file:
+    with tools.file_open("l10n_mx_edi/data/res.city.csv", "r") as csv_file:
         for row in csv.DictReader(csv_file, delimiter='|', fieldnames=['l10n_mx_edi_code', 'name', 'state_xml_id']):
             state = env.ref('base.%s' % row['state_xml_id'], raise_if_not_found=False)
             res_city_vals_list.append({

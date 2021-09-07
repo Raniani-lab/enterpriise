82ebca0c2b6dd013c8104ff4057bbf17fe9a50b4
svs-odoo <svs@odoo.com>
2021-09-07 15:09:56 +0000

    [IMP] stock_barcode: filter on product gs1
    In the Barcode App, user can filter pickings by scanning a product's
    barcode, but before this commit, that doesn't work with GS1 barcode.
    Now, it will try to decompose GS1 barcode and then checks if it has a
    product part on it. If it's the case, the pickings will be filtered.
    
    task-2641528
    
    closes odoo/enterprise#25389
    
    Signed-off-by: Arnold Moyaux <arm@odoo.com>

--- stock_barcode/models/product_product.py
+++ stock_barcode/models/product_product.py
@@ -1,12 +1,54 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import re
 from odoo import models, api
+from odoo.exceptions import ValidationError
 
 
 class Product(models.Model):
     _inherit = 'product.product'
     _barcode_field = 'barcode'
+
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+        # Override to cut off the padding if using GS1 and searching on barcode
+        # if the barcode is only digits to keep the original barcode part only.
+        for i, arg in enumerate(args):
+            if not isinstance(arg, (list, tuple)) or len(arg) != 3:
+                continue
+            field_name, operator, value = arg
+            if field_name != 'barcode' or operator not in ['ilike', 'not ilike', '=', '!=']:
+                continue
+            nomenclature = self.env.company.nomenclature_id
+            if not nomenclature.is_gs1_nomenclature:
+                break
+
+            parsed_data = []
+            try:
+                parsed_data += nomenclature.parse_barcode(value) or []
+            except (ValidationError, ValueError):
+                pass
+
+            replacing_operator = 'ilike' if operator in ['ilike', '='] else 'not ilike'
+            # If it's a valid GS1 barcode, checks if a product data was found.
+            for data in parsed_data:
+                if data['rule'].type != 'product':
+                    continue
+                value = data['value']
+                match = re.match('0*([0-9]+)$', value)
+                if match:
+                    unpadded_barcode = match.groups()[0]
+                    if unpadded_barcode != value:
+                        args[i] = (field_name, replacing_operator, unpadded_barcode)
+                break
+            # The barcode isn't a valid GS1 barcode, checks if it can be unpadded.
+            if not parsed_data:
+                match = re.match('0+([0-9]+)$', value)
+                if match:
+                    args[i] = (field_name, replacing_operator, match.groups()[0])
+            break
+        return super()._search(args, offset=offset, limit=limit, order=order, count=count, access_rights_uid=access_rights_uid)
 
     @api.model
     def _get_fields_stock_barcode(self):

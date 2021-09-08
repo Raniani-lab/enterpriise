63dd98508139a4d0ee91aab922f10fc0dfe5f6f4
Kevin Baptiste <kba@odoo.com>
2021-09-08 10:53:53 +0000

    [FIX] industry_fsm: correctly set default project on task
    Always take the project with the lowest sequence on the current company.
    
    closes odoo/enterprise#21192
    
    Taskid: 2641479
    X-original-commit: d7f2af755c0e54860f1a7e0c3be2d9f4bb0ca9e0
    Signed-off-by: LTU-Odoo <IT-Ideas@users.noreply.github.com>
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- industry_fsm/models/project.py
+++ industry_fsm/models/project.py
@@ -45,10 +45,8 @@ class Task(models.Model):
     def default_get(self, fields_list):
         result = super(Task, self).default_get(fields_list)
         if 'project_id' in fields_list and not result.get('project_id') and self._context.get('fsm_mode'):
-            if self.env.context.get('default_company_id'):
-                fsm_project = self.env['project.project'].search([('is_fsm', '=', True), ('company_id', '=', self.env.context.get('default_company_id'))], order='sequence', limit=1)
-            else :
-                fsm_project = self.env['project.project'].search([('is_fsm', '=', True)], order='sequence', limit=1)
+            company_id = self.env.context.get('default_company_id') or self.env.company.id
+            fsm_project = self.env['project.project'].search([('is_fsm', '=', True), ('company_id', '=', company_id)], order='sequence', limit=1)
             result['project_id'] = fsm_project.id
 
         date_begin = result.get('planned_date_begin')

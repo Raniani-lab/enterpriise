d3153708fad6dc383343bd6033f933b92c2e4441
William Henrotin <whe@odoo.com>
2021-09-09 11:31:54 +0000

    [FIX] quality_control: resize img in template
    No need to use ImageProcess tool to resize the quality check picture. It
    can be done more easily in the template.
    
    Task : 2524922
    
    X-original-commit: 3d15b6e3b726edbc1c9494360a1c4e06b2169b05
    Part-of: odoo/enterprise#20812

--- quality_control/report/worksheet_custom_reports.py
+++ quality_control/report/worksheet_custom_reports.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import base64
 from odoo import api, models
-from odoo.tools import image_process
-from odoo.addons.web.controllers.main import Binary
 
 
 class QualityCustomReport(models.AbstractModel):
@@ -13,18 +10,12 @@ class QualityCustomReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        def resize_picture(b64source):
-            if not b64source:
-                b64source = base64.b64encode(Binary.placeholder())
-            return image_process(b64source, size=(750, 750))
-
         docs = self.env['quality.check'].browse(docids).sudo()
 
         return {
             'doc_ids': docids,
             'doc_model': 'quality.check',
             'docs': docs,
-            'resize_picture': resize_picture
         }
 
 class QualityCustomInternalReport(models.AbstractModel):

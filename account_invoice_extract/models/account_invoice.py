26c0c60c6f6cb5ec24848a8e84d34bf6abd23d21
Louis Baudoux <lba@odoo.com>
2021-09-23 15:33:15 +0000

    [FIX] account_invoice_extract: fix wrong journal used
    When writing the invoice lines found by the OCR, the default journal
    was always used, even when it had been manually changed prior to the
    OCR update.
    
    opw #2638309
    
    closes odoo/enterprise#21396
    
    X-original-commit: 8e3a9e7b521c0074737758b1d8e73fc56dc272db
    Signed-off-by: Florian Daloze (fda) <fda@odoo.com>
    Signed-off-by: Louis Baudoux <lobaudoux@users.noreply.github.com>

--- account_invoice_extract/models/account_invoice.py
+++ account_invoice_extract/models/account_invoice.py
@@ -722,7 +722,7 @@ class AccountMove(models.Model):
             # we need to make sure the type is in the context as _get_default_journal uses it
             self_ctx = self.with_context(default_move_type=self.move_type) if 'default_move_type' not in self._context else self
             self_ctx = self_ctx.with_company(self.company_id.id)
-            self_ctx = self_ctx.with_context(default_journal_id=self_ctx._get_default_journal().id)
+            self_ctx = self_ctx.with_context(default_journal_id=self_ctx.journal_id.id)
         with Form(self_ctx) as move_form:
             move_form.date = datetime.strptime(move_form.date, tools.DEFAULT_SERVER_DATE_FORMAT).date()
             if not move_form.partner_id:

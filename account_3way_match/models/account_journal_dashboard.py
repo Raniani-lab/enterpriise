066143dc3660ad4a7a8c6e6dc36b9715798e3f72
Achraf (abz) <abz@odoo.com>
2021-09-28 13:21:16 +0000

    [FIX] account_3way_match: Fix bills to pay filter
    Since https://github.com/odoo/enterprise/pull/19531 The filter was wrong because the two conditions (sql request server-side and of the bills to pay filter) are not the same.
    
    https://github.com/odoo/enterprise/blob/fdad6a7d1411a6f1f3685d6b40e2a81f30682b0c/account_3way_match/views/account_invoice_view.xml#L28
    https://github.com/odoo/enterprise/blob/55998d8a05ae7c4d0eaee0b47874efa184a902f1/account_3way_match/models/account_journal_dashboard.py#L28
    
    This means that we can have cases where:
    
    It is displayed in the dashboard: 1 Bill To Pay
    But once we click on it, the Bill To Pay filter doesn't show us any records
    
    It can happen when the due date is equal to today.
    
    OPW-2513075
    
    closes odoo/enterprise#21329
    
    X-original-commit: 32f83d89d54a805ffa5dd6ded289c965d3a611bf
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_3way_match/models/account_journal_dashboard.py
+++ account_3way_match/models/account_journal_dashboard.py
@@ -25,7 +25,7 @@ class AccountJournal(models.Model):
                    amount_residual as amount_total, currency_id AS currency
                    FROM account_move
                    WHERE journal_id = %(journal_id)s
-                   AND (release_to_pay = 'yes' OR invoice_date_due <= %(today)s)
+                   AND (release_to_pay = 'yes' OR invoice_date_due < %(today)s)
                    AND state = 'posted'
                    AND payment_state in ('not_paid', 'partial');""",
                    {'journal_id': self.id, 'today': fields.Date.today()})

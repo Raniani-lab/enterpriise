880e6239294681cc3c97244fd04cc9b314d696ff
Patrick Hoste <pko@odoo.com>
2021-10-11 08:57:32 +0000

    [IMP] web_studio: adapt test after changes in community
    PURPOSE
    
    Fix a test after modifying the _message_add_suggested_recipients method.
    
    LINKS
    
    Task-2580065
    PR : odoo/enterprise#20689
    
    Related: odoo/odoo#76812
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- web_studio/tests/test_ir_model.py
+++ web_studio/tests/test_ir_model.py
@@ -70,7 +70,7 @@ class TestStudioIrModel(TransactionCase):
         })
         # ensure the partner is suggested in email and sms communication
         mail_suggested_recipients = bfr._message_get_suggested_recipients()
-        self.assertIn((self.partner_elon.id, '"Elon Tusk" <elon@spacex.com>', 'Contact'),
+        self.assertIn((self.partner_elon.id, '"Elon Tusk" <elon@spacex.com>', None, 'Contact'),
                       mail_suggested_recipients.get(bfr.id),
                       'custom partner field should be suggested in mail communications')
         sms_suggested_recipients = bfr._sms_get_partner_fields()

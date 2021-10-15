daac48a5c8d6a21365ea200b7b4c834ab53bcbed
Xavier Morel <xmo@odoo.com>
2021-10-15 10:53:40 +0000

    [FIX] industry_fsm_report, quality_control_worksheet: batch demo data
    To demonstrate the amazingness of worksheets the demo data of these
    modules go and create a bunch of custom fields.
    
    As a result these go through the entire flush/setup_models/init_models
    once per field, which is pretty expensive.
    
    Bulk-creating those custom fields should only require re-setting up
    the models once for the entire batch skipping the vast majority of the
    overhead. Worksheets create a custom model to which we have to attach
    our custom fields, so CSVs are not an option.
    
    Part-of: odoo/enterprise#21746

--- industry_fsm_report/models/worksheet_template.py
+++ industry_fsm_report/models/worksheet_template.py
@@ -35,3 +35,46 @@ class WorksheetTemplate(models.Model):
     @api.model
     def _get_project_task_module_name(self):
         return 'industry_fsm_report'
+
+    @api.model
+    def _create_demo_data_fsm(self):
+        # create demo data in batch for performance reasons (avoid multiple calls to setup_models)
+        model_id = self.env.ref('industry_fsm_report.fsm_worksheet_template2').model_id.id
+        self.env['ir.model.fields'].create([{
+            'name': 'x_intervention_type',
+            'ttype': 'selection',
+            'field_description': 'Intervention Type',
+            'selection': "[('first_installation','First installation'),('technical_maintenance','Technical maintenance')]",
+            'model_id': model_id,
+        }, {
+            'name': 'x_description',
+            'ttype': 'text',
+            'field_description': 'Description of the Intervention',
+            'model_id': model_id,
+        }, {
+            'name': 'x_manufacturer',
+            'ttype': 'many2one',
+            'relation': 'res.partner',
+            'field_description': 'Manufacturer',
+            'model_id': model_id,
+        }, {
+            'name': 'x_checkbox',
+            'ttype': 'boolean',
+            'field_description': 'I hereby certify that this device meets the requirements of an acceptable device at the time of testing.',
+            'model_id': model_id,
+        }, {
+            'name': 'x_serial_number',
+            'ttype': 'char',
+            'field_description': 'Serial Number',
+            'model_id': model_id,
+        }, {
+            'name': 'x_date',
+            'ttype': 'date',
+            'field_description': 'Date',
+            'model_id': model_id,
+        }, {
+            'name': 'x_worker_signature',
+            'ttype': 'binary',
+            'field_description': 'Worker Signature',
+            'model_id': model_id,
+        }])

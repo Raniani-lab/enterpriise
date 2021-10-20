3c396fa6da8347f2db6d718a74bea4847c388304
Vincent Schippefilt <vsc@odoo.com>
2021-10-20 13:08:45 +0000

    [MOV] documents_spreadsheet: split autofill
    Split the autofill declaration by feature
    
    Part-of: odoo/enterprise#21804

--- documents_spreadsheet_bundle/static/src/pivot/autofill.js
+++ documents_spreadsheet_bundle/static/src/pivot/autofill.js
@@ -1,20 +1,15 @@
-/** @odoo-module alias=documents_spreadsheet.Autofill */
+/** @odoo-module */
 
-import { getNumberOfListFormulas } from "../helpers/odoo_functions_helpers";
-import spreadsheet from "../o_spreadsheet_loader";
+import spreadsheet from "../o_spreadsheet/o_spreadsheet_extended";
 
 const { autofillModifiersRegistry, autofillRulesRegistry } = spreadsheet.registries;
+const { autofillModifiersRegistry } = spreadsheet.registries;
 
 const UP = 0;
 const DOWN = 1;
 const LEFT = 2;
 const RIGHT = 3;
 
-//--------------------------------------------------------------------------
-// Autofill Component
-//--------------------------------------------------------------------------
-class AutofillTooltip extends owl.Component {}
-AutofillTooltip.template = "documents_spreadsheet.AutofillTooltip";
 
 //--------------------------------------------------------------------------
 // Autofill Rules
@@ -37,20 +32,6 @@ autofillRulesRegistry
             cell && cell.isFormula() && cell.content.match(/=.*PIVOT.*PIVOT\.POSITION/),
         generateRule: () => ({ type: "PIVOT_POSITION_UPDATER", current: 0 }),
         sequence: 1,
-    })
-    .add("autofill_list", {
-        condition: (cell) =>
-            cell && cell.isFormula() && getNumberOfListFormulas(cell.content) === 1,
-        generateRule: (cell, cells) => {
-            const increment = cells.filter(
-                (cell) =>
-                    cell &&
-                    cell.isFormula()&&
-                    getNumberOfListFormulas(cell.content) === 1
-            ).length;
-            return { type: "LIST_UPDATER", increment, current: 0 };
-        },
-        sequence: 3,
     });
 
 //--------------------------------------------------------------------------
@@ -148,56 +129,6 @@ autofillModifiersRegistry
                           props: { content },
                       }
                     : undefined,
-            };
-        },
-    })
-    .add("LIST_UPDATER", {
-        apply: (rule, data, getters, direction) => {
-            rule.current += rule.increment;
-            let isColumn;
-            let steps;
-            switch (direction) {
-                case UP:
-                    isColumn = false;
-                    steps = -rule.current;
-                    break;
-                case DOWN:
-                    isColumn = false;
-                    steps = rule.current;
-                    break;
-                case LEFT:
-                    isColumn = true;
-                    steps = -rule.current;
-                    break;
-                case RIGHT:
-                    isColumn = true;
-                    steps = rule.current;
-            }
-            const content = getters.getNextListValue(
-                getters.getFormulaCellContent(data.sheetId, data.cell),
-                isColumn,
-                steps
-            );
-            let tooltip = {
-                props: {
-                    content,
-                },
-            };
-            if (content && content !== data.content) {
-                tooltip = {
-                    props: {
-                        content: getters.getTooltipListFormula(content, isColumn),
-                    },
-                };
-            }
-            return {
-                cellData: {
-                    style: undefined,
-                    format: undefined,
-                    border: undefined,
-                    content,
-                },
-                tooltip,
             };
         },
     });

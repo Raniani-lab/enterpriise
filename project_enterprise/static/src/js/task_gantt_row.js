ed868dffc18fbacb324539447e4a9bc9834b826a
Thibault Libioulle <tle@odoo.com>
2021-10-25 15:47:46 +0000

    [IMP] project_enterprise: display allocated hours in popover
    This commit adds the allocated hours information on the gantt popover.
    
    The allocated hours are computed based on the user's working calendar
    information.
    If there are multiple assignees, the working hours is the sum of the
    allocated hours for each user.
    
    This commit takes advantage of the PR #21135 and copy the way the
    allocated hours are computed in planning slots.
    
    task-2664117
    
    closes odoo/enterprise#21877
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- project_enterprise/static/src/js/task_gantt_row.js
+++ project_enterprise/static/src/js/task_gantt_row.js
@@ -1,5 +1,6 @@
 /** @odoo-module **/
 
+import fieldUtils from 'web.field_utils';
 import GanttRow from 'web_gantt.GanttRow';
 import { getDateFormatForScale } from "./task_gantt_utils";
 
@@ -20,6 +21,17 @@ const TaskGanttRow = GanttRow.extend({
             const slotKey = slot.start.format(dateFormat);
             slot.milestonesInfo = this.viewInfo.slotsMilestonesDict[slotKey];
         });
+    },
+    /**
+     * Add allocated hours formatted to context
+     *
+     * @private
+     * @override
+     */
+    _getPopoverContext: function () {
+        const data = this._super.apply(this, arguments);
+        data.allocatedHoursFormatted = fieldUtils.format.float_time(data.allocated_hours);
+        return data;
     },
 });
 

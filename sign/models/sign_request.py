Raniani-lab/enterpriise

commit b6e0f26745872483fe77330e38fb8ed18dc54bd0

[IMP] sign: reassign signatory
configure in sign roles to enable Change Authorized
enable reassign signatory in the form view of a sign request

Before, if the person who have to sign a document is sick or go away, we are blocked, nobody can sign the document.
This feature is really usefull when the document have to be by "Someone from HR Officer Department", or something else.
So the aims of this feature is to give the possibility to change a signatory.

task-2510209

closes odoo/enterprise#18384

Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

sign/models/sign_request.py
@@ -223,7 +223,7 @@ class SignRequest(models.Model):
     def open_logs(self):
         self.ensure_one()
         return {
-            "name": _("Access History"),
+            "name": _("Activity Logs"),
             "type": "ir.actions.act_window",
             "res_model": "sign.log",
             'view_mode': 'tree,form',
@@ -624,9 +624,10 @@ class SignRequest(models.Model):
         if attachment_ids:
             attachment_ids.write({'res_model': sign_request._name, 'res_id': sign_request.id})
             sign_request.write({'attachment_ids': [Command.set(attachment_ids.ids)]})
-        sign_request.message_subscribe(partner_ids=followers)
-        sign_request.activity_update(sign_users)
         sign_request.set_signers(signers)
+        # All CCers and signers are followers of the sign request.
+        sign_request.message_subscribe(partner_ids=followers + sign_request.request_item_ids.partner_id.ids)
+        sign_request.activity_update(sign_users)
         if send:
             sign_request.action_sent()
         if without_mail:
@@ -671,7 +672,7 @@ class SignRequestItem(models.Model):
 
     access_token = fields.Char(required=True, default=_default_access_token, readonly=True)
     access_via_link = fields.Boolean('Accessed Through Token')
-    role_id = fields.Many2one('sign.item.role', string="Role")
+    role_id = fields.Many2one('sign.item.role', string="Role", readonly=True)
     sms_number = fields.Char(related='partner_id.mobile', readonly=False, depends=(['partner_id']), store=True)
     sms_token = fields.Char('SMS Token', readonly=True)
 
@@ -683,11 +684,56 @@ class SignRequestItem(models.Model):
         ("completed", "Completed")
     ], readonly=True, default="draft")
 
-    signer_email = fields.Char(compute="_compute_email", readonly=False, store=True)
+    signer_email = fields.Char(compute="_compute_email", store=True)
     is_mail_sent = fields.Boolean(readonly=True, copy=False, help="The signature mail has been sent.")
+    change_authorized = fields.Boolean(related='role_id.change_authorized')
 
     latitude = fields.Float(digits=(10, 7))
     longitude = fields.Float(digits=(10, 7))
+
+    def write(self, vals):
+        if vals.get('partner_id') is False:
+            raise UserError(_("You need to define a signatory"))
+        request_items_reassigned = self.env['sign.request.item']
+        if vals.get('partner_id'):
+            request_items_reassigned |= self.filtered(lambda sri: self.partner_id.id != vals['partner_id'])
+            if any(sri.state not in ['sent', 'draft']
+                   or sri.sign_request_id.state not in ['sent', 'canceled']
+                   or not sri.role_id.change_authorized
+                   for sri in request_items_reassigned):
+                raise UserError(_("You cannot reassign this signatory"))
+            new_sign_partner = self.env['res.partner'].browse(vals.get('partner_id'))
+            old_sign_users = self.env['res.users'].search([
+                ('partner_id.id', 'in', request_items_reassigned.partner_id.ids),
+                ('groups_id', 'in', [self.env.ref('sign.group_sign_employee').id])
+            ], limit=len(request_items_reassigned.partner_id))
+            for request_item in request_items_reassigned:
+                # remove old activities for internal users
+                old_sign_user = old_sign_users.filtered(lambda usr: usr.partner_id.id == request_item.partner_id.id)
+                if old_sign_user:
+                    request_item.sign_request_id.activity_unlink(['mail.mail_activity_data_todo'], user_id=old_sign_user.id)
+                # create logs
+                request_item.sign_request_id.message_post(
+                    body=_('The contact of %(role)s has been changed from %(old_partner)s to %(new_partner)s.',
+                           role=request_item.role_id.name, old_partner=request_item.partner_id.name, new_partner=new_sign_partner.name))
+
+            # add new followers
+            request_items_reassigned.sign_request_id.message_subscribe(partner_ids=[vals.get('partner_id')])
+            # add new activities for internal users
+            new_sign_user = self.env['res.users'].search([
+                ('partner_id.id', '=', vals.get('partner_id')),
+                ('groups_id', 'in', [self.env.ref('sign.group_sign_employee').id])
+            ], limit=1)
+            if new_sign_user:
+                self.sign_request_id.activity_update(new_sign_user)
+
+        res = super(SignRequestItem, self).write(vals)
+
+        # change access token
+        for request_item in request_items_reassigned.filtered(lambda sri: sri.is_mail_sent):
+            request_item.access_token = self._default_access_token()
+            request_item.is_mail_sent = False
+        return res
 
     def action_draft(self):
         for request_item in self:
@@ -874,21 +920,10 @@ class SignRequestItem(models.Model):
         for signature_request in self:
             signature_request.access_url = '/my/signature/%s' % signature_request.id
 
-    @api.depends('partner_id')
+    @api.depends('partner_id.email')
     def _compute_email(self):
-        for sign_request_item in self:
+        for sign_request_item in self.filtered(lambda sri: sri.state in ["draft", "sent"]):
             sign_request_item.signer_email = sign_request_item.partner_id.email_normalized
-
-    def _update_email(self):
-        for sign_request_item in self:
-            sign_request_item.signer_email = sign_request_item.partner_id.email_normalized
-            sign_request_item.access_token = self.env['sign.request']._default_access_token()
-            if sign_request_item.is_mail_sent:
-                sign_request_item.sign_request_id.message_post(
-                    body=_('The mail address of %s has been updated. The request will be automatically resent.',
-                           sign_request_item.partner_id.name))
-                self.env['sign.log']._create_log(sign_request_item, 'update_mail', is_request=False)
-                sign_request_item.resend_sign_access()
 
 
 class SignRequestItemValue(models.Model):

hr_contract_sign/data/hr_contract_data.xml
@@ -3,6 +3,7 @@
     <data>
         <record model="sign.item.role" id="sign_item_role_job_responsible">
             <field name="name">HR Responsible</field>
+            <field name="change_authorized" eval="True"/>
             <field name="color" eval="11"/>
         </record>
     </data>

sign/data/sign_data.xml
@@ -188,6 +188,7 @@
 
     <record model="sign.item.role" id="sign_item_role_company">
         <field name="name">Company</field>
+        <field name="change_authorized" eval="True"/>
         <field name="color" eval="9"/>
     </record>
 

sign/models/res_partner.py
@@ -35,6 +35,16 @@ class ResPartner(models.Model):
         partners_email_changed = self.filtered(lambda r: r.email != vals['email']) if 'email' in vals else None
         res = super(ResPartner, self).write(vals)
         if partners_email_changed:
-            sign_request_items = self.env['sign.request.item'].sudo().search([('partner_id', 'in', partners_email_changed.ids), ('state', '!=', 'completed'), ('sign_request_id.active', '=', True)])
-            sign_request_items._update_email()
+            request_items = self.env['sign.request.item'].sudo().search([
+                ('partner_id', 'in', partners_email_changed.ids),
+                ('state', '!=', 'completed'),
+                ('sign_request_id.active', '=', True),
+                ('is_mail_sent', '=', True)])
+            for request_item in request_items:
+                request_item.sign_request_id.message_post(
+                    body=_('The mail address of %(partner)s has been updated. The request will be automatically resent.',
+                           partner=request_item.partner_id.name))
+                self.env['sign.log']._create_log(request_item, 'update_mail', is_request=False)
+                request_item.access_token = self.env['sign.request']._default_access_token()
+                request_item.resend_sign_access()
         return res

sign/models/sign_template.py
@@ -312,7 +312,8 @@ class SignItemParty(models.Model):
     color = fields.Integer()
     default = fields.Boolean(required=True, default=False)
 
-    sms_authentification = fields.Boolean('SMS Authentication', default=False,)
+    sms_authentification = fields.Boolean('SMS Authentication')
+    change_authorized = fields.Boolean('Change Authorized')
 
     @api.model
     def add(self, name):

sign/report/sign_log_reports.xml
@@ -52,7 +52,7 @@
                                 <tr>
                                     <td>
                                         <span t-field="log.action"/>
-                                        <img t-if="log.action != 'save' and log.sign_request_item_id.signature" class="text-left" t-att-src="image_data_uri(log.sign_request_item_id.signature)" style="max-height: 40px"/>
+                                        <img t-if="log.action == 'sign' and log.sign_request_item_id.signature" class="text-left" t-att-src="image_data_uri(log.sign_request_item_id.signature)" style="max-height: 40px"/>
                                     </td>
                                     <td> <span t-field="log.partner_id"/>
                                     <t t-if="log.partner_id.email">

sign/tests/test_common.py
@@ -106,6 +106,18 @@ class TestSignCommon(TransactionCase):
             'company_id': cls.company_id.id,
         })
 
+        cls.partner_3_id = cls.env['res.partner'].create({
+            'name': 'Martine Poulichette',
+            'street': '59 rue des Wallons',
+            'city': 'Louvain-la-Neuve',
+            'zip': '1348',
+            'country_id': cls.env.ref("base.be").id,
+            'phone': '+0032476543211',
+            'email': 'martine.poulichette.a@example.com',
+            'email_normalized': 'martine.poulichette.a@example.com',
+            'company_id': cls.company_id.id,
+        })
+
         cls.single_role_sign_request = cls.create_sign_request(cls, cls.template, [cls.partner_id.id])
         cls.multi_role_sign_request = cls.create_sign_request(cls, cls.template_multi_role, [cls.partner_id.id, cls.partner_2_id.id])
         cls.default_role_sign_request = cls.create_sign_request(cls, cls.template_without_sign_items, [cls.partner_id.id])

sign/tests/test_sign_request.py
@@ -3,6 +3,7 @@
 from .test_common import TestSignCommon
 from unittest.mock import patch
 from odoo.addons.sign.models.sign_log import SignLog
+from odoo.exceptions import UserError
 
 
 class TestSignRequest(TestSignCommon):
@@ -30,6 +31,25 @@ class TestSignRequest(TestSignCommon):
         token_c = request_item.access_token
         self.assertEqual(request_item.signer_email, "laurie.poiret.b@example.com", 'email address should be laurie.poiret.b@example.com')
         self.assertEqual(token_c, token_b, "sign request item's access token should be not changed after the document is signed by the signer")
+
+    @patch.object(SignLog, "_create_log")
+    def test_sign_request_item_reassign_signatory(self, _create_log):
+        sign_request = self.single_role_sign_request
+        request_item_ids = sign_request.request_item_ids
+        request_item = request_item_ids[0]
+        token_a = request_item.access_token
+        self.assertEqual(request_item.signer_email, "laurie.poiret.a@example.com", 'email address should be laurie.poiret.a@example.com')
+        self.assertEqual(request_item.is_mail_sent, True, 'email should be sent')
+        self.assertEqual(request_item.role_id.change_authorized, False, 'Role employee is not allowed to be changed')
+        with self.assertRaises(UserError):
+            request_item.write({'partner_id': self.partner_3_id.id})
+        with self.assertRaises(UserError):
+            request_item.write({'partner_id': False})
+        request_item.role_id.change_authorized = True
+        request_item.write({'partner_id': self.partner_3_id.id})
+        self.assertEqual(request_item.signer_email, "martine.poulichette.a@example.com", 'email address should be martine.poulichette.a@example.com')
+        self.assertNotEqual(token_a, request_item.access_token, "sign request item's access token should be changed")
+        self.assertEqual(request_item.is_mail_sent, False, 'email should not be sent')
 
     def test_templates_send_accesses(self):
         for sign_request in [self.default_role_sign_request, self.multi_role_sign_request, self.single_role_sign_request]:

commit 4a568519304810ae6fae74123356af17a5b9b302

[FIX] sign: fix template share after reassign
fix the bug introduced from
commit: b6e0f26745872483fe77330e38fb8ed18dc54bd0
task-2510209 odoo/enterprise#18384

before this commit:
the template share flow will be blocked, since it will try to change signer from False to a real one

closes odoo/enterprise#22696

X-original-commit: c6c8b5a4c348810a9529190d5ad3afa1830d18d8
Signed-off-by: Arnaud Joset <arj@odoo.com>

sign/models/sign_request.py
@@ -761,7 +761,7 @@ class SignRequestItem(models.Model):
             request_items_reassigned |= self.filtered(lambda sri: self.partner_id.id != vals['partner_id'])
             if any(sri.state not in ['sent', 'draft']
                    or sri.sign_request_id.state not in ['sent', 'canceled']
-                   or not sri.role_id.change_authorized
+                   or (sri.partner_id and not sri.role_id.change_authorized)
                    for sri in request_items_reassigned):
                 raise UserError(_("You cannot reassign this signatory"))
             new_sign_partner = self.env['res.partner'].browse(vals.get('partner_id'))
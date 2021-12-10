55e70c787076b57065eaddd8a98abf053bc3b364
Paolo (pgi) <pgi@odoo.com>
2021-12-10 15:26:00 +0000

    [FIX] account: Manual write-off with tax fixed
    When the Reconciliation Model had a tax on the writeoff,
    the journal_id wasn't populated by widget.get_reconciliation_dict_from_model(),
    preventing the reconciliation from happening.
    
    Ticket link: https://www.odoo.com/web#id=2689002&model=project.task
    
    opw-2689002
    
    closes odoo/enterprise#22891
    
    X-original-commit: 9c4d9c75316368043d4c139dccf9fc8bb7147fa5
    Related: odoo/odoo#81396
    Signed-off-by: Paolo Gatti (pgi) <pgi@odoo.com>

--- account_accountant/tests/test_reconciliation_widget.py
+++ account_accountant/tests/test_reconciliation_widget.py
@@ -2,6 +2,7 @@ import logging
 import odoo.tests
 import time
 from odoo.addons.account.tests.common import TestAccountReconciliationCommon
+from odoo import fields
 
 _logger = logging.getLogger(__name__)
 
@@ -554,3 +555,88 @@ class TestReconciliationWidget(TestAccountReconciliationCommon):
         }])
 
         self.assertEqual(invoice.amount_residual, 350)
+
+    def test_manual_writeoff_with_tax_opw2689002(self):
+        """ When the Reconciliation Model had a tax on the writeoff,
+            the journal_id wasn't populated by widget.get_reconciliation_dict_from_model()
+        """
+        today = fields.Date.today().strftime('%Y-%m-%d')
+        model = self.env["account.reconcile.model"].create({
+            'active': True,
+            'name': 'My Writeoff',
+            'sequence': 5,
+            'company_id': self.company.id,
+            'rule_type': 'writeoff_button',
+            'match_journal_ids': [(6, 0, self.bank_journal_euro.ids)],
+            'line_ids': [(0, 0, {
+                'label': 'My Writeoff line',
+                'journal_id': self.general_journal.id,
+                'account_id': self.expense_account.id,
+                'amount_type': 'percentage',
+                'amount_string': '100',
+                'tax_ids': [(6, 0, self.tax_purchase_a.ids)],
+                'force_tax_included': True,
+            })],
+        })
+        bank_statement = self.env['account.bank.statement'].create({
+            'journal_id': self.bank_journal_euro.id,
+            'date': today,
+            'name': 'Bank Statement TEST',
+            'reference': 'Useless ref',
+            'line_ids': [(0, 0, {
+                'amount': -200,
+                'payment_ref': 'payment_ref',
+                'partner_id': self.partner_agrolait.id
+            })],
+        })
+        bank_statement.button_post()
+
+        widget = self.env['account.reconciliation.widget']
+        widget.process_bank_statement_line(bank_statement.line_ids.ids, [{
+            "lines_vals_list": [{
+                "account_id": self.account_rsa.id,
+                "balance": 200,
+                "name": "payment_ref"
+            }],
+            "partner_id": self.partner_agrolait.id,
+            "to_check": False
+        }])
+        bank_statement.button_validate_or_action()
+        bank_statement_move_line = bank_statement.move_line_ids.search([("debit", "=", 200.0)])
+
+        invoice = self.env['account.move'].create({
+            'move_type': 'in_invoice',
+            'partner_id': self.partner_agrolait.id,
+            'date': today,
+            'invoice_date': today,
+            'currency_id': self.company.currency_id.id,
+            'invoice_line_ids': [(0, 0, {
+                'name': self.product_b.name,
+                'product_id': self.product_b.id,
+                'quantity': 20,
+                'tax_ids': [(6, 0, [self.tax_purchase_a.id])],
+                'price_unit': 10.0,
+            })],
+        })
+        invoice.action_post()
+        invoice_line = invoice.line_ids[-1]
+        self.assertEqual(invoice.payment_state, 'not_paid')
+
+        values = [{
+            "id": None,
+            "type": None,
+            "mv_line_ids": [bank_statement_move_line.id, invoice_line.id],
+            "new_mv_line_dicts": [{
+                'account_id': line['account_id']['id'],
+                'analytic_tag_ids': [(6, 0, line['analytic_tag_ids'] or [])],
+                'balance': line['balance'],
+                'journal_id': line['journal_id']['id'] if 'journal_id' in line else False,
+                'name': line['name'],
+                'reconcile_model_id': line['reconcile_model_id'],
+                'tax_ids': [(6, 0, [x['id'] for x in line['tax_ids']]) if 'tax_ids' in line else None],
+                'tax_repartition_line_id': line.get('tax_repartition_line_id', None),
+            } for line in widget.get_reconciliation_dict_from_model(
+                model.id, None, residual_balance=42, widget_partner_id=self.partner_agrolait.id)]
+        }]
+        widget.process_move_lines(values)
+        self.assertEqual(invoice.payment_state, 'paid')

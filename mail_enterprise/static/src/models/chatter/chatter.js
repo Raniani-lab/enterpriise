3246e8820654ce3bf128ce8c2db3c8d6bf072ef8
Louis Wicket (wil) <wil@odoo.com>
2021-12-13 16:01:01 +0000

    [IMP] mail_enterprise: remove outdated comment
    closes odoo/enterprise#22869
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- mail_enterprise/static/src/models/chatter/chatter.js
+++ mail_enterprise/static/src/models/chatter/chatter.js
@@ -5,10 +5,6 @@ import { addFields } from '@mail/model/model_core';
 // ensure that the model definition is loaded before the patch
 import '@mail/models/chatter/chatter';
 
-/**
- * This should be moved inside the mail_enterprise
- * mail_enterprise/static/src/models/chatter/chatter.js
- */
 addFields('mail.chatter', {
     /**
      * The chatter is inside .form_sheet_bg class

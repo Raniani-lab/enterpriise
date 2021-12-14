ad7f18dbfa32a52781a24536bb3256ba8c3110da
Prakash Prajapati <ppr@odoo.com>
2021-12-14 12:14:22 +0000

    [IMP] planning, _(*): generic improvement for the planning
    _* = project_timesheet_forecast, sale_planning, project_timesheet_forecast_sale
    
    The purpose of the commit is to improve the UX and usability planning.
    
    So in this commit improve the planning analysis report, filters, form views and
    some feature usability.
    
    - Added the following filters in planning.
       - billable
       - non billable
    
    closes odoo/enterprise#23423
    task-2714137
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- planning/models/planning.py
+++ planning/models/planning.py
@@ -84,8 +84,8 @@ class Planning(models.Model):
         ('planning', 'Planning'),
         ('forecast', 'Forecast')
     ], compute='_compute_allocation_type')
-    allocated_hours = fields.Float("Allocated Hours", compute='_compute_allocated_hours', store=True, readonly=False)
-    allocated_percentage = fields.Float("Allocated Time (%)", default=100,
+    allocated_hours = fields.Float("Allocated Time", compute='_compute_allocated_hours', store=True, readonly=False)
+    allocated_percentage = fields.Float("Allocated Time %", default=100,
         compute='_compute_allocated_percentage', store=True, readonly=False,
         help="Percentage of time the employee is supposed to work during the shift.",
         group_operator="avg")

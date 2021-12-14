7d71e7ae6839a938d13c248aa36c142149544d94
Louis Wicket (wil) <wil@odoo.com>
2021-12-14 14:51:12 +0000

    [IMP] mail_enterprise, *: rename thread model
    Rename javascript model `mail.thread` to `Thread` in order to
    distinguish javascript models from python models.
    
    Part of task-2701674.
     * = account_followup, website_helpdesk_livechat
    
    closes odoo/enterprise#22910
    
    closes odoo/enterprise#22863
    
    Related: odoo/odoo#81433
    Related: odoo/odoo#81325
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- mail_enterprise/static/src/models/chatter/chatter.js
+++ mail_enterprise/static/src/models/chatter/chatter.js
@@ -5,7 +5,7 @@ import { addFields } from '@mail/model/model_core';
 // ensure that the model definition is loaded before the patch
 import '@mail/models/chatter/chatter';
 
-addFields('mail.chatter', {
+addFields('Chatter', {
     /**
      * The chatter is inside .form_sheet_bg class
      */

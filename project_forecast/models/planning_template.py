f10b576343cdd54e2de15c303802a0d721645fa6
Kamlesh Pathekar <kpt@odoo.com>
2021-12-29 05:14:46 +0000

    [IMP] project_forecast: improve shift template duplicate
    Before this commit, when we duplicate the shift template, the task and project
    set were not copied.
    
    Purpose
    =======
    When duplicating a shift template, copy their task and project too.
    
    closes #23212
    task-2725993

--- project_forecast/models/planning_template.py
+++ project_forecast/models/planning_template.py
@@ -7,9 +7,9 @@ class PlanningTemplate(models.Model):
     _inherit = 'planning.slot.template'
 
     project_id = fields.Many2one('project.project', string="Project",
-                                 company_dependent=True, domain="[('allow_forecast', '=', True)]")
+                                 company_dependent=True, domain="[('allow_forecast', '=', True)]", copy=True)
     task_id = fields.Many2one('project.task', string="Task",
-                              company_dependent=True, domain="[('project_id', '=?', project_id)]")
+                              company_dependent=True, domain="[('project_id', '=?', project_id)]", copy=True)
 
     @api.onchange('project_id')
     def _onchange_project_id(self):

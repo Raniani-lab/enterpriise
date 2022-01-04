07fbf42a658bc9dc6e0c9ee484d9ca093529e061
Eleftherios Kritsotakis <elkr@odoo.com>
2022-01-04 15:35:41 +0000

    [IMP] pos_{settle_due, loyalty}: simplify PartnerListScreen
    In this commit we follow a set of changes to
    simplify the view of PartnerListScreen. This includes:
    
    - merge loyalty points and total due in one column
    - move "settle due" button and "more info" link
      into details edit
    
    task-2669747
    
    Part-of: odoo/enterprise#23544

--- pos_settle_due/static/src/js/PartnerListScreen.js
+++ pos_settle_due/static/src/js/PartnerListScreen.js
@@ -3,14 +3,40 @@ odoo.define('pos_settle_due.PartnerListScreen', function (require) {
 
     const PartnerListScreen = require('point_of_sale.PartnerListScreen');
     const Registries = require('point_of_sale.Registries');
-    const { useListener } = require("@web/core/utils/hooks");
 
     const POSSettleDuePartnerListScreen = (PartnerListScreen) =>
         class extends PartnerListScreen {
             setup() {
                 super.setup();
-                // trigger to close this screen (from being shown as tempScreen)
-                useListener('discard', this.back);
+            }
+            get isBalanceDisplayed() {
+                return true;
+            }
+            get partnerLink() {
+                return `/web#model=res.partner&id=${this.state.editModeProps.partner.id}`;
+            }
+            async settleCustomerDue() {
+                const totalDue = this.state.editModeProps.partner.total_due;
+                const paymentMethods = this.env.pos.payment_methods.filter(
+                    (method) => this.env.pos.config.payment_method_ids.includes(method.id) && method.type != 'pay_later'
+                );
+                const selectionList = paymentMethods.map((paymentMethod) => ({
+                    id: paymentMethod.id,
+                    label: paymentMethod.name,
+                    item: paymentMethod,
+                }));
+                const { confirmed, payload: selectedPaymentMethod } = await this.showPopup('SelectionPopup', {
+                    title: this.env._t('Select the payment method to settle the due'),
+                    list: selectionList,
+                });
+                if (!confirmed) return;
+                this.state.selectedPartner = this.state.editModeProps.partner;
+                this.confirm(); // make sure the PartnerListScreen resolves and properly closed.
+                const newOrder = this.env.pos.add_new_order();
+                const payment = newOrder.add_paymentline(selectedPaymentMethod);
+                payment.set_amount(totalDue);
+                newOrder.set_partner(this.state.selectedPartner);
+                this.showScreen('PaymentScreen');
             }
             async refreshTotalDue() {
                 const partnersWithUpdatedFields = await this.rpc({

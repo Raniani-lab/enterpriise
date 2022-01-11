8bf49d107c5d06a8a9f29c77e9c8c5068eac8933
Audric Onockx (auon) <auon@odoo.com>
2022-01-11 09:54:45 +0000

    [FIX] web_map : show group names when grouping by many to many
    Steps :
    - In a project, create a task T1 with 1 assignee and anotherone T2 with to
    assignees.
    - Go to map view and group by responsible.
    
    Issue :
    - T1's group name is "Undefined"
    - T2's group name is a number
    
    Cause :
    - Assignees is a many2many field. The method called when creating
    a record assigns to those fields a list of ids i/o a tuple (list in js) [id, name].
    - So, the groups are formed interpreting the first element of this list
    as id, and the second one as name.
    - When this list only contains 1 id, there is no second element.
    - When the list contains 2 ids, the second element is an id.
    
    Fix :
    - many2many fields' values are a list of tuples.
    - Groups are formed iterating on this list.
    
    opw-2723312
    
    closes odoo/enterprise#23516
    
    X-original-commit: 1b7be6438e9b81498c7f06892491a70ab9cc8618
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- web_map/static/tests/legacy/map_view_tests.js
+++ web_map/static/tests/legacy/map_view_tests.js
@@ -17,6 +17,7 @@ QUnit.module('mapView', {
                     sequence: { string: "sequence", type: 'integer' },
                     partner_id: { string: "partner", type: "many2one", relation: 'res.partner' },
                     another_partner_id: { string: "another relation", type: "many2one", relation: 'res.partner}' },
+                    user_ids: {string: "assignees", type:"many2many", relation: 'res.users'}
                 },
                 records: [
                     { id: 1, display_name: "project", partner_id: 1 }
@@ -30,8 +31,8 @@ QUnit.module('mapView', {
 
                 twoRecords: {
                     records: [
-                        { id: 1, display_name: "FooProject", sequence: 1, partner_id: [1] },
-                        { id: 2, display_name: 'BarProject', sequence: 2, partner_id: [2] },
+                        { id: 1, display_name: "FooProject", sequence: 1, partner_id: [1], user_ids: [1] },
+                        { id: 2, display_name: 'BarProject', sequence: 2, partner_id: [2], user_ids: [1, 2] },
                     ],
                     length: 2
                 },
@@ -165,7 +166,21 @@ QUnit.module('mapView', {
                             'contact_address_complete': 'Cfezfezfefes'
                         }
                     ],
-            }
+            },
+
+            'res.users': {
+                fields: {
+                    name: { string: 'User', type: 'char' },
+                    sequence: { string: 'sequence', type: 'integer' }
+                },
+                twoRecords: {
+                    records: [
+                        { id: 1, name: 'Au', sequence: 1 },
+                        { id: 2, name: 'On', sequence: 2 },
+                        ],
+                    length: 2
+                },
+            },
         };
         testUtils.mock.patch(MapModel, {
             // set delay to 0 as _fetchCoordinatesFromAddressOSM is mocked
@@ -2165,6 +2180,46 @@ QUnit.module('mapView', {
         assert.strictEqual($(first.childNodes[0]).text(), '2', 'The number of the last record for this partner is 2, so the displayed number_icon should be 2.');
         assert.strictEqual($(first.childNodes[1]).text(), '2', 'There are two records for this partner, so the marker badge should be 2.');
         assert.strictEqual($(second.childNodes[0]).text(), '1', 'The number of the only record for this partner is 1, so the displayed number_icon should be 1.');
+
+        map.destroy();
+    });
+
+    QUnit.test('Test groupby responsible displays correct group names', async function (assert) {
+        assert.expect(2);
+
+        const tasks = this.data['project.task'].twoRecords;
+        const partners = this.data['res.partner'].twoRecordsAddressCoordinates;
+        const users = this.data['res.users'].twoRecords.records;
+        const usersNameGet = users.map(user => [user.id, user.name]);
+
+        const map = await createView({
+            View: MapView,
+            model: 'project.task',
+            data: this.data,
+            arch: '<map res_partner="partner_id" routing="true">' +
+                      '<field name="user_ids" string="User"/>' +
+                  '</map>',
+            mockRPC(route) {
+                switch (route) {
+                    case '/web/dataset/search_read':
+                        return Promise.resolve(tasks);
+                    case '/web/dataset/call_kw/res.partner/search_read':
+                        return Promise.resolve(partners);
+                    case '/web/dataset/call_kw/res.users/name_get':
+                        return Promise.resolve(usersNameGet);
+                }
+                return Promise.resolve();
+            },
+            session: {
+                map_box_token: 'token'
+            },
+            groupBy: ['user_ids'],
+        });
+
+        const [header, group1, group2] = $('.o_pin_list_container').children();
+
+        assert.strictEqual(group1.childNodes[0].textContent, users[0].name, 'The first group should be named like the first user.');
+        assert.strictEqual(group2.childNodes[0].textContent, users[1].name, 'The second group should be named like the second user.');
 
         map.destroy();
     });

f96322ab38c34963b6a3a9d4e1482d76ab4c0a1d
Daniel Blanco <daniel@blancomartin.cl>
2022-01-19 15:46:44 +0000

    [FIX] l10n_cl_edi: raise warning/errors about missing data to invoice, identifying the partner or the invoice that lacks of the data (for mass posting)
    [I18N] l10n_cl_edi: correct the translation adding additional '%s'
    define partner and company as translatable
    [I18N] l10n_cl_edi: add translation for company and partner in po and pot files
    
    closes odoo/enterprise#24541
    
    X-original-commit: 367236dbfc4c484ef69d65463f6d46b88d2b8330
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- l10n_cl_edi/models/account_move.py
+++ l10n_cl_edi/models/account_move.py
@@ -537,8 +537,9 @@ class AccountMove(models.Model):
                 not (self.partner_id.l10n_cl_dte_email and self.company_id.l10n_cl_dte_email) and
                 not self.l10n_latam_document_type_id._is_doc_type_export() and
                 not self.l10n_latam_document_type_id._is_doc_type_ticket()):
-            raise UserError(_('The %s has not a DTE email defined. This is mandatory for electronic invoicing.') %
-                            (_('partner') if not self.partner_id.l10n_cl_dte_email else _('company')))
+            raise UserError(_('The %s %s has not a DTE email defined. This is mandatory for electronic invoicing.') %
+                            (_('partner') if not (self.partner_id.l10n_cl_dte_email or
+                                                  self.commercial_partner_id.l10n_cl_dte_email) else _('company'), self.partner_id.name))
         if datetime.strptime(self._get_cl_current_strftime(), '%Y-%m-%dT%H:%M:%S').date() < self.invoice_date:
             raise UserError(
                 _('The stamp date and time cannot be prior to the invoice issue date and time. TIP: check '
@@ -564,25 +565,25 @@ class AccountMove(models.Model):
                 not self.partner_id.l10n_cl_activity_description):
             raise UserError(_(
                 'There is not an activity description configured in the '
-                'customer record. This is mandatory for electronic invoicing for this type of '
-                'document. Please go to the partner record and set the activity description'))
+                'customer %s record. This is mandatory for electronic invoicing for this type of '
+                'document. Please go to the partner record and set the activity description') % self.partner_id.name)
         if not self.l10n_latam_document_type_id._is_doc_type_electronic_ticket() and not self.partner_id.street:
             raise UserError(_(
-                'There is no address configured in your customer record. '
+                'There is no address configured in your customer %s record. '
                 'This is mandatory for electronic invoicing for this type of document. '
-                'Please go to the partner record and set the address'))
+                'Please go to the partner record and set the address') % self.partner_id.name)
         if (self.l10n_latam_document_type_id.code in ['34', '41', '110', '111', '112'] and
                 self.amount_untaxed != self.amount_total):
-            raise UserError(_('It seems that you are using items with taxes in exempt documents in invoice %s.'
+            raise UserError(_('It seems that you are using items with taxes in exempt documents in invoice %s - %s.'
                               ' You must either:\n'
                               '   - Change the document type to a not exempt type.\n'
                               '   - Set an exempt fiscal position to remove taxes automatically.\n'
                               '   - Use products without taxes.\n'
-                              '   - Remove taxes from product lines.') % self.name)
+                              '   - Remove taxes from product lines.') % (self.id, self.name))
         if self.l10n_latam_document_type_id.code == '33' and self.amount_untaxed == self.amount_total:
-            raise UserError(_('All the items you are billing in invoice %s, have no taxes.\n'
+            raise UserError(_('All the items you are billing in invoice %s - %s, have no taxes.\n'
                               ' If you need to bill exempt items you must either use exempt invoice document type (34),'
-                              ' or at least one of the items should have vat tax.') % self.name)
+                              ' or at least one of the items should have vat tax.') % (self.id, self.name))
 
         self._l10n_cl_edi_validate_boletas()
 

187716165cf85fe323cfdb89d50101cddf1c81c1
Yannick Tivisse <yti@odoo.com>
2022-01-27 10:14:37 +0000

    [FIX] hr_contract: Exclude gaps on first contract date computation
    Purpose
    =======
    
    This field is used for several things, like the simple holiday pay recovery.
    
    Normally, we expect the HR officers to create a new employee, instead of
    unarchiving the old one, but this could happen.
    
    If someone leaves the company, and come back, take the new entry date as
    first contract date instead of the original one.
    
    closes odoo/enterprise#23790
    
    Taskid: 2745947
    X-original-commit: 8db4050ead0cd88b6cdef07af07e0cb833de8c06
    Related: odoo/odoo#83493
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- l10n_be_hr_payroll/models/hr_payslip.py
+++ l10n_be_hr_payroll/models/hr_payslip.py
@@ -390,7 +390,7 @@ class Payslip(models.Model):
         # If any day in the month is not covered by the contract dates coverage
         # the entire month is not taken into account for the proratization
         contracts = self.employee_id.contract_ids.filtered(lambda c: c.state not in ['draft', 'cancel'] and c.structure_type_id == self.struct_id.type_id)
-        first_contract_date = self.contract_id.first_contract_date
+        first_contract_date = self.contract_id.employee_id._get_first_contract_date(no_gap=False)
         if not contracts or not first_contract_date:
             return 0.0
         # Only employee with at least 6 months of XP can benefit from the 13th month bonus
@@ -401,7 +401,7 @@ class Payslip(models.Model):
                 or (first_contract_date.month > 7)):
             return 0.0
 
-        date_from = self.contract_id.first_contract_date
+        date_from = first_contract_date
         date_to = self.date_to + relativedelta(day=31)
 
         basic = self.contract_id._get_contract_wage()
@@ -415,6 +415,10 @@ class Payslip(models.Model):
             n_months = min(12, self._compute_number_complete_months_of_work(date_from, date_to, contracts))
             # 2. Deduct absences
             presence_prorata = self._compute_presence_prorata(date_from, date_to, contracts)
+
+        # Could happen for contracts with gaps
+        if n_months < 6:
+            return 0.0
 
         fixed_salary = basic * n_months / 12 * presence_prorata
 

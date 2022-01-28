23031c9802485527ffa0da03e7c8a8b22429db73
Victor Feyens <vfe@odoo.com>
2022-01-28 14:37:36 +0000

    [IMP] *sale*: clean sale report model & API
    following methods changes in community
    
    * remove unused join on crm teams defined in website_sale_dashboard
    * Remove the redefinition of invoice_status in website_sale_dashboard
    and make it depend on sale_enterprise
    * adapt to the new API & hooks in the base sale module
    
    closes odoo/enterprise#24626
    
    Related: odoo/odoo#85125
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- website_sale_dashboard/__manifest__.py
+++ website_sale_dashboard/__manifest__.py
@@ -9,7 +9,7 @@ This module adds a new dashboard view in the Website application.
 This new type of view contains some basic statistics, a graph, and a pivot subview that allow you to get a quick overview of your online sales.
 It also provides new tools to analyse your data.
     """,
-    'depends': ['website_sale', 'web_dashboard'],
+    'depends': ['website_sale', 'web_dashboard', 'sale_enterprise'],
     'data': [
         'views/dashboard_view.xml',
         'views/res_config_settings_views.xml'

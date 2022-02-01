53511e8eeb43a9aef9dd8eeeba4ccd123a57a037
Kartik Chavda <kcv@odoo.com>
2022-02-01 11:42:00 +0000

    [ADD] sale_project_forecast: write unit test
    closes: #23374
    task-2732325
    
    closes odoo/enterprise#24810
    
    X-original-commit: 38ba7f756f7e71acba9a697518faa3c09cb80f8d
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- sale_project_forecast/tests/test_sale_planning.py
+++ sale_project_forecast/tests/test_sale_planning.py
@@ -28,6 +28,29 @@ class TestSaleForecast(TestSalePlanning):
             'product_uom_qty': 10,
         })
 
+        product_task_in_project1 = cls.env['product.product'].create({
+            'name': 'Task in Project',
+            'type': 'service',
+            'service_tracking': 'task_in_project',
+        })
+        product_task_in_project2 = cls.env['product.product'].create({
+            'name': 'Task in Project 2',
+            'type': 'service',
+            'service_tracking': 'task_in_project',
+        })
+        sale_order = cls.env['sale.order'].create({
+            'partner_id': cls.planning_partner.id,
+        })
+        cls.sale_order_line1 = cls.env['sale.order.line'].create({
+            'order_id': sale_order.id,
+            'product_id': product_task_in_project1.id,
+        })
+        cls.sale_order_line2 = cls.env['sale.order.line'].create({
+            'order_id': sale_order.id,
+            'product_id': product_task_in_project2.id,
+        })
+        sale_order.action_confirm()
+
     @classmethod
     def setUpEmployees(cls):
         super().setUpEmployees()
@@ -55,3 +78,27 @@ class TestSaleForecast(TestSalePlanning):
             Slot.action_plan_sale_order(view_domain=[('start_datetime', '=', '2021-07-25 00:00:00'), ('end_datetime', '=', '2021-07-31 23:59:59')])
         slot = so.order_line.planning_slot_ids.filtered('start_datetime')
         self.assertEqual(slot.employee_id, self.employee_wout, 'Planning should be assigned to the employee with sol\'s product role as default role')
+
+    def test_sale_line_id_value_depending_project_and_task(self):
+        line1_project = self.sale_order_line1.project_id
+        line1_task = self.sale_order_line1.task_id
+        line2_project = self.sale_order_line2.project_id
+        line2_task = self.sale_order_line2.task_id
+
+        slot1 = self.env['planning.slot'].create({
+            'project_id': line1_project.id,
+        })
+        self.assertEqual(slot1.sale_line_id, line1_project.sale_line_id, 'Sale order item of Planning should be same as project')
+
+        slot1.write({'project_id': line2_project.id})
+        #changing project of slot should not change to new project's sol if sol of slot is already set
+        self.assertEqual(slot1.sale_line_id, line1_project.sale_line_id, 'Sale order item of Planning should not change to new project\'s sol if it\'s already set')
+
+        slot2 = self.env['planning.slot'].create({
+            'task_id': line2_task.id,
+        })
+        self.assertEqual(slot2.sale_line_id, line2_task.sale_line_id, 'Sale order item of Planning should be same as task\'s sol')
+
+        slot2.write({'task_id': line1_task.id})
+        #changing task of slot should not change to new task's sol if sol of slot is already set
+        self.assertEqual(slot2.sale_line_id, line2_task.sale_line_id, 'Sale order item of Planning should not change to new task\'s sol if it\'s already set')

5d0d438425ebaaead0a90207dbab4fc0420d88bc
Camille Spiritus <casp@odoo.com>
2022-02-01 15:00:53 +0000

    [FIX] account_reports: default selected journal Group = first in list
    The chosen default behaviour for the Journal Groups options in the Accounting reports used to be that if no previous options were to be retrieved, the first existing Journal Group would be selected.
    
    883a01847c12b2603ff86adce4e856d2e96c2a2a changed this default behaviour. This PR re-introduces it.
    
    task-2749426
    
    closes odoo/enterprise#24978
    
    X-original-commit: 3cbf8e8ee2a0c4022310c56d2b5884677d494a6b
    Signed-off-by: Olivier Colson <oco@odoo.com>

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -92,29 +92,37 @@ class AccountReport(models.AbstractModel):
             return
         all_journal_groups = self._get_filter_journal_groups()
         all_journals = self._get_filter_journals()
-        journal_map = {}
         journals_sel = []
-        journals_sel_ids = []
-        if previous_options and previous_options.get('journals'):
-            journal_map = dict((opt['id'], opt['selected']) for opt in previous_options['journals'] if opt['id'] != 'divider' and 'selected' in opt)
-            for journal in previous_options['journals']:
-                if journal.get('selected'):
-                    journals_sel.append(journal)
-                    journals_sel_ids.append(journal.get('id'))
-
-        # Compute the displayed options in the Journals dropdown menu.
         options['journals'] = []
-
         group_selected = False
+        if previous_options and previous_options.get('journals'):
+            for journal in previous_options['journals']:
+                if isinstance(journal.get('id'), int) and journal.get('id') in all_journals.ids and journal.get('selected'):
+                    journals_sel.append(journal)
+        # In case no previous_options exist, the default behaviour is to select the first Journal Group that exists.
+        elif all_journal_groups:
+            selected_journals = all_journals - all_journal_groups[0].excluded_journal_ids
+            for journal in selected_journals:
+                journals_sel.append({
+                    'id': journal.id,
+                    'name': journal.name,
+                    'code': journal.code,
+                    'type': journal.type,
+                    'selected': True,
+                })
+        # Create the dropdown menu
         if all_journal_groups:
             options['journals'].append({'id': 'divider', 'name': _('Journal Groups')})
             for group in all_journal_groups:
-                group_ids = (all_journals - group.excluded_journal_ids).ids
-                if not group_selected and journals_sel_ids == group_ids:
+                group_journal_ids = (all_journals.filtered(lambda x: x.company_id == group.company_id) - group.excluded_journal_ids).ids
+                if not group_selected and journals_sel \
+                        and len(journals_sel) == len(group_journal_ids) \
+                        and all(journal_opt['id'] in group_journal_ids for journal_opt in journals_sel):
                     group_selected = group
-                options['journals'].append({'id': 'group', 'name': group.name, 'ids': group_ids})
+                options['journals'].append({'id': 'group', 'name': group.name, 'ids': group_journal_ids})
 
         previous_company = False
+        journals_selection = {opt['id'] for opt in journals_sel} # If empty: means everything is selected
         for journal in all_journals:
             if journal.company_id != previous_company:
                 options['journals'].append({'id': 'divider', 'name': journal.company_id.name})
@@ -124,7 +132,7 @@ class AccountReport(models.AbstractModel):
                 'name': journal.name,
                 'code': journal.code,
                 'type': journal.type,
-                'selected': journal_map.get(journal.id),
+                'selected': journal.id in journals_selection or not journals_selection,
             })
 
         # Compute the displayed option name
@@ -137,7 +145,8 @@ class AccountReport(models.AbstractModel):
         elif len(journals_sel) == 6:
             options['name_journal_group'] = ', '.join(jrnl['code'] for jrnl in journals_sel) + _(" and one other")
         else:
-            options['name_journal_group'] = ', '.join(jrnl['code'] for jrnl in journals_sel) + _(" and %s others", len(journals_sel) - 5)
+            options['name_journal_group'] = ', '.join(jrnl['code'] for jrnl in journals_sel[:5]) + _(" and %s others",
+                                                                                                     len(journals_sel) - 5)
 
     @api.model
     def _get_options_journals(self, options):

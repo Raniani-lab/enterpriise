cdab1fb41c9f374c2e36dec6e506d956b3db5f52
William Braeckman <wbr@odoo.com>
2022-02-03 14:03:18 +0000

    [FIX] hr_contract_sign: fix traceback
    Fixes a traceback when doing a sign request on a contract due to
    invalid python code.
    
    TaskId-2753339
    
    closes odoo/enterprise#24048
    
    X-original-commit: c0816a4e231dd1475d6eddbc25a73b9125d9038a
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- hr_contract_sign/wizard/hr_contract_sign_document_wizard.py
+++ hr_contract_sign/wizard/hr_contract_sign_document_wizard.py
@@ -117,7 +117,7 @@ class HrContractSignDocumentWizard(models.TransientModel):
             res = sign_request.initialize_new(
                 template_id=sign_request_values[0].id,
                 signers=sign_request_values[1],
-                followers=self.follower_ids.ids.append(self.responsible_id.partner_id.id),
+                followers=self.follower_ids.ids + [self.responsible_id.partner_id.id],
                 reference=_('Signature Request - %s', sign_request_values[0].name),
                 subject=self.subject,
                 message=self.message

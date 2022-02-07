e7e7d547af4be04023a809c3e3e94edd271dae3b
Kevin Baptiste <kba@odoo.com>
2022-02-07 11:06:20 +0000

    [IMP] data_cleaning: set proper ondelete rule
    The field `cleaning_model_id` on `data_cleaning.record` was set to `set
    null` which doesn't make sense in this case.
    
    closes odoo/enterprise#24055
    
    Taskid: 2755289
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- data_cleaning/models/data_cleaning_record.py
+++ data_cleaning/models/data_cleaning_record.py
@@ -13,7 +13,7 @@ class DataCleaningRecord(models.Model):
     name = fields.Char('Record Name', compute='_compute_values', compute_sudo=True)
     rule_ids = fields.Many2many('data_cleaning.rule', string='Rule', required=True, ondelete='cascade')
     field_id = fields.Many2one('ir.model.fields', string='Field')
-    cleaning_model_id = fields.Many2one('data_cleaning.model', string='Cleaning Model')
+    cleaning_model_id = fields.Many2one('data_cleaning.model', string='Cleaning Model', ondelete='cascade')
     field_name = fields.Char(related='field_id.name')
     action = fields.Char('Actions', compute='_compute_values', compute_sudo=True)
 

e7b8e57e51894786eec2c93884d02f653e85e425
Audric Onockx (auon) <auon@odoo.com>
2022-02-07 11:15:21 +0000

    [FIX] sale_renting : display correct return time in sol description
    Steps :
    Create a Rental Order.
    Add a SOL with Product Meeting Room.
    In the wizard, as Dates : Tomorrow 10:00 - Tomorrow 12:00.
    Validate.
    
    Issue :
    See, in SOL Description : "Meeting Room 02/08/2022 10:00:00 to 13:00:00".
    Expected : "... to 12:00:00".
    
    Cause :
    In previous versions, user tz was not taken into account in format_time,
    so we used to first convert it to user tz before passing it to this method.
    
    Fix :
    Now, user tz is taken into account in format_time, so we don't convert
    it.
    
    closes odoo/enterprise#24305
    
    Opw: 2746372
    X-original-commit: b8bcc2a6ef08e932794c81876b0881682ea8daad
    Signed-off-by: Onockx Audric (auon) <auon@odoo.com>
    Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

--- sale_renting/models/sale.py
+++ sale_renting/models/sale.py
@@ -222,10 +222,7 @@ class RentalOrderLine(models.Model):
         if self.pickup_date.replace(tzinfo=UTC).astimezone(timezone(self.env.user.tz or 'UTC')).replace(tzinfo=None).date()\
              == self.return_date.replace(tzinfo=UTC).astimezone(timezone(self.env.user.tz or 'UTC')).replace(tzinfo=None).date():
             # If return day is the same as pickup day, don't display return_date Y/M/D in description.
-            return_date = self.return_date
-            if return_date:
-                return_date = return_date.replace(tzinfo=UTC).astimezone(timezone(self.env.user.tz or 'UTC')).replace(tzinfo=None)
-            return_date_part = format_time(self.with_context(use_babel=True).env, return_date, tz=self.env.user.tz, time_format=False)
+            return_date_part = format_time(self.with_context(use_babel=True).env, self.return_date, tz=self.env.user.tz, time_format=False)
         else:
             return_date_part = format_datetime(self.with_context(use_babel=True).env, self.return_date, tz=self.env.user.tz, dt_format=False)
 

61c7cd9c908985a2167286dac31772484e2196e1
momegahed <mome@odoo.com>
2022-02-07 16:01:58 +0000

    [FIX] sale_subscription: intrastat code isn't set
    If applied, this commit will fix the following bug by adding
    intrastat country to generated invoices if the partner country is
    an intrastat country.
    
    Steps to reproduce:
    
    1- install subscriptions - intrastat
    2- Create a user u from an intrastat country
    3- Create a new subscription for u with a payment plan
    4- edit the payment plan to anything other than manual
    5- click on "Generate Invoice" action button
    6- the generated Invoice doesn't have intrastat country set
    
    Bug:
    
    the ```intrastat_country_id``` is not being set while creating
    the invoice
    
    Fix:
    
    Add intrastat_country_id to invoice
    
    OPW-2693107
    
    closes odoo/enterprise#24663
    
    X-original-commit: 9672179fe9e8c2e0e5517a3071a5f4cd9d71ee89
    Signed-off-by: William André (wan) <wan@odoo.com>

--- sale_intrastat/models/account_move.py
+++ sale_intrastat/models/account_move.py
@@ -7,17 +7,9 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    intrastat_country_id = fields.Many2one(
-        compute='_compute_intrastat_country_id', store=True)
-
     def _get_invoice_intrastat_country_id(self):
         # OVERRIDE
         self.ensure_one()
         if self.is_sale_document():
             return self.partner_shipping_id.country_id.id
         return super(AccountMove, self)._get_invoice_intrastat_country_id()
-
-    @api.depends('partner_shipping_id')
-    def _compute_intrastat_country_id(self):
-        for move in self:
-            move.intrastat_country_id = move._get_invoice_intrastat_country_id()

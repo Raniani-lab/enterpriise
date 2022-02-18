270f224721fc9826a2062f78f1e22ffd2be55fb5
dht-odoo <dht@odoo.com>
2022-02-18 13:05:43 +0000

    [FIX] website_calendar: allow user to create appointment type
    Before this commit user was not able to create appointment type
    from website if user's timezone is not set as, appointment_tz
    is a required field in 'calendar.appointment.type'.
    
    This commit will allow user to create appointment typw
    also if user's timezone is not set. For that we have
    create_and_get_website_url method and assigning
    tz to appointment_tz. For tz value we have used the
    website_visitors timezone as once the visitor visits
    the website, his timezone is stored in cookies.
    So, we are using that tz here.
    
    TaskId-2745881
    
    closes odoo/enterprise#25329
    
    X-original-commit: 6bc03dcd48f4e3d84283286286d2a7b8feaa890d
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- website_appointment/tests/test_appointment.py
+++ website_appointment/tests/test_appointment.py
@@ -2,11 +2,46 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.addons.appointment.tests.common import AppointmentCommon
+from odoo.addons.website.tests.test_website_visitor import MockVisitor
 from odoo.exceptions import ValidationError
+from odoo.tests.common import new_test_user
 from odoo.tests import users
 
 
-class WebsiteAppointmentTest(AppointmentCommon):
+class WebsiteAppointmentTest(AppointmentCommon, MockVisitor):
+
+    def test_create_appointment_type_from_website(self):
+        """ Test that when creating an appointment type from the website, we use
+        the visitor's timezone as fallback for the user's timezone """
+        user = new_test_user(self.env, "test_user_1", groups="appointment.group_calendar_manager", email="test_user_1@nowhere.com", password="P@ssw0rd!", tz="")
+        visitor = self.env['website.visitor'].create({"name": 'Test Visitor', "partner_id": user.partner_id.id})
+        AppointmentType = self.env['calendar.appointment.type']
+        with self.mock_visitor_from_request(force_visitor=visitor):
+            # Test appointment timezone when user and visitor both don't have timezone
+            AppointmentType.with_user(user).create_and_get_website_url(**{'name': 'Appointment UTC Timezone'})
+            self.assertEqual(
+                AppointmentType.search([
+                    ('name', '=', 'Appointment UTC Timezone')
+                ]).appointment_tz, 'UTC'
+            )
+
+            # Test appointment timezone when user doesn't have timezone and visitor have timezone
+            visitor.timezone = 'Europe/Brussels'
+            AppointmentType.with_user(user).create_and_get_website_url(**{'name': 'Appointment Visitor Timezone'})
+            self.assertEqual(
+                AppointmentType.search([
+                    ('name', '=', 'Appointment Visitor Timezone')
+                ]).appointment_tz, visitor.timezone
+            )
+
+            # Test appointment timezone when user has timezone
+            user.tz = 'Asia/Calcutta'
+            AppointmentType.with_user(user).create_and_get_website_url(**{'name': 'Appointment User Timezone'})
+            self.assertEqual(
+                AppointmentType.search([
+                    ('name', '=', 'Appointment User Timezone')
+                ]).appointment_tz, user.tz
+            )
 
     @users('admin')
     def test_is_published_custom_appointment_type(self):

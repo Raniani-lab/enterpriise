fde4f784e0afbe723e27014e7a6be06719cf26a0
Nicolas Pierre <npi@odoo.com>
2022-02-21 14:39:22 +0000

    [ADD] mrp_subcontracting_quality: new module
    Add a new bridge module to allow subcontractors to delete the move lines they
    created when quality module is installed.
    
    closes odoo/enterprise#24647
    
    Task: 2645298
    Community-pr: https://github.com/odoo/odoo/pull/83465
    Enterprise-pr: https://github.com/odoo/enterprise/pull/24647
    Related: odoo/upgrade#3402
    Related: odoo/odoo#83465
    Signed-off-by: Arnold Moyaux <arm@odoo.com>

--- quality_control/models/stock_move_line.py
+++ quality_control/models/stock_move_line.py
@@ -37,11 +37,14 @@ class StockMoveLine(models.Model):
         return super().write(vals)
 
     def unlink(self):
-        self.check_ids.filtered(lambda qc: qc._check_to_unlink()).unlink()
+        self._unlink_quality_check()
         return super(StockMoveLine, self).unlink()
 
     def action_open_quality_check_wizard(self):
         return self.check_ids.action_open_quality_check_wizard()
+
+    def _unlink_quality_check(self):
+        self.check_ids.filtered(lambda qc: qc._check_to_unlink()).unlink()
 
     def _create_quality_check_at_write(self, vals):
         return vals.get('qty_done')
@@ -73,7 +76,7 @@ class StockMoveLine(models.Model):
         for ml in self:
             quality_points_product = quality_points_by_product_picking_type.get((ml.product_id, ml.move_id.picking_type_id), set())
             quality_points_all_products = ml._get_quality_points_all_products(quality_points_by_product_picking_type)
-            quality_points = self.env['quality.point'].search([('id', 'in', list(quality_points_product | quality_points_all_products))])
+            quality_points = self.env['quality.point'].sudo().search([('id', 'in', list(quality_points_product | quality_points_all_products))])
             for quality_point in quality_points:
                 if quality_point.check_execute_now():
                     check_values = ml._get_check_values(quality_point)

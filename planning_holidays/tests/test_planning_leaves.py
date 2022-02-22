5fb9a057a8f02df4ded9ea10fda1270a4353ff81
Kamlesh Pathekar <kpt@odoo.com>
2022-02-22 06:53:07 +0000

    [IMP] planning_holidays: update the slot duration and working day count
    Reproducing Steps:
    -Installed Planning App
    -Visible working_days_count field in the slot
    -Create Slot-(10-1-2021 to 14-1-2021) One Week(5-day working)
    -Add public holiday(12-1-2021)
    -If we check the working day of the slot, 5 days appear but 4 should appear
    
    Prior to this commit:
    The calculation of the working days not taking leaves into account
    for working day slots.
    
    After this commit:
    Calling the method _compute_allocated_hours and _compute_working_days_count
    methods for impacted shifts of planning.slot when leave is added, validated
    and the date of leave is changed. So that allocated_hours and working_days_count
    include holiday hours.
    
    task-2731462
    
    closes odoo/enterprise#24619
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- planning_holidays/tests/test_planning_leaves.py
+++ planning_holidays/tests/test_planning_leaves.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import datetime
 
+from freezegun import freeze_time
 from .test_common import TestCommon
 
 
@@ -81,3 +82,54 @@ class TestPlanningLeaves(TestCommon):
         })
         self.assertEqual(slot_3.with_context(include_past=True).leave_warning, "patrick is on time off from the 01/06/2020 to the 01/10/2020. \n",
                          "should show the start of the 1st leave and end of the 2nd")
+
+    def test_shift_update_according_time_off(self):
+        """ working day and allocated hours of planning slot are update according to public holiday
+        Test Case
+        ---------
+            1) Create slot
+            2) Add public holiday
+            3) Checked the allocated hour and working days count of slot
+            4) Unlink the public holiday
+            5) Checked the allocated hour and working days count of slot
+        """
+        with freeze_time("2020-04-10"):
+            today = datetime.datetime.today()
+            self.env.cr._now = today # used to force create_date, as sql is not wrapped by freeze gun
+
+            ethan = self.env['hr.employee'].create({
+                'create_date': today,
+                'name': 'ethan',
+                'tz': 'UTC',
+                'employee_type': 'freelance',
+            })
+
+            slot = self.env['planning.slot'].create({
+                'resource_id': ethan.resource_id.id,
+                'employee_id': ethan.id,
+                'start_datetime': datetime.datetime(2020, 4, 20, 8, 0), # Monday
+                'end_datetime': datetime.datetime(2020, 4, 24, 17, 0),
+            })
+
+            initial_slot = {
+                'working_day': slot.working_days_count,
+                'allocated_hours': slot.allocated_hours,
+            }
+
+            # Add the public holiday
+            public_holiday = self.env['resource.calendar.leaves'].create({
+                'name': 'Public holiday',
+                'calendar_id': ethan.resource_id.calendar_id.id,
+                'date_from': datetime.datetime(2020, 4, 21, 8, 0), # Wednesday
+                'date_to': datetime.datetime(2020, 4, 21, 17, 0),
+            })
+
+            self.assertNotEqual(slot.working_days_count, initial_slot.get('working_day'), 'Working days should be updated')
+            self.assertNotEqual(slot.allocated_hours, initial_slot.get('allocated_hours'), 'Allocated hours should be updated')
+
+            # Unlink the public holiday
+            public_holiday.unlink()
+            self.assertDictEqual(initial_slot, {
+                'working_day': slot.working_days_count,
+                'allocated_hours': slot.allocated_hours
+                }, "The Working days and Allocated hours should be updated")

7bef4dc5bb70b2d6c72e087b58227de0a565221b
Thibault Delavallée <tde@odoo.com>
2022-03-02 12:46:53 +0000

    [FW][MOV] appointment(_hr): extract work internal availability internal method
    Currently availability check is done in a big ``_is_staff_user_available``
    method. Splitting its content eases code understanding and prepares incoming
    improvements about performance.
    
    Purpose is to ease code reading and understanding by moving a strange internal
    method checking working hours availability into its own model method. Its code
    is left untouched, just moved. Docstrings are added and updated while moving
    the code.
    
    This eases future improvements for performance as well as forward port.
    
    Task-2766574 (Appointment: improve performance)
    
    X-original-commit: odoo/enterprise@385c064027b6c60bad45237cacad24cca8c8fa74
    Part-of: odoo/enterprise#25002

--- appointment_hr/models/calendar_appointment_type.py
+++ appointment_hr/models/calendar_appointment_type.py
@@ -53,47 +53,59 @@ class CalendarAppointmentType(models.Model):
         if not self.work_hours_activated:
             return is_calendar_free
 
-        def is_work_available(start_dt, end_dt, intervals):
-            """ check if the slot is contained in the employee's work hours (defined by intervals)
-            """
-            def find_start_index():
-                """ find the highest index of intervals for which the start_date (element [0]) is before (or at) start_dt
-                """
-                def recursive_find_index(lower_bound, upper_bound):
-                    if upper_bound - lower_bound <= 1:
-                        if intervals[upper_bound][0] <= start_dt:
-                            return upper_bound
-                        return lower_bound
-                    index = (upper_bound + lower_bound) // 2
-                    if intervals[index][0] <= start_dt:
-                        return recursive_find_index(index, upper_bound)
-                    else:
-                        return recursive_find_index(lower_bound, index)
-
-                if start_dt <= intervals[0][0] - tolerance:
-                    return -1
-                if end_dt >= intervals[-1][1] + tolerance:
-                    return -1
-                return recursive_find_index(0, len(intervals) - 1)
-
-            if not intervals:
-                return False
-
-            tolerance = timedelta(minutes=1)
-            start_index = find_start_index()
-            if start_index != -1:
-                for index in range(start_index, len(intervals)):
-                    if intervals[index][1] >= end_dt - tolerance:
-                        return True
-                    if len(intervals) == index + 1 or intervals[index + 1][0] - intervals[index][1] > tolerance:
-                        return False
-            return False
-
         if is_calendar_free and staff_user.sudo().employee_id:
             # The user is free but he has a configured employee, let's check if the slot fits into his working schedule
-            return is_work_available(slot['UTC'][0], slot['UTC'][1], availability_additional_values['work_schedules'].get(staff_user.id, False))
+            return self._slot_availability_is_user_working(slot['UTC'][0], slot['UTC'][1], availability_additional_values['work_schedules'].get(staff_user.id, False))
         else:
             return is_calendar_free
+
+    def _slot_availability_is_user_working(self, start_dt, end_dt, intervals):
+        """ Check if the slot is contained in the given work hours (defined by
+        intervals). Those are linked to a given employee (user with working hours
+        activated).
+
+        TDE NOTE: internal method ``is_work_available`` of ``_slots_available``
+        made as explicit method in 15.0 but left untouched. To clean in 15.3+.
+
+        :param datetime start_dt: beginning of slot boundary. Not timezoned UTC;
+        :param datetime end_dt: end of slot boundary. Not timezoned UTC;
+        :param intervals: list of tuples defining working hours boundaries. If no
+          intervals are given we consider employee does not work during this slot.
+          See ``Resource._work_intervals_batch()`` for more details;
+
+        :return bool: whether employee is available for this slot;
+        """
+        def find_start_index():
+            """ find the highest index of intervals for which the start_date
+            (element [0]) is before (or at) start_dt """
+            def recursive_find_index(lower_bound, upper_bound):
+                if upper_bound - lower_bound <= 1:
+                    if intervals[upper_bound][0] <= start_dt:
+                        return upper_bound
+                    return lower_bound
+                index = (upper_bound + lower_bound) // 2
+                if intervals[index][0] <= start_dt:
+                    return recursive_find_index(index, upper_bound)
+                return recursive_find_index(lower_bound, index)
+
+            if start_dt <= intervals[0][0] - tolerance:
+                return -1
+            if end_dt >= intervals[-1][1] + tolerance:
+                return -1
+            return recursive_find_index(0, len(intervals) - 1)
+
+        if not intervals:
+            return False
+
+        tolerance = timedelta(minutes=1)
+        start_index = find_start_index()
+        if start_index != -1:
+            for index in range(start_index, len(intervals)):
+                if intervals[index][1] >= end_dt - tolerance:
+                    return True
+                if len(intervals) == index + 1 or intervals[index + 1][0] - intervals[index][1] > tolerance:
+                    return False
+        return False
 
     @api.model
     def _prepare_availability_additional_values(self, available_staff_users, first_day, last_day):

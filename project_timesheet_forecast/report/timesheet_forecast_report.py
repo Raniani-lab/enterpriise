295a1759e77a331e0ead2e8b8df4a0ae9e40f9cd
Barad Mahendra <mba@openerp.com>
2022-03-10 12:47:51 +0000

    [IMP] project_timesheet_forecast(_sale): refactor the timesheet and planning analysis report
    Purpose of the task is to technically refactor the timesheet and planning
    analysis report.
    
    So in this commit, split the query into a method so it will be easily
    overrided by the other modules and no need to re-write the whole query again.
    
    task-2791087
    
    closes odoo/enterprise#25183
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- project_timesheet_forecast/report/timesheet_forecast_report.py
+++ project_timesheet_forecast/report/timesheet_forecast_report.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import tools
-from odoo import api, fields, models
-
 from psycopg2 import sql
+
+from odoo import api, fields, models, tools
 
 class TimesheetForecastReport(models.Model):
 
@@ -25,57 +24,99 @@ class TimesheetForecastReport(models.Model):
     difference = fields.Float('Remaining Hours', readonly=True)
     user_id = fields.Many2one('res.users', string='Assigned to', readonly=True)
 
-    def init(self):
-        tools.drop_view_if_exists(self.env.cr, self._table)
-        query = """
+    @api.model
+    def _select(self):
+        select_str = """
+            SELECT
+                d::date AS entry_date,
+                F.employee_id AS employee_id,
+                E.company_id AS company_id,
+                F.task_id AS task_id,
+                F.project_id AS project_id,
+                F.user_id AS user_id,
+                0.0 AS effective_hours,
+                F.allocated_hours / NULLIF(F.working_days_count, 0) AS planned_hours,
+                F.allocated_hours / NULLIF(F.working_days_count, 0) AS difference,
+                'forecast' AS line_type,
+                F.id AS id
+        """
+        return select_str
+
+    @api.model
+    def _from(self):
+        from_str = """
+            FROM generate_series(
+                (SELECT min(start_datetime) FROM planning_slot)::date,
+                (SELECT max(end_datetime) FROM planning_slot)::date,
+                '1 day'::interval
+            ) d
+                LEFT JOIN planning_slot F ON d::date >= F.start_datetime::date AND d::date <= F.end_datetime::date
+                LEFT JOIN hr_employee E ON F.employee_id = E.id
+                LEFT JOIN resource_resource R ON E.resource_id = R.id
+        """
+        return from_str
+
+    @api.model
+    def _where(self):
+        where_str = """
+            WHERE
+                EXTRACT(ISODOW FROM d.date) IN (
+                    SELECT A.dayofweek::integer+1 FROM resource_calendar_attendance A WHERE A.calendar_id = R.calendar_id
+                )
+        """
+        return where_str
+
+    @api.model
+    def _select_union(self):
+        select_str = """
+            SELECT
+                A.date AS entry_date,
+                E.id AS employee_id,
+                E.company_id AS company_id,
+                A.task_id AS task_id,
+                A.project_id AS project_id,
+                A.user_id AS user_id,
+                A.unit_amount / UOM.factor * HOUR_UOM.factor AS effective_hours,
+                0.0 AS planned_hours,
+                -A.unit_amount / UOM.factor * HOUR_UOM.factor AS difference,
+                'timesheet' AS line_type,
+                -A.id AS id
+        """
+        return select_str
+
+    @api.model
+    def _from_union(self):
+        from_str = """
+            FROM account_analytic_line A
+                LEFT JOIN hr_employee E ON A.employee_id = E.id
+                LEFT JOIN uom_uom UOM ON A.product_uom_id = UOM.id,
+                (
                     SELECT
-                        d::date AS entry_date,
-                        F.employee_id AS employee_id,
-                        E.company_id AS company_id,
-                        F.task_id AS task_id,
-                        F.project_id AS project_id,
-                        F.user_id AS user_id,
-                        0.0 AS effective_hours,
-                        F.allocated_hours / NULLIF(F.working_days_count, 0) AS planned_hours,
-                        F.allocated_hours / NULLIF(F.working_days_count, 0) AS difference,
-                        'forecast' AS line_type,
-                        F.id AS id
-                    FROM generate_series(
-                        (SELECT min(start_datetime) FROM planning_slot)::date,
-                        (SELECT max(end_datetime) FROM planning_slot)::date,
-                        '1 day'::interval
-                    ) d
-                        LEFT JOIN planning_slot F ON d::date >= F.start_datetime::date AND d::date <= F.end_datetime::date
-                        LEFT JOIN hr_employee E ON F.employee_id = E.id
-                        LEFT JOIN resource_resource R ON E.resource_id = R.id
-                    WHERE
-                        EXTRACT(ISODOW FROM d.date) IN (
-                            SELECT A.dayofweek::integer+1 FROM resource_calendar_attendance A WHERE A.calendar_id = R.calendar_id
-                        )
-                ) UNION (
-                    SELECT
-                        A.date AS entry_date,
-                        E.id AS employee_id,
-                        E.company_id AS company_id,
-                        A.task_id AS task_id,
-                        A.project_id AS project_id,
-                        A.user_id AS user_id,
-                        A.unit_amount / UOM.factor * HOUR_UOM.factor AS effective_hours,
-                        0.0 AS planned_hours,
-                        -A.unit_amount / UOM.factor * HOUR_UOM.factor AS difference,
-                        'timesheet' AS line_type,
-                        -A.id AS id
-                    FROM account_analytic_line A
-                        LEFT JOIN hr_employee E ON A.employee_id = E.id
-                        LEFT JOIN uom_uom UOM ON A.product_uom_id = UOM.id,
-                        (
-                            SELECT
-                                U.factor
-                            FROM uom_uom U
-                            WHERE U.id = %s
-                        ) HOUR_UOM
-                    WHERE A.project_id IS NOT NULL
+                        U.factor
+                    FROM uom_uom U
+                    WHERE U.id = %s
+                ) HOUR_UOM
         """ % (self.env.ref('uom.product_uom_hour').id)
+        return from_str
+
+    @api.model
+    def _where_union(self):
+        where_str = """
+            WHERE A.project_id IS NOT NULL
+        """
+        return where_str
+
+    def init(self):
+        query = "(%s %s %s) UNION (%s %s %s)" % (
+            self._select(),
+            self._from(),
+            self._where(),
+            self._select_union(),
+            self._from_union(),
+            self._where_union()
+        )
+
+        tools.drop_view_if_exists(self.env.cr, self._table)
         self.env.cr.execute(
             sql.SQL("CREATE or REPLACE VIEW {} as ({})").format(
                 sql.Identifier(self._table),

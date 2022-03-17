daeba9b212eb7a438869482cc5ca23acb69eb034
Florian Damhaut <flda@odoo.com>
2022-03-17 13:21:28 +0000

    [FIX] project_forecast: fix traceback on forecast for multiple task
    Step to reproduce:
    - Create task
    - Add a Planned Date
    - Create a subtask
    - Try to access forecast
    
    Current behaviour:
    - Traceback
     We use `min` on a list that may contain `False`
    
    Behaviour after PR:
    - Filter False value before and check if there is at least
     one non-False value in the lot else fallback on default action
    
    opw-2792373
    
    closes odoo/enterprise#25380
    
    X-original-commit: ebabda353666316449d940d45dea34e857931a67
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- project_forecast/models/project.py
+++ project_forecast/models/project.py
@@ -94,7 +94,8 @@ class Task(models.Model):
         if first_slot:
             action_context.update({'initialDate': first_slot.start_datetime})
         else:
-            min_date = min(allowed_tasks.mapped('planned_date_begin'))
+            planned_tasks = allowed_tasks.filtered('planned_date_begin')
+            min_date = min(planned_tasks.mapped('planned_date_begin')) if planned_tasks else False
             if min_date and min_date > datetime.datetime.now():
                 action_context.update({'initialDate': min_date})
         action['context'] = action_context

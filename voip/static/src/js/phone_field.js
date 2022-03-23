767805948398950fa5303d961428e004968a9792
luvi <luvi@odoo.com>
2022-03-23 09:53:54 +0000

    [FIX] voip: phone field _onClick triggered twice
    This commit is the counterpart of a community fix
    in the web module. The click handler is only
    triggered once and a phone call will no longer
    raise an error.
    A test has been added in the voip module to check
    that the handler is only called once.
    
    PR community: odoo/odoo#86928
    
    closes odoo/enterprise#25558
    
    X-original-commit: c4b5a2be44050004ea900c81ff6d660d8c974ed5
    Related: odoo/odoo#87196
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- voip/static/src/js/phone_field.js
+++ voip/static/src/js/phone_field.js
@@ -9,9 +9,6 @@ const Phone = basicFields.FieldPhone;
  * Override of FieldPhone to use the DialingPanel to perform calls on clicks.
  */
 Phone.include({
-    events: Object.assign({}, Phone.prototype.events, {
-        'click': '_onClick',
-    }),
 
     //--------------------------------------------------------------------------
     // Private
@@ -57,7 +54,10 @@ Phone.include({
      * @private
      * @param {MouseEvent} ev
      */
-    async _onClick(ev) {
+    async _onClickLink(ev) {
+        if (ev.target.matches("a")) {
+            ev.stopImmediatePropagation();
+        }
         if (this.mode !== 'readonly' || !window.RTCPeerConnection || !window.MediaStream || !navigator.mediaDevices) {
             return;
         }

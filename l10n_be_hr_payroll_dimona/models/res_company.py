354f81307bf8963dc7a580e3551bbd8e51cc1dba
Yannick Tivisse <yti@odoo.com>
2022-04-15 11:13:11 +0000

    [FIX] l10n_be_hr_payroll_dimona: Allow DIMONA automation
    Purpose
    =======
    
    Communicate with the web service REST API to introduce a new declaration
    or close/update/cancel an existing one.
    
    closes odoo/enterprise#28002
    
    Taskid: 2747027
    X-original-commit: 2181749ee22b42cbea21e2dd31f234cc56644281
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

---
+++ l10n_be_hr_payroll_dimona/models/res_company.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    onss_expeditor_number = fields.Char(
+        string="ONSS Expeditor Number", groups="base.group_system",
+        help="ONSS Expeditor Number provided when registering service on the technical user")
+    onss_pem_certificate = fields.Binary(
+        string="PEM Certificate", groups="base.group_system",
+        help="Certificate to allow access to batch declarations")
+    onss_pem_passphrase = fields.Char(
+        string="PEM Passphrase", groups="base.group_system",
+        help="Certificate to allow access to batch declarations")

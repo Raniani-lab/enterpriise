d441b5f2052cb6857dbe8a8cfdb1364d8452be55
Kamlesh Pathekar <kpt@odoo.com>
2022-05-02 10:37:20 +0000

    [IMP] planning: add the 'End Hour' in the planning slot template
    In this commit, an end hour field is added to the planning slot template.
    The end hour will be calculated from the duration days and start hour
    which will be based on the company's calendar.
    
    task-2831921
    
    closes odoo/enterprise#27050
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- planning/models/planning_template.py
+++ planning/models/planning_template.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import math
+import pytz
 
 from datetime import datetime, timedelta, time, date
 from odoo import api, fields, models, _
@@ -17,8 +18,10 @@ class PlanningTemplate(models.Model):
     name = fields.Char('Hours', compute="_compute_name")
     sequence = fields.Integer('Sequence', index=True)
     role_id = fields.Many2one('planning.role', string="Role")
-    start_time = fields.Float('Start Hour', default=0, group_operator=None, default_export_compatible=True)
-    duration = fields.Float('Duration (Hours)', default=0, group_operator=None, default_export_compatible=True)
+    start_time = fields.Float('Planned Hours', default=0, group_operator=None, default_export_compatible=True)
+    duration = fields.Float('Duration', default=0, group_operator=None, default_export_compatible=True)
+    end_time = fields.Float('End Hour', compute='_compute_name', group_operator=None)
+    duration_days = fields.Integer('Duration Days', compute='_compute_name')
 
     _sql_constraints = [
         ('check_start_time_lower_than_24', 'CHECK(start_time <= 24)', 'The start hour cannot be greater than 24.'),
@@ -28,15 +31,39 @@ class PlanningTemplate(models.Model):
 
     @api.depends('start_time', 'duration')
     def _compute_name(self):
+        calendar = self.env.company.resource_calendar_id
+        user_tz = pytz.timezone(self.env['planning.slot']._get_tz())
+        today = date.today()
         for shift_template in self:
             start_time = time(hour=int(shift_template.start_time), minute=round(math.modf(shift_template.start_time)[0] / (1 / 60.0)))
             duration = timedelta(hours=int(shift_template.duration), minutes=round(math.modf(shift_template.duration)[0] / (1 / 60.0)))
             end_time = datetime.combine(date.today(), start_time) + duration
+            start_datetime = user_tz.localize(datetime.combine(today, start_time))
+            shift_template.duration_days, shift_template.end_time = self._get_company_work_duration_data(calendar, start_datetime, shift_template.duration)
             shift_template.name = '%s - %s %s' % (
                 format_time(shift_template.env, start_time, time_format='short').replace(':00 ', ' '),
                 format_time(shift_template.env, end_time.time(), time_format='short').replace(':00 ', ' '),
-                _('(%s days span)') % (duration.days + 1) if duration.days > 0 else ''
+                _('(%s days span)') % (shift_template.duration_days) if shift_template.duration_days > 1 else ''
             )
+
+    @api.model
+    def _get_company_work_duration_data(self, calendar, start_datetime, duration):
+        """"
+            Taking company's working calendar into account get the `hours` and
+            `days` from start_time and duration expressed in time and hours.
+
+            :param start_time: reference time
+            :param duration: reference duration in hours
+
+            Returns a tuple (duration, end_time) expressed as days and as hours.
+        """
+        end_datetime = calendar.plan_hours(duration, start_datetime, compute_leaves=True)
+        if duration == 0 and start_datetime.hour == 0:
+            end_datetime = end_datetime.replace(hour=0)
+        return (
+            math.ceil(calendar.get_work_duration_data(start_datetime, end_datetime)['days']),
+            timedelta(hours=end_datetime.hour, minutes=end_datetime.minute).total_seconds() / 3600,
+        )
 
     def name_get(self):
         result = []

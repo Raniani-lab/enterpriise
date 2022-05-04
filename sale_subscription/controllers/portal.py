1e6e6c061fbcafca2de69a37341c0c80987423cc
Horacio Tellez <hote@odoo.com>
2022-05-04 13:02:32 +0000

    [FIX] payment: avoid a mismatch between partner's and acquirer's company
    In a multi-company environment, a partner of company A should not be able
    to make payments for company B. With this commit, if we detect a
    mismatch between the companies, a UserError is raised.
    
    Task - 2627751
    
    closes odoo/enterprise#27245
    
    X-original-commit: cd2ae071cbfbe45ba07d37d725466257633d0e05
    Related: odoo/odoo#91189
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Horacio Tellez Perez (hote) <hote@odoo.com>

--- sale_subscription/controllers/portal.py
+++ sale_subscription/controllers/portal.py
@@ -113,6 +113,11 @@ class CustomerPortal(portal.CustomerPortal):
         if report_type in ('html', 'pdf', 'text'):
             return self._show_report(model=order_sudo, report_type=report_type, report_ref='sale.action_report_saleorder', download=download)
 
+        # Make sure that the partner's company matches the subscription's company.
+        payment_portal.PaymentPortal.ensure_matching_companies(
+            order_sudo.partner_id, order_sudo.company_id
+        )
+
         acquirers_sudo = request.env['payment.acquirer'].sudo()._get_compatible_acquirers(
             order_sudo.company_id.id,
             order_sudo.partner_id.id,

f2b21f274087eb97e6c576262ef61e60ec7f6ad1
Julien Alardot (jual) <jual@odoo.com>
2022-05-05 13:16:11 +0000

    [ADD] l10n_pe_reports: Add BS & Pnl reports documents
    Add balance sheet and profit and loss reports document that are missing
    
    closes odoo/enterprise#33183
    
    Task: task-2774275
    Related: odoo/odoo#104073
    Signed-off-by: Julien Alardot (jual) <jual@odoo.com>
    Signed-off-by: William André (wan) <wan@odoo.com>

---
+++ l10n_pe_reports/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Peru - Accounting Reports',
+    'icon': '/l10n_pe/static/description/icon.png',
+    'version': '1.0',
+    'category': 'Accounting/Localizations/Reporting',
+    'author': "Odoo SA",
+    'description': """
+        Accounting reports for Peru
+    """,
+    'depends': [
+        'account_reports',
+        'l10n_pe',
+    ],
+    'data': [
+        "data/balance_sheet.xml",
+        "data/profit_loss.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+    'website': 'https://www.odoo.com/app/accounting',
+    'license': 'OEEL-1',
+}

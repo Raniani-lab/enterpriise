bf0d3e886540b1e8c5daf00834eea718baf990a6
Guillaume (guva) <guva@odoo.com>
2022-05-10 17:28:46 +0000

    [FIX] stock_accountant: filter input/output account in reco widget
    Don't show the stock input/output accounts
    in the reconciliation widget.
    
    Steps to reproduce:
    
    - A product category PC, costing method AVCO,
      automated valuation and custom stock input(STI)/output(STO)
      account (reconcile = True)
    - A Journal Entry with the STI
      Move line :
      | Account | Debit | Credit |
      |---------|-------|--------|
      |   STI   |  0.0  |  50.0  |
      | XXXXXXX |  50.0 |   0.0  |
    
    - Create a bank statement with amount corresponding with
      the journal entry then reconcile
    -> Line for custom stock input STI account shows up in misc tab
       on reconciliation widget
    
    Fetch all accounts relative to stock accounts properties, in
    order to filter them in the domain.
    
    opw-2792862
    
    closes odoo/enterprise#29514
    
    X-original-commit: 38498df3771ce4a572435d81936d639602db7ebf
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Guillaume Vanleynseele (guva) <guva@odoo.com>

--- stock_accountant/models/account_bank_statement.py
+++ stock_accountant/models/account_bank_statement.py
@@ -11,15 +11,21 @@ class AccountBankStatementLine(models.Model):
         domain = super()._get_default_amls_matching_domain()
 
         blacklisted_stock_account_ids = set()
-        for account_property in [
+        account_stock_properties_names = [
             'property_stock_account_input',
             'property_stock_account_output',
             'property_stock_account_input_categ_id',
             'property_stock_account_output_categ_id',
-        ]:
-            account = self.env['ir.property']._get(account_property, "product.category")
-            if account:
-                blacklisted_stock_account_ids.add(account.id)
+        ]
+
+        properties = self.env['ir.property'].sudo().search([
+            ('name', 'in', account_stock_properties_names),
+            ('company_id', '=', self.env.company.id),
+            ('value_reference', '!=', False),
+        ])
+        if properties:
+            accounts = properties.mapped(lambda p: p.get_by_record())
+            blacklisted_stock_account_ids.update(accounts.ids)
 
         if blacklisted_stock_account_ids:
             return expression.AND([domain, [('account_id', 'not in', tuple(blacklisted_stock_account_ids))]])

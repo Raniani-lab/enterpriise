53c7fc280018569cddb7d13a177b557c99a3701b
Raouf <abgh@odoo.com>
2022-05-18 15:34:34 +0000

    [IMP] project_timesheet_forecast: add new measures to reports
    In this commit:
    New measures are added to the reports in 'timesheets & planning analysis'
    - planned costs: nb of planned hours * the timesheet cost set on the employee
    - effective costs: nb of effective hours * the timesheet cost set on the employee
    - planned revenues: nb of planned hours * the unit price per hour
     (or day if the encoding uom is in days) of the SOL linked to the shift
    - effective revenues: nb of effective hours * the unit price per hour
     (or day if the encoding uom is in days) of the SOL linked to the timesheet entry
    - planned margin: planned revenues - planned costs
    - effective margin: effective revenues - effective costs
    - planned billable hours: nb of planned hours linked to an SOL
    - effective billable hours: nb of effective hours linked to an SOL
    - planned non billable hours: nb of planned hours not linked to an SOL
    - effective non billable hours: nb of effective hours not linked to an SOL
    - initially planned hours:
     planned_hours field of the project.task linked to the timesheet entry / planning slot
    
    task-2849291
    
    Part-of: odoo/enterprise#27287

--- project_timesheet_forecast/report/timesheet_forecast_report.py
+++ project_timesheet_forecast/report/timesheet_forecast_report.py
@@ -23,6 +23,8 @@ class TimesheetForecastReport(models.Model):
     difference = fields.Float('Remaining Hours', readonly=True)
     user_id = fields.Many2one('res.users', string='Assigned to', readonly=True)
     is_published = fields.Boolean(readonly=True)
+    effective_costs = fields.Float('Effective Costs', readonly=True)
+    planned_costs = fields.Float('Planned Costs', readonly=True)
 
     @api.model
     def _select(self):
@@ -34,7 +36,9 @@ class TimesheetForecastReport(models.Model):
                 F.project_id AS project_id,
                 F.user_id AS user_id,
                 0.0 AS effective_hours,
+                0.0 As effective_costs,
                 F.allocated_hours / GREATEST(F.working_days_count, 1) AS planned_hours,
+                (F.allocated_hours / GREATEST(F.working_days_count, 1) * E.hourly_cost) AS planned_costs,
                 F.allocated_hours / GREATEST(F.working_days_count, 1) AS difference,
                 'forecast' AS line_type,
                 F.id AS id,
@@ -65,7 +69,9 @@ class TimesheetForecastReport(models.Model):
                 A.project_id AS project_id,
                 A.user_id AS user_id,
                 A.unit_amount / UOM.factor * HOUR_UOM.factor AS effective_hours,
+                (A.unit_amount / UOM.factor * HOUR_UOM.factor) * E.hourly_cost AS effective_costs,
                 0.0 AS planned_hours,
+                0.0 AS planned_costs,
                 -A.unit_amount / UOM.factor * HOUR_UOM.factor AS difference,
                 'timesheet' AS line_type,
                 -A.id AS id,
@@ -75,18 +81,21 @@ class TimesheetForecastReport(models.Model):
 
     @api.model
     def _from_union(self):
-        from_str = """
+        return """
             FROM account_analytic_line A
                 LEFT JOIN hr_employee E ON A.employee_id = E.id
-                LEFT JOIN uom_uom UOM ON A.product_uom_id = UOM.id,
-                (
-                    SELECT
-                        U.factor
-                    FROM uom_uom U
-                    WHERE U.id = %s
-                ) HOUR_UOM
+        """
+
+    @api.model
+    def _from_union_timesheet_uom(self):
+        return """
+            LEFT JOIN uom_uom UOM ON A.product_uom_id = UOM.id,
+            (
+                SELECT U.factor
+                  FROM uom_uom U
+                 WHERE U.id = %s
+            ) HOUR_UOM
         """ % (self.env.ref('uom.product_uom_hour').id)
-        return from_str
 
     @api.model
     def _where_union(self):
@@ -96,11 +105,12 @@ class TimesheetForecastReport(models.Model):
         return where_str
 
     def init(self):
-        query = "(%s %s) UNION (%s %s %s)" % (
+        query = "(%s %s) UNION (%s %s %s %s)" % (
             self._select(),
             self._from(),
             self._select_union(),
             self._from_union(),
+            self._from_union_timesheet_uom(),
             self._where_union()
         )
 
--- project_timesheet_forecast_sale/report/__init__.py
+++ project_timesheet_forecast_sale/report/__init__.py
@@ -2,3 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import planning_analysis_report
+from . import timesheet_forecast_report

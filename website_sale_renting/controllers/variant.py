1899e0a35c05fcec84ec0eab205d262dc629fa19
Danial Sheikhani <shda@odoo.com>
2022-05-20 11:41:48 +0000

    [IMP] website_sale: Add prevent_zero_price_sale option
    Relates to odoo/odoo#87952
    
    task-2694860
    
    closes odoo/enterprise#27594
    
    Signed-off-by: William Braeckman (wbr) <wbr@odoo.com>

--- website_sale_renting/controllers/variant.py
+++ website_sale_renting/controllers/variant.py
@@ -13,8 +13,8 @@ class WebsiteSaleRentingVariantController(WebsiteSaleVariantController):
     ):
         """ Override to parse and add to context optional pickup and return dates.
         """
+        context = context if context is not None else {}
         if start_date and end_date:
-            context = context if context is not None else {}
             context.update(start_date=parse_date(start_date), end_date=parse_date(end_date))
         return super().get_combination_info_website(
             *args, context=context, start_date=start_date, end_date=end_date, **kwargs

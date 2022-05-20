b611a8b0e8c0f5fe84a59e159706d8b330a349c7
tsm-odoo <tsm@odoo.com>
2022-05-20 14:34:14 +0000

    [IMP] documents: prepare activity menu tests to wowl env
    This PR prepares the ground for the one introducing the new environment in the discuss app.
    Indeed, the start helper won't be able to handle widget anymore. To solve this issues,
    we're now adding the activityMenu widget to the systrayMenu.Items array, this will be
    mapped by the createWebClient helper to component and added to the new systray item
    registry.
    
    task-2582313
    
    closes odoo/enterprise#27614
    
    Related: odoo/odoo#91953
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- documents/static/tests/documents_systray_activity_menu_tests.js
+++ documents/static/tests/documents_systray_activity_menu_tests.js
@@ -3,7 +3,11 @@
 import ActivityMenu from '@mail/js/systray/systray_activity_menu';
 import { start } from '@mail/../tests/helpers/test_utils';
 
+import session from 'web.session';
+import { Items as legacySystrayItems } from 'web.SystrayMenu';
 import testUtils from 'web.test_utils';
+import { registerCleanup } from '@web/../tests/helpers/cleanup';
+import { patchWithCleanup } from '@web/../tests/helpers/utils';
 
 QUnit.module('documents', {}, function () {
     QUnit.module('documents_systray_activity_menu_tests.js');
@@ -11,42 +15,39 @@ QUnit.module('documents', {}, function () {
     QUnit.test('activity menu widget: documents request button', async function (assert) {
         assert.expect(6);
 
-        const actionServiceInterceptor = ev => {
-                const { action } = ev.data;
-                assert.strictEqual(action, 'documents.action_request_form',
-                    "should open the document request form");
-        };
-        const { widget } = await start({
+        patchWithCleanup(session, {
+            async user_has_group(group) {
+                if (group === 'documents.group_documents_user') {
+                    assert.step('user_has_group:documents.group_documents_user');
+                    return true;
+                }
+                return this._super(...arguments);
+            },
+        });
+        legacySystrayItems.push(ActivityMenu);
+        registerCleanup(() => legacySystrayItems.pop());
+        const { wowlEnv: env } = await start({
+            hasWebClient: true,
             async mockRPC(route, args) {
                 if (args.method === 'systray_get_activities') {
                     return [];
                 }
-                return this._super.apply(this, arguments);
             },
-            intercepts: {
-                do_action: actionServiceInterceptor,
-            },
-            session: {
-                async user_has_group(group) {
-                    if (group === 'documents.group_documents_user') {
-                        assert.step('user_has_group:documents.group_documents_user');
-                        return true;
-                    }
-                    return this._super(...arguments);
-                },
+        });
+        patchWithCleanup(env.services.action, {
+            doAction(action) {
+                assert.strictEqual(action, 'documents.action_request_form',
+                    "should open the document request form");
             },
         });
 
-        const activityMenu = new ActivityMenu(widget);
-        await activityMenu.appendTo($('#qunit-fixture'));
-
-        await testUtils.dom.click(activityMenu.$('> .dropdown-toggle'));
-        assert.hasClass(activityMenu.$('.dropdown-menu'), 'show',
+        await testUtils.dom.click(document.querySelector('.dropdown-toggle[title="Activities"]'));
+        assert.hasClass(document.querySelector('.dropdown-menu'), 'show',
             "dropdown should be expanded");
         assert.verifySteps(['user_has_group:documents.group_documents_user']);
-        assert.containsOnce(activityMenu, '.o_sys_documents_request');
-        await testUtils.dom.click(activityMenu.$('.o_sys_documents_request'));
-        assert.doesNotHaveClass(activityMenu.$('.dropdown-menu'), 'show',
+        assert.containsOnce(document.body, '.o_sys_documents_request');
+        await testUtils.dom.click(document.querySelector('.o_sys_documents_request'));
+        assert.doesNotHaveClass(document.querySelector('.dropdown-menu'), 'show',
             "dropdown should be collapsed");
     });
 });

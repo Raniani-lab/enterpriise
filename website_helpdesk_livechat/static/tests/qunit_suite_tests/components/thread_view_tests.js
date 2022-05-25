ad10ce88a447de5ad31ffa8ab6e3057cfee414ea
Sébastien Theys <seb@odoo.com>
2022-05-25 10:57:49 +0000

    [IMP] website_helpdesk_livechat: remove createThreadViewComponent
    Use real parent instead (Discuss in this case).
    
    closes odoo/enterprise#27757
    
    Related: odoo/odoo#92253
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- website_helpdesk_livechat/static/tests/qunit_suite_tests/components/thread_view_tests.js
+++ website_helpdesk_livechat/static/tests/qunit_suite_tests/components/thread_view_tests.js
@@ -1,11 +1,6 @@
 /** @odoo-module **/
 
-import { insertAndReplace, replace } from '@mail/model/model_field_command';
-import {
-    afterNextRender,
-    start,
-    startServer,
-} from '@mail/../tests/helpers/test_utils';
+import { start, startServer } from '@mail/../tests/helpers/test_utils';
 
 QUnit.module('website_helpdesk_livechat', {}, function () {
 QUnit.module('components', {}, function () {
@@ -25,7 +20,12 @@ QUnit.test('[technical] /helpdesk command gets a body as kwarg', async function 
     });
     const [mailChannelPartnerId] = pyEnv['mail.channel.partner'].search([['channel_id', '=', mailChannelId1], ['partner_id', '=', pyEnv.currentPartnerId]]);
     pyEnv['mail.channel.partner'].write([mailChannelPartnerId], { seen_message_id: mailMessageId1 });
-    const { createThreadViewComponent, messaging } = await start({
+    const { click, insertText, openDiscuss } = await start({
+        discuss: {
+            params: {
+                default_active_id: `mail.channel_${mailChannelId1}`,
+            },
+        },
         mockRPC(route, { model, method, kwargs }) {
             if (model === 'mail.channel' && method === 'execute_command_helpdesk') {
                 assert.step(`execute command helpdesk. body: ${kwargs.body}`);
@@ -34,20 +34,10 @@ QUnit.test('[technical] /helpdesk command gets a body as kwarg', async function 
             return this._super(...arguments);
         },
     });
-    const thread = messaging.models['Thread'].findFromIdentifyingData({
-        id: mailChannelId1,
-        model: 'mail.channel'
-    });
-    const threadViewer = messaging.models['ThreadViewer'].create({
-        hasThreadView: true,
-        qunitTest: insertAndReplace(),
-        thread: replace(thread),
-    });
-    await createThreadViewComponent(threadViewer.threadView);
+    await openDiscuss();
 
-    document.querySelector('.o_ComposerTextInput_textarea').focus();
-    await afterNextRender(() => document.execCommand('insertText', false, "/helpdesk something"));
-    await afterNextRender(() => document.querySelector('.o_Composer_buttonSend').click());
+    await insertText('.o_ComposerTextInput_textarea', "/helpdesk something");
+    await click('.o_Composer_buttonSend');
     assert.verifySteps([
         'execute command helpdesk. body: /helpdesk something',
     ]);

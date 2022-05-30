eaeae4e6711b04c66c03ddf8242c5b0ff55fc8aa
Kevin Baptiste <kba@odoo.com>
2022-05-30 11:38:03 +0000

    [IMP] l10n_be_hr_payroll_fleet: add new Dashboard alert
    Adds a Dashboard alert for vehicles assigned to a driver but without
    running contract.
    
    closes odoo/enterprise#27919
    
    Related: odoo/odoo#92604
    Taskid: 2856135
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- l10n_be_hr_payroll_fleet/models/hr_payslip.py
+++ l10n_be_hr_payroll_fleet/models/hr_payslip.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
-
+from odoo import _, api, fields, models
+from odoo.exceptions import AccessError
 
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
@@ -34,3 +34,39 @@ class HrPayslip(models.Model):
                 'data/hr_rule_parameter_data.xml',
                 'data/cp200_employee_salary_data.xml',
             ])]
+
+    @api.model
+    def _get_dashboard_warnings(self):
+        res = super()._get_dashboard_warnings()
+
+        try:
+            self.env['fleet.vehicle'].check_access_rights('read')
+            self.env['fleet.vehicle.log.contract'].check_access_rights('read')
+        except AccessError:
+            return res
+
+        self.env.cr.execute("""
+            SELECT v.id
+              FROM fleet_vehicle v
+             WHERE v.driver_employee_id IS NOT NULL
+               AND NOT EXISTS (SELECT 1
+                                 FROM fleet_vehicle_log_contract c
+                                WHERE c.vehicle_id = v.id
+                                  AND c.company_id = v.company_id
+                                  AND c.active IS TRUE
+                                  AND c.state = 'open')
+               AND v.company_id IN %s
+               AND v.active IS TRUE
+          GROUP BY v.id
+        """, (tuple(self.env.companies.ids), ))
+        vehicles_no_contract = [vid[0] for vid in self.env.cr.fetchall()]
+
+        if vehicles_no_contract:
+            no_contract = _('Vehicles With Drivers And Without Running Contract')
+            res.append({
+                'string': no_contract,
+                'count': len(vehicles_no_contract),
+                'action': self._dashboard_default_action(no_contract, 'fleet.vehicle', vehicles_no_contract),
+            })
+
+        return res

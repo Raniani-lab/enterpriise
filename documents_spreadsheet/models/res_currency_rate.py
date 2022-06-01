fe6c0a90e589ac50b512186d4e36a4dafa950ff5
Rémi Rahir <rar@odoo.com>
2022-06-01 15:35:17 +0000

    [FIX] documents_spreadsheet: use the correct default date
    The defaut `name` (which is a Date) of a `res.currency.rate` is computed
    based on its creator's TZ. The look up of currency rates in spreadsheet
    should follow the same logic.
    
    Task 2869705
    
    closes odoo/enterprise#27951
    
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- documents_spreadsheet/models/res_currency_rate.py
+++ documents_spreadsheet/models/res_currency_rate.py
@@ -14,7 +14,7 @@ class ResCurrencyRate(models.Model):
         if not currency_from or not currency_to:
             return False
         company = self.env.company
-        date = fields.Date.from_string(date) if date else fields.Date.today()
+        date = fields.Date.from_string(date) if date else fields.Date.context_today(self)
         return Currency._get_conversion_rate(currency_from, currency_to, company, date)
 
     @api.model

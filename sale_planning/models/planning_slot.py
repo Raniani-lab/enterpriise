e40d051a8208577cd67144dd4f4972fb3b118067
Audric Onockx (auon) <auon@odoo.com>
2022-06-08 14:05:08 +0000

    [FIX] sale_planning: allow user without employee rights to plan order
    Steps :
    Create and login with a user with rights:
    	> planning : admin
    	> employee : none
    Create a SO with a planning service.
    Go to planning > Plan orders.
    
    Issue :
    Traceback :
    hr.employee.public.default_planning_role_id is not a valid field.
    
    Cause :
    planning_slot._get_employee_per_priority() searches for available employee.
    It calls hr_employee._search(), which checks if user has acces to hr_employee.
    If not, hr_employee_public._search().
    Yet, default_planning_role_id field is hr_employee's.
    
    Fix :
    In planning_slot._get_employee_per_priority(), search with sudo.
    
    closes odoo/enterprise#28407
    
    X-original-commit: 0bc75e316813cbdf2edb8f81844c63abc1e8f4dc
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- sale_planning/models/planning_slot.py
+++ sale_planning/models/planning_slot.py
@@ -433,7 +433,7 @@ class PlanningSlot(models.Model):
             ], ['employee_id', 'end_datetime:max'], ['employee_id'], orderby='end_datetime desc')
             cache[priority] = [res['employee_id'][0] for res in search]
         elif priority == 'default_role':
-            search = self.env['hr.employee'].search_read([
+            search = self.env['hr.employee'].sudo().search_read([
                 ('default_planning_role_id', '=', self.role_id.id),
                 ('id', 'not in', employee_ids_to_exclude),
             ], ['id'])

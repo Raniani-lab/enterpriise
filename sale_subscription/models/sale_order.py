27005ded1d83c25a4524cb2af14fda69ec90d0f6
Arnaud Joset <arj@odoo.com>
2022-06-13 12:06:19 +0000

    [IMP] sale_subscription:  default template_id
    Before this commit, we did not had a default template when the SO was set 'to_renew'.
    
    Taskid: 2855699
    X-original-commit: abcbd3895579c4c221feb60c9efacba421dd1157
    Part-of: odoo/enterprise#28725

--- sale_subscription/models/sale_order.py
+++ sale_subscription/models/sale_order.py
@@ -890,6 +890,13 @@ class SaleOrder(models.Model):
         else:
             return super(SaleOrder, self)._get_portal_return_action()
 
+    def _find_mail_template(self, force_confirmation_template=False):
+        template_id = super()._find_mail_template(force_confirmation_template=False)
+        if not force_confirmation_template and self.is_subscription:
+            if self.to_renew:
+                template_id = self.env['ir.model.data']._xmlid_to_res_id('sale_subscription.mail_template_subscription_alert', raise_if_not_found=False)
+        return template_id
+
     ####################
     # Invoicing Methods #
     ####################

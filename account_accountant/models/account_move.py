98be51b576c58bdbe266685f869d3ebd857bfc27
Laurent Smet <las@odoo.com>
2022-06-15 13:31:24 +0000

    [IMP] account_accountant: Search amls sorted by closest amount with the statement line
    Amls are now sorted by their proximity with the statement line amount with a delta of 5% max.
    After that, the regular order is applied.
    
    closes odoo/enterprise#28744
    
    X-original-commit: 3f200dc53f423d12af5bbafc144af4e56b2e3e32
    Related: odoo/odoo#94287
    Signed-off-by: Quentin De Paoli <qdp@odoo.com>
    Signed-off-by: Laurent Smet <las@odoo.com>

--- account_accountant/models/account_move.py
+++ account_accountant/models/account_move.py
@@ -27,6 +27,30 @@ class AccountMoveLine(models.Model):
 
     move_attachment_ids = fields.One2many('ir.attachment', compute='_compute_attachment')
 
+    @api.model
+    def _generate_order_by(self, order_spec, query):
+        # EXTENDS base
+        # Hack for the bank reconciliation widget to force the candidates amls having exactly the same amount as the
+        # statement line to be on top of the list.
+        query = super()._generate_order_by(order_spec, query)
+        if not query:
+            return query
+
+        if 'bank_rec_widget_st_line_amount' in self._context:
+            query_split = query.split(' ORDER BY ')
+            st_currency_id = self._context['bank_rec_widget_st_line_currency_id']
+            st_amount = self._context['bank_rec_widget_st_line_amount']
+            st_amount_percent = st_amount * 0.05
+            additional_queries = f'''
+                CASE WHEN "account_move_line"."currency_id" != {st_currency_id}
+                THEN ABS({st_amount_percent})
+                ELSE LEAST(ABS("account_move_line"."amount_residual_currency" - {st_amount}), ABS({st_amount_percent}))
+                END
+            '''
+            query = ' ORDER BY ' + ', '.join([additional_queries, query_split[1]])
+
+        return query
+
     def _compute_attachment(self):
         for record in self:
             record.move_attachment_ids = self.env['ir.attachment'].search(expression.OR(record._get_attachment_domains()))

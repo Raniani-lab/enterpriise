c77f27da626c57e599635ff01230d38948c43efc
Louis Wicket (wil) <wil@odoo.com>
2022-06-24 08:00:05 +0000

    [IMP] voip_onsip: remove no longer useful code
    closes odoo/enterprise#28835
    
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- voip_onsip/models/__init__.py
+++ voip_onsip/models/__init__.py
@@ -1,6 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import onsip_configurator
 from . import res_config_settings
 from . import res_users_settings
 from . import res_users

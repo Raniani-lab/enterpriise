ad35805dd324124544b7b2c8e762fff8fb8ff0a9
Louis Wicket (wil) <wil@odoo.com>
2022-06-27 12:52:01 +0000

    [IMP] voip: use Device/hasRtcSupport in VoIP
    closes odoo/enterprise#28900
    
    Community: https://github.com/odoo/odoo/pull/94683
    Related: odoo/odoo#94683
    Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

--- voip/static/src/js/user_agent.js
+++ voip/static/src/js/user_agent.js
@@ -425,7 +425,7 @@ const UserAgent = Class.extend(mixins.EventDispatcherMixin, ServicesMixin, {
                 isConnecting: true,
                 message: _t("Connecting..."),
             });
-            if (!window.RTCPeerConnection || !window.MediaStream || !navigator.mediaDevices) {
+            if (!this.messaging.device.hasRtcSupport || !navigator.mediaDevices) {
                 this._triggerError(
                     _t("Your browser could not support WebRTC. Please check your configuration."));
                 return;

Raniani-lab/enterpriise

commit ad35805dd324124544b7b2c8e762fff8fb8ff0a9

[IMP] voip: use Device/hasRtcSupport in VoIP
closes odoo/enterprise#28900

Community: https://github.com/odoo/odoo/pull/94683
Related: odoo/odoo#94683
Signed-off-by: Sébastien Theys (seb) <seb@odoo.com>

voip/static/src/js/user_agent.js
@@ -425,7 +425,7 @@ const UserAgent = Class.extend(mixins.EventDispatcherMixin, ServicesMixin, {
                 isConnecting: true,
                 message: _t("Connecting..."),
             });
-            if (!window.RTCPeerConnection || !window.MediaStream || !navigator.mediaDevices) {
+            if (!this.messaging.device.hasRtcSupport || !navigator.mediaDevices) {
                 this._triggerError(
                     _t("Your browser could not support WebRTC. Please check your configuration."));
                 return;

commit b3357eed1c6c28d5bad56bff33f651c28b8b4f03

[FIX] voip: fix typo causing voip to crash
Fix typo introduced in commit ad35805dd324124544b7b2c8e762fff8fb8ff0a9.

closes odoo/enterprise#28927

Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

voip/static/src/js/user_agent.js
@@ -425,7 +425,7 @@ const UserAgent = Class.extend(mixins.EventDispatcherMixin, ServicesMixin, {
                 isConnecting: true,
                 message: _t("Connecting..."),
             });
-            if (!this.messaging.device.hasRtcSupport || !navigator.mediaDevices) {
+            if (!this._messaging.device.hasRtcSupport || !navigator.mediaDevices) {
                 this._triggerError(
                     _t("Your browser could not support WebRTC. Please check your configuration."));
                 return;
94baf7bc2a196dac9ddd5c2569e32c37fb795a46
Victor Feyens <vfe@odoo.com>
2022-07-11 09:05:08 +0000

    [MOV] sale_stock_renting: rename sale_rental.py
    Part-of: odoo/enterprise#29376

--- sale_stock_renting/models/__init__.py
+++ sale_stock_renting/models/__init__.py
@@ -4,5 +4,5 @@ from . import product_product
 from . import product_template
 from . import res_company
 from . import res_config_settings
-from . import sale_rental
+from . import sale_order_line
 from . import stock_lot

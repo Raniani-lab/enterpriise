561640d657f160e0233671834d88110612854d9c
William Braeckman <wbr@odoo.com>
2022-07-15 15:14:03 +0000

    [FIX] website_sale_renting_product_configurator: fix context keys
    Due to some changes the route was not functioning properly anymore, as
    the 'context' arguments was directly parsed as the environment's
    context.
    This meant that the overrides that are done for `website_sale_renting`
    weren't working anymore making it impossible to add rental products with
    product configurator to the cart.
    
    closes odoo/enterprise#29558
    
    X-original-commit: 1979507ad9655b169f7c609ffc8cb48000e016c0
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- website_sale_renting_product_configurator/controllers/configurator.py
+++ website_sale_renting_product_configurator/controllers/configurator.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import http
+from odoo.tools import frozendict
+from odoo.http import request
 
 from odoo.addons.website_sale_product_configurator.controllers.main import WebsiteSaleProductConfiguratorController
 from odoo.addons.website_sale_renting.controllers.product import parse_date
@@ -9,13 +11,13 @@ from odoo.addons.website_sale_renting.controllers.product import parse_date
 class RentingConfiguratorController(WebsiteSaleProductConfiguratorController):
 
     @http.route()
-    def show_advanced_configurator_website(self, *args, kwargs=None, **kw):
+    def show_advanced_configurator_website(self, *args, **kw):
         """Special route to use website logic in get_combination_info override.
         This route is called in JS by appending _website to the base route.
         """
-        if kwargs and 'context' in kwargs:
-            context = kwargs['context']
-            if context.get('start_date') and context.get('end_date'):
-                context['start_date'] = parse_date(context.get('start_date'))
-                context['end_date'] = parse_date(context.get('end_date'))
-        return super().show_advanced_configurator_website(*args, kwargs=kwargs, **kw)
+        if request.env.context.get('start_date') and request.env.context.get('end_date'):
+            context = {**request.env.context}
+            context['start_date'] = parse_date(context.get('start_date'))
+            context['end_date'] = parse_date(context.get('end_date'))
+            request.env.context = frozendict(context)
+        return super().show_advanced_configurator_website(*args, **kw)

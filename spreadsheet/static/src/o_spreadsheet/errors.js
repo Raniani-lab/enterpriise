bb615b25fdb95f9a04aaedbebc7b5b5bdc9254b1
Lucas Lefèvre <lul@odoo.com>
2022-07-18 09:17:06 +0000

    [IMP] spreadsheet: don't display loading cells as errors
    Currently, cell which are currently loading data are displayed
    as errors, with a red upper-right corner.
    This is a bit "scary" for the user to see all this red, even
    if it should not since it's perfectly normal.
    
    Part-of: odoo/enterprise#29570

--- spreadsheet/static/src/o_spreadsheet/errors.js
+++ spreadsheet/static/src/o_spreadsheet/errors.js
@@ -3,10 +3,10 @@
 import { _t } from "@web/core/l10n/translation";
 import spreadsheet from "./o_spreadsheet_extended";
 
-const { EvaluationError } = spreadsheet.helpers;
+const { EvaluationError, CellErrorLevel } = spreadsheet.helpers;
 
 export class LoadingDataError extends EvaluationError {
     constructor() {
-        super(_t("Loading..."), _t("Data is loading"));
+        super(_t("Loading..."), _t("Data is loading"), CellErrorLevel.silent);
     }
 }

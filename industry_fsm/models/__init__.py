9f4471939fcade091054ae90ff7349dce371bcca
Xavier BOL (xbo) <xbo@odoo.com>
2022-08-01 13:44:30 +0000

    [MOV] industry_fsm: split project file into 2 files
    Before this commit, the project python file in model folder in
    industry_fsm module contains 2 models, `project.project` and
    `project.task`.
    
    Since this file is very long, this commit splits the file into 2 to have
    one model per python file in model folder of this module.
    
    task-2605513
    
    closes odoo/enterprise#25250
    
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- industry_fsm/models/__init__.py
+++ industry_fsm/models/__init__.py
@@ -3,6 +3,7 @@
 
 from . import res_company
 from . import res_partner
-from . import project
+from . import project_project
+from . import project_task
 from . import res_config_settings
 from . import ir_ui_menu

66da7d1bbb7c3e46ae387d2cc94be5a002ee1461
Romain Derie <rde@odoo.com>
2022-08-08 15:53:56 +0000

    [IMP] website_enterprise: adapt the frontend mini navbar
    Community counterpart commit is introducing back the fact that the
    "go to backend" icon in the frontend should display the app list
    directly.
    But this is not needed in enterprise as that "go to backend" button is
    landing on the nice enterprise app switcher.
    
    closes odoo/enterprise#30218
    
    Related: odoo/odoo#97745
    Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

--- website_enterprise/__manifest__.py
+++ website_enterprise/__manifest__.py
@@ -10,6 +10,7 @@ This module overrides community website features and introduces enterprise look 
     'depends': ['website'],
     'data': [
         'data/website_data.xml',
+        'views/website_enterprise_templates.xml',
     ],
     'installable': True,
     'auto_install': True,

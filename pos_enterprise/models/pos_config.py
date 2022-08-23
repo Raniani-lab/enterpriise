c823fb833d0e598ec8b61e74397789e90accef12
Yannick Tivisse <yti@odoo.com>
2022-08-23 08:35:56 +0000

    [IMP] pos_enterprise: Remove pos_config_form js_class
    Purpose
    =======
    
    The logic has been moved from the pos.config to the res.config.settings
    model/views, and therefore this js_class is not used anymore.
    
    closes odoo/enterprise#30658
    
    Taskid: 2958057
    Related: odoo/odoo#98641
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- pos_enterprise/models/pos_config.py
+++ pos_enterprise/models/pos_config.py
@@ -8,10 +8,3 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     module_pos_iot = fields.Boolean('IoT Box', related="is_posbox")
-
-    def _get_modules_to_check(self, changed_fields):
-        result = super()._get_modules_to_check(changed_fields)
-        for field in changed_fields:
-            if field == 'is_posbox':
-                result.append('iot')
-        return result

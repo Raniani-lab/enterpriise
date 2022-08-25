ffed92e0755f250df0b235ecc7b1d295fa2169e7
mafo-odoo <mafo@odoo.com>
2022-08-25 12:45:05 +0000

    [FIX] sign: fix auto fields for chained automatic fields
    Before this commit, automatic sign fields with several level
    would not work anymore. E.G. 'company_id.country_id.name'
    
    It was not compatible with https://github.com/odoo/enterprise/commit/c5cca88d1b46ac7e03e6354c3e98698a6f7e183b
    
    As this feature is more important than the multi level automatic field,
    we decide to revert  https://github.com/odoo/enterprise/commit/c5cca88d1b46ac7e03e6354c3e98698a6f7e183b, keep the tests
    and we will see in master if the special case of float and currency is needed.
    
    opw-2960711
    
    closes odoo/enterprise#31267
    
    X-original-commit: 1fe9393cbd0db457ea44985a9a3c378d118bc706
    Signed-off-by: Arnaud Joset <arj@odoo.com>
    Signed-off-by: Yannick Tivisse (yti) <yti@odoo.com>

--- sign/tests/test_sign_controllers.py
+++ sign/tests/test_sign_controllers.py
@@ -23,31 +23,7 @@ class TestSignController(SignRequestCommon):
             values = self.SignController.get_document_qweb_context(sign_request.id, token=token_a)
             sign_type = list(filter(lambda sign_type: sign_type["name"] == "Text", values["sign_item_types"]))[0]
             latitude = sign_type["auto_field"]
-            self.assertEqual(latitude, "0.0000000")
-
-    # test monetary auto_field display
-    def test_sign_controller_monetary(self):
-        sign_request = self.create_sign_request_no_item(signer=self.partner_1, cc_partners=self.partner_4)
-        text_type = self.env['sign.item.type'].search([('name', '=', 'Text')])
-        # no monetary field is set on res.partner with only the sign module
-        # credit is added in the account module and the test should not be
-        # run if this module is not installed
-        if 'credit' not in self.partner_1:
-            return
-        # we set the text type defaut value with the partner.credit
-        text_type.auto_field = 'credit'
-        monetary_field = self.partner_1._fields['credit']
-        monetary_field.currency_field = 'currency_id'
-        # we set the partner currency as the euro and change its precison to 5 decimals
-        euro_currency = self.env['res.currency'].search([('name', '=', 'EUR')], limit=1)
-        self.partner_1.currency_id = euro_currency
-        euro_currency.rounding = 0.00001
-        token_a = self.env["sign.request.item"].search([('sign_request_id', '=', sign_request.id)]).access_token
-        with MockRequest(sign_request.env):
-            values = self.SignController.get_document_qweb_context(sign_request.id, token=token_a)
-            sign_type = list(filter(lambda sign_type: sign_type["name"] == "Text", values["sign_item_types"]))[0]
-            credit = sign_type["auto_field"]
-            self.assertEqual(credit, "0.00000")
+            self.assertEqual(latitude, 0)
 
     # test auto_field with wrong partner field
     def test_sign_controller_dummy_fields(self):
@@ -55,3 +31,17 @@ class TestSignController(SignRequestCommon):
         # we set a dummy field that raises an error
         with self.assertRaises(ValidationError):
             text_type.auto_field = 'this_is_not_a_partner_field'
+
+    # test auto_field with multiple sub steps
+    def test_sign_controller_multi_step_auto_field(self):
+        self.partner_1.company_id = self.env.ref('base.main_company')
+        self.partner_1.company_id.country_id = self.env.ref('base.be').id
+        sign_request = self.create_sign_request_no_item(signer=self.partner_1, cc_partners=self.partner_4)
+        text_type = self.env['sign.item.type'].search([('name', '=', 'Text')])
+        text_type.auto_field = 'company_id.country_id.name'
+        token_a = self.env["sign.request.item"].search([('sign_request_id', '=', sign_request.id)]).access_token
+        with MockRequest(sign_request.env):
+            values = self.SignController.get_document_qweb_context(sign_request.id, token=token_a)
+            sign_type = list(filter(lambda sign_type: sign_type["name"] == "Text", values["sign_item_types"]))[0]
+            country = sign_type["auto_field"]
+            self.assertEqual(country, "Belgium")

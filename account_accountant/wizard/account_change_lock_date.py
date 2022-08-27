812dce455f458ae7eea13f944c6979a8664252e1
Yenthe Van Ginneken <yenthevg@gmail.com>
2022-08-27 14:27:12 +0000

    [IMP] account_account: allow overriding/extending lock date values
    closes odoo/enterprise#30813
    
    Signed-off-by: Martin Trigaux (mat) <mat@odoo.com>

--- account_accountant/wizard/account_change_lock_date.py
+++ account_accountant/wizard/account_change_lock_date.py
@@ -24,14 +24,16 @@ class AccountChangeLockDate(models.TransientModel):
         help='Prevents Tax Returns modification prior to the defined date (Journal Entries involving taxes). '
              'The Tax Return Lock Date is automatically set when the corresponding Journal Entry is posted.')
 
+    def _prepare_lock_date_values(self):
+        return {
+            'period_lock_date': self.period_lock_date,
+            'fiscalyear_lock_date': self.fiscalyear_lock_date,
+            'tax_lock_date': self.tax_lock_date,
+        }
 
     def change_lock_date(self):
         if self.user_has_groups('account.group_account_manager'):
-            self.env.company.sudo().write({
-                'period_lock_date': self.period_lock_date,
-                'fiscalyear_lock_date': self.fiscalyear_lock_date,
-                'tax_lock_date': self.tax_lock_date,
-            })
+            self.env.company.sudo().write(self._prepare_lock_date_values())
         else:
             raise UserError(_('Only Billing Administrators are allowed to change lock dates!'))
         return {'type': 'ir.actions.act_window_close'}

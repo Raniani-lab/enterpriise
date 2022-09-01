2441cad8d677601d06170440a49123223f895f76
Pierre Paridans <app@odoo.com>
2022-09-01 13:27:24 +0000

    [FIX] web_enterprise: PivotRenderer crashes on mobile
    Steps to reproduce:
    - On a small screen (aka mobile)
    - Open any Pivot view
    => error about calling `querySelectorAll on null`
    
    The Pivot view's small screen customization uses the `this.el` which
    doesn't exist anymore in the "modern" codebase.
    
    This commit get rid of the `this.el` and uses a reference to the root
    node instead.
    
    closes odoo/enterprise#31029
    
    X-original-commit: 37c129b824a82c7a40031326667bd76e249c7d36
    Related: odoo/odoo#99498
    Signed-off-by: Adrien Dieudonné (adr) <adr@odoo.com>
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- web_enterprise/static/src/views/pivot/pivot_renderer.js
+++ web_enterprise/static/src/views/pivot/pivot_renderer.js
@@ -3,14 +3,15 @@
 import { patch } from "@web/core/utils/patch";
 import { PivotRenderer } from "@web/views/pivot/pivot_renderer";
 
-const { useEffect } = owl;
+const { useEffect, useRef } = owl;
 
 patch(PivotRenderer.prototype, "web_enterprise.PivotRendererMobile", {
     setup() {
         this._super();
+        this.root = useRef("root");
         if (this.env.isSmall) {
             useEffect(() => {
-                const tooltipElems = this.el.querySelectorAll("*[data-tooltip]");
+                const tooltipElems = this.root.el.querySelectorAll("*[data-tooltip]");
                 for (const el of tooltipElems) {
                     el.removeAttribute("data-tooltip");
                     el.removeAttribute("data-tooltip-position");

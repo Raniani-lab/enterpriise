193c98276927e5d0ee8c0cdffa16555b6de81e89
Simon Genin (ges) <ges@odoo.com>
2022-09-01 09:39:21 +0000

    [FIX] web_studio: fix file load order for list renderer patch
    closes odoo/enterprise#30957
    
    Signed-off-by: Samuel Degueldre <sad@odoo.com>

--- web_studio/static/tests/views/disable_patch.js
+++ web_studio/static/tests/views/disable_patch.js
@@ -3,4 +3,6 @@
 import { unpatch } from "@web/core/utils/patch";
 import { ListRenderer } from "@web/views/list/list_renderer";
 
+import "@web_studio/views/list/list_renderer";
+
 unpatch(ListRenderer.prototype, "web_studio.ListRenderer");

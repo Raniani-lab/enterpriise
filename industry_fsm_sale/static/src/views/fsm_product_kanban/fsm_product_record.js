a8a11ea9da09ebbe4fcae90e42aa1bb6fbf4ac9f
Julien Mougenot <jum@odoo.com>
2022-09-07 14:16:02 +0000

    [FIX] industry_fsm_sale: Fix filtering by progress bar in kanban
    This commit adapts the FSM product record along with a refactor of the
    "reloadRecords" method: the method now always triggers a
    "record-updated" event and notifies the components.
    
    closes odoo/enterprise#31126
    
    Related: odoo/odoo#99748
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- industry_fsm_sale/static/src/views/fsm_product_kanban/fsm_product_record.js
+++ industry_fsm_sale/static/src/views/fsm_product_kanban/fsm_product_record.js
@@ -1,28 +1,28 @@
 /** @odoo-module */
 
-import { Record } from '@web/views/relational_model';
+import { Record } from "@web/views/relational_model";
 
 export class FsmProductRecord extends Record {
+    /**
+     * @override
+     */
     async _update(changes) {
-        if ('fsm_quantity' in changes && Object.keys(changes).length === 1) {
-            const action = await this.model.orm.call(this.resModel, 'set_fsm_quantity', [this.resId, changes.fsm_quantity], { context: this.context });
+        if ("fsm_quantity" in changes && Object.keys(changes).length === 1) {
+            const action = await this.model.orm.call(
+                this.resModel,
+                "set_fsm_quantity",
+                [this.resId, changes.fsm_quantity],
+                { context: this.context }
+            );
             if (action && action !== true) {
                 await this.model.action.doAction(action, {
-                    onClose: () => {
-                        this._reloadRecords();
-                    }
+                    onClose: () => this.model.reloadRecords(this),
                 });
             } else {
-                await this._reloadRecords();
+                await this.model.reloadRecords(this);
             }
             return;
         }
         super._update(changes);
-    }
-
-    async _reloadRecords() {
-        await this.model.reloadRecords(this.resId);
-        this.model.trigger("record-updated", { record: this });
-        this.model.notify();
     }
 }

1983b9949ac1d79536cd92bcece777a355acd113
maximilien(malb) <malb@odoo.com>
2022-09-09 11:54:22 +0000

    [IMP] account,account_batch_payment,account_sepa_direct_debit: small improvement in multiple views
    Change made:
    1. In the bills: Payment reference and recipient bank will be editable all the time
    2. Change in the filter of payment view
    3. Search amount in the payment view
    
    closes odoo/enterprise#31226
    
    Task-id: 2978033
    Related: odoo/odoo#99990
    Related: odoo/upgrade#3880
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_sepa/__manifest__.py
+++ account_sepa/__manifest__.py
@@ -16,7 +16,6 @@
         'views/account_journal_dashboard_view.xml',
         'views/account_journal_views.xml',
         'views/res_config_settings_views.xml',
-        'views/account_payment.xml',
         'views/account_batch_payment_views.xml',
     ],
     'post_init_hook': 'init_initiating_party_names',

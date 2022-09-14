f7f94b951a043822f49bf8fd3fdc5ab4120da16b
Pierre Paridans <app@odoo.com>
2022-09-14 11:08:57 +0000

    [IMP] web_enterprise: normalize ListView's layout
    This commit normalizes the ListView's layout allowing to simplify it
    and reduce disparities between community and enterprise.
    
    It also allows to remove the `list_view_extra.scss` replacement.
    
    Part-of: odoo/enterprise#31511

--- web_enterprise/__manifest__.py
+++ web_enterprise/__manifest__.py
@@ -45,7 +45,6 @@ This module modifies the web addon to provide Enterprise design and responsivene
             ('replace', 'web/static/src/legacy/scss/form_view_extra.scss', 'web_enterprise/static/src/legacy/scss/form_view.scss'),
             ('replace', 'web/static/src/legacy/scss/list_view_extra.scss', 'web_enterprise/static/src/legacy/scss/list_view.scss'),
             ('replace', 'web/static/src/views/form/form_view_extra.scss', 'web_enterprise/static/src/views/form/form_view.scss'),
-            ('replace', 'web/static/src/views/list/list_view_extra.scss', 'web_enterprise/static/src/views/list/list_view.scss'),
 
             'web_enterprise/static/src/legacy/scss/dropdown.scss',
             'web_enterprise/static/src/legacy/scss/base_settings_mobile.scss',

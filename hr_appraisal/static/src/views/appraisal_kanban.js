e99b738dec6b7a3e2939461944c340f6f7ffb8b3
William Braeckman <wbr@odoo.com>
2022-09-20 12:20:13 +0000

    [IMP] hr_appraisal: remove duplicated code
    Directly use the employee kanban model instead of redefining our own
    copy.
    
    Part-of: odoo/enterprise#31516

--- hr_appraisal/static/src/views/appraisal_kanban.js
+++ hr_appraisal/static/src/views/appraisal_kanban.js
@@ -1,20 +1,11 @@
 /** @odoo-module */
 
 import { registry } from '@web/core/registry';
-import { patch } from '@web/core/utils/patch';
 
 import { kanbanView } from '@web/views/kanban/kanban_view';
-import { KanbanModel } from '@web/views/kanban/kanban_model';
-
-import { EmployeeChatMixin } from '@hr/mixins/chat_mixin';
-
-export class AppraisalKanbanModel extends KanbanModel {}
-
-export class AppraisalKanbanRecord extends KanbanModel.Record {}
-patch(AppraisalKanbanRecord.prototype, 'appraisal_kanban_record_mixin', EmployeeChatMixin);
-AppraisalKanbanModel.Record = AppraisalKanbanRecord;
+import { EmployeeKanbanModel } from '@hr/views/kanban_view';
 
 registry.category('views').add('appraisal_kanban', {
     ...kanbanView,
-    Model: AppraisalKanbanModel,
+    Model: EmployeeKanbanModel,
 });

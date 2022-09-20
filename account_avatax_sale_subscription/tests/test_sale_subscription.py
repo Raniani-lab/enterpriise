0a4f7d80fa8be8dfa0b34d6ba1e3d87d81710ada
Joren Van Onder <jov@odoo.com>
2022-09-20 19:28:43 +0000

    [FIX] account_avatax_sale_subscription: charge taxes via the cron
    Invoices were being paid via a payment token before the taxes were
    computed.
    
    The already existing _create_recurring_invoice override won't cover
    subscriptions that are auto-charged via a token. For those
    _create_recurring_invoice will call _handle_automatic_invoices which
    calls _do_payment.
    
    opw-2990784
    
    closes odoo/enterprise#32135
    
    X-original-commit: fd626e0707cc99a10ea6ed78401b68adbb8f1456
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_avatax_sale_subscription/tests/test_sale_subscription.py
+++ account_avatax_sale_subscription/tests/test_sale_subscription.py
@@ -5,13 +5,16 @@ from odoo.addons.sale_subscription.tests.common_sale_subscription import TestSub
 
 @tagged("-at_install", "post_install")
 class TestSaleSubscriptionAvalara(TestAccountAvataxCommon, TestSubscriptionCommon):
-    def test_01_subscription_avatax_called(self):
-        partner = self.user_portal.partner_id
-        partner.with_company(self.fp_avatax.company_id).property_account_position_id = self.fp_avatax
-        partner.country_id = self.env.ref('base.us')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        partner = cls.user_portal.partner_id
+        partner.with_company(cls.fp_avatax.company_id).property_account_position_id = cls.fp_avatax
+        partner.country_id = cls.env.ref('base.us')
         partner.zip = '94134'
-        partner.state_id = self.env.ref('base.state_us_5') # California
+        partner.state_id = cls.env.ref('base.state_us_5') # California
 
+    def test_01_subscription_avatax_called(self):
         self.env.ref('product.product_category_all').avatax_category_id = self.env.ref('account_avatax.D0000000')
         self.subscription.action_confirm()
 
@@ -22,4 +25,24 @@ class TestSaleSubscriptionAvalara(TestAccountAvataxCommon, TestSubscriptionCommo
             capture.val and capture.val['json']['referenceCode'],
             invoices[0].name,
             'Should have queried avatax for the right taxes on the new invoice.'
+        )
+
+    def test_02_subscription_do_payment(self):
+        invoice_values = self.subscription._prepare_invoice()
+        new_invoice = self.env["account.move"].create(invoice_values)
+
+        payment_method = self.env['payment.token'].create({
+            'payment_details': 'Jimmy McNulty',
+            'partner_id': self.subscription.partner_id.id,
+            'provider_id': self.provider.id,
+            'provider_ref': 'Omar Little'
+        })
+
+        with self._capture_request({'lines': [], 'summary': []}) as capture:
+            self.subscription._do_payment(payment_method, new_invoice)
+
+        self.assertEqual(
+            capture.val and capture.val['json']['referenceCode'],
+            new_invoice.name,
+            'Should have queried avatax before initiating the payment transaction.'
         )

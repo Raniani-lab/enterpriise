538ea87f68363a94fe0df761f3db474955648b3e
PoMa <poma@odoo.com>
2022-09-28 13:09:34 +0000

    [IMP] account_accountant: Add reconciliation cog to bank recon
    [IMP] handle too many and too large recon models
    [FIX] searchbar not displayed on the down panel of bank recon form
    [FIX] non-standard table look for top panel of bank recon form
    [FIX] Liquidity lines are bold instead of shaded
    [FIX] Two columns displayed on the left of the bank recon in large screens
    
    Part-of: odoo/enterprise#31934

--- account_accountant/static/src/components/bank_reconciliation/bank_rec_widget_form_reco_models_widget.js
+++ account_accountant/static/src/components/bank_reconciliation/bank_rec_widget_form_reco_models_widget.js
@@ -1,15 +1,23 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
+import { useService } from "@web/core/utils/hooks";
+
 
 const { Component } = owl;
 
 export class BankRecWidgetFormRecoModelsWidget extends Component {
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    }
 
     get record() {
         return this.env.model.root;
     }
-    
+
     getRenderValues(){
         return this.record.data.reco_models_widget;
     }
@@ -23,9 +31,21 @@ export class BankRecWidgetFormRecoModelsWidget extends Component {
             await this.record.update({todo_command: `mount_line_in_edit,${line_index}`})
         }
     }
+    async _onClickCreateReconciliationModel(ev) {
+        return this.actionService.doAction({
+            type: "ir.actions.act_window",
+            res_model: "account.reconcile.model",
+            views: [[false, "form"]],
+            target: "current",
+            context: {
+                default_match_journal_ids: this.record.data.journal_id,
+            }
+        });
+    }
 
 }
 
 BankRecWidgetFormRecoModelsWidget.template = "account_accountant.bank_rec_widget_form_reco_models_widget";
+BankRecWidgetFormRecoModelsWidget.components = { Dropdown, DropdownItem };
 
 registry.category("fields").add("bank_rec_widget_form_reco_models_widget", BankRecWidgetFormRecoModelsWidget);

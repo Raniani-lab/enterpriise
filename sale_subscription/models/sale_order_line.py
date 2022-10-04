1f7fcd4d3aa35d271210fa55be22e2921dc8098c
Arnaud Joset <arj@odoo.com>
2022-10-04 09:59:12 +0000

    [FIX] sale_subscription: round price_unit in _compute_parent_line_id
    Before this commit, parent_line_id was sometimes falsy because the price_unit
    could not be matched between new lines and child order line. E.G.
    Matching lines sale.order.line(10103548,) (sale.order(2282811,), product.product(18306,), uom.uom(116,), -326.4)
    Matching lines sale.order.line() (sale.order(2282811,), product.product(18306,), uom.uom(116,), -326.40000000000003) (modifié)
    
    It could happen with new regords in onchange trigger where the ORM is doing record._update_cache(values, validate=False)
    and validate=False will prevent float rounding.
    
    X-original-commit: 1e8d89971857fc9ce47df1bdcde1e12d6407ba38
    Part-of: odoo/enterprise#32572

--- sale_subscription/models/sale_order_line.py
+++ sale_subscription/models/sale_order_line.py
@@ -202,9 +202,14 @@ class SaleOrderLine(models.Model):
         for line in self:
             if not line.order_id.subscription_id or not line.product_id.recurring_invoice:
                 continue
+            # We use a rounding to avoid -326.40000000000003 != -326.4 for new records.
             matching_line_ids = parent_line_ids.filtered(
                 lambda l:
-                (l.order_id, l.product_id, l.product_uom, l.price_unit) == (line.order_id.subscription_id, line.product_id, line.product_uom, line.price_unit)
+                (l.order_id, l.product_id, l.product_uom, l.order_id.currency_id,
+                 l.order_id.currency_id.round(l.price_unit) if l.order_id.currency_id else round(l.price_unit, 2)) ==
+                (line.order_id.subscription_id, line.product_id, line.product_uom, line.order_id.currency_id,
+                 line.order_id.currency_id.round(line.price_unit) if line.order_id.currency_id else round(line.price_unit, 2)
+                 )
             )
             if matching_line_ids:
                 line.parent_line_id = matching_line_ids.ids[-1]

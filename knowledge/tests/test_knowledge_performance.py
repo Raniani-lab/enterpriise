35e4107e3199b5484aa651b1f714ff32a6a71495
Thibault Delavallée <tde@odoo.com>
2022-10-04 08:10:34 +0000

    [PERF] knowledge: update counters to current runbot
    Purpose is to have accurate counters, allowing to dive into code and
    try to find performance improvements. Those are updated according to
    latest runbot results, time of this commit.
    
    Some tests have lower results, notably thanks to odoo/odoo@fe4299a079b5a7ae2eaa67caefbe3eaf747e1b93
    
    Task-3004723
    
    X-original-commit: fe697170a02f29f256595194821f0dfbc4ee61f4
    Part-of: odoo/enterprise#32450

--- knowledge/tests/test_knowledge_performance.py
+++ knowledge/tests/test_knowledge_performance.py
@@ -25,7 +25,7 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @warmup
     def test_article_creation_single_shared_grandchild(self):
         """ Test with 2 levels of hierarchy in a private/shared environment """
-        with self.assertQueryCount(employee=28):
+        with self.assertQueryCount(employee=24):
             _article = self.env['knowledge.article'].create({
                 'body': '<p>Hello</p>',
                 'name': 'Article in shared',
@@ -37,7 +37,7 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @users('employee')
     @warmup
     def test_article_creation_single_workspace(self):
-        with self.assertQueryCount(employee=25):
+        with self.assertQueryCount(employee=21):
             _article = self.env['knowledge.article'].create({
                 'body': '<p>Hello</p>',
                 'name': 'Article in workspace',
@@ -61,7 +61,7 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @users('employee')
     @warmup
     def test_article_creation_multi_shared_grandchild(self):
-        with self.assertQueryCount(employee=55):
+        with self.assertQueryCount(employee=51):
             _article = self.env['knowledge.article'].create([
                 {'body': '<p>Hello</p>',
                  'name': f'Article {index} in workspace',
@@ -73,21 +73,21 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @users('employee')
     @warmup
     def test_article_favorite(self):
-        with self.assertQueryCount(employee=11):  # knowledge only: 11
+        with self.assertQueryCount(employee=9):
             shared_article = self.shared_children[0].with_env(self.env)
             shared_article.action_toggle_favorite()
 
     @users('employee')
     @warmup
     def test_article_home_page(self):
-        with self.assertQueryCount(employee=20):
+        with self.assertQueryCount(employee=18):
             self.env['knowledge.article'].action_home_page()
 
     @mute_logger('odoo.addons.base.models.ir_rule', 'odoo.addons.mail.models.mail_mail', 'odoo.models.unlink', 'odoo.tests')
     @users('employee')
     @warmup
     def test_article_invite_members(self):
-        with self.assertQueryCount(employee=85):  # knowledge only: 85
+        with self.assertQueryCount(employee=81):
             shared_article = self.shared_children[0].with_env(self.env)
             partners = (self.customer + self.partner_employee_manager + self.partner_employee2).with_env(self.env)
             shared_article.invite_members(partners, 'write')
@@ -96,6 +96,6 @@ class KnowledgePerformanceCase(KnowledgeCommonWData):
     @warmup
     def test_article_move_to(self):
         before_id = self.workspace_children[0].id
-        with self.assertQueryCount(employee=32):
+        with self.assertQueryCount(employee=24):  # knowledge: 23
             writable_article = self.workspace_children[1].with_env(self.env)
             writable_article.move_to(parent_id=writable_article.parent_id.id, before_article_id=before_id)

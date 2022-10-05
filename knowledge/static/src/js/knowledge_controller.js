ee6e630718dae0f8ac06d8d9bc8d479d09cc6984
Michael (mcm) <mcm@odoo.com>
2022-10-05 11:33:19 +0000

    [FIX] web_enterprise,knowledge: better manage borderless inputs
    This commit refactors the style for borderless inputs and
    adds a special class to better manage where they should be used.
    
    closes odoo/enterprise#32324
    
    Related: odoo/odoo#102259
    Signed-off-by: Bruno Boi (boi) <boi@odoo.com>

--- knowledge/static/src/js/knowledge_controller.js
+++ knowledge/static/src/js/knowledge_controller.js
@@ -2,7 +2,13 @@
 
 import { FormController } from '@web/views/form/form_controller';
 
-export class KnowledgeArticleFormController extends FormController {}
+export class KnowledgeArticleFormController extends FormController {
+    get className() {
+        const result = super.className;
+        result["o_form_with_borderless_input"] = false;
+        return result;
+    }
+}
 
 // Open articles in edit mode by default
 KnowledgeArticleFormController.defaultProps = {

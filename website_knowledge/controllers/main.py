3121f9c075e3a63e002173b0d8da8344e9456dce
amdi-odoo <amdi@odoo.com>
2022-10-05 08:56:25 +0000

    [IMP] {website_}knowledge: handle website conflicts
    Purpose
    =======
    Improve the UI and the redirection for logged in users.
    
    Specifications
    ==============
    Hide the livechat button from knowledge since it
    should not appear nor be accessible.
    
    Redirect anonymous users on the login page if
    they are trying to access an existing but
    unpublished article.
    
    To prevent a broken UI look, adapt the size
    of the portal users virality message and hide it on
    mobile devices.
    
    Task-2936605
    
    closes odoo/enterprise#32556
    
    X-original-commit: 1b4e3898f7225fc620563fac06b47886f533da68
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

--- website_knowledge/controllers/main.py
+++ website_knowledge/controllers/main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import werkzeug
+
 from odoo import http
+from odoo.http import request
 from odoo.addons.knowledge.controllers.main import KnowledgeController
 
 
@@ -10,6 +13,13 @@ class KnowledgeWebsiteController(KnowledgeController):
     # Override routes to display articles to public users
     @http.route('/knowledge/article/<int:article_id>', type='http', auth='public', website=True, sitemap=False)
     def redirect_to_article(self, **kwargs):
+        if request.env.user._is_public():
+            article = request.env['knowledge.article'].sudo().browse(kwargs['article_id'])
+            if not article.exists():
+                raise werkzeug.exceptions.NotFound()
+            if not article.website_published:
+                # public users can't access articles that are not published, let them login first
+                return request.redirect('/web/login?redirect=/knowledge/article/%s' % kwargs['article_id'])
         return super().redirect_to_article(**kwargs)
 
     @http.route('/knowledge/home', type='http', auth='public', website=True, sitemap=False)

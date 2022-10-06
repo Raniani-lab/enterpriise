Raniani-lab/enterpriise

commit 1a6abc731071f39e22693f0c1d54889e3ffdc5ff

[FIX] *_mrp_workorder_*: tablet view refactoring
This commit redo the "tablet view" of workorder with a new client
action. The main component is now an action calling an Owl component
made of two small form views (one for the workorder, one for the current
quality check) and some widgets. The main feature is the ability to see
every quality checks to do (the `Step` component) directly on the tablet
view.

The current quality check has its own view so we get rid of a bunch of
related fields on mrp_workorder.

Task: 2427466
Part-of: odoo/enterprise#23032

mrp_workorder/static/src/components/menuPopup.js
@@ -1,0 +1,48 @@
+/** @odoo-module **/
+
+import { useService } from "@web/core/utils/hooks";
+const { Component } = owl;
+
+class MenuPopup extends Component {
+
+    setup() {
+        this.rpc = useService('rpc');
+        this.orm = useService('orm');
+        this.action = useService('action');
+    }
+
+    get step() {
+        return this.props.popupData.selectedStepId;
+    }
+
+    get title() {
+        return this.props.popupData.title;
+    }
+
+    block() {
+        const options = {
+            additionalContext: { default_workcenter_id: this.props.popupData.workcenterId },
+            onClose: this.props.onClose,
+        };
+        this.props.onClosePopup('menu');
+        this.action.doAction('mrp.act_mrp_block_workcenter_wo', options);
+    }
+
+    async callAction(method) {
+        const action = await this.orm.call(
+            'mrp.workorder',
+            method,
+            [[this.props.popupData.workorderId]]
+        );
+        this.props.onClosePopup('menu');
+        this.action.doAction(action, { onClose: this.props.onClose });
+    }
+
+    cancel() {
+        this.props.onClosePopup('menu');
+    }
+}
+
+MenuPopup.template = 'mrp_workorder.MenuPopup';
+
+export default MenuPopup;

mrp_workorder/static/src/components/menuPopup.xml
@@ -1,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<templates id="template" xml:space="preserve">
+    <t t-name="mrp_workorder.MenuPopup" owl="1">
+        <div role="dialog" class="popup">
+            <div class="popup-selection">
+                <header class="title h3" t-esc="title"/>
+                <div class="button_list">
+                    <button class="btn btn-danger text-uppercase" t-on-click="block">Block</button>
+                    <button class="btn btn-primary" t-on-click="() => this.callAction('button_scrap')">Scrap</button>
+                    <button class="btn btn-primary" t-on-click="() => this.callAction('action_add_component')">Add Component</button>
+                    <button class="btn btn-primary" t-on-click="() => this.callAction('action_add_byproduct')" name="addByProduct">Add By-product</button>
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="btn cancel text-uppercase" t-on-click="cancel">Cancel</div>
+            </footer>
+        </div>
+    </t>
+</templates>

mrp_workorder/static/src/components/tablet.js
@@ -1,0 +1,264 @@
+/** @odoo-module **/
+
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import core from "web.core";
+import ViewsWidget from '@mrp_workorder/widgets/views_widget';
+import StepComponent from '@mrp_workorder/components/step';
+import ViewsWidgetAdapter from '@mrp_workorder/components/views_widget_adapter';
+import MenuPopup from '@mrp_workorder/components/menuPopup';
+
+const {useState, useRef, useEffect, onWillStart, EventBus, Component, markup} = owl;
+
+/**
+ * Main Component
+ * Gather the workorder and its quality check information.
+ */
+
+class Tablet extends Component {
+    //--------------------------------------------------------------------------
+    // Lifecycle
+    //--------------------------------------------------------------------------
+    setup() {
+        this.rpc = useService('rpc');
+        this.orm = useService('orm');
+        this.notification = useService('notification');
+        this.pdfIFrame = useRef("o_tablet_document_iframe");
+        this.state = useState({
+            selectedStepId: 0,
+            workingState: "",
+        });
+
+        this.popup = useState({
+            menu: {
+                isShown: false,
+                data: {},
+            }
+        });
+        this.workorderId = this.props.action.context.active_id;
+        this.additionalContext = this.props.action.context;
+        this.ViewsWidget = ViewsWidget;
+        this.bus = new EventBus();
+        this.bus.on('refresh', this, async () => {
+            await this.getState();
+            this.render();
+        });
+        this.bus.on('workorder_event', this, (method) => {
+            this[method]();
+        });
+        this.bus.on('exit', this, this.exit);
+        core.bus.on('barcode_scanned', this, this._onBarcodeScanned);
+        onWillStart(async () => {
+            await this._onWillStart();
+        });
+
+        useEffect(() => {
+            this.updatePdf();
+            this._scrollToHighlighted();
+        });
+    }
+
+    _scrollToHighlighted() {
+        let selectedLine = document.querySelector('.o_tablet_timeline .o_tablet_step.o_selected');
+        if (selectedLine) {
+            // If a line is selected, checks if this line is entirely visible
+            // and if it's not, scrolls until the line is.
+            const headerHeight = document.querySelector('.o_legacy_form_view').offsetHeight.height;
+            const lineRect = selectedLine.getBoundingClientRect();
+            const page = document.querySelector('.o_tablet_timeline');
+            // Computes the real header's height (the navbar is present if the page was refreshed).
+            let scrollCoordY = false;
+            if (lineRect.top < headerHeight) {
+                scrollCoordY = lineRect.top - headerHeight + page.scrollTop;
+            } else if (lineRect.bottom > window.innerHeight) {
+                const pageRect = page.getBoundingClientRect();
+                scrollCoordY = page.scrollTop - (pageRect.bottom - lineRect.bottom);
+            }
+            if (scrollCoordY !== false) { // Scrolls to the line only if it's not entirely visible.
+                page.scroll({ left: 0, top: scrollCoordY, behavior: this._scrollBehavior });
+                this._scrollBehavior = 'smooth';
+            }
+        }
+    }
+
+    async getState() {
+        this.data = await this.orm.call(
+            'mrp.workorder',
+            'get_workorder_data',
+            [this.workorderId],
+        );
+        this.steps = this.data['quality.check'];
+        this.state.selectedStepId = this.data['mrp.workorder'].current_quality_check_id;
+        this.state.workingState = this.data.working_state;
+    }
+
+    async exit(ev) {
+        await new Promise(resolve => this.bus.trigger('save', resolve));
+        this._buttonClick('button_pending');
+        this.env.config.historyBack();
+    }
+
+    async _buttonClick(method) {
+        const res = await this.orm.call('mrp.workorder', method, [this.workorderId]);
+        await this.getState();
+        return res;
+    }
+
+    async selectStep(id) {
+        await this.saveCurrentStep(id);
+    }
+
+    async saveCurrentStep(newId) {
+        await new Promise(resolve => this.bus.trigger('save', resolve));
+        this.orm.write(
+            'mrp.workorder',
+            [this.workorderId],
+            {current_quality_check_id: newId},
+        );
+        this.state.selectedStepId = newId;
+    }
+
+    updatePdf() {
+        let page = 0;
+        let queryObj = {};
+        if (this.selectedStep.worksheet_document) {
+            page = 1;
+            queryObj = {
+                model: 'quality.check',
+                field: 'worksheet_document',
+                id: this.state.selectedStepId,
+            };
+        } else if (this.data.operation.worksheet && this.selectedStep.worksheet_page) {
+            page = this.selectedStep.worksheet_page;
+            queryObj = {
+                model: 'mrp.routing.workcenter',
+                field: 'worksheet',
+                id: this.data.operation.id,
+            };
+        }
+        if (page && queryObj) {
+            const queryString = new URLSearchParams(queryObj).toString()
+            const fileURI = encodeURIComponent('/web/content?' + queryString);
+            const viewerURL = '/web/static/lib/pdfjs/web/viewer.html?file=';
+            const src = viewerURL + fileURI + '#page=' + page;
+            this.pdfIFrame.el.setAttribute('src', src);
+            this.pdfIFrame.el.removeAttribute('style');
+            // Once the PDF viewer is loaded, hides everything except the page.
+            this.pdfIFrame.el.addEventListener('load', () => {
+                this.pdfIFrame.el.contentDocument.querySelector('.toolbar').style.display = 'none';
+                this.pdfIFrame.el.contentDocument.querySelector('#viewerContainer').style.top = '0';
+                this.pdfIFrame.el.contentDocument.querySelector('body').style.background = 'none';
+                this.pdfIFrame.el.contentDocument.querySelector('#viewerContainer').style.boxShadow = 'none';
+            });
+        }
+    }
+
+    get worksheet() {
+        if (this.selectedStep) {
+            return this.selectedStep.worksheet_document || (this.selectedStep.worksheet_page && this.data.operation.worksheet);
+        } else {
+            return this.data.operation.worksheet;
+        }
+    }
+
+    get selectedStep() {
+        return this.state.selectedStepId && this.steps.find(
+            l => l.id === this.state.selectedStepId
+        );
+    }
+
+    get viewsWidgetData() {
+        const data = {
+            workorder: {
+                model: 'mrp.workorder',
+                view: 'mrp_workorder.mrp_workorder_view_form_tablet',
+                additionalContext: this.additionalContext,
+                params: {currentId: this.workorderId},
+                currentId: this.workorderId,
+                bus: this.bus
+            },
+            check: {
+                model: 'quality.check',
+                view: 'mrp_workorder.quality_check_view_form_tablet',
+                additionalContext: this.additionalContext,
+                params: {currentId: this.state.selectedStepId},
+                currentId: this.state.selectedStepId,
+                bus: this.bus
+            },
+        };
+        return data;
+    }
+
+    get checkInstruction() {
+        let note = this.data['mrp.workorder'].operation_note;
+        if (note && note !== '<p><br></p>') {
+            return markup(note);
+        } else {
+            return undefined;
+        }
+    }
+
+    get isBlocked() {
+        return this.state.workingState === 'blocked';
+    }
+
+    showPopup(props, popupId) {
+        this.popup[popupId].isShown = true;
+        this.popup[popupId].data = props;
+    }
+
+    closePopup(popupId) {
+        this.getState();
+        this.popup[popupId].isShown = false;
+    }
+
+    async onCloseRerender() {
+        await this.getState();
+        this.render();
+    }
+
+    openMenuPopup() {
+        this.showPopup({
+            title: 'Menu',
+            workcenterId: this.data['mrp.workorder'].workcenter_id,
+            selectedStepId: this.state.selectedStepId,
+            workorderId: this.workorderId,
+        }, 'menu');
+    }
+
+    async _onWillStart() {
+        await this.getState();
+    }
+
+    _onBarcodeScanned(barcode) {
+        const commands = {
+            'O-BTN.pause': this._buttonClick.bind(this, 'action_pending'),
+            'O-BTN.next': this.validate.bind(this),
+            'O-BTN.prev': this.previousStep.bind(this),
+            'O-BTN.skip': this.nextStep.bind(this),
+            'O-BTN.cloWO': this._buttonClick.bind(this, 'do_finish'),
+            'O-BTN.cloMO': this._buttonClick.bind(this, 'action_open_manufacturing_order'),
+            'O-BTN.pass': this._buttonClick.bind(this, 'do_pass'),
+            'O-BTN.fail': this._buttonClick.bind(this, 'do_fail'),
+            'O-BTN.record': this.recordProduction.bind(this),
+        };
+        if (commands[barcode]) {
+            commands[barcode]();
+        } else {
+            this.orm.call('quality.check', 'on_barcode_scanned', [this.state.selectedStepId]);
+        }
+
+    }
+}
+
+Tablet.props = ['action', '*'];
+Tablet.template = 'mrp_workorder.Tablet';
+Tablet.components = {
+    StepComponent,
+    ViewsWidgetAdapter,
+    MenuPopup,
+};
+
+registry.category('actions').add('tablet_client_action', Tablet);
+
+export default Tablet;

commit c56e0b2e585a47bb1a95a9aeea4106f5d519180f

[FIX] *+mrp_workorder_*: various fixes on the workorder tablet view
This commit fixes several issues on the tablet view introduced in 1a6abc73107

    1. Make Background of work orders in work center control panel white
    2. Increase size of text in tablet interface
    3. Make Worksheet Type quality check effective on workorder
    4. Not possible to move ECO stage, for ECO created from tablet view
    5. Keep session employee logged in

Task : 2991906

Part-of: odoo/enterprise#31935

mrp_workorder/models/mrp_workorder.py
@@ -516,7 +516,7 @@ class MrpProductionWorkcenterLine(models.Model):
         ele = self.check_ids.filtered(lambda check: not check.previous_check_id)
         sorted_check_list = []
         while ele:
-            sorted_check_list.append(ele.id)
+            sorted_check_list += ele.ids
             ele = ele.next_check_id
         data = {
             'mrp.workorder': self.read(self._get_fields_for_tablet(), load=False)[0],

mrp_workorder/static/src/components/menuPopup.js
@@ -42,14 +42,16 @@ class MenuPopup extends Component {
         this.props.onClosePopup('menu');
     }
 
-    async proposeChange(changeType, title) {
+    async proposeChange(changeType, title, message) {
         const action = await this.orm.call(
             'mrp.workorder',
             'action_propose_change',
             [[this.props.popupData.workorderId], changeType, title],
         );
         this.props.onClosePopup('menu');
-        this.action.doAction(action, { onClose: this.props.onClose });
+        this.action.doAction(action, { onClose: () => {
+            this.props.onClose(message);
+        }});
     }
 }
 

mrp_workorder/static/src/components/menuPopup.xml
@@ -12,9 +12,9 @@
 
                     <span><h2>Suggest a Worksheet improvement</h2></span>
                     <t t-if="step">
-                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('update_step', 'Update Instructions')">Update Instructions</button>
-                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('remove_step', 'Remove Step')">Delete this Step</button>
-                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('set_picture', 'Change Picture')">Set a New picture</button>
+                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('update_step', 'Update Instructions', 'Your New Instructions feedback was created')">Update Instructions</button>
+                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('remove_step', 'Remove Step', 'Your feedback to delete this step was created')">Delete this Step</button>
+                        <button class="btn btn-primary" t-on-click="() => this.proposeChange('set_picture', 'Change Picture', '')">Set a New picture</button>
                     </t>
                     <button class="btn btn-primary" t-on-click="() => this.callAction('action_add_step')">Add a Step</button>
                 </div>

mrp_workorder/static/src/components/tablet.js
@@ -23,6 +23,7 @@ class Tablet extends Component {
     setup() {
         this.rpc = useService('rpc');
         this.orm = useService('orm');
+        this.notification = useService('notification');
         this.state = useState({
             selectedStepId: 0,
             workingState: "",
@@ -210,7 +211,10 @@ class Tablet extends Component {
         this.popup[popupId].isShown = false;
     }
 
-    async onCloseRerender() {
+    async onCloseRerender(message) {
+        if (message) {
+            this.notification.add(this.env._t(message), {type: 'success'});
+        }
         await this.getState();
         this.render();
     }

mrp_workorder/static/src/components/tablet.scss
@@ -63,15 +63,16 @@ $o-wo-tablet-text: color-contrast($o-wo-tablet-bg, $body-color, #FFF);
             align-items: stretch;
             padding: 0 $o-wo-tablet-padding*0.5;
             margin-top: 0;
+            font-size: 1.3em;
             &.form_top {
-                font-size: 1.3em;
                 min-height: 100px;
                 border-bottom: 1px solid $o-wo-tablet-border-bg;
                 background-color: $o-wo-tablet-bg-dark;
                 padding-bottom: 0;
             }
-            .o_m2m_avatar_empty {
+            span.o_tag {
                 background-color: $o-wo-tablet-bg-dark;
+                box-shadow: none;
             }
             .o_unblock {
                 z-index: 100;
@@ -204,6 +205,9 @@ $o-wo-tablet-text: color-contrast($o-wo-tablet-bg, $body-color, #FFF);
                     border: none;
                     width: 100%;
                     height: 100%;
+                }
+                img {
+                    max-width: 75%;
                 }
 
             }

mrp_workorder/views/quality_views.xml
@@ -66,17 +66,8 @@
                 <field name="production_id" invisible="1"/>
                 <div class="o_workorder_bar">
                     <div class="o_workorder_bar_left">
-                        <div class="o_workorder_bar_content">
-                            <field name="note" readonly="1"/>
-                        </div>
-                        <div class="o_workorder_bar_content">
-                            <field name="additional_note" readonly="1"/>
-                        </div>
-                        <div class="o_workorder_bar_content">
-                            <div class="workorder_picture" attrs="{'invisible': [('test_type', '!=', 'picture')]}">
-                                <field name="picture" widget="image"/>
-                            </div>
-                            <div attrs="{'invisible': [('test_type', 'not in', ('register_byproducts', 'register_consumed_materials'))]}">
+                        <div class="o_workorder_bar_content" attrs="{'invisible': [('test_type', 'not in', ('register_byproducts', 'register_consumed_materials'))]}">
+                            <div>
                                 <field name="component_id" options="{'no_open': True}" readonly="1"/>
                             </div>
                         </div>
@@ -92,6 +83,17 @@
                                 <div class="o_form_label ps-5" attrs="{'invisible': [('component_tracking', '!=', 'serial')]}">Serial</div>
                                 <div class="o_form_label ps-5" attrs="{'invisible': [('component_tracking', '!=', 'lot')]}">Lot</div>
                                 <field name="lot_id" class="text-center" context="{'default_product_id': component_id, 'default_company_id': company_id, 'active_mo_id': test_type == 'register_consumed_materials' and production_id or False}" attrs="{'invisible': [('component_tracking', '=', 'none')]}" domain="[('product_id', '=', component_id), '|', ('company_id', '=', False), ('company_id', '=', company_id)]"/>
+                            </div>
+                        </div>
+                        <div class="o_workorder_bar_content">
+                            <field name="note" readonly="1"/>
+                        </div>
+                        <div class="o_workorder_bar_content">
+                            <field name="additional_note" readonly="1"/>
+                        </div>
+                        <div class="o_workorder_bar_content">
+                            <div class="workorder_picture" attrs="{'invisible': [('test_type', '!=', 'picture')]}">
+                                <field name="picture" widget="image"/>
                             </div>
                         </div>
                     </div>
@@ -178,8 +180,7 @@
                     <field name="company_id" invisible="1"/>
                     <field name="product_id" invisible="1"/>
                     <field name="workorder_id" invisible="1"/>
-                    <field name="name" string="Instructions"/>
-                    <field name="additional_note" string="Reason"/>
+                    <field name="note" string="Instruction"/>
                 </group>
                 <footer>
                     <button class="btn btn-primary" type="object" name="add_check_in_chain" string="Save"/>

mrp_workorder/wizard/propose_change.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import SUPERUSER_ID, fields, models, _
 from markupsafe import Markup
+
+from odoo import SUPERUSER_ID, api, fields, models, _
 
 
 class ProposeChange(models.TransientModel):
@@ -36,11 +37,7 @@ class ProposeChange(models.TransientModel):
         self.ensure_one()
         self.step_id.note = self.note
         if self.workorder_id.production_id.bom_id:
-            message = _("<b>New Instruction suggested by %s</b><br/>"
-                 "%s<br/>", self._workorder_name(), self.note)
-            if self.comment:
-                message += _("<b>Reason: %s</b>", self.comment)
-            body = Markup(message)
+            body = Markup(_("<b>New Instruction suggested by %s</b><br/>%s<br/><b>Reason: %s</b>")) % (self._workorder_name(), self.note, self.comment)
             self.env['mail.activity'].sudo().create({
                 'res_model_id': self.env.ref('mrp.model_mrp_bom').id,
                 'res_id': self.workorder_id.production_id.bom_id.id,
@@ -54,7 +51,7 @@ class ProposeChange(models.TransientModel):
         self.ensure_one()
         bom = self.step_id.workorder_id.production_id.bom_id
         if bom:
-            body = Markup(_("<b>%s suggests to delete this instruction</b>", self._workorder_name()))
+            body = Markup(_("<b>%s suggests to delete this instruction</b>")) % self._workorder_name()
             self.env['mail.activity'].sudo().create({
                 'res_model_id': self.env.ref('mrp.model_mrp_bom').id,
                 'res_id': bom.id,
@@ -63,14 +60,19 @@ class ProposeChange(models.TransientModel):
                 'summary': _('BoM feedback %s (%s)', self.step_id.title, self.workorder_id.production_id.name),
                 'note': body,
             })
-        self.step_id.do_fail()
+
+    @api.model
+    def image_url(self, record, field):
+        """ Returns a local url that points to the image field of a given browse record. """
+        return '/web/image/%s/%s/%s' % (record._name, record.id, field)
 
     def _do_set_picture(self):
         self.ensure_one()
         self.step_id.worksheet_document = self.picture
         bom = self.step_id.workorder_id.production_id.bom_id
         if bom:
-            body = Markup(_("<b>%s suggests to use this document as instruction</b>", self._workorder_name()))
+            body = Markup(_("<b>%s suggests to use this document as instruction</b><br/><img style='max-width: 75%%' class='img-fluid' src=%s/>"))\
+                % (self._workorder_name(), self.image_url(self, 'picture'))
             self.env['mail.activity'].sudo().create({
                 'res_model_id': self.env.ref('mrp.model_mrp_bom').id,
                 'res_id': bom.id,
@@ -79,10 +81,3 @@ class ProposeChange(models.TransientModel):
                 'summary': _('BoM feedback %s (%s)', self.step_id.title, self.workorder_id.production_id.name),
                 'note': body,
             })
-        self.env['ir.attachment'].create({
-            'name': 'coucou',
-            'res_id': bom.id,
-            'res_model': bom._name,
-            'datas': self.picture,
-            'type': 'binary',
-        })

mrp_workorder_hr/__manifest__.py
@@ -11,7 +11,7 @@ This module allows Employees (and not users) to log in to a workorder using a ba
 The actual till still requires one user but an unlimited number of employees can log on to that till and complete manufacturing tasks.
     """,
 
-    'depends': ['mrp_workorder', 'hr_hourly_cost'],
+    'depends': ['mrp_workorder', 'hr_hourly_cost', 'hr'],
 
     'data': [
         'views/hr_employee_views.xml',
@@ -20,7 +20,7 @@ The actual till still requires one user but an unlimited number of employees can
         'views/mrp_operation_views.xml',
     ],
     'installable': True,
-    'auto_install': True,
+    'auto_install': ['mrp_workorder', 'hr'],
     'assets': {
         'web.assets_backend': [
             'mrp_workorder_hr/static/src/**/*.js',

mrp_workorder_hr/models/mrp_workcenter.py
@@ -1,5 +1,7 @@
-# -*- coding: utf-8 -*-
+from ast import literal_eval
+
 from odoo import models, fields
+from odoo.http import request
 
 
 class MrpWorkcenter(models.Model):
@@ -9,7 +11,16 @@ class MrpWorkcenter(models.Model):
     employee_ids = fields.Many2many(
         'hr.employee', string="employees with access",
         help='if left empty, all employees can log in to the workcenter')
+    currency_id = fields.Many2one(related='company_id.currency_id')
     employee_costs_hour = fields.Monetary(string='Employee Hourly Cost', currency_field='currency_id', default=0.0)
+
+    def action_work_order(self):
+        action = super().action_work_order()
+        # for the call to literal_eval
+        context = action.get('context', '{}')
+        context = context.replace('active_id', str(self.id))
+        action['context'] = dict(literal_eval(context), employee_id=request.session.get('employee_id'))
+        return action
 
 
 class MrpWorkcenterProductivity(models.Model):

mrp_workorder_plm/models/mrp_workorder.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, Command
 
 
 class QualityCheck(models.Model):
@@ -42,6 +42,8 @@ class QualityCheck(models.Model):
             'team_id': self.team_id.id,
             'company_id': self.company_id.id,
             'test_type_id': self.env.ref('quality.test_type_instructions', raise_if_not_found=False).id,
+            'picking_type_ids': [Command.link(self.workorder_id.production_id.picking_type_id.id)],
+            'note': self.note,
         }
         point = self.env['quality.point'].create(quality_point_data)
         self.point_id = point

mrp_workorder_plm/wizard/propose_change.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from markupsafe import Markup
+
 from odoo import models
 
 
@@ -43,6 +45,7 @@ class ProposeChange(models.TransientModel):
 
     def _do_update_step(self):
         eco = self._get_eco()
+        self.step_id.note = self.note
         # get the step on the new bom related to the one we want to update
         new_step = eco.new_bom_id.operation_ids.quality_point_ids.filtered(lambda p: p._get_comparison_values() == self.step_id.point_id._get_comparison_values())
         new_step.note = self.note
@@ -52,11 +55,11 @@ class ProposeChange(models.TransientModel):
         # get the step on the new bom related to the one we want to delete
         new_step = eco.new_bom_id.operation_ids.quality_point_ids.filtered(lambda p: p._get_comparison_values() == self.step_id.point_id._get_comparison_values())
         new_step.unlink()
-        self.step_id.do_fail()
 
     def _do_set_picture(self):
         eco = self._get_eco()
+        self.step_id.worksheet_document = self.picture
         # get the step on the new bom related to the one we want to delete
         new_step = eco.new_bom_id.operation_ids.quality_point_ids.filtered(lambda p: p._get_comparison_values() == self.step_id.point_id._get_comparison_values())
-        new_step.worksheet_document = self.picture
+        new_step.note = Markup("<img class='img-fluid' src=%s/>") % self.image_url(self, 'picture')
         new_step.source_document = 'step'

quality_mrp_workorder/models/quality.py
@@ -23,8 +23,7 @@ class QualityCheck(models.Model):
 
     def do_fail(self):
         self.ensure_one()
-        super().do_fail()
-        return self.workorder_id._change_quality_check(position='next')
+        return super().do_fail()
 
     def do_measure(self):
         self.ensure_one()

quality_mrp_workorder/views/quality_views.xml
@@ -98,7 +98,7 @@
                 <button name="action_next" type="object" class="btn-success" attrs="{'invisible': [('test_type', '!=', 'passfail')]}" barcode_trigger="pass" string="Pass"/>
                 <button name="do_measure" type="object" class="btn-primary" string="Validate" attrs="{'invisible': [('test_type', '!=', 'measure')]}" barcode_trigger="next"/>
             </xpath>
-            <xpath expr="//div[hasclass('workorder_picture')]" position="before">
+            <xpath expr="//field[@name='note']" position="before">
                 <div attrs="{'invisible': [('test_type', '!=', 'measure')]}" class="o_workorder_bar_left o_workorder_bar_content">
                     <label for="measure" string="Measure:" />
                     <field name="measure" class="oe_inline"/>

quality_mrp_workorder_worksheet/models/quality.py
@@ -10,7 +10,7 @@ class QualityCheck(models.Model):
 
     def action_worksheet_check(self):
         self.ensure_one()
-        self.action_worksheet_check()
+        self.super().action_worksheet_check()
         return self._next()
 
     def action_fill_sheet(self):
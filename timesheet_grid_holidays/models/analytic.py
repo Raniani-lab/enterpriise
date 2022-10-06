f00a935017631230b20c24cdb7ecde09f9d684fb
Kartik Chavda <kcv@odoo.com>
2022-10-06 09:58:16 +0000

    [IMP] timesheet_grid: add redirect warning
    This commit add redirect warning dialog when user try to
    merge time off related timesheets and add view time off
    button in that dialog which redirect user to time off's
    that are related to that timesheets.
    
    task-2888607
    
    closes odoo/enterprise#29678
    
    Related: odoo/odoo#96359
    Signed-off-by: Laurent Stukkens (ltu) <ltu@odoo.com>

--- timesheet_grid_holidays/models/analytic.py
+++ timesheet_grid_holidays/models/analytic.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models
+from odoo import models, _
+from odoo.exceptions import RedirectWarning, UserError
 
 
 class AnalyticLine(models.Model):
@@ -11,3 +12,12 @@ class AnalyticLine(models.Model):
     def _should_not_display_timer(self):
         self.ensure_one()
         return super()._should_not_display_timer() or self.task_id.is_timeoff_task
+
+    def action_merge_timesheets(self):
+        if self.holiday_id:
+            if not self.env.user.has_group('hr_holidays.group_hr_holidays_user') and self.env.user not in self.holiday_id.sudo().user_id:
+                raise UserError(_('You cannot merge timesheets that are linked to time off requests. Please use the Time Off application to modify or cancel your time off requests instead.'))
+            warning_msg = _('You cannot merge timesheets that are linked to time off requests. Please use the Time Off application to modify or cancel your time off requests instead.')
+            action = self._get_redirect_action()
+            raise RedirectWarning(warning_msg, action, _('View Time Off'))
+        return super().action_merge_timesheets()

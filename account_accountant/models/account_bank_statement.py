e367d4d93de2aaf7ff30cb9aeaa4443b542dacc1
Laurent Smet <las@odoo.com>
2022-10-06 08:11:07 +0000

    [IMP] account_accountant: Log a message on the chatter when auto-validating a bank transaction
    X-original-commit: 59d2059931839ec2476aca8579a43973c526014b
    Part-of: odoo/enterprise#32550

--- account_accountant/models/account_bank_statement.py
+++ account_accountant/models/account_bank_statement.py
@@ -115,6 +115,12 @@ class AccountBankStatementLine(models.Model):
             wizard._action_trigger_matching_rules()
             if wizard.state == 'valid' and wizard.matching_rules_allow_auto_reconcile:
                 wizard.button_validate(async_action=False)
+
+                st_line.move_id.message_post(body=_(
+                    "This bank transaction has been automatically validated using the reconciliation model '%s'.",
+                    ', '.join(st_line.move_id.line_ids.reconcile_model_id.mapped('name')),
+                ))
+
                 nb_auto_reconciled_lines += 1
         st_lines.write({'cron_last_check': datetime_now})
 

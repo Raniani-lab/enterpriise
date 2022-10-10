7221806c1379aad51f29b81d2d14013138222a20
Damien Bouvy <dbo@odoo.com>
2022-10-10 09:05:53 +0000

    [FIX] knowledge,spreadsheet: highlight fields
    Use the o_field_highlight class to make sure that fields in sidepanels
    for page properties (knowledge) or e.g. filter edition (spreadsheet).
    
    closes odoo/enterprise#32597
    
    Related: odoo/odoo#102823
    Signed-off-by: Michaël Mattiello <mcm@odoo.com>

--- knowledge/static/src/js/knowledge_controller.js
+++ knowledge/static/src/js/knowledge_controller.js
@@ -2,13 +2,7 @@
 
 import { FormController } from '@web/views/form/form_controller';
 
-export class KnowledgeArticleFormController extends FormController {
-    get className() {
-        const result = super.className;
-        result["o_form_with_borderless_input"] = false;
-        return result;
-    }
-}
+export class KnowledgeArticleFormController extends FormController {}
 
 // Open articles in edit mode by default
 KnowledgeArticleFormController.defaultProps = {

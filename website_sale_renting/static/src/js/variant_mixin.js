f28a3c2c70cfaa9ae5103b93732a3f558033db01
William Braeckman <wbr@odoo.com>
2022-10-10 10:46:21 +0000

    [IMP] website_sale_renting: improve product page for rental products
    Cleans up pricing information on the product page as well as add a
    pricing table under the product to provide more information to the user.
    
    TaskId-3012969
    
    X-original-commit: bb85cbd3d6f9c28b532e9b66b54e05a3ce1fd76c
    Part-of: odoo/enterprise#32655

--- website_sale_renting/static/src/js/variant_mixin.js
+++ website_sale_renting/static/src/js/variant_mixin.js
@@ -50,5 +50,41 @@ VariantMixin._onChangeCombination = function (ev, $parent, combination) {
     $duration.text(combination.current_rental_duration);
     $unit.text(combination.current_rental_unit);
 
+    // Update pricing table
+    const pricingTable = document.querySelector("#oe_wsale_rental_pricing_table tbody");
+    if (pricingTable) {
+        updatePricingTable(pricingTable, combination.pricing_table);
+    }
     return result;
+};
+
+
+function updatePricingTable(table, tableInfo) {
+    let lines = table.querySelectorAll("tr");
+    const neededLines = tableInfo.length;
+    if (lines.length > neededLines) {
+        let diff = lines.length - neededLines;
+        while (diff) {
+            lines[lines.length - diff--].remove();
+        }
+    } else if (lines.length < neededLines) {
+        let diff = neededLines - lines.length;
+        while (diff--) {
+            const line = document.createElement("tr");
+            const name = document.createElement("td");
+            const price = document.createElement("td");
+            name.classList.add("w-50");
+            price.classList.add("w-50", "text-muted");
+            line.appendChild(name);
+            line.appendChild(price);
+            table.appendChild(line);
+        }
+        lines = table.querySelectorAll("tr");
+    }
+    for (let idx = 0; idx < neededLines; idx++) {
+        const info = tableInfo[idx];
+        const line = lines[idx];
+        line.querySelector("td:nth-of-type(1)").innerText = info[0];
+        line.querySelector("td:nth-of-type(2)").innerText = info[1];
+    }
 };

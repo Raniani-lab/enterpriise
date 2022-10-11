a40211a1a6542a065e107574952120005b4f1104
William Braeckman <wbr@odoo.com>
2022-10-11 13:46:22 +0000

    [FIX] documents: fix invalid source for some files
    The source for the attachment viewer could sometimes not work as you
    might not always have direct access to an attachment but have to go
    through the document instead.
    This commit ensures we use the document instead of the attachment to get
    the data to have a proper right access check.
    
    TaskId-3015878
    
    X-original-commit: b99216ed6f0f6051f928ed94f92927a7e5b89c07
    Part-of: odoo/enterprise#33415

--- documents/static/tests/documents_kanban_tests.js
+++ documents/static/tests/documents_kanban_tests.js
@@ -822,7 +822,7 @@ QUnit.module('documents_kanban_tests.js', {
         await click(target, '.o_preview_available');
 
         assert.containsOnce(target, '.o_AttachmentViewer_buttonSplit', "should have a pdf splitter");
-        assert.containsOnce(target, 'iframe[data-src="/web/static/lib/pdfjs/web/viewer.html?file=/web/content/2?model=documents.document"]',
+        assert.containsOnce(target, 'iframe[data-src="/web/static/lib/pdfjs/web/viewer.html?file=/web/content/2?model%3Ddocuments.document"]',
             "should have an iframe with the correct pdfviewer src");
 
         await click(target, '.o_AttachmentViewer_headerItemButtonClose');

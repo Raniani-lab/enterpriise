b54274440b05af3f38eabd2b3ad78a7a22b12aac
Julien Van Roy <juvr@odoo.com>
2022-10-17 09:40:05 +0000

    [FIX] account,account_accountant: fix the demo tour
    This commit fixes the account_accountant demo tour.
    Some attributes in the html have changed and the reconciliation widget was refactored.
    Fix the broken account_tour_upload_bill view.
    Fix the test of account_accountant_tour (missing import in __init__)
    
    closes odoo/enterprise#33210
    
    X-original-commit: 5142195c7f4b68141c319a0b67815e95367253ec
    Related: odoo/odoo#104110
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Julien Van Roy <juvr@odoo.com>

--- account_accountant/tests/test_ui.py
+++ account_accountant/tests/test_ui.py
@@ -7,4 +7,7 @@ import odoo.tests
 @odoo.tests.tagged('-at_install', 'post_install')
 class TestUi(odoo.tests.HttpCase):
     def test_accountant_tour(self):
+        all_moves = self.env['account.move'].search([('move_type', '!=', 'entry')])
+        all_moves.button_draft()
+        all_moves.with_context(force_delete=True).unlink()
         self.start_tour("/web", 'account_accountant_tour', login="admin")

03f1a4f310ce3f78c28f947094319071c2b1db04
Jérémy Hennecart (jeh) <jeh@odoo.com>
2022-10-19 12:26:24 +0000

    [IMP] appointment_crm: add stat info for leads
    Add a staff info showing the number of leads linked to an
    appointment type.
    
    task-2984657
    
    Part-of: odoo/enterprise#32945

--- appointment_crm/models/appointment_type.py
+++ appointment_crm/models/appointment_type.py
@@ -1,10 +1,41 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from collections import defaultdict
+
+from odoo import api, fields, models
+
 
 class AppointmentType(models.Model):
     _inherit = "appointment.type"
 
     lead_create = fields.Boolean(string="Create Opportunities",
         help="For each scheduled appointment, create a new opportunity and assign it to the responsible user.")
+    lead_ids = fields.Many2many('crm.lead', string="Leads/Opportunity", compute="_compute_lead_ids", groups="sales_team.group_sale_salesman")
+    lead_count = fields.Integer('Leads Count', compute="_compute_lead_ids", groups="sales_team.group_sale_salesman")
+
+    @api.depends('meeting_ids', 'meeting_ids.opportunity_id')
+    @api.depends_context('allowed_company_ids')
+    def _compute_lead_ids(self):
+        allowed_company_ids = [False, *self.env.context.get('allowed_company_ids', [])]
+        appointment_lead_data = self.env['calendar.event'].sudo().read_group(
+            [('appointment_type_id', 'in', self.ids), ('opportunity_id.company_id', 'in', allowed_company_ids)],
+            ['appointment_type_id', 'opportunity_id'],
+            ['appointment_type_id', 'opportunity_id'],
+            lazy=False)
+        appointment_lead_mapped_data = defaultdict(list)
+        for data in appointment_lead_data:
+            appointment_type_id = data['appointment_type_id'][0]
+            opportunity_id = data['opportunity_id'][0]
+            appointment_lead_mapped_data.setdefault(appointment_type_id, list()).append(opportunity_id)
+        for appointment in self:
+            lead_ids = appointment_lead_mapped_data[appointment.id]
+            leads = self.env['crm.lead'].browse(lead_ids)._filter_access_rules('read')
+            appointment.lead_ids = leads
+            appointment.lead_count = len(leads)
+
+    def action_appointment_leads(self):
+        self.ensure_one()
+        action = self.env['ir.actions.act_window']._for_xml_id('crm.crm_lead_opportunities')
+        action['domain'] = [('id', 'in', self.lead_ids.ids)]
+        return action

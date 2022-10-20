7bc11ceed152d065d45e59cbb9db2b9fc97b03ab
Romain Estievenart <res@odoo.com>
2022-10-20 11:35:26 +0000

    [FIX] web_enterprise: darkmode fix typo in name
    This commit fixes a mismatch between "theme" and "scheme" wording in the
    color scheme switching mechanism.
    
    closes odoo/enterprise#33189
    
    X-original-commit: b841b5ef0d29e87a5013177564f43435dfe7df79
    Related: odoo/odoo#104080
    Signed-off-by: Pierre Paridans (app) <app@odoo.com>

--- web_enterprise/static/src/webclient/color_scheme/color_scheme_menu_items.js
+++ web_enterprise/static/src/webclient/color_scheme/color_scheme_menu_items.js
@@ -7,8 +7,8 @@ export function switchColorSchemeItem(env) {
         description: env._t("Dark Mode"),
         callback: () => {
             const cookie = env.services.cookie.current.color_scheme;
-            const theme = cookie === "dark" ? "light" : "dark";
-            env.services.color_scheme.switchToColorScheme(theme);
+            const scheme = cookie === "dark" ? "light" : "dark";
+            env.services.color_scheme.switchToColorScheme(scheme);
         },
         isChecked: env.services.cookie.current.color_scheme === "dark",
         sequence: 30,

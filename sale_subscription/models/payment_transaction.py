9107ca7807388548e583a22e17a5af1e8c67c04b
Valentin Vallaeys (vava) <vava@odoo.com>
2022-11-04 12:44:53 +0000

    [IMP] sale_subscription: automatic tokenization
    The tokenization is forced for all users paying a subscription. The
    checkbox asking whether to save the payment details is therefore
    hidden and a message is informing the users about it. This also
    impacts users that are not logged in.
    
    task-3048314
    
    closes odoo/enterprise#33666
    
    X-original-commit: 7c16f64f02182c2a14a33689507110fa9d5cbfea
    Related: odoo/odoo#105070
    Signed-off-by: Antoine Vandevenne (anv) <anv@odoo.com>
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>
    Co-authored-by: Antoine Vandevenne (anv) <anv@odoo.com>

--- sale_subscription/models/payment_transaction.py
+++ sale_subscription/models/payment_transaction.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
@@ -46,38 +45,4 @@ class PaymentTransaction(models.Model):
                     order_to_update_ids |= order
             order_to_update_ids._update_next_invoice_date()
             order_to_update_ids.order_line._reset_subscription_qty_to_invoice()
-        return res
-
-
-class PaymentToken(models.Model):
-    _name = 'payment.token'
-    _inherit = 'payment.token'
-
-    def _handle_archiving(self):
-        """ Override of payment to void the token on linked subscriptions.
-
-        :return: None
-        """
-        super()._handle_archiving()
-
-        linked_subscriptions = self.env['sale.order'].search([('payment_token_id', 'in', self.ids)])
-        linked_subscriptions.write({'payment_token_id': None})
-
-    def get_linked_records_info(self):
-        """ Override of payment to add information about subscriptions linked to the current token.
-
-        Note: self.ensure_one()
-
-        :return: The list of information about linked subscriptions
-        :rtype: list
-        """
-        res = super().get_linked_records_info()
-        subscriptions = self.env['sale.order'].search([('payment_token_id', '=', self.id)])
-        for sub in subscriptions:
-            res.append({
-                'description': subscriptions._description,
-                'id': sub.id,
-                'name': sub.name,
-                'url': sub.get_portal_url()
-            })
         return res

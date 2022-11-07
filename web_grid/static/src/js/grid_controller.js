8b7258e470123baf8ff2cdc3e48bd6a6fbb67445
Joseph Caburnay <jcb@odoo.com>
2022-11-07 14:27:37 +0000

    [FIX] web_{gantt,grid,studio}: close dialog when click m2o link
    When clicking the Internal Link of a many2one field from the
    FormViewDialog, the dialog should close because the view has
    changed to the form view of the linked record.
    
    closes odoo/enterprise#33784
    
    X-original-commit: ba0e95fe42696dcf44b8feddeb302f04876fcbdc
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- web_grid/static/src/js/grid_controller.js
+++ web_grid/static/src/js/grid_controller.js
@@ -40,6 +40,17 @@ var GridController = AbstractController.extend({
         this.displayEmpty = params.displayEmpty;
         this.mutex = new concurrency.Mutex();
     },
+    /**
+     * @override
+     * Close the dialog if it is open.
+     */
+    on_detach_callback() {
+        if (this.closeDialog) {
+            this.closeDialog();
+            this.closeDialog = undefined;
+        }
+        return this._super.apply(this, arguments);
+    },
 
     //--------------------------------------------------------------------------
     // Public
@@ -139,7 +150,7 @@ var GridController = AbstractController.extend({
      */
     _addLine() {
         const options = this._getFormDialogOptions()
-        Component.env.services.dialog.add(FormViewDialog, options);
+        this.closeDialog = Component.env.services.dialog.add(FormViewDialog, options);
     },
     /**
      * @private

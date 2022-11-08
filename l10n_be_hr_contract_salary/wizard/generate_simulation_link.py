0962a9f2571705200be3a8409ea6f7739763e9f0
sofiagvaladze <sgv@odoo.com>
2022-11-08 16:12:34 +0000

    [IMP] l10n_be_hr_contract_salary: Show 'new_car' only when needed
    Purpose:
    Generally, the user is not able to pick the new car if the amount of used
    cars is already reached. But it can be overruled by ticking the field 'new_car'
    on the 'Generate a Simulation Link' wizard. Thus it makes sense to show
    'new_car' only when it is needed.
    
    Now 'new_car' is always visible on the wizard.
    
    After this commit the field 'new_car' will only be visible on the wizard if
    the amount of used cars is already reached.
    
    task - 3035820
    
    closes odoo/enterprise#33788
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- l10n_be_hr_contract_salary/wizard/generate_simulation_link.py
+++ l10n_be_hr_contract_salary/wizard/generate_simulation_link.py
@@ -12,9 +12,15 @@ class GenerateSimulationLink(models.TransientModel):
                                                                          raise_if_not_found=False))
 
     new_car = fields.Boolean(string="Force New Cars List", help="The employee will be able to choose a new car even if the maximum number of used cars available is reached.")
+    show_new_car = fields.Boolean(compute='_compute_show_new_car')
     car_id = fields.Many2one('fleet.vehicle', string='Default Vehicle', domain="[('vehicle_type', '=', 'car')]", help="Default employee's company car. If left empty, the default value will be the employee's current car.")
     l10n_be_canteen_cost = fields.Float(string="Canteen Cost")
 
     def _get_url_triggers(self):
         res = super()._get_url_triggers()
         return res + ['new_car', 'car_id', 'contract_type_id', 'l10n_be_canteen_cost']
+
+    @api.depends('contract_id.available_cars_amount', 'contract_id.max_unused_cars')
+    def _compute_show_new_car(self):
+        for wizard in self:
+            wizard.show_new_car = wizard.contract_id.available_cars_amount >= wizard.contract_id.max_unused_cars

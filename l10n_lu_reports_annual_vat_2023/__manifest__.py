232861fa80a97341208907340526ab6c263f00e4
aliya <alta@odoo.com>
2022-11-28 16:11:39 +0000

    [IMP] l10n_lu_reports_annual_vat_2023: add a PDF export
    Users want to be able to have a preview of the entire report before they submit it on the ECDF portal. Clicking through the tabs of the annual declaration is not very convenient and doesn't provide a good overview.
    Given the structure of the l10n_lu report, exporting it in the pdf format requires some workarounds. However, this is temporary as the entire Annual tax report will be completely reworked in master.
    
    task-3103724
    
    closes odoo/enterprise#36525
    
    X-original-commit: ee70ede5f09bbeb26a05c3deb2a57d7ce86cddfa
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Aliya Tastemirova (alta) <alta@odoo.com>

--- l10n_lu_reports_annual_vat_2023/__manifest__.py
+++ l10n_lu_reports_annual_vat_2023/__manifest__.py
@@ -11,8 +11,15 @@ Annual VAT report for Luxembourg - 2023 update
     'category': 'Accounting/Accounting',
     'depends': ['l10n_lu_reports'],
     'data': [
+        'report/l10n_lu_annual_report.xml',
+        'report/l10n_lu_annual_report_pdf_template.xml',
         'views/l10n_lu_yearly_tax_report_manual_views.xml',
     ],
+    'assets': {
+        'web.report_assets_common': [
+            'l10n_lu_reports_annual_vat_2023/static/src/scss/**/*',
+        ],
+    },
     'license': 'OEEL-1',
     'auto_install': True,
 }

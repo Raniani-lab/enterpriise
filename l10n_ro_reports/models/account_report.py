aadc08f3d5fba2e928635c5d6ac4ffe2f4823771
Nicolas (vin) <vin@odoo.com>
2022-12-02 10:50:39 +0000

    [FIX] l10n_ro_reports: fix model type
    The models inheriting account.report.custom.handler should all be
    AbstractModel, while this one was made as a model.
    Correct it now to avoid any potential issues in the future, and a
    warning about access rights now.
    
    See https://github.com/odoo/enterprise/pull/33176
    
    closes odoo/enterprise#34603
    
    Related: odoo/odoo#107096
    Signed-off-by: William André (wan) <wan@odoo.com>

--- l10n_ro_reports/models/account_report.py
+++ l10n_ro_reports/models/account_report.py
@@ -6,7 +6,7 @@ Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models
 
 
-class ReportAccountFinancialReport(models.Model):
+class ReportAccountFinancialReport(models.AbstractModel):
     _name = "l10n_ro.report.handler"
     _inherit = "account.report.custom.handler"
     _description = "Report custom handler for romanian financial reports"

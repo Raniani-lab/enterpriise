f70b4ac137b04e87e192b2b7eb767db005550b6d
JF Aubert <ajf@odoo.com>
2022-12-14 13:27:36 +0000

    [IMP] mrp_workorder: Make it easier to find workorders
    By allowing to search on typed text even when the focus is not in Search input
    
    Task: 2907698
    Part-of: odoo/enterprise#34963

--- mrp_workorder/static/src/views/kanban/mrp_workorder_kanban_view.js
+++ mrp_workorder/static/src/views/kanban/mrp_workorder_kanban_view.js
@@ -2,11 +2,13 @@
 
 import { kanbanView } from "@web/views/kanban/kanban_view";
 import { MrpWorkorderKanbanController } from "./mrp_workorder_kanban_controller";
+import { MrpWorkorderKanbanRenderer } from "./mrp_workorder_kanban_renderer";
 import { registry } from "@web/core/registry";
 
 export const MrpWorkorderKanbanView = {
     ...kanbanView,
     Controller: MrpWorkorderKanbanController,
+    Renderer: MrpWorkorderKanbanRenderer,
     buttonTemplate: 'mrp_workorder.overviewButtonsKanban',
 };
 

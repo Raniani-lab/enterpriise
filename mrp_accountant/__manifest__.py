92c03853e351620fec55654ee5d6c92a92c54e02
yhu-odoo <yhu@odoo.com>
2022-12-16 15:48:51 +0000

    [ADD] {mrp,stock}_accountant: add default stock accounts and production cost
    account
    
    1. We introduced a new Production Cost account that can be set on
    locations with Production type. When products move into/out of a
    production location. The account entry will be post on this account
    instead of previous Stock in/out account.
    
    2. On the accounting setting page, we can now set the default values for
    all stock accounts. Users can also disable automatically accounting for
    stock there.
    
    Task-3046333
    
    closes odoo/enterprise#37653
    
    Related: odoo/odoo#113973
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- mrp_accountant/__manifest__.py
+++ mrp_accountant/__manifest__.py
@@ -2,14 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': "Stock Accounting",
+    'name': "Mrp Accounting",
     'version': "1.0",
     'category': 'Inventory/Inventory',
-    'summary': "Bridge between Stock and Accounting",
+    'summary': "Bridge between Mrp and Accounting",
     'description': """
-Filters the stock lines out of the reconciliation widget
+        Automatic accounting for MRP
     """,
-    'depends': ['stock_account', 'account_accountant'],
+    'depends': ['mrp_account', 'account_accountant'],
     'data': [
         'views/res_config_settings_views.xml',
     ],
---
+++ mrp_accountant/models/res_config_settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    property_stock_account_production_cost_id = fields.Many2one(
+        'account.account', "Production Account",
+        domain="[('company_id', '=', allowed_company_ids[0]), ('deprecated', '=', False)]",
+        compute='_compute_property_stock_account',
+        inverse='_set_property_stock_account_production_cost_id')
+
+    @api.model
+    def _get_account_stock_properties_names(self):
+        return super()._get_account_stock_properties_names() + [
+            'property_stock_account_production_cost_id',
+        ]
+
+    def _set_property_stock_account_production_cost_id(self):
+        for record in self:
+            record._set_property('property_stock_account_production_cost_id')

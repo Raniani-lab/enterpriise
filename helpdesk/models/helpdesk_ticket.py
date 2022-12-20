316274b40ebb8d41eb7a62a75fc0534f12f15c92
Victor Piryns (pivi) <pivi@odoo.com>
2022-12-20 10:37:49 +0000

    [FIX] helpdesk: display inline image from email in ticket description
    Current behaviour:
    When sending an email with an inlined image to an alias of a helpdesk,
    the image is broken in the task description.
    
    Expected behaviour:
    The image should be rendered correctly.
    
    Steps to reproduce:
    - Install Helpdesk
    - In Settings add a custom email server and set a domain alias
    - Create a new Helpdesk and in it's settings create an email alias
    - Send an email with an *inlined* image. (not attachment)\
    - Image is not rendered correctly in the newly created task.
    
    Reason for the problem:
    Email with an embedded/inlined images are multipart, one is containing
    the base64 encoded image and has a `content-id`, another one is the
    html body that has an `<img/>` which source is a `cid` that references
    the `content-id` of the encoded image. When parsing the email when
    creating the ticket, we only take into account the `html` part of the email,
    which becomes the `message.body` which is used to fill in the description.
    Therefor we have an `<img src="cid:..."/>` that references nothing.
    
    Fix:
    Use the already parsed and sanitized `message.body` from the message's thread,
    which converts the `content-ids` into relative links to web images.
    We fill with that the description's body.
    
    Affected versions:
    - saas-15.2
    - saas-15.3
    - 16.0
    - master
    
    opw-3062444
    
    closes odoo/enterprise#35535
    
    X-original-commit: a24f0e78493b3082f655947e1a12198f586a58ce
    Related: odoo/odoo#109343
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>
    Signed-off-by: Piryns Victor (pivi) <pivi@odoo.com>

--- helpdesk/models/helpdesk_ticket.py
+++ helpdesk/models/helpdesk_ticket.py
@@ -869,7 +869,7 @@ class HelpdeskTicket(models.Model):
 
     @api.model
     def message_new(self, msg, custom_values=None):
-        values = dict(custom_values or {}, partner_email=msg.get('from'), partner_id=msg.get('author_id'), description=msg.get('body'))
+        values = dict(custom_values or {}, partner_email=msg.get('from'), partner_id=msg.get('author_id'))
         ticket = super(HelpdeskTicket, self.with_context(mail_notify_author=True)).message_new(msg, custom_values=values)
         partner_ids = [x.id for x in self.env['mail.thread']._mail_find_partner_from_emails(self._ticket_email_split(msg), records=ticket) if x]
         customer_ids = [p.id for p in self.env['mail.thread']._mail_find_partner_from_emails(tools.email_split(values['partner_email']), records=ticket) if p]
@@ -900,6 +900,9 @@ class HelpdeskTicket(models.Model):
                     ('partner_id', '=', False),
                     ('partner_email', '=', new_partner.email),
                     ('stage_id.fold', '=', False)]).write({'partner_id': new_partner.id})
+        # use the sanitized body of the email from the message thread to populate the ticket's description
+        if not self.description and message.subtype_id == self._creation_subtype() and self.partner_id == message.author_id:
+            self.description = message.body
         return super(HelpdeskTicket, self)._message_post_after_hook(message, msg_vals)
 
     def _track_template(self, changes):

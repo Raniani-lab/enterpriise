96a7874a6bd50ce5f357ecdd94414d699a058aaa
Viktors Lipskis (vili) <vili@odoo.com>
2022-12-20 09:16:21 +0000

    [FIX] documents_hr: fix send access link
    When archiving an employee, the private email address of the employee can now be
    edited if the access link is going to be sent.
    
    task-3046435
    
    closes odoo/enterprise#34288
    
    Related: odoo/odoo#108314
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- documents_hr/wizard/hr_departure_wizard.py
+++ documents_hr/wizard/hr_departure_wizard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HrDepartureWizard(models.TransientModel):
@@ -14,7 +14,27 @@ class HrDepartureWizard(models.TransientModel):
     send_hr_documents_access_link = fields.Boolean(
         string="Send Access Link",
         default=_get_default_send_hr_documents_access_link,
-        help="Send an email to the user with a share link to all the documents he owns.")
+        help="Send a share link to the private email of the employee with all the HR files he owns in Documents app")
+
+    private_email = fields.Char(compute='_compute_private_mail', inverse='_inverse_private_mail')
+
+    @api.depends('employee_id')
+    def _compute_private_mail(self):
+        for wiz in self:
+            wiz.private_email = wiz.employee_id.private_email
+
+    def _inverse_private_mail(self):
+        for wiz in self:
+            if wiz.employee_id.address_home_id:
+                wiz.employee_id.address_home_id.email = wiz.private_email
+            else:
+                partner = self.env['res.partner'].create({
+                    'is_company': False,
+                    'type': 'private',
+                    'name': wiz.employee_id.name,
+                    'email': wiz.private_email,
+                })
+                wiz.employee_id.address_home_id = partner.id
 
     def action_register_departure(self):
         super().action_register_departure()

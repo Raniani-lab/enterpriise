Raniani-lab/enterpriise

commit 3d6dbf04f52551a1bf7324d82b482ea88f5b3a7b

[FIX] sale_stock_renting: schedule props validation
Since 16.0, following OWL refactoring, props are validated
in debug mode.  One of the props being validated in standard
views is the record id, aka resID.

This id expects a number (integer), but it seems that the
auto-generated ids for the `sale.rental.schedule` SQL view
were not numbers but strings, including useless parenthesis
from the SQL view query.

This commit makes sure the id is a valid number, avoiding
the traceback raised by the props validation (when opening
the form view of a `sale.rental.schedule` record in debug
mode.

Part-of: odoo/enterprise#34338

sale_stock_renting/report/rental_schedule.py
@@ -27,7 +27,7 @@ class RentalSchedule(models.Model):
         return f"""COALESCE(lot_info.name, NULLIF(t.name->>'{lang}', ''), t.name->>'en_US') as product_name"""
 
     def _id(self):
-        return """CONCAT(lot_info.lot_id, pdg.max_id, sol.id) as id"""
+        return """CAST(CONCAT(lot_info.lot_id, pdg.max_id, sol.id) AS INTEGER) as id"""
 
     def _quantity(self):
         return """
@@ -117,12 +117,16 @@ class RentalSchedule(models.Model):
                         ON res.stock_lot_id=lot.id
                         OR pickedup.stock_lot_id=lot.id
                 ),
-                sol_id_max (id) AS
-                    (SELECT MAX(id) FROM sale_order_line),
-                lot_id_max (id) AS
-                    (SELECT MAX(id) FROM stock_lot),
-                padding (max_id) AS
-                    (SELECT CASE when lot_id_max > sol_id_max then lot_id_max ELSE sol_id_max END as max_id from lot_id_max, sol_id_max)
+                padding(max_id) AS (
+                    SELECT
+                        MAX(id) as max
+                    FROM
+                        (
+                            SELECT max(id) as id from stock_lot
+                            UNION
+                            SELECT max(id) as id from sale_order_line
+                        ) AS whatever
+                )
         """
 
     def _select(self):

commit 93aa8971eb51ad340e5a50b7731329374d2bd347

[FIX] sale_stock_renting: avoid out of range for type integer
Issue introduced with 3d6dbf04f52551a1bf7324d82b482ea88f5b3a7b.
As "id" is a concat of 3 id, it can get bigger than the 4 bytes
limit for INTEGER columns.

In those case, an error is raised when the user tries to open
the reporting view:

psycopg2.errors.NumericValueOutOfRange: value "61326216613" is
out of range for type integer

opw-3107034

closes odoo/enterprise#35350

Signed-off-by: Victor Feyens (vfe) <vfe@odoo.com>

sale_stock_renting/report/rental_schedule.py
@@ -27,7 +27,7 @@ class RentalSchedule(models.Model):
         return f"""COALESCE(lot_info.name, NULLIF(t.name->>'{lang}', ''), t.name->>'en_US') as product_name"""
 
     def _id(self):
-        return """CAST(CONCAT(lot_info.lot_id, pdg.max_id, sol.id) AS INTEGER) as id"""
+        return """CAST(CONCAT(lot_info.lot_id, pdg.max_id, sol.id) AS NUMERIC) as id"""
 
     def _quantity(self):
         return """
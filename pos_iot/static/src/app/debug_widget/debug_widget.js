5e50de9f977e8c09a0c06a9601c541f2f79cdacd
Samuel Degueldre <sad@odoo.com>
2023-01-03 14:23:06 +0000

    [REF] pos_iot: adapt DebugWidget overrides
    The debug widget was extracted out of Chrome and into a main component,
    this commit adapts the overrides on the debug widget.
    
    closes odoo/enterprise#35406
    
    Related: odoo/odoo#108891
    Signed-off-by: Trinh Jacky (trj) <trj@odoo.com>

---
+++ pos_iot/static/src/app/debug_widget/debug_widget.js
@@ -0,0 +1,15 @@
+/** @odoo-module */
+
+import { DebugWidget } from "@point_of_sale/app/debug_widget/debug_widget";
+import { patch } from "@web/core/utils/patch";
+
+patch(DebugWidget, "pos_iot.DebugWidget", {
+    /**
+     * @override
+     */
+    refreshDisplay() {
+        if (this.env.proxy.display) {
+            this.env.proxy.display.action({ action: "display_refresh" });
+        }
+    },
+});

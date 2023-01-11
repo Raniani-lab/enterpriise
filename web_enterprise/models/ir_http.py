8f610490fb80400140e72e52237a1a12180b8ba1
Julien Castiaux <juc@odoo.com>
2023-01-11 17:24:17 +0000

    [IMP] core: make some cookies expire on logout
    Some cookies were left around even when the user logged out.
    
    The next user would not use the default light mode in case the last
    user was in dark mode.
    
    task-3077421
    
    closes odoo/enterprise#35685
    
    Related: odoo/odoo#108998
    Signed-off-by: Julien Castiaux <juc@odoo.com>

--- web_enterprise/models/ir_http.py
+++ web_enterprise/models/ir_http.py
@@ -4,10 +4,15 @@
 import json
 
 from odoo import models
+from odoo.http import request
 
 
 class Http(models.AbstractModel):
     _inherit = 'ir.http'
+
+    @classmethod
+    def _post_logout(cls):
+        request.future_response.set_cookie('color_scheme', max_age=0)
 
     def webclient_rendering_context(self):
         """ Overrides community to prevent unnecessary load_menus request """

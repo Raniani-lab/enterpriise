0f0e33c5f7684aeb840f2b9beb5d76a72e58318e
Chenyun Yang <chya@odoo.com>
2023-01-11 11:43:02 +0000

    [IMP] spreadsheet_edition: zip images to xlsx file
    This commits implements the zipping of images when exporting xlsx file.
    
    When exporting an o-spreadsheet to excel file, the files are sent by
    the client to be zipped by the server (we don't have any zip lib
    client-side)
    
    To reduce the networking load, only the image path is sent.
    The server then replaces the path with the image data in the xlsx file.
    
    task 3125701
    
    closes odoo/enterprise#38296
    
    Related: odoo/odoo#115514
    Signed-off-by: Lucas Lefèvre (lul) <lul@odoo.com>

--- spreadsheet_edition/controllers/main.py
+++ spreadsheet_edition/controllers/main.py
@@ -4,6 +4,7 @@ import json
 import io
 
 from odoo import http
+from odoo.exceptions import MissingError
 from odoo.http import request, content_disposition, Controller
 
 logger = logging.getLogger(__name__)
@@ -11,11 +12,29 @@ logger = logging.getLogger(__name__)
 
 class SpreadsheetController(Controller):
 
+    def _get_file_content(self, file_path):
+        _, args = request.env['ir.http']._match(file_path)
+        file_record = request.env['ir.binary']._find_record(
+            xmlid=args.get('xmlid'),
+            res_model=args.get('model', 'ir.attachment'),
+            res_id=args.get('id'),
+        )
+        return request.env['ir.binary']._get_stream_from(file_record).read()
+
     def _generate_xlsx_content(self, files):
         stream = io.BytesIO()
         with zipfile.ZipFile(stream, 'w', compression=zipfile.ZIP_DEFLATED) as doc_zip:
             for f in files:
-                doc_zip.writestr(f['path'], f['content'])
+                # to reduce networking load, only the image path is sent.
+                # It's replaced by the image content here.
+                if 'imagePath' in f:
+                    try:
+                        content = self._get_file_content(f['imagePath'])
+                        doc_zip.writestr(f['path'], content)
+                    except MissingError:
+                        pass
+                else:
+                    doc_zip.writestr(f['path'], f['content'])
 
         return stream.getvalue()
 

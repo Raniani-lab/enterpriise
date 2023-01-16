a42f6c571dc38231cd34b9270a14daac6ebe0ce2
Laurent Smet <las@odoo.com>
2023-01-16 14:52:17 +0000

    [FIX] account_accountant_batch_payment: Fix perf of batch payments on bank reco widget
    Exclude payments having NULL batch_payment_id and already reconciled batch payments from the candidates batches.
    
    closes odoo/enterprise#35878
    
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_accountant_batch_payment/models/bank_rec_widget.py
+++ account_accountant_batch_payment/models/bank_rec_widget.py
@@ -38,7 +38,10 @@ class BankRecWidget(models.Model):
                     ARRAY_AGG(account_move_line.id) AS aml_ids
                 FROM {tables}
                 JOIN account_payment pay ON pay.id = account_move_line.payment_id
+                JOIN account_batch_payment batch ON batch.id = pay.batch_payment_id
                 WHERE {where_clause}
+                    AND pay.batch_payment_id IS NOT NULL
+                    AND batch.state != 'reconciled'
                 GROUP BY pay.batch_payment_id
             ''',
             where_params,
@@ -83,7 +86,7 @@ class BankRecWidget(models.Model):
                 dynamic_filter['domain'] = str(dynamic_filter['domain'])
 
             # Collect the available batch payments.
-            available_amls_in_batch_payments = self._fetch_available_amls_in_batch_payments()
+            available_amls_in_batch_payments = wizard._fetch_available_amls_in_batch_payments()
 
             wizard.batch_payments_widget = {
                 'domain': [('id', 'in', list(available_amls_in_batch_payments.keys()))],
@@ -103,7 +106,7 @@ class BankRecWidget(models.Model):
 
             if batch_payment_x_amls:
                 batch_payments = wizard.line_ids.source_batch_payment_id
-                available_amls_in_batch_payments = self._fetch_available_amls_in_batch_payments(batch_payments=batch_payments)
+                available_amls_in_batch_payments = wizard._fetch_available_amls_in_batch_payments(batch_payments=batch_payments)
                 selected_batch_payment_ids = [
                     x.id
                     for x in batch_payments

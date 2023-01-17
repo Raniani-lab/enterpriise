bb13ee1f58e265396adf78a27cf405275b9182c1
Géry Debongnie <ged@odoo.com>
2023-01-17 14:32:33 +0000

    [REM] mail_enterprise: remove form renderer override
    This commit is the counterpart of PR
    https://github.com/odoo/odoo/pull/110168
    
    The goal is to remove the code that adds chatter to old form views,
    since it is no longer useful in practice.
    
    closes odoo/enterprise#35924
    
    Related: odoo/odoo#110168
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- mail_enterprise/__manifest__.py
+++ mail_enterprise/__manifest__.py
@@ -28,7 +28,6 @@ screen devices.
         'web.assets_backend': [
             'mail_enterprise/static/src/components/*/*.js',
             'mail_enterprise/static/src/scss/mail_enterprise_mobile.scss',
-            'mail_enterprise/static/src/widgets/*/*.js',
             'mail_enterprise/static/src/components/*/*.xml',
         ],
         'web.assets_tests': [

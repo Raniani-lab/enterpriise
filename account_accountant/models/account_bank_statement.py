8e1ce57b0afc0008a1590a4ccb2606b61035d940
Habib (ayh) <ayh@odoo.com>
2023-02-14 15:12:18 +0000

    [FIX] account_accountant: bank statement balances not updated
    When creating a bank statement from the bank recon list view or bank statement line form view, the ending balance is not properly computed in the default_get method.
    This PR enables the computation of the ending balances, and displays the warning message on the statement form view.
    See the corresponding community PR.
    
    closes odoo/enterprise#37074
    
    Related: odoo/odoo#112710
    Signed-off-by: Laurent Smet <las@odoo.com>

--- account_accountant/models/account_bank_statement.py
+++ account_accountant/models/account_bank_statement.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from lxml import etree
 from odoo import _, api, fields, models
 
 from dateutil.relativedelta import relativedelta
@@ -33,6 +34,29 @@ class AccountBankStatement(models.Model):
                 'res_id': statement.id,
             })
         return statement_report_action.report_action(docids=self)
+
+    # -------------------------------------------------------------------------
+    # LOW-LEVEL METHODS
+    # -------------------------------------------------------------------------
+
+    @api.model
+    def get_view(self, view_id=None, view_type='form', **options):
+        # EXTENDS base
+        # include field 'create_date' to avoid a module update (required to compute balance start)
+        # TO BE REMOVED IN MASTER
+        res = super().get_view(view_id=view_id, view_type=view_type, options=options)
+        if view_type == 'form':
+            form_view = self.env.ref('account_accountant.view_bank_statement_form_bank_rec_widget')
+            tree = etree.fromstring(res['arch'])
+            if res.get('id') == form_view.id and len(tree.xpath("//field[@name='create_date']")) == 0:
+                arch_tree = etree.fromstring(form_view.arch)
+                arch_tree.insert(0, etree.Element('field', attrib={
+                    'name': 'create_date',
+                    'invisible': '1',
+                }))
+                form_view.sudo().write({'arch': etree.tostring(arch_tree, encoding='unicode')})
+                return super().get_view(view_id=view_id, view_type=view_type, options=options)
+        return res
 
 class AccountBankStatementLine(models.Model):
     _inherit = 'account.bank.statement.line'
@@ -155,3 +179,25 @@ class AccountBankStatementLine(models.Model):
             remaining_st_line = self.env['account.bank.statement.line'].browse(remaining_line_id)
             if nb_auto_reconciled_lines or not remaining_st_line.cron_last_check:
                 self.env.ref('account_accountant.auto_reconcile_bank_statement_line')._trigger()
+
+    # -------------------------------------------------------------------------
+    # LOW-LEVEL METHODS
+    # -------------------------------------------------------------------------
+
+    @api.model
+    def get_view(self, view_id=None, view_type='form', **options):
+        # EXTENDS base
+        # include widget on field 'statement_id' to avoid a module update (required to get line_ids in multi-edit mode)
+        # TO BE REMOVED IN MASTER
+        res = super().get_view(view_id=view_id, view_type=view_type, options=options)
+        if view_type == 'tree':
+            tree_view = self.env.ref('account_accountant.view_bank_statement_line_tree_bank_rec_widget')
+            tree = etree.fromstring(res['arch'])
+            xpath = "//field[@name='statement_id']"
+            field_elems = tree.xpath(xpath)
+            if res.get('id') == tree_view.id and field_elems and not field_elems[0].get('widget'):
+                arch_tree = etree.fromstring(tree_view.arch)
+                arch_tree.xpath(xpath)[0].attrib['widget'] = "bankrec_many2one_multi_id"
+                tree_view.with_context({}).sudo().write({'arch': etree.tostring(arch_tree, encoding='unicode')})
+                return super().get_view(view_id=view_id, view_type=view_type, options=options)
+        return res

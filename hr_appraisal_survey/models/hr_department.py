fc991c27480e6e4de839f03646f4c60b6902fa77
dasz <dasz@odoo.com>
2023-02-15 15:23:06 +0000

    [IMP] hr_appraisal_survey: adapt survey inherited views to appraisal
    hide buttons not used in appraisal flow and rename views
    task 3127424
    
    closes odoo/enterprise#37387
    
    Related: odoo/odoo#121283
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- hr_appraisal_survey/models/hr_department.py
+++ hr_appraisal_survey/models/hr_department.py
@@ -11,5 +11,6 @@ class hr_department(models.Model):
         return self.env.company.appraisal_survey_template_id
 
     appraisal_survey_template_id = fields.Many2one('survey.survey', string='Appraisal Survey',
-        help='This field is used with 360 Feedback setting on Appraisal App, the aim is to define a default Survey Template related to this department.',
-         default=_get_default_appraisal_survey_template_id)
+        domain=[('survey_type', '=', 'appraisal')], default=_get_default_appraisal_survey_template_id,
+        help='This field is used with 360 Feedback setting on Appraisal App, the aim is to define a default Survey Template related to this department.'
+    )

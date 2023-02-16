508a9ba97533c85a80db8254f8c2cc372e1b53fc
Abderraouf Ghrissi (abgh) <abgh@odoo.com>
2023-02-16 11:29:03 +0000

    [IMP] project: add task quick create shortcuts
    In this commit,
    We did the needed changes related to replacing name field in
    project.task quick create kanban box by display_name field, more
    details about the changes in the related Community PR.
    
    Related Prs:
    - Community: https://github.com/odoo/odoo/pull/112821
    
    task-3145203
    
    closes odoo/enterprise#37166
    
    Related: odoo/odoo#112821
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/static/tests/tours/task_gantt_tour_test.js
+++ project_enterprise/static/tests/tours/task_gantt_tour_test.js
@@ -65,7 +65,7 @@ registry.category("web_tour.tours").add('project_enterprise_tour', {
         trigger: '.o-kanban-button-new',
         extra_trigger: '.o_kanban_group:eq(0)'
     }, {
-        trigger: '.o_kanban_quick_create div.o_field_char[name=name] input',
+        trigger: '.o_kanban_quick_create div.o_field_char[name=display_name] input',
         extra_trigger: '.o_kanban_project_tasks',
         run: 'text New task'
     }, {

256f29d430c6eb809b766abbbe9a6193870e5720
Julien (jula) <jula@odoo.com>
2023-02-17 10:22:52 +0000

    [FIX] l10n_nl_reports: only ISO-3166 codes in XAF report
    __Current behavior before PR:__
    While exporting a XAF report, an error is raised if the report contains partners that are located in countries that are not part of ISO-3166 (like Kosovo).
    This happens because the report is validated using the attachment "XmlAuditfileFinancieel3.2.xsd" which does not contains the country code of Kosovo (XK).
    
    __Desired behavior after PR is merged:__
    - When exporting a XAF report, the valid country codes are parsed from the attachment mentionned above using lxml.
      Since 16.0, the attachment is fetched from a distant source. So it is possible that it is not already in the DB. In this case, it is fetched with the `_l10n_nl_reports_load_xsd_files` method before the country codes are parsed.
    
      If some partners are located in countries not present in the XSD (and thus forbidden by the dutch tax authorities), the user is prompted via a `RedirectWarning` to change the address of the concerned partners.
      The user is also given the possibility to continue and skip the fields `<country>` and `<taxRegistrationCountry>` in the report.
    
    - The name of the XSD is now stored inside an attribute in the class `IrAttachment` from this module; this way it is not written at multiple places in the code.
    
    __To test the feature:__
    - Intall `l10n_nl_reports`
    - Switch to "NL Company"
    - Create a partner with Kosovo as country in the address
    - Register the payement of an invoice for this partner
    - Go to Accounting > Reporting > General Ledger > XAF
    
    OPW-3085277
    
    closes odoo/enterprise#36717
    
    X-original-commit: 05ba9bc
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- l10n_nl_reports/models/ir_attachment.py
+++ l10n_nl_reports/models/ir_attachment.py
@@ -3,10 +3,13 @@
 
 import logging
 
+from lxml import etree
+
 from odoo import api, models, tools
 
 _logger = logging.getLogger(__name__)
 
+XSD_NAME = 'XmlAuditfileFinancieel3.2.xsd'
 
 class IrAttachment(models.Model):
     _inherit = 'ir.attachment'
@@ -14,10 +17,32 @@ class IrAttachment(models.Model):
     @api.model
     def _l10n_nl_reports_load_xsd_files(self, force_reload=False):
         url = 'https://www.softwarepakketten.nl/upload/auditfiles/xaf/20140402_AuditfileFinancieelVersie_3_2.zip'
-        xsd_name = 'XmlAuditfileFinancieel3.2.xsd'
-        tools.load_xsd_files_from_url(self.env, url, 'xsd_nl_xaf_3.2.zip', force_reload=force_reload, xsd_names_filter=xsd_name)
+        tools.load_xsd_files_from_url(self.env, url, 'xsd_nl_xaf_3.2.zip', force_reload=force_reload, xsd_names_filter=XSD_NAME)
         return
 
     @api.model
-    def l10n_nl_reports_validate_xml_from_attachment(self, xml_content, xsd_name):
-        return tools.validate_xml_from_attachment(self.env, xml_content, xsd_name, self._l10n_nl_reports_load_xsd_files)
+    def l10n_nl_reports_validate_xml_from_attachment(self, xml_content):
+        return tools.validate_xml_from_attachment(self.env, xml_content, XSD_NAME, self._l10n_nl_reports_load_xsd_files)
+
+    @api.model
+    def l10n_nl_reports_load_iso_country_codes(self):
+        if self.env.context.get('skip_xsd', False):
+            return set()
+
+        def load_xsd():
+            attachment = self.search([('name', '=', XSD_NAME)])
+            if attachment:
+                return attachment
+            self._l10n_nl_reports_load_xsd_files()
+            return self.search([('name', '=', XSD_NAME)])
+
+        attachment = load_xsd()
+        if not attachment:
+            return set()
+
+        country_code_container = etree.fromstring(attachment.raw).find(
+            './/{http://www.w3.org/2001/XMLSchema}simpleType[@name="CountrycodeIso3166"]')
+        return set(
+            e.attrib['value']
+            for e in country_code_container.findall('.//{http://www.w3.org/2001/XMLSchema}enumeration')
+        )

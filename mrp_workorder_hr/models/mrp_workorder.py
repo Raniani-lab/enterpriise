623083dafeb2ba1935f1889101d41a7aee094ac7
PNO <pno@odoo.com>
2023-03-08 13:03:26 +0000

    [FIX] mrp_workorder_hr: duration_unit and duration_percent are not being computed
    Currently, when a workcenter requires a login (allow_employees = True), the fields duration_unit and the duration_percent of workorders are not being computed.
    After this commit, both fields are computed the same way as when no login is required.
    
    OPW-3201557
    
    closes odoo/enterprise#38807
    
    X-original-commit: 9915ff674f95547ae4d81dec24c98c86f9c6f131
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- mrp_workorder_hr/models/mrp_workorder.py
+++ mrp_workorder_hr/models/mrp_workorder.py
@@ -38,6 +38,11 @@ class MrpWorkorder(models.Model):
                 wo_ids_without_employees.add(wo.id)
                 continue
             wo.duration = wo.get_duration()
+            wo.duration_unit = round(wo.duration / max(wo.qty_produced, 1), 2)
+            if wo.duration_expected:
+                wo.duration_percent = 100 * (wo.duration_expected - wo.duration) / wo.duration_expected
+            else:
+                wo.duration_percent = 0
         return super(MrpWorkorder, self.env['mrp.workorder'].browse(wo_ids_without_employees))._compute_duration()
 
     @api.depends('employee_ids')

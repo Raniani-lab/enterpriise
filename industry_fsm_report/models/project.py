0aaae1c7c554778821cf395a5c0c397caa42d195
Kartik Chavda <kcv@odoo.com>
2023-03-17 11:59:32 +0000

    [FIX] industry_fsm_report: update method name according to recent refactoring
    Before this commit method to get recurring field was not update
    with recent refactoring of recurrence feature.
    
    This commit update that method to make worksheet template field
    recurring field.
    
    closes odoo/enterprise#38357
    
    X-original-commit: 33be6451168b996f31c216ae2176feb4bc457c03
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- industry_fsm_report/models/project.py
+++ industry_fsm_report/models/project.py
@@ -125,5 +125,5 @@ class ProjectTaskRecurrence(models.Model):
     _inherit = 'project.task.recurrence'
 
     @api.model
-    def _get_recurring_fields(self):
-        return ['worksheet_template_id'] + super(ProjectTaskRecurrence, self)._get_recurring_fields()
+    def _get_recurring_fields_to_copy(self):
+        return ['worksheet_template_id'] + super()._get_recurring_fields_to_copy()

3b3a439587e76989fe08fb5f7a80252d94aaaa22
Tiffany Chang (tic) <tic@odoo.com>
2023-03-17 15:12:32 +0000

    [FIX] quality_mrp: correctly set both production and comp lot
    Steps to reproduce:
    - install quality_mrp_workorder
    - create 2 SN tracked products: test_manufacture, test_component
    - add 2 test_component into stock (i.e. 2 SNs)
    - create BoM for test_manufacture w/ 1 operation + test_component
      consumed in the operation
    - create + confirm a MO for test_manufacture
    - open WO tablet view + set both finished_lot_id (using dropdown,
      NOT '+' button) and lot_id (set lot_id to the NOT reserved SN)
    
    Expected behavior:
    - finished_product_check_ids shows test_component - (your selected sn)
    
    Actual behavior:
    - finished_product_check_ids shows test_component - (original reserved
      sn)
    
    Issue was due to 2 problems:
    
    1. The `quality.check` form view IS NOT saved when it's `lot_id` value
    is changed, but the `mrp.workorder` form view IS saved when it's
    `finished_lot_id` is saved => the selected `lot_id` change is discarded
    
    2. https://github.com/odoo/enterprise/blob/88031ec9e4a3db7ac96a7e3d9feaf44305c6c46b/quality_mrp/models/quality.py#L31-L33
    
    is always called, even if the compute is triggered by the
    production_id.lot_production_id, which means the selected
    qc.lot_id would be overwritten by the qc.move_line_id.lot_id
    in the super:
    
    https://github.com/odoo/enterprise/blob/88031ec9e4a3db7ac96a7e3d9feaf44305c6c46b/quality_control/models/quality.py#L164-L169
    
    Unfortunately because _compute_lot_line_id is being used to handle
    different use cases, the move_line `write` must also be overridden.
    
    Cases to be handled (i.e. not broken with this fix):
    - QC set on MO (i.e. quality.point on manufacturing operation) for SN
      tracked product, both product to manufacture and component to use
      (QC.lot_id should ALWAYS match sml.lot_id)
    - sml.lot_id is changed => all QCs associated with it (MO or operation
      related) will have their lot_id updated to match
    - `register_consumed_materials` QC lot_id is changed to NOT match
      sml.lot = allowed => QC validation = sml.lot_id should be updated to
      match QC.lot_id
    
    opw-3129597
    
    closes odoo/enterprise#39645
    
    X-original-commit: 9877c70a7c0bba84dc67d2095c227cd2aa4147c6
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- mrp_workorder/static/tests/tours/tour_test_workorder.js
+++ mrp_workorder/static/tests/tours/tour_test_workorder.js
@@ -551,3 +551,61 @@ registry.category("web_tour.tours").add('test_update_step', {test: true, steps: 
     { trigger: 'button[name=do_finish]' },
     { trigger: '.o_searchview_input' },
 ]});
+
+registry.category("web_tour.tours").add('test_serial_tracked_and_register', {test: true, steps: [
+    {
+        trigger: '.o_tablet_client_action',
+        run: function() {
+            helper.assert($('input[id="finished_lot_id_0"]').val(), 'Magic Potion_1');
+        }
+    },
+    { trigger: '.o_tablet_client_action' },
+    {
+        // sn should have been updated to match move_line sn
+        trigger: 'div.o_field_widget[name="lot_id"] input ',
+        run: function() {
+            helper.assert($('input[id="lot_id_0"]').val(), 'Magic_2');
+        }
+    },
+    { trigger: '.o_tablet_client_action' },
+    { trigger: '.btn[name="button_start"]' },
+    {
+        trigger: 'div.o_field_widget[name="lot_id"] input ',
+        position: 'bottom',
+        run: 'text Magic_3',
+    },
+    { trigger: '.ui-menu-item > a:contains("Magic_3")' },
+    { trigger: '.o_tablet_client_action' },
+    {
+        trigger: 'div.o_field_widget[name="finished_lot_id"] input ',
+        position: 'bottom',
+        run: 'text Magic Potion_2',
+    },
+    { trigger: '.ui-menu-item > a:contains("Magic Potion_2")' },
+    {
+        // comp sn shouldn't change when produced sn is changed
+        trigger: 'div.o_field_widget[name="lot_id"] input',
+        run: function() {
+            helper.assert($('input[id="lot_id_0"]').val(), 'Magic_3');
+        }
+    },
+    { trigger: '.o_tablet_client_action' },
+    {
+        trigger: 'div.o_field_widget[name="lot_id"] input ',
+        position: 'bottom',
+        run: 'text Magic_1',
+    },
+    { trigger: '.ui-menu-item > a:contains("Magic_1")' },
+    { trigger: '.o_tablet_client_action' },
+    {
+        // produced sn shouldn't change when comp sn is changed
+        trigger: 'div.o_field_widget[name="finished_lot_id"] input ',
+        run: function() {
+            helper.assert($('input[id="finished_lot_id_0"]').val(), 'Magic Potion_2');
+        }
+    },
+    { trigger: '.o_tablet_client_action' },
+    { trigger: '.btn-primary[name="action_next"]' },
+    { trigger: 'button[name=do_finish]' },
+    { trigger: '.o_searchview_input' },
+]});
--- quality_mrp_workorder/tests/test_quality_check_workorder.py
+++ quality_mrp_workorder/tests/test_quality_check_workorder.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo.tests import Form
+from odoo.tests import Form, HttpCase, tagged
 from odoo.addons.mrp.tests.common import TestMrpCommon
+from odoo.addons.mrp_workorder.tests import test_tablet_client_action
 
 
 class TestQualityCheckWorkorder(TestMrpCommon):
@@ -115,4 +116,94 @@ class TestQualityCheckWorkorder(TestMrpCommon):
         self.assertRecordValues(mo.move_raw_ids.move_line_ids + mo.move_finished_ids.move_line_ids, [
             {'qty_done': 1, 'lot_id': component_sn.id},
             {'qty_done': 1, 'lot_id': finished_sn.id},
+        ])
+
+
+@tagged('post_install', '-at_install')
+class TestPickingWorkorderClientActionQuality(test_tablet_client_action.TestWorkorderClientActionCommon, HttpCase):
+
+    def test_register_consumed_materials_01(self):
+        """
+        Process a MO based on a BoM with one operation. That operation has one
+        step: register the used component. Both finished product and component
+        are tracked by serial. Changing both the finished product and the
+        component serial at the same time should record both values.
+
+        Also ensure if there is an overlapping quality.point for MOs (i.e. not
+        a WO step), the SNs are as expected between:
+        WO step <-> MO QC <-> move_line.lot_id
+        regardless of which one is changed. All 3 use case occurring at the same
+        time is unlikely, but helps tests expected behavior for the possible
+        combinations of these use cases within 1 test
+        """
+
+        warehouse = self.env['stock.warehouse'].search([('company_id', '=', self.env.company.id)], limit=1)
+
+        finished = self.potion
+        component = self.ingredient_1
+        (finished | component).write({
+            'type': 'product',
+            'tracking': 'serial',
+        })
+
+        finished_sn, component_sn = self.env['stock.lot'].create([{
+            'name': p.name + "_1",
+            'product_id': p.id,
+            'company_id': self.env.company.id,
+        } for p in (finished, component)])
+        finished_sn2, component_sn2 = self.env['stock.lot'].create([{
+            'name': p.name + "_2",
+            'product_id': p.id,
+            'company_id': self.env.company.id,
+        } for p in (finished, component)])
+        component_sn3 = self.env['stock.lot'].create({
+            'name': component.name + "_3",
+            'product_id': component.id,
+            'company_id': self.env.company.id,
+        })
+        self.env['stock.quant']._update_available_quantity(component, warehouse.lot_stock_id, 1, lot_id=component_sn)
+        self.env['stock.quant']._update_available_quantity(component, warehouse.lot_stock_id, 1, lot_id=component_sn2)
+        self.env['stock.quant']._update_available_quantity(component, warehouse.lot_stock_id, 1, lot_id=component_sn3)
+
+        type_register_materials = self.env.ref('mrp_workorder.test_type_register_consumed_materials')
+        self.wizarding_step_1.test_type_id = type_register_materials
+        self.wizarding_step_2.unlink()
+
+        self.env['quality.point'].create({
+            'product_ids': [component.id],
+            'picking_type_ids': [(4, warehouse.manu_type_id.id)],
+            'measure_on': 'move_line',
+        })
+
+        mo_form = Form(self.env['mrp.production'])
+        mo_form.bom_id = self.bom_potion
+        mo = mo_form.save()
+        mo.action_confirm()
+
+        self.assertEqual(mo.move_raw_ids.move_line_ids.lot_id, component_sn, "Unexpected reserved SN for MO component")
+        self.assertEqual(mo.check_ids.lot_line_id, component_sn, "MO level QC should have been created with reserved SN")
+        self.assertEqual(len(mo.workorder_ids.check_ids), 1, "WO and its step should have been created")
+        self.assertEqual(mo.workorder_ids.check_ids.lot_id, component_sn, "WO's component lot should match reserved SN")
+
+        mo.move_raw_ids.move_line_ids.lot_id = component_sn2
+        mo.lot_producing_id = finished_sn
+
+        self.assertEqual(mo.move_raw_ids.move_line_ids.lot_id, component_sn2, "Changing final product sn shouldn't affect component sn")
+        self.assertEqual(mo.check_ids.lot_line_id, component_sn2, "MO level QC should update to match ml.lot_id")
+        self.assertEqual(mo.workorder_ids.check_ids.lot_id, component_sn2, "WO's component lot should update to match ml.lot_id")
+        wo = mo.workorder_ids[0]
+        url = self._get_client_action_url(wo.id)
+
+        self.start_tour(url, 'test_serial_tracked_and_register', login='admin', timeout=120)
+
+        self.assertEqual(mo.workorder_ids.finished_lot_id, finished_sn2)
+        self.assertEqual(mo.workorder_ids.check_ids.lot_id, component_sn, "WO level QC should be using newly selected SN")
+        self.assertEqual(mo.check_ids.lot_line_id, component_sn, "MO level QC should update to match completed WO sn")
+        self.assertEqual(mo.move_raw_ids.move_line_ids.lot_id, component_sn, "MO component SN should update to match completed WO sn")
+        mo.check_ids.do_pass()
+        mo.button_mark_done()
+
+        self.assertRecordValues(mo.move_raw_ids.move_line_ids + mo.move_finished_ids.move_line_ids, [
+            {'qty_done': 1, 'lot_id': component_sn.id},
+            {'qty_done': 1, 'lot_id': finished_sn2.id},
         ])

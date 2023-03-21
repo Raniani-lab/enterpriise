23c19761dbff0c4ffc7db44c27e97fe4f9b3c2ee
Maximilien (malb) <malb@odoo.com>
2023-03-21 13:50:23 +0000

    [IMP] account_report: Adding title, vat number, options and address
    Before this PR, when printing a report the title was missing, this has already been addressed in this PR: #36163.
    In addition, we had to add the vat number and the address in the header of the report. This PR also add the options of the reports in the pdf.
    
    Task-id: 3171683
    X-original-commit: 641854d0fb8e86a04538705ff519b1fca709297c
    Part-of: odoo/enterprise#42004

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -4303,7 +4303,8 @@ class AccountReport(models.Model):
             landscape=landscape,
             specific_paperformat_args={
                 'data-report-margin-top': 10,
-                'data-report-header-spacing': 10
+                'data-report-header-spacing': 10,
+                'data-report-margin-bottom': 15,
             }
         )
 

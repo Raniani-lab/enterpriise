316c4afda7947ecb51364796d3967bcd64d8a5dd
Louis Wicket (wil) <wil@odoo.com>
2023-03-22 09:15:04 +0000

    [IMP] mail: sync activities data between tabs
    Task-2714099.
    
    closes odoo/enterprise#38537
    
    Related: odoo/odoo#115986
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- approvals/static/src/web/activity/activity_service_patch.js
+++ approvals/static/src/web/activity/activity_service_patch.js
@@ -6,7 +6,7 @@ import { patch } from "@web/core/utils/patch";
 
 patch(ActivityService.prototype, "approvals/web/activity", {
     insert(data) {
-        const activity = this._super(data);
+        const activity = this._super(...arguments);
         if ("approver_id" in data && "approver_status" in data) {
             if (!data.approver_id) {
                 delete activity.approval;

dddddc2f9b6ecad2fcedb91ff53176ab8dd5c1e2
Vincent Larcin <vila@odoo.com>
2023-03-22 15:52:49 +0000

    [IMP] documents_project_sale: prevent multi-company issue with templates
    This commit adds a check preventing setting a different company on a product
    and its workspace template, thus avoiding potential multi-company issues.
    
    Task-3186695
    
    Part-of: odoo/enterprise#38586

--- documents_project_sale/models/product_template.py
+++ documents_project_sale/models/product_template.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
@@ -15,3 +16,14 @@ class ProductTemplate(models.Model):
     def _compute_documents_allowed_company_id(self):
         for template in self:
             template.documents_allowed_company_id = template.company_id if template.company_id else self.env.company
+
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        if self.template_folder_id and self.template_folder_id.company_id and self.company_id != self.template_folder_id.company_id:
+            self.template_folder_id = False
+
+    @api.constrains('template_folder_id')
+    def _check_company_is_folder_company(self):
+        for product in self:
+            if product.template_folder_id and product.template_folder_id.company_id and product.company_id != product.template_folder_id.company_id:
+                raise UserError(_('The "%s" workspace template should either be in the "%s" company like this product or be open to all companies.', product.template_folder_id.name, product.company_id.name))

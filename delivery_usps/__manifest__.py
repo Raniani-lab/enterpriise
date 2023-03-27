84b451b1931448aacba8b1c2a71cd60cf9e79743
Valentin Vallaeys (vava) <vava@odoo.com>
2023-03-27 13:19:12 +0000

    [FIX] delivery_usps: fix stock dependency
    Since the split of delivery from stock, there is a new module bridge
    `stock_delivery`, and delivery does not depend on stock anymore. All
    shipping connectors now rely on this new bridge module. USPS was
    forgotten in the original PR.
    
    closes odoo/enterprise#38847
    
    X-original-commit: 826803cd2d3c5e182871b6cbcf2e2a795ddf7a66
    Related: odoo/odoo#116747
    Signed-off-by: Vallaeys Valentin (vava) <vava@odoo.com>

--- delivery_usps/__manifest__.py
+++ delivery_usps/__manifest__.py
@@ -7,7 +7,7 @@
     'sequence': 305,
     'version': '1.0',
     'application': True,
-    'depends': ['delivery', 'mail'],
+    'depends': ['stock_delivery', 'mail'],
     'data': [
         'data/delivery_usps_data.xml',
         'views/delivery_usps_view.xml',

fc012b07aa567b897a3e1667efbdd1920f1a8925
Nishit Prajapati <nir@odoo.com>
2023-03-28 13:37:40 +0000

    [IMP] l10n_in_hr_payroll: add a unique constraint in india payroll
    In this commit, we have added a unique constraint for the following fields:
      - l10n_in_uan
      - l10n_in_pan
      - 10n_in_esic_number
    
    task-3220775
    
    closes odoo/enterprise#38051
    
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- l10n_in_hr_payroll/models/hr_employee.py
+++ l10n_in_hr_payroll/models/hr_employee.py
@@ -10,3 +10,9 @@ class HrEmployee(models.Model):
     l10n_in_uan = fields.Char(string='UAN', groups="hr.group_hr_user")
     l10n_in_pan = fields.Char(string='PAN', groups="hr.group_hr_user")
     l10n_in_esic_number = fields.Char(string='ESIC Number', groups="hr.group_hr_user")
+
+    _sql_constraints = [
+        ('unique_l10n_in_uan', 'unique (l10n_in_uan)', 'This UAN already exists'),
+        ('unique_l10n_in_pan', 'unique (l10n_in_pan)', 'This PAN already exists'),
+        ('unique_l10n_in_esic_number', 'unique (l10n_in_esic_number)', 'This ESIC Number already exists'),
+    ]

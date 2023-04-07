b4257d9005ccd7a30d43f571c7161e3819a0343d
Aurélien Warnon <awa@odoo.com>
2023-04-07 09:04:15 +0000

    [MOV] (website_)knowledge: move portal search tour to website_knowledge
    This commit is a preparation commit for the upcoming feature that will allow
    portal users to access the knowledge.articles using the OWL backend form view.
    
    That means that portal users will no longer be redirected to the portal
    readonly view, making the "knowledge_portal_search_tour" irrelevant for them.
    It is however still relevant for *public* users when articles are published.
    
    We thus move the code and the logic to the website_knowledge module and
    slightly modify the test to publish the articles instead of granting access to
    the portal user.
    
    We also remove the part that was testing favoriting an article as public users
    do not have access to that feature.
    
    The core of the tour code, meant to test the search feature, is unchanged.
    
    Task-3056280
    
    Part-of: odoo/enterprise#38575

--- knowledge/static/tests/tours/knowledge_portal_search_tour.js
+++ knowledge/static/tests/tours/knowledge_portal_search_tour.js
@@ -6,9 +6,7 @@
  * - Check that tree contains all articles
  * - Write search term in search bar
  * - Check that search tree renders the correct matching articles
- * - Set active article as favorite
  * - Clean search bar
- * - Check that the active article was correctly added to the favorite tree
  */
 
 import { registry } from "@web/core/registry";
@@ -52,17 +50,10 @@ registry.category("web_tour.tours").add('knowledge_portal_search_tour', {
     content: "Check that search tree doesn't contain 'Sibling Article'",
     trigger: '.o_knowledge_tree:not(:has(.o_article_name:contains("Sibling Article")))',
     run() {},
-}, { // Set active article as favorite
-    content: 'Set active article (My Article) as favorite',
-    trigger: '.o_favorites_toggle_button',
 }, { // Clean search bar
     content: "Clean search bar",
     trigger: '.knowledge_search_bar',
     run: function (action) {
         action.remove_text("", ".knowledge_search_bar");
     },
-}, { // Check that article was correctly added to the favorite tree
-    content: "Check that 'My Article' was correctly added to the favorite tree",
-    trigger: '.o_favorite_container .o_article .o_article_name:contains("My Article")',
-    run() {},
 }]});

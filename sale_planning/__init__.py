601ecd36dd651e6f759ae8e296b7e28ab2faed7d
Xavier Morel <xmo@odoo.com>
2023-04-14 11:09:07 +0000

    [FIX] sale_planning: incorrect `uninstall_hook` signature
    Infinite employment hack:
    
    - step 1, fix `uninstall_hook` with the wrong signature, which breaks
      the uninstall tests (odoo/odoo#118197)
    - step 2: add a new `uninstall_hook` with the wrong signature, so the
      uninstall tests remain not working (odoo/enterprise#39710)
    - step 3: profit
    
    closes odoo/enterprise#39798
    
    X-original-commit: a1a95fde1ba046e7e6e204c30bab738dbd3e9f3c
    Signed-off-by: Xavier Morel (xmo) <xmo@odoo.com>

--- sale_planning/__init__.py
+++ sale_planning/__init__.py
@@ -5,11 +5,11 @@ from . import controllers
 from . import models
 from . import report
 
-def uninstall_hook(cr, registry):
+def uninstall_hook(env):
     # remove plannings which are not scheduled
-    cr.execute("DELETE FROM planning_slot WHERE start_datetime is null OR end_datetime is null")
+    env.cr.execute("DELETE FROM planning_slot WHERE start_datetime is null OR end_datetime is null")
     # restore required
-    cr.execute("""
+    env.cr.execute("""
         ALTER TABLE planning_slot
             ALTER COLUMN start_datetime SET NOT NULL,
             ALTER COLUMN end_datetime SET NOT NULL

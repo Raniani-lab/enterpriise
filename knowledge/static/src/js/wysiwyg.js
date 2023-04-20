bcfa64ced851c13a8488d792ba0be6e669fcb8ee
Thomas Josse (thjo) <thjo@odoo.com>
2023-04-20 10:41:21 +0000

    [FIX] knowledge: removing the command from the website
    This commit removes the `/article` command from the website editor for the
    following reason:
    
    > The reason is that the knowledge behaviours are not designed to work with the
    > website editor, and it is "by chance" that the command is "working" in stable
    > versions.
    
    On the master branch with the new collaborative mode it even introduces
    unwanted behaviors.
    So for these reasons, we are removing the command from the website editor.
    
    task-3280704
    
    X-original-commit: f50a33269758ee51cbb62b9b92c2c628b3440123
    Part-of: odoo/enterprise#42615

--- knowledge/static/src/js/wysiwyg.js
+++ knowledge/static/src/js/wysiwyg.js
@@ -90,6 +90,7 @@ Wysiwyg.include({
             priority: 10,
             description: _t('Link an article'),
             fontawesome: 'fa-file',
+            isDisabled: () => this.options.isWebsite || this.options.inIframe,
             callback: () => {
                 this._insertArticleLink();
             },

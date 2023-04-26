0e1b898b7e2641d655bc2505eb759156b2e804e2
Stanislas Gueniffey <stgu@odoo.com>
2023-04-26 09:34:49 +0000

    [FIX] l10n_ec_edi: TaxTotalsComponentForWithhold
    - Adapts l10n_ec_edi.TaxTotalsComponentForWithhold to the latest changes
    of its base class account.TaxTotalsComponent, which renames its
    formatting method to `formatData`
    https://github.com/odoo/odoo/pull/117509
    - Removes some unnecessary JS code from the component
    
    task-3272412
    task-3277363
    
    closes odoo/enterprise#40354
    
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- l10n_ec_edi/static/src/components/tax_totals/tax_totals_for_withhold.js
+++ l10n_ec_edi/static/src/components/tax_totals/tax_totals_for_withhold.js
@@ -1,26 +1,18 @@
 /** @odoo-module **/
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { registry } from "@web/core/registry";
-import { TaxTotalsComponent, TaxGroupComponent } from "@account/components/tax_totals/tax_totals";
+import { TaxTotalsComponent } from "@account/components/tax_totals/tax_totals";
 
 /**
  * Tax totals component specific to withholds.
- * In withholds, is not included in the total.
+ * In withholds, the base is not included in the total.
  * The base may be displayed to keep accountants happy, for example as part of the formatted values.
  */
 export class TaxTotalsComponentForWithhold extends TaxTotalsComponent {
-    setup() {
-        super.setup();
-    }
-
-    _computeTotalsFormat() {
+    formatData(props) {
         // Prevents super's formatting method from running
         // (its logic is not compatible with withholds)
-        return;
+        this.totals = props.value;
     }
 }
 
-TaxTotalsComponentForWithhold.template = "account.TaxTotalsField";
-TaxTotalsComponentForWithhold.components = { TaxGroupComponent };
-TaxTotalsComponentForWithhold.props = standardFieldProps;
 registry.category("fields").add("account-tax-totals-field-for-withhold", TaxTotalsComponentForWithhold);

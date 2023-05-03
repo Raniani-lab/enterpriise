91a395b861bdd488437810820e851f8608ebd0a3
Antoine Vandevenne (anv) <anv@odoo.com>
2023-05-03 18:22:12 +0200

    [FIX] payment(_*): make a proper usage of promises
    Several functions were explicitly encapsulating their return values in a
    `Promise` when it was unnecessary.
    
    task-2882677
    
    Part-of: odoo/enterprise#40666

--- payment_sepa_direct_debit/static/src/js/payment_form.js
+++ payment_sepa_direct_debit/static/src/js/payment_form.js
@@ -14,16 +14,16 @@ const sepaDirectDebitMixin = {
      * @param {string} code - The code of the selected payment option's provider
      * @param {number} paymentOptionId - The id of the selected payment option
      * @param {string} flow - The online payment flow of the selected payment option
-     * @return {Promise}
+     * @return {void}
      */
     _prepareInlineForm: function (code, paymentOptionId, flow) {
         if (code !== 'sepa_direct_debit') {
-            return this._super(...arguments);
+            this._super(...arguments);
+            return;
         } else if (flow === 'token') {
-            return Promise.resolve(); // Don't show the form for tokens
+            return; // Don't show the form for tokens.
         }
         this._setPaymentFlow('direct');
-        return Promise.resolve();
     },
 
     /**
@@ -34,21 +34,22 @@ const sepaDirectDebitMixin = {
      * @param {string} code - The code of the payment option provider.
      * @param {number} paymentOptionId - The id of the payment option handling the transaction.
      * @param {string} flow - The online payment flow of the transaction.
-     * @return {Promise}
+     * @return {void}
      */
     _processPayment: function (code, paymentOptionId, flow) {
         if (code !== 'sepa_direct_debit' || flow === 'token') {
-            return this._super(...arguments); // Tokens are handled by the generic flow
+            this._super(...arguments); // Tokens are handled by the generic flow.
+            return;
         }
 
         const ibanInput = document.getElementById(`o_sdd_iban_${paymentOptionId}`);
         if (!ibanInput.reportValidity()) {
             this._enableButton(); // The submit button is disabled at this point, enable it
-            this.call("ui", "unblock"); // The page is blocked at this point, unblock it
-            return Promise.resolve(); // Let the browser request to fill out required fields
+            this.call('ui', 'unblock'); // The page is blocked at this point, unblock it
+            return; // Let the browser request to fill out required fields
         }
 
-        return this._super(...arguments);
+        this._super(...arguments);
     },
 
     /**
@@ -59,16 +60,17 @@ const sepaDirectDebitMixin = {
      * @param {string} code - The code of the provider.
      * @param {number} providerId - The id of the provider handling the transaction.
      * @param {object} processingValues - The processing values of the transaction.
-     * @return {Promise}
+     * @return {void}
      */
     _processDirectPayment: function (code, providerId, processingValues) {
         if (code !== 'sepa_direct_debit') {
-            return this._super(...arguments);
+            this._super(...arguments);
+            return;
         }
 
         // Assign the SDD mandate corresponding to the IBAN to the transaction.
         const ibanInput = document.getElementById(`o_sdd_iban_${providerId}`);
-        return this._rpc({
+        this._rpc({
             route: '/payment/sepa_direct_debit/set_mandate',
             params: {
                 'reference': processingValues.reference,

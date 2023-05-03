d399f2be13e62c2119f58ac7a1594833c764ff9d
Ahmed Khalaf <ahkh@odoo.com>
2023-05-03 07:16:50 +0000

    [IMP] industry_fsm_stock: update function signature
    This commit uses the the new function signature done in the community
    
    closes odoo/enterprise#40633
    
    Pr: odoo/odoo#116803
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- industry_fsm_stock/models/stock_move.py
+++ industry_fsm_stock/models/stock_move.py
@@ -9,16 +9,16 @@ from collections import defaultdict
 class StockMove(models.Model):
     _inherit = ['stock.move']
 
-    def _update_reserved_quantity(self, need, available_quantity, location_id, lot_id=None, package_id=None, owner_id=None, strict=True):
+    def _update_reserved_quantity(self, need, location_id, quant_ids=None, lot_id=None, package_id=None, owner_id=None, strict=True):
         if self.product_id.tracking == 'none':
-            return super()._update_reserved_quantity(need, available_quantity, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+            return super()._update_reserved_quantity(need, location_id, quant_ids=quant_ids, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
         lot = self.sale_line_id.sudo().fsm_lot_id
         if lot:
-            return super()._update_reserved_quantity(need, available_quantity, location_id, lot_id=lot, package_id=package_id, owner_id=owner_id, strict=strict)
+            return super()._update_reserved_quantity(need, location_id, quant_ids=quant_ids, lot_id=lot, package_id=package_id, owner_id=owner_id, strict=strict)
 
         so_lines_with_fsm_lot = self.group_id.stock_move_ids.sale_line_id.sudo().filtered(lambda l: l.product_id == self.product_id and l.fsm_lot_id)
         if not so_lines_with_fsm_lot:
-            return super()._update_reserved_quantity(need, available_quantity, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+            return super()._update_reserved_quantity(need, location_id, quant_ids=quant_ids, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
 
         already_used_by_lots = defaultdict(float)
         for ml in self.move_dest_ids.move_orig_ids.move_line_ids:
@@ -35,14 +35,12 @@ class StockMove(models.Model):
             sol_qty = so_line.product_uom._compute_quantity(so_line.product_uom_qty, so_line.product_id.uom_id)
             lot_need = max(0, sol_qty - already_used_by_lots[lot])
             lot_need = min(need, lot_need)
-            lot_available_quantity = self._get_available_quantity(self.location_id, package_id=package_id, lot_id=lot)
-            taken = super()._update_reserved_quantity(lot_need, lot_available_quantity, location_id, lot_id=lot, package_id=package_id, owner_id=owner_id, strict=strict)
+            taken = super()._update_reserved_quantity(lot_need, location_id, quant_ids=quant_ids, lot_id=lot, package_id=package_id, owner_id=owner_id, strict=strict)
             need -= taken
-            available_quantity -= taken
             reserved += taken
 
         if float_is_zero(need, precision_rounding=self.product_id.uom_id.rounding):
             return reserved
 
-        reserved += super()._update_reserved_quantity(need, available_quantity, location_id, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
+        reserved += super()._update_reserved_quantity(need, location_id, quant_ids=quant_ids, lot_id=lot_id, package_id=package_id, owner_id=owner_id, strict=strict)
         return reserved

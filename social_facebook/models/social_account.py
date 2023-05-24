bc4e6555b9d671cdb100f9fa38977ce364e6c22f
std-odoo <std@odoo.com>
2023-05-24 08:48:04 +0000

    [FIX] social_facebook: upgrade to the latest API version
    Purpose
    =======
    The API 10.0 will be deprecated on Jun 08, 2023, and so we upgrade
    to the latest version.
    
    Now we use a variable to store the versioned API URL to make
    the future API migrations easier.
    
    Task-3337765
    
    closes odoo/enterprise#42344
    
    X-original-commit: 522d8e2c1b1a765e2ec20c407a34a342e7ec12c5
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>
    Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>
    Signed-off-by: Stéphane Debauche (std) <std@odoo.com>

--- social_facebook/models/social_account.py
+++ social_facebook/models/social_account.py
@@ -42,11 +42,11 @@ class SocialAccountFacebook(models.Model):
         super(SocialAccountFacebook, (self - facebook_accounts))._compute_statistics()
 
         for account in facebook_accounts:
-            insights_endpoint_url = url_join(self.env['social.media']._FACEBOOK_ENDPOINT, "/v10.0/%s/insights" % account.facebook_account_id)
+            insights_endpoint_url = url_join(self.env['social.media']._FACEBOOK_ENDPOINT_VERSIONED, "%s/insights" % account.facebook_account_id)
             statistics_30d = account._compute_statistics_facebook(insights_endpoint_url)
             statistics_360d = account._compute_statistics_facebook_360d(insights_endpoint_url)
 
-            page_global_stats = requests.get(url_join(self.env['social.media']._FACEBOOK_ENDPOINT, "/v10.0/%s" % account.facebook_account_id),
+            page_global_stats = requests.get(url_join(self.env['social.media']._FACEBOOK_ENDPOINT_VERSIONED, account.facebook_account_id),
                 params={
                     'fields': 'fan_count',
                     'access_token': account.facebook_access_token
@@ -83,7 +83,7 @@ class SocialAccountFacebook(models.Model):
         return total_statistics
 
     def _compute_statistics_facebook(self, endpoint_url, date_preset='last_30d', since=None, until=None):
-        """ Check https://developers.facebook.com/docs/graph-api/reference/v10.0/insights for more information
+        """ Check https://developers.facebook.com/docs/graph-api/reference/v17.0/insights for more information
         about the endpoint used.
         e.g of data structure returned by the endpoint:
         [{

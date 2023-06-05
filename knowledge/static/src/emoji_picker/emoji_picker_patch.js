7bd002b94efe008efc6e04401a6f2ca59ab2fc64
Alexandre Kühn <aku@odoo.com>
2023-06-05 14:00:49 +0000

    [IMP] knowledge: move emoji picker from header to bottom
    closes odoo/enterprise#41959
    
    Related: odoo/odoo#116060
    Signed-off-by: Alexandre Kühn (aku) <aku@odoo.com>

--- knowledge/static/src/emoji_picker/emoji_picker_patch.js
+++ knowledge/static/src/emoji_picker/emoji_picker_patch.js
@@ -5,13 +5,12 @@ import { EmojiPicker } from "@mail/core/common/emoji_picker";
 import { patch } from "@web/core/utils/patch";
 
 EmojiPicker.props.push("hasRemoveFeature?");
-Object.assign(EmojiPicker.defaultProps, { hasRemoveFeature: false });
 
 patch(EmojiPicker.prototype, "knowledge", {
     removeEmoji() {
         this.props.onSelect(false);
         this.gridRef.el.scrollTop = 0;
-        this.props.close();
-        this.props.onClose();
+        this.props.close?.();
+        this.props.onClose?.();
     },
 });

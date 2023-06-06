80abaa0c9c6979c42e3c520d91f924c9ddb36102
lrizzi1 <lrfd@odoo.com>
2023-06-06 16:04:06 +0000

    [FIX] sale_temporal: add a case in the compute subscription display method
    aparently the rental module increase the number of possible units
    adding 'hour' as an option. The module don't increase the possible
    values on the cited method which causes some errors displaying
    products on the website and/or trigerring the compute method
    in a record that has hours as unit
    
    opw-3341017
    
    closes odoo/enterprise#45698
    
    X-original-commit: 6794d9f7f0ccaa546ebc6911be36f215aca62b10
    Signed-off-by: Lucas Rizzi Falasca Duarte (lrfd) <lrfd@odoo.com>
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sale_temporal/models/sale_order_recurrence.py
+++ sale_temporal/models/sale_order_recurrence.py
@@ -57,11 +57,11 @@ class SaleOrderRecurrence(models.Model):
     @api.depends('duration', 'unit')
     def _compute_temporal_unit_display(self):
         for order in self:
-            if order.unit == 'day':
-                order.temporal_unit_display = _('per %s days', order.duration) if order.duration > 1 else _('per day')
-            elif order.unit == 'week':
+            if order.unit == 'week':
                 order.temporal_unit_display = _('per %s weeks', order.duration) if order.duration > 1 else _('per week')
             elif order.unit == 'month':
                 order.temporal_unit_display = _('per %s months', order.duration) if order.duration > 1 else _('per month')
             elif order.unit == 'year':
                 order.temporal_unit_display = _('per %s years', order.duration) if order.duration > 1 else _('per year')
+            else:
+                order.temporal_unit_display = _('This recurrence unit is not supported.')

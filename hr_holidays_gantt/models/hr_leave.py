c78161403291ecd42edaab35fbb276aba3e152f7
Dossogne Bertrand <bedo@odoo.com>
2023-06-07 14:02:25 +0000

    [ADD] hr_holidays_contract_gantt: add bridge module
    This commit adds an additional module to make the bridge between
    the hr_holidays_gantt and the hr_contract modules as the non-working
    days display might vary according to different contracts.
    This change has been made in order to be consistent with the
    changes made for the calendar in community in the hr_contract module.
    
    task-3060722
    
    closes odoo/enterprise#42125
    
    Related: odoo/odoo#121153
    Signed-off-by: Kevin Baptiste <kba@odoo.com>

--- hr_holidays_gantt/models/hr_leave.py
+++ hr_holidays_gantt/models/hr_leave.py
@@ -139,36 +139,9 @@ class HrLeave(models.Model):
     def gantt_unavailability(self, start_date, end_date, scale, group_bys=None, rows=None):
         start_datetime = fields.Datetime.from_string(start_date)
         end_datetime = fields.Datetime.from_string(end_date)
-        employee_ids = set()
-
-        # function to "mark" top level rows concerning employees
-        # the propagation of that item to subrows is taken care of in the traverse function below
-        def tag_employee_rows(rows):
-            for row in rows:
-                group_bys = row.get('groupedBy')
-                res_id = row.get('resId')
-                if group_bys:
-                    # if employee_id is the first grouping attribute, we mark the row
-                    if group_bys[0] == 'employee_id' and res_id:
-                        employee_id = res_id
-                        employee_ids.add(employee_id)
-                        row['employee_id'] = employee_id
-                    # else we recursively traverse the rows where employee_id appears in the group_by
-                    elif 'employee_id' in group_bys:
-                        tag_employee_rows(row.get('rows'))
-
-        tag_employee_rows(rows)
+        employee_ids = tag_employee_rows(rows)
         employees = self.env['hr.employee'].browse(employee_ids)
         leaves_mapping = employees.mapped('resource_id')._get_unavailable_intervals(start_datetime, end_datetime)
-
-        # function to recursively replace subrows with the ones returned by func
-        def traverse(func, row):
-            new_row = dict(row)
-            if new_row.get('employee_id'):
-                for sub_row in new_row.get('rows'):
-                    sub_row['employee_id'] = new_row['employee_id']
-            new_row['rows'] = [traverse(func, row) for row in new_row.get('rows')]
-            return func(new_row)
 
         cell_dt = timedelta(hours=1) if scale in ['day', 'week'] else timedelta(hours=12)
 
@@ -187,3 +160,31 @@ class HrLeave(models.Model):
             return new_row
 
         return [traverse(inject_unvailabilty, row) for row in rows]
+
+
+# function to "mark" top level rows concerning employees
+# the propagation of that item to subrows is taken care of in the traverse function below
+def tag_employee_rows(rows):
+    employee_ids = set()
+    for row in rows:
+        group_bys = row.get('groupedBy')
+        res_id = row.get('resId')
+        if group_bys:
+            # if employee_id is the first grouping attribute, we mark the row
+            if group_bys[0] == 'employee_id' and res_id:
+                employee_id = res_id
+                employee_ids.add(employee_id)
+                row['employee_id'] = employee_id
+            # else we recursively traverse the rows where employee_id appears in the group_by
+            elif 'employee_id' in group_bys:
+                tag_employee_rows(row.get('rows'))
+    return employee_ids
+
+# function to recursively replace subrows with the ones returned by func
+def traverse(func, row):
+    new_row = dict(row)
+    if new_row.get('employee_id'):
+        for sub_row in new_row.get('rows'):
+            sub_row['employee_id'] = new_row['employee_id']
+    new_row['rows'] = [traverse(func, row) for row in new_row.get('rows')]
+    return func(new_row)

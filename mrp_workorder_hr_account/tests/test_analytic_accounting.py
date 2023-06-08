d79c3fc2a6c5ec386742fc8daab2c7f09af3d901
Touati Djamel (otd) <otd@odoo.com>
2023-06-08 10:55:27 +0000

    [FIX] mrp_workorder_hr_account: avoid a traceback when adding AA in MO
    Steps to reproduce the bug:
    - Create a storable product “P1”
    - create a MO to produce one unit of “P1”:
        - be sure that no workorder is set
        - Confirm the MO
        - Add any analytic account
        - Try to mark done the MO
    
    Problem:
    Traceback is triggered, the “_create_analytic_entry” function is called
    without any “mrp.workcentre.productivity”:
    https://github.com/odoo/enterprise/blob/d4f65146dacf0c9c27577ce0965fe1589d548171/mrp_workorder_hr_account/models/mrp_workorder.py#L15-L16
    
    Solution:
    In cases where there are multiple work orders with time_ids,
    we would have a traceback due to the "ensure_one", so it is
    better to make the "_create_analytic_entry” function multi
    
    opw-3346372
    
    closes odoo/enterprise#42313
    
    X-original-commit: 63dbc0d4b8eb5d61bbc4b7324a40c7c59e1687d3
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>
    Signed-off-by: Djamel Touati (otd) <otd@odoo.com>

--- mrp_workorder_hr_account/tests/test_analytic_accounting.py
+++ mrp_workorder_hr_account/tests/test_analytic_accounting.py
@@ -65,3 +65,75 @@ class TestMrpAnalyticAccountHr(TestMrpAnalyticAccount):
         mo.analytic_account_id = new_account
         self.assertEqual(employee2_aa_line.account_id, new_account)
         self.assertEqual(employee1_aa_line.account_id, new_account)
+
+    def test_mrp_analytic_account_without_workorder(self):
+        """
+        Test adding an analytic account to a confirmed manufacturing order without a work order.
+        """
+        product = self.env['product.product'].create({
+            'name': 'Test Product',
+            'type': 'product',
+        })
+        component = self.env['product.product'].create({
+            'name': 'Test  Component',
+            'type': 'product',
+        })
+        mo_form = Form(self.env['mrp.production'])
+        mo_form.product_id = product
+        mo_form.product_qty = 1.0
+        with mo_form.move_raw_ids.new() as move:
+            move.product_id = component
+            move.product_uom_qty = 1
+        mo = mo_form.save()
+        mo.action_confirm()
+        self.assertEqual(mo.state, 'confirmed')
+
+        mo.analytic_account_id = self.analytic_account
+        self.assertEqual(mo.analytic_account_id, self.analytic_account)
+
+        mo_form = Form(mo)
+        mo_form.qty_producing = 1.0
+        mo = mo_form.save()
+        mo.button_mark_done()
+        self.assertEqual(mo.state, 'done')
+
+    def test_mrp_analytic_account_with_workorder(self):
+        """
+        Test adding an analytic account to a confirmed manufacturing order with work orders.
+        """
+        # add a workorder to the BoM
+        self.bom.write({
+            'operation_ids': [(0, 0, {
+                    'name': 'Test Operation 2',
+                    'workcenter_id': self.workcenter.id,
+                    'time_cycle': 60,
+                })
+            ]
+        })
+        mo_form = Form(self.env['mrp.production'])
+        mo_form.product_id = self.product
+        mo_form.bom_id = self.bom
+        mo_form.product_qty = 1.0
+        mo = mo_form.save()
+        mo.action_confirm()
+        self.assertEqual(mo.state, 'confirmed')
+
+        # set the workcenter to not "Requires Log In" to start the timer in WO and create "mrp.workcenter.productivity"
+        self.workcenter.allow_employee = False
+
+        # start the workorders
+        mo.workorder_ids[0].button_start()
+        mo.workorder_ids[1].button_start()
+        self.assertEqual(mo.workorder_ids[0].state, 'progress')
+        self.assertTrue(mo.workorder_ids[0].time_ids)
+        self.assertEqual(mo.workorder_ids[1].state, 'progress')
+        self.assertTrue(mo.workorder_ids[1].time_ids)
+
+        mo.analytic_account_id = self.analytic_account
+        self.assertEqual(mo.analytic_account_id, self.analytic_account)
+
+        mo_form = Form(mo)
+        mo_form.qty_producing = 1.0
+        mo = mo_form.save()
+        mo.button_mark_done()
+        self.assertEqual(mo.state, 'done')

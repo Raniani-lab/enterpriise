89ca69e928fd5a282bb43dd610b0fbfc08a8105a
Thomas Lefebvre (thle) <thle@odoo.com>
2023-06-14 07:57:49 +0000

    [FIX] industry_fsm, project: add a default project
    Steps to reproduce:
    -------------------
    - create a stage without selecting a project.
    
    Issue:
    ------
    The new stage is inaccessible.
    
    Cause:
    ------
    When the project is not defined, the following ORM command is used:
    `[6, False, []]` for the `project_ids` field.
    
    Solution:
    ---------
    When we create a project stage (project task type),
    it is essential that we link it to a project.
    Add the possibility to override `_get_default_project_ids`.
    Set default project as first fsm project if not project in
    the context.
    
    opw-3322992
    
    closes odoo/enterprise#43134
    
    X-original-commit: 7fd5dc49479e7cbc339fe3fc376fe32d6e1e6529
    Related: odoo/odoo#126338
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- industry_fsm/models/__init__.py
+++ industry_fsm/models/__init__.py
@@ -6,5 +6,6 @@ from . import res_partner
 from . import project_project
 from . import project_task
 from . import project_task_recurrence
+from . import project_task_type
 from . import res_config_settings
 from . import ir_ui_menu

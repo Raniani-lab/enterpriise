ae616936977a80bc1df602a3ccb05f005d8b1a8b
oco-odoo <oco@odoo.com>
2023-06-14 12:04:25 +0000

    [FIX] account_sepa: don't generate CdtrAgt node when no BIC on creditor account
    Following the SEPA guidelines (https://www.europeanpaymentscouncil.eu/sites/default/files/kb/file/2020-11/EPC132-08%20SCT%20C2PSP%20IG%202021%20V1.0.pdf , page 62 of the documents ; 63 of the pdf), this node should only be present if it contains a BIC. If no BIC is available, it shouldn't be there. Some banks were refusing our the files generated by Odoo because of that.
    
    OPW 3327389
    
    closes odoo/enterprise#42697
    
    X-original-commit: ad7e37ad3d34add70d9a8a6e9efe78a6ebe5ebc4
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>

--- account_sepa/models/account_journal.py
+++ account_sepa/models/account_journal.py
@@ -314,10 +314,12 @@ class AccountJournal(models.Model):
     def _skip_CdtrAgt(self, partner_bank, pain_version, local_instrument):
         return (
             self.env.context.get('skip_bic', False)
-            # Creditor Agent can be omitted with IBAN and QR-IBAN accounts
-            or pain_version == 'pain.001.001.03.ch.02'
-            and (self._is_qr_iban({'partner_bank_id' : partner_bank.id, 'journal_id' : self.id})
-                 or local_instrument == 'CH01')
+            or not partner_bank.bank_id.bic
+            or (
+                # Creditor Agent can be omitted with IBAN and QR-IBAN accounts
+                pain_version == 'pain.001.001.03.ch.02'
+                and (self._is_qr_iban({'partner_bank_id' : partner_bank.id, 'journal_id' : self.id}) or local_instrument == 'CH01')
+            )
         )
 
     def _get_CdtTrfTxInf(self, PmtInfId, payment, sct_generic, pain_version, local_instrument=None):

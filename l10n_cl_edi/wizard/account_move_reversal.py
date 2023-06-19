1b0dc953a971976cf72ac145c9af468e77fd80ed
Sohail Jaidi (soja) <soja@odoo.com>
2023-06-19 14:44:44 +0000

    [FIX] l10n_cl_edi: fixing chili credit note wizard after refactoring of generic credit note wizard
    Previous to this commit:
    After removal of the "refund_ method" field (which had 3 possible values), the cl credit note wizard is not working as intented anymore.
    
    Each option triggered specific values for "only text correction" field and "SII Reference Code".
    
    Now the values have to be triggered differently.
    
    After this commit:
    Here is how it will work now:
    Case 1: 
    
    If the the boolean "only text correction" is set as true by the user: 
    
    - Display the  Fields  "Original Text" and "New corrected text"  and set the field "SII reference code" to "2. Corrects Referenced Document Text". 
    
    Case 2:
    
    If the user keeps the boolean false, then the SII reference code should be set by default the "1. Cancels Referenced Document".
    
    closes odoo/enterprise#43327
    
    Task: 3374344
    X-original-commit: b459e81368cbbcbbfef4a4cbec8cc0a445c337f0
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- l10n_cl_edi/wizard/account_move_reversal.py
+++ l10n_cl_edi/wizard/account_move_reversal.py
@@ -9,23 +9,23 @@ class AccountMoveReversal(models.TransientModel):
     _inherit = "account.move.reversal"
 
     l10n_cl_edi_reference_doc_code = fields.Selection([
-        ('1', '1. Corrects Referenced Document Text'),
-        ('2', '2. Corrects Referenced Document Amount')
+        ('1', '1. Cancels Referenced Document'),
+        ('2', '2. Corrects Referenced Document Text'),
+        ('3', '3. Corrects Referenced Document Amount'),
     ], string='SII Reference Code')
     l10n_cl_is_text_correction = fields.Boolean('Only Text Correction')
     l10n_cl_original_text = fields.Char('Original Text', help='This is the text that is intended to be changed')
     l10n_cl_corrected_text = fields.Char('New Corrected Text', help='This is the text that should say')
-
-    @api.onchange('l10n_cl_is_text_correction')
-    def _set_l10n_cl_edi_reference_doc_code(self):
-        for record in self:
-            record.l10n_cl_edi_reference_doc_code = '1' if record.l10n_cl_is_text_correction else '2'
 
     def reverse_moves(self, is_modify=False):
         if self.company_id.country_id.code != "CL":
             return super().reverse_moves(is_modify)
         if self.move_type != 'entry' and not self.reason:
             raise UserError(_('You need to provide a reason for the refund. '))
+        if is_modify:
+            self.l10n_cl_edi_reference_doc_code = '1'
+        else:
+            self.l10n_cl_edi_reference_doc_code = '2' if self.l10n_cl_is_text_correction else '3'
         return super(AccountMoveReversal, self.with_context(
             default_l10n_cl_edi_reference_doc_code=self.l10n_cl_edi_reference_doc_code,
             default_l10n_cl_original_text=self.l10n_cl_original_text,

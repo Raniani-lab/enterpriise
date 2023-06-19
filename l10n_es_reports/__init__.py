55e118ea1a06725813997806ea166844af6291b1
Xavier ALT <xal@odoo.com>
2023-06-19 10:38:46 +0000

    [FIX] l10n_es_reports: only create boe sequences for companies in Spain
    In a multi-company database, once the Spanish localization is installed
    we are creating BOE's mod_347, mod_439 sequences for all companies.
    
      With this commit we now only create those sequence on-demand avoiding
    to have unnecessary sequences for companies that are not located in Spain
    
    closes odoo/enterprise#45541
    
    X-original-commit: 9ebbaabb4d45ccdc463b473aaa06df4ec74858cc
    Signed-off-by: John Laterre (jol) <jol@odoo.com>

--- l10n_es_reports/__init__.py
+++ l10n_es_reports/__init__.py
@@ -3,11 +3,3 @@
 
 from . import models
 from . import wizard
-
-
-def _setup_mod_sequences(env):
-    """ Creates a distinct sequence for each existing company,
-    for both mod 347 and mod 349 BOE export.
-    """
-    all_companies = env['res.company'].search([])
-    all_companies._create_mod_boe_sequences()

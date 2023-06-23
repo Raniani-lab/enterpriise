301b11fbf36496a9da3990bdbb3d542766cc5dc9
althaf shaik <alsh@odoo.com>
2023-06-23 06:03:18 +0000

    [FIX] pos_restaurant_preparation_display: open display for preparation display
    When user selects wrong product for wrong 'pos_config_id' in
    'Preparation Display' and ties to 'Open Display' traceback raises.
    
    To produce the issue:
    
    1) Install 'pos_restaurant_preparation_display'
    2) Open pos and click on 'new session' for Shop
    3) Now select any product and click payments then payment method and validate
    4) Now click 'backend' in three lines of right corner
    5) Now click Orders/Preparation Display in menuitems
    6) Create a new Preparation Display and select 'Point of sale' as Shop
    7) Select any product in product category except 'Chairs'.
    8) Save the record and back to 'Preparation Display' in menuitems/Orders
    9) Click on "Open Display" of the record which we created.
    
    Error: "TypeError: 'NoneType' object does not support item assignment" appears.
    
    On '_export_for_ui' method in 'pos_preparation_display_order', order_for_ui
    variable trying to assign an item .
    
    https://github.com/odoo/enterprise/blob/6abe363298db59bc2d695abeabbda44403f6959a/pos_restaurant_preparation_display/models/preparation_display_order.py#L7-L17
    
    In the above usecase because of in supercall 'order_for_ui' return None and
    when trying for item assignment it leads to the above traceback.
    
    Sentry-4265840428
    
    closes odoo/enterprise#44009
    
    X-original-commit: 0ef74413e95ab60e76b4c711c07b802340f35dcf
    Signed-off-by: David Monnom (moda) <moda@odoo.com>

--- pos_restaurant_preparation_display/models/preparation_display_order.py
+++ pos_restaurant_preparation_display/models/preparation_display_order.py
@@ -9,12 +9,13 @@ class PosPreparationDisplayOrder(models.Model):
     def _export_for_ui(self, preparation_display):
         order_for_ui = super()._export_for_ui(preparation_display)
 
-        order_for_ui['table'] = {
-            'id': self.pos_order_id.table_id.id,
-            'seats': self.pos_order_id.table_id.seats,
-            'name': self.pos_order_id.table_id.name,
-            'color': self.pos_order_id.table_id.color,
-        }
+        if order_for_ui:
+            order_for_ui['table'] = {
+                'id': self.pos_order_id.table_id.id,
+                'seats': self.pos_order_id.table_id.seats,
+                'name': self.pos_order_id.table_id.name,
+                'color': self.pos_order_id.table_id.color,
+            }
 
         return order_for_ui
 

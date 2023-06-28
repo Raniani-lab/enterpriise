dcc334c6c1c62f1585f38bb4be8d72037719521a
vlst <vlst@odoo.com>
2023-06-28 20:00:27 +0000

    [IMP] l10n_mx_edi_pos: Partner Editor Reactivity
    At the moment, the partner editor in pos does not use the owl
    reactivity system, using instead an onchange function on each
    input and manually keeping track of state.
    
    This approach is overcomplicated and leads to bugs.
    
    The necessity of this task first appeared because of one such bug,
    namely: the `state` input options not changing in order to reflect
    the selected `country`.
    
    Instead of finding a patch for this problem, we decided in this PR
    to replace the old logic, making use of `useState` and `t-model`.
    
    closes odoo/enterprise#43372
    
    Task: 3323874
    Related: odoo/odoo#126021
    Signed-off-by: Joseph Caburnay (jcb) <jcb@odoo.com>

--- l10n_mx_edi_pos/static/src/partner_details_edit/partner_details_edit.js
+++ l10n_mx_edi_pos/static/src/partner_details_edit/partner_details_edit.js
@@ -2,25 +2,25 @@
 
 import { PartnerDetailsEdit } from "@point_of_sale/app/screens/partner_list/partner_editor/partner_editor";
 import { patch } from "@web/core/utils/patch";
-const { useState } = owl;
 
 patch(PartnerDetailsEdit.prototype, "l10n_mx_edi_pos.PartnerDetailsEdit", {
-    setup(){
+    setup() {
         this._super(...arguments);
-        this.state = useState({
-            display_mx_fields: this.props.partner.country_code === 'MX',
-        });
+        this.changes.l10n_mx_edi_fiscal_regime = this.getPartnerMxFiscalRegime();
+        this.changes.l10n_mx_edi_no_tax_breakdown = this.props.partner.l10n_mx_edi_no_tax_breakdown;
     },
-    //@override
-    captureChange(event) {
-        const { company, l10n_mx_country_id } = this.pos;
-        if (company.country.code === 'MX' && event.target.name === 'country_id') {
-            this.state.display_mx_fields = l10n_mx_country_id.toString() === event.target.value;
-        }
-        this._super(event);
+    isMexicoSelected() {
+        return (
+            this.pos.countries.find((country) => country.code === "MX").id ===
+            parseInt(this.changes.country_id)
+        );
     },
-    captureChangeNoTaxBreakdown(event) {
-        event.target.toggleAttribute('checked');
-        this.changes[event.target.name] = event.target.checked;
-    }
+    getPartnerMxFiscalRegime() {
+        return (
+            this.props.partner.l10n_mx_edi_fiscal_regime &&
+            this.pos.l10n_mx_edi_fiscal_regime.find(
+                (regime) => regime.value === this.props.partner.l10n_mx_edi_fiscal_regime
+            )?.value
+        );
+    },
 });

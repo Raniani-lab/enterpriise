d92d9398fa70c7f0d0c207c3d668646c85510034
Raúl <raul.ruilova@trescloud.com>
2023-06-29 22:23:44 +0000

    [FIX] l10n_ec_edi: send 'round' context to force compute with 'round per line' for withholding type taxes
    In certain specific values, the calculation of the withholding taxes are affected by the rounding configuration 'round globally' creating a variation of 0.01 cents in the calculation of the field l10n_ec_withhold_tax_amount
    
    This results in a difference between the totals for withholding tax lines and the lines in the accounting entries in the withholding
    
    To avoid this problem, we ensure to send the 'round' variable as True by context in withholding taxes to force 'round per line'
    
    closes odoo/enterprise#44132
    
    X-original-commit: 322595a04a0e7bb765923d5e2159938f3e501bc5
    Signed-off-by: Josse Colpaert <jco@odoo.com>
    Signed-off-by: Laurent Smet (las) <las@odoo.com>

--- l10n_ec_edi/models/account_tax.py
+++ l10n_ec_edi/models/account_tax.py
@@ -28,3 +28,18 @@ class AccountTax(models.Model):
         string='Tax Support',
         help='Indicates if the purchase invoice supports tax credit or cost or expenses, conforming table 5 of ATS'
     )
+    def compute_all(self, price_unit, currency=None, quantity=1.0, product=None, partner=None, is_refund=False, handle_price_include=True, include_caba_tags=False, fixed_multiplicator=1):
+        """
+        Check that the tax is of the withholding type. Send the 'round' context as True, in this way we force the withholdings
+        to be computed with 'round per line', not by 'round globally'.
+        """
+        withholding_tax_groups = [
+            'withhold_vat_sale',
+            'withhold_vat_purchase',
+            'withhold_income_sale',
+            'withhold_income_purchase'
+        ]
+        is_withhold_tax = self.filtered(lambda x: x.tax_group_id.l10n_ec_type in withholding_tax_groups)
+        if is_withhold_tax:
+            return super(AccountTax, self.with_context(round=True)).compute_all(price_unit, currency, quantity, product, partner, is_refund=is_refund, handle_price_include=handle_price_include, include_caba_tags=include_caba_tags, fixed_multiplicator=fixed_multiplicator)
+        return super(AccountTax, self).compute_all(price_unit, currency, quantity, product, partner, is_refund=is_refund, handle_price_include=handle_price_include, include_caba_tags=include_caba_tags, fixed_multiplicator=fixed_multiplicator)

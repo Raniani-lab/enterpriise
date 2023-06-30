eccf5698697871646ddf0250c5dde1e8867bc9a5
oco-odoo <oco@odoo.com>
2023-06-30 11:56:26 +0000

    [FIX] account_reports_cash_basis: fix bad forward-port of test
    'cash_basis' option key becomes 'report_cash_basis' in 16.0. This was wrongly forward-ported from the original 15.0 fix in https://github.com/odoo/enterprise/commit/f2833e48feefe7be7d0c9f99de9f447bc3b4af97.
    
    Putting 'cash_basis' in the options had no effect. The test was only considering one invoice, with a payment made prior to it: in this case, the invoice has to appear at the invoice date ... which is exactly what will happen when not using the cash basis feature. So, in this case, the report was called without the cash basis, but returned the expected result. We make this more robust by creating a second invoice without any payment at the same date as the existing one: this invoice will not appear in the report when using the cash basis method, ensuring the result of the report will be different with or without the option.
    
    closes odoo/enterprise#43443
    
    Signed-off-by: William André (wan) <wan@odoo.com>

--- account_reports_cash_basis/tests/test_account_reports_cash_basis.py
+++ account_reports_cash_basis/tests/test_account_reports_cash_basis.py
@@ -170,11 +170,14 @@ class TestAccountReports(TestAccountReportsCommon):
             'payment_date': payment_date,
         })._create_payments()
 
+        # Make a second invoice without payment; it will allow being sure the cash basis options is well used when computing the report
+        # (as it will then not appear in its lines)
+        self.init_invoice('out_invoice', amounts=[100.0], partner=self.partner_a, invoice_date=invoice_date, post=True)
+
         # Check the impact in the reports: the invoice date should be the one the invoice appears at, since it greater than the payment's
         report = self.env.ref('account_reports.general_ledger_report')
 
-        options = self._generate_options(report, payment_date, payment_date)
-        options['cash_basis'] = True
+        options = self._generate_options(report, payment_date, payment_date, default_options={'report_cash_basis': True})
 
         self.assertLinesValues(
             # pylint: disable=C0326
@@ -190,8 +193,7 @@ class TestAccountReports(TestAccountReportsCommon):
             ],
         )
 
-        options = self._generate_options(report, invoice_date, invoice_date)
-        options['cash_basis'] = True
+        options = self._generate_options(report, invoice_date, invoice_date, default_options={'report_cash_basis': True})
 
         self.assertLinesValues(
             # pylint: disable=C0326

e58c8c566df4eba6e332aa678af47c0eaf439149
dise <dise@odoo.com>
2023-07-10 09:40:59 +0000

    [IMP] project_enterprise: add test for the task gantt view
    This commit will add tests to the project_enterprise module to check
    that the group expand method in project_task works accordingly to the
    specs.
    
    The test will add tests cases such as :
    - A user with a planned task within the last period (the group expand
      will match)
    - A user with a planned task in the future outside of the current period
      (the group expand will match)
    - A user with a closed planned task (the group expand won't match)
    - A user with a planned task in a different project (the group expand
      will match, but only in the other project)
    
    task-3414550
    
    closes odoo/enterprise#43757
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- project_enterprise/tests/__init__.py
+++ project_enterprise/tests/__init__.py
@@ -6,4 +6,5 @@ from . import test_smart_schedule
 from . import test_task_dependencies
 from . import test_task_flow
 from . import test_task_group_expand
+from . import test_task_gantt_view
 from . import test_ui

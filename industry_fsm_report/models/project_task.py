098bc787b66dfc3ffeb51fbbda9d2f741dae9d30
Xavier Bol (xbo) <xbo@odoo.com>
2023-07-11 17:16:01 +0200

    [MOV] industry_fsm_report: split file to get one model per file
    Part-of: odoo/enterprise#44632

--- industry_fsm_report/models/project_task.py
+++ industry_fsm_report/models/project_task.py
@@ -1,38 +1,17 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from ast import literal_eval
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
 
 
-class Project(models.Model):
-    _inherit = "project.project"
-
-    worksheet_template_id = fields.Many2one(
-        'worksheet.template', compute="_compute_worksheet_template_id", store=True, readonly=False,
-        string="Default Worksheet",
-        domain="[('res_model', '=', 'project.task'), '|', ('company_ids', '=', False), ('company_ids', 'in', company_id)]")
-
-    @api.depends('allow_worksheets')
-    def _compute_worksheet_template_id(self):
-        default_worksheet = self.env.ref('industry_fsm_report.fsm_worksheet_template', False)
-        for project in self:
-            if not project.worksheet_template_id:
-                if project.allow_worksheets and default_worksheet:
-                    project.worksheet_template_id = default_worksheet.id
-                else:
-                    project.worksheet_template_id = False
-
-
-class Task(models.Model):
+class ProjectTask(models.Model):
     _inherit = "project.task"
 
     worksheet_template_id = fields.Many2one(
         'worksheet.template', string="Worksheet Template",
         compute='_compute_worksheet_template_id', store=True, readonly=False, tracking=True,
         domain="[('res_model', '=', 'project.task'), '|', ('company_ids', '=', False), ('company_ids', 'in', company_id)]",
-        group_expand='_read_group_worksheet_template_id',
+        group_expand='_group_expand_worksheet_template_id',
         help="Create templates for each type of intervention you have and customize their content with your own custom fields.")
     worksheet_count = fields.Integer(compute='_compute_worksheet_count')
     worksheet_color = fields.Integer(related='worksheet_template_id.color')
@@ -46,7 +25,7 @@ class Task(models.Model):
 
     @api.depends('worksheet_count')
     def _compute_display_conditions_count(self):
-        super(Task, self)._compute_display_conditions_count()
+        super()._compute_display_conditions_count()
         for task in self:
             if task.allow_worksheets and task.worksheet_count:
                 task.display_satisfied_conditions_count += 1
@@ -72,10 +51,8 @@ class Task(models.Model):
         # Change worksheet when the project changes, not project.allow_worksheet (YTI To confirm)
         for task in self:
             if not task.worksheet_template_id:
-                if task.project_id.allow_worksheets:
-                    task.worksheet_template_id = task.project_id.worksheet_template_id.id
-                else:
-                    task.worksheet_template_id = False
+                task.worksheet_template_id = task.parent_id.worksheet_template_id.id\
+                    if task.parent_id else task.project_id.worksheet_template_id.id
 
     @api.depends('worksheet_template_id')
     def _compute_worksheet_count(self):
@@ -90,24 +67,28 @@ class Task(models.Model):
             record.worksheet_count = worksheet_count
 
     @api.model
-    def _read_group_worksheet_template_id(self, worksheets, domain, order):
-        if self._context.get('fsm_mode'):
-            dom_tuples = [dom for dom in domain if isinstance(dom, (list, tuple)) and len(dom) == 3]
-            if any(d[0] == 'worksheet_template_id' and d[1] in ('=', 'ilike') for d in dom_tuples):
-                filter_domain = self._expand_domain_m2o_groupby(dom_tuples, 'worksheet_template_id')
-                return self.env['worksheet.template'].search(filter_domain, order=order)
-        return worksheets
+    def _group_expand_worksheet_template_id(self, worksheets, domain, order):
+        start_date = self._context.get('gantt_start_date')
+        scale = self._context.get('gantt_scale')
+        if not (start_date and scale):
+            return worksheets
+        domain = self._expand_domain_dates(domain)
+        search_on_comodel = self._search_on_comodel(domain, "worksheet_template_id", "worksheet.template", order)
+        if search_on_comodel:
+            return search_on_comodel
+        else:
+            return self.search(domain).worksheet_template_id
 
     def action_fsm_worksheet(self):
         # We check that comment is not empty, otherwise it means that a `worksheet` has been generated
         # through the use of the mail template in fsm (obviously prior installing industry_fsm_report)
         if not self.worksheet_template_id or self.comment:
-            return super(Task, self).action_fsm_worksheet()
+            return super().action_fsm_worksheet()
         action = self.worksheet_template_id.action_id.sudo().read()[0]
         worksheet = self.env[self.worksheet_template_id.sudo().model_id.model].search([('x_project_task_id', '=', self.id)])
         context = literal_eval(action.get('context', '{}'))
         action.update({
-            'res_id': worksheet.id if worksheet else False,
+            'res_id': worksheet.id,
             'views': [(False, 'form')],
             'context': {
                 **context,
@@ -120,10 +101,3 @@ class Task(models.Model):
     def _is_fsm_report_available(self):
         self.ensure_one()
         return self.worksheet_count or self.timesheet_ids
-
-class ProjectTaskRecurrence(models.Model):
-    _inherit = 'project.task.recurrence'
-
-    @api.model
-    def _get_recurring_fields_to_copy(self):
-        return ['worksheet_template_id'] + super()._get_recurring_fields_to_copy()

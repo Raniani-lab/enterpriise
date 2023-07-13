40b14700e5ce38becbc5722049e5ba8f931a60c1
Adrien Minne (adrm) <adrm@odoo.com>
2023-07-13 08:57:50 +0000

    [IMP] spreadsheet: detect mismatch in user/spreadsheet locale
    This commit add a small indicator in the spreadsheet's control panel
    to indicate if the user's locale is different from the spreadsheet's.
    
    closes odoo/enterprise#44665
    
    Task: 3389491
    X-original-commit: f406637585ab5be80fd991ad0e34d7913af71ffc
    Related: odoo/odoo#129857
    Signed-off-by: Rémi Rahir (rar) <rar@odoo.com>

--- spreadsheet_edition/static/src/bundle/actions/control_panel/spreadsheet_control_panel.js
+++ spreadsheet_edition/static/src/bundle/actions/control_panel/spreadsheet_control_panel.js
@@ -5,8 +5,10 @@ import { useService } from "@web/core/utils/hooks";
 import { SpreadsheetName } from "./spreadsheet_name";
 import { SpreadsheetShareButton } from "@spreadsheet/components/share_button/share_button";
 import { session } from "@web/session";
-
-import { Component, onWillUnmount, useState } from "@odoo/owl";
+import { _lt } from "@web/core/l10n/translation";
+import { sprintf } from "@web/core/utils/strings";
+import { Component, onWillUnmount, useState, onWillStart } from "@odoo/owl";
+import { helpers } from "@odoo/o-spreadsheet";
 
 /**
  * @typedef {import("@spreadsheet_edition/bundle/actions/spreadsheet_component").User} User
@@ -16,21 +18,32 @@ export class SpreadsheetControlPanel extends Component {
     setup() {
         this.controlPanelDisplay = {};
         this.actionService = useService("action");
+        this.userService = useService("user");
+        this.ormService = useService("orm");
         this.breadcrumbs = useState(this.env.config.breadcrumbs);
         this.collaborative = useState({
             isSynced: true,
             connectedUsers: [{ name: session.username, id: session.id }],
+        });
+        this.locale = useState({
+            mismatchedLocaleTitle: "",
         });
         const model = this.props.model;
         if (model) {
             model.on("update", this, this.syncState.bind(this));
             onWillUnmount(() => model.off("update", this));
         }
+        onWillStart(async () => {
+            this.userLocale = await this.ormService.call("res.lang", "get_user_spreadsheet_locale");
+        });
     }
 
     syncState() {
         this.collaborative.isSynced = this.props.model.getters.isFullySynchronized();
         this.collaborative.connectedUsers = this.getConnectedUsers();
+        if (this.userLocale) {
+            this.locale.mismatchedLocaleTitle = this.mismatchedLocaleTitle;
+        }
     }
 
     /**
@@ -69,6 +82,40 @@ export class SpreadsheetControlPanel extends Component {
             }
         }
         return connectedUsers;
+    }
+
+    get mismatchedLocaleTitle() {
+        const spreadsheetLocale = this.props.model.getters.getLocale();
+
+        const title = sprintf(
+            _lt("Difference between user locale (%s) and spreadsheet locale (%s):"),
+            this.userLocale.code,
+            spreadsheetLocale.code
+        );
+        const comparison = this.getLocalesComparison(spreadsheetLocale, this.userLocale);
+
+        return comparison ? title + "\n" + comparison : "";
+    }
+
+    getLocalesComparison(spreadsheetLocale, userLocale) {
+        const differences = [];
+        if (spreadsheetLocale.dateFormat !== userLocale.dateFormat) {
+            differences.push(sprintf(_lt("- current Date Format: %s"), userLocale.dateFormat));
+        }
+
+        if (
+            spreadsheetLocale.thousandsSeparator !== userLocale.thousandsSeparator ||
+            spreadsheetLocale.decimalSeparator !== userLocale.decimalSeparator
+        ) {
+            differences.push(
+                sprintf(
+                    _lt("- current Number Format: %s"),
+                    helpers.formatValue(1234567.89, { format: "#,##0.00", locale: userLocale })
+                )
+            );
+        }
+
+        return differences.join("\n");
     }
 }
 

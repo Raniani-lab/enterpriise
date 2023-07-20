245920761fa3f7ee81ab66f54e72aa9aa9bc3fad
Xavier Bol (xbo) <xbo@odoo.com>
2023-07-20 20:51:03 +0000

    [FIX] project_enterprise: update tour according to modules installed
    Before this commit, the `project_enterprise_tour` has a step to
    check allow billable during the project creation, this step can only
    be done if `sale_project` module is installed but since
    `project_enterprise` does not depend on that module the tour will
    fail if only project_enterprise is installed.
    
    This commit moves the project_enterprise_tour in project module
    and rename the tour by `project_test_tour`. It also updates the
    tour to be able to go to gantt and map views when `project_enterprise`
    module is installed.
    
    X-original-commit: 9a0bc2c96ded06e9339f17aa4e638d8a0e5b7f2a
    Part-of: odoo/enterprise#44566

--- project_enterprise/static/tests/tours/task_gantt_tour_test.js
+++ project_enterprise/static/tests/tours/task_gantt_tour_test.js
@@ -1,7 +1,11 @@
 /** @odoo-module **/
 
+/**
+ * Add custom steps to go to map and gantt view in Project app
+ */
 import { registry } from "@web/core/registry";
-import { stepUtils } from "@web_tour/tour_service/tour_utils";
+import "@project/../tests/tours/project_tour";
+import { patch } from "@web/core/utils/patch";
 
 function openProjectUpdateAndReturnToTasks(view, viewClass) {
     return [{
@@ -26,105 +30,40 @@ function openProjectUpdateAndReturnToTasks(view, viewClass) {
     ];
 }
 
-registry.category("web_tour.tours").add('project_enterprise_tour', {
-    test: true,
-    url: '/web',
-    steps: () => [
-    stepUtils.showAppsMenuItem(), {
-        trigger: '.o_app[data-menu-xmlid="project.menu_main_pm"]',
-    }, {
-        trigger: '.o-kanban-button-new',
-        extra_trigger: '.o_project_kanban',
-        width: 200,
-    }, {
-        trigger: '.o_project_name input',
-        run: 'text New Project'
-    }, {
-        trigger: 'div[name="allow_billable"] input',
-        run: 'click',
-    }, {
-        trigger: '.o_open_tasks',
-        run: function (actions) {
-            actions.auto('.modal:visible .btn.btn-primary');
-        },
-    }, {
-        trigger: ".o_kanban_project_tasks .o_column_quick_create .input-group",
-        run: function (actions) {
-            actions.text("New", this.$anchor.find("input"));
-        },
-    }, {
-        trigger: ".o_kanban_project_tasks .o_column_quick_create .o_kanban_add",
-        auto: true,
-    }, {
-        trigger: ".o_kanban_project_tasks .o_column_quick_create .input-group",
-        extra_trigger: '.o_kanban_group',
-        run: function (actions) {
-            actions.text("Done", this.$anchor.find("input"));
-        },
-    }, {
-        trigger: ".o_kanban_project_tasks .o_column_quick_create .o_kanban_add",
-        auto: true,
-    }, {
-        trigger: '.o-kanban-button-new',
-        extra_trigger: '.o_kanban_group:eq(0)'
-    }, {
-        trigger: '.o_kanban_quick_create div.o_field_char[name=display_name] input',
-        extra_trigger: '.o_kanban_project_tasks',
-        run: 'text New task'
-    }, {
-        trigger: '.o_kanban_quick_create .o_kanban_add',
-        extra_trigger: '.o_kanban_project_tasks'
-    }, {
-        trigger: '.o_switch_view.o_gantt',
-        content: 'Open Gantt View',
-    }, {
-        trigger: '.o_gantt_button_add',
-        content: 'Add a task in gantt',
-    }, {
-        trigger: '.o_field_text[name="name"] textarea',
-        content: 'Set task name',
-        run: 'text New task',
-    }, {
-        trigger: 'div[name="user_ids"].o_field_many2many_tags_avatar input',
-        content: 'Assign the task to you',
-        run: 'click',
-    }, {
-        trigger: 'ul.ui-autocomplete a .o_avatar_many2x_autocomplete',
-        content: 'Assign the task to you',
-    }, {
-        trigger: 'a[name="sub_tasks_page"]',
-        content: 'Open sub-tasks notebook section',
-        run: 'click',
-    }, {
-        trigger: '.o_field_subtasks_one2many .o_list_renderer a[role="button"]',
-        content: 'Add a subtask',
-        run: 'click',
-    }, {
-        trigger: '.o_field_subtasks_one2many div[name="name"] input',
-        content: 'Set subtask name',
-        run: 'text new subtask'
-    }, {
-        trigger: 'button[special="save"]',
-        extra_trigger: '.o_field_many2many_tags_avatar .o_m2m_avatar',
-        content: 'Save task',
-    }, {
-        trigger: ".o_gantt_progress_bar",
-        content: "See user progress bar",
-        run: function () {
-            const $progressbar = $(".o_gantt_progress_bar:eq(0)");
-            if ($progressbar.length) {
-                if ($progressbar.find("span:eq(0)")[0].style.width === '') {
-                    console.error("Progress bar should be displayed");
+patch(registry.category("web_tour.tours").get("project_test_tour"), "patch_project_enterprise_tour", {
+    steps() {
+        const originalSteps = this._super();
+        const taskCreationStepIndex = originalSteps.findIndex((step) => step.id === "quick_create_tasks");
+
+        originalSteps.splice(taskCreationStepIndex + 1, 0, {
+            trigger: '.o_switch_view.o_gantt',
+            content: 'Open Gantt View',
+        }, {
+            trigger: '.o_gantt_button_add',
+            content: 'Add a task in gantt',
+        });
+
+        originalSteps.splice(originalSteps.length, 0, {
+            trigger: ".o_gantt_progress_bar",
+            content: "See user progress bar",
+            run: function () {
+                const $progressbar = $(".o_gantt_progress_bar:eq(0)");
+                if ($progressbar.length) {
+                    if ($progressbar.find("span:eq(0)")[0].style.width === '') {
+                        console.error("Progress bar should be displayed");
+                    }
+                    if (!$progressbar[0].classList.contains("o_gantt_group_danger")) {
+                        console.error("Progress bar should be displayed in danger");
+                    }
+                } else {
+                    console.error("Not able to select progressbar");
                 }
-                if (!$progressbar[0].classList.contains("o_gantt_group_danger")) {
-                    console.error("Progress bar should be displayed in danger");
-                }
-            } else {
-                console.error("Not able to select progressbar");
             }
-        }
-    }, ...openProjectUpdateAndReturnToTasks("Gantt", "o_gantt_view"), {
-        trigger: '.o_switch_view.o_map',
-        content: 'Open Map View',
-    }, ...openProjectUpdateAndReturnToTasks("Map", "o_map_view"),
-]});
+        }, ...openProjectUpdateAndReturnToTasks("Gantt", "o_gantt_view"), {
+            trigger: '.o_switch_view.o_map',
+            content: 'Open Map View',
+        }, ...openProjectUpdateAndReturnToTasks("Map", "o_map_view"));
+
+        return originalSteps;
+    }
+});

f5e4746d49b7a8a2e67ebabe76c86c978d77e1b7
Adrien Widart (awt) <awt@odoo.com>
2023-07-24 12:01:51 +0000

    [FIX] {sale_purchase,account}_inter_company_rules: company's child
    The sync SO-PO or INV-BILL does not work with company's child
    
    To reproduce the issue:
    1. Have two companies C1, C2
    2. On C1 side, Settings:
       - Inter-Company Transactions: Synchronize Sales and Purchase Order
    3. Switch to C2
    4. Create a partner P:
       - Individual
       - Company name: C1
    5. Create and confirm a SO:
       - Customer: P
    6. Switch to C1
    7. Open purchase orders
    
    Error: There is not any PO for the confirmed SO
    
    Same issue will occur with invoices/bills.
    
    OPW-3319118
    
    closes odoo/enterprise#45235
    
    X-original-commit: dca0775deaa70378b38909d046261d7e397202e2
    Signed-off-by: Arnold Moyaux (arm) <arm@odoo.com>
    Signed-off-by: Adrien Widart (awt) <awt@odoo.com>

--- account_inter_company_rules/models/res_company.py
+++ account_inter_company_rules/models/res_company.py
@@ -25,5 +25,5 @@ class res_company(models.Model):
 
     @api.model
     def _find_company_from_partner(self, partner_id):
-        company = self.search([('partner_id', '=', partner_id)], limit=1)
+        company = self.sudo().search([('partner_id', 'parent_of', partner_id)], limit=1)
         return company or False

267afb5941a15d6c82b3e93e42076150a7e53e76
Adrien Schoffeniels <adsc@odoo.com>
2023-07-25 12:46:01 +0000

    [FIX] (website_)knowledge: fix various layout issues
    Purpose:
    --------
    This commit includes various small layout fixes in knowledge:
    - Add white space under the workspace section of the sidebar if this
    section is empty (it was only done for portal users)
    - Fix the icon layout for articles in public view (it was hiding the
    article's first lines)
    - Count published articles on which the user has been invited in the
    knowledge articles entry of the user's "/my" page (it only counted
    articles accessible by the user that were not published)
    
    Task-3433622
    
    closes odoo/enterprise#45346
    
    X-original-commit: 7a8fde88122901bb1eb8af53100605a9b0d6b54f
    Signed-off-by: Warnon Aurélien (awa) <awa@odoo.com>

--- website_knowledge/controllers/portal.py
+++ website_knowledge/controllers/portal.py
@@ -7,4 +7,9 @@ from odoo.addons.knowledge.controllers.portal import KnowledgePortal
 class KnowledgePortalWebsite(KnowledgePortal):
 
     def _prepare_knowledge_article_domain(self):
-        return [('website_published', '=', False)]
+        """For portal users, we want to count the articles to which the user
+        has direct access to (articles to which the user has been invited to).
+        That excludes published articles, except the ones for which there is a
+        member configuration for this user.
+        """
+        return [('user_has_access', '=', True)]

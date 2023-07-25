381818979ccb3ef6d55bfd744d6d510ca4b68626
Rahul Reddiar <rare@odoo.com>
2023-07-25 07:08:06 +0000

    [FIX] {quality_}mrp_workorder: fix user error for quality control type
    Before this commit
    ==================
    If any of the work-order has an operation with undone quality checks
    the user error won't show up when triggering 'Done' inside WO list view.
    
    Steps to produce
    ================
    - Create a bom product having work-order that includes an operation having
      quality control with the type 'Print Label'.
    - Create a MO and confirm it.
    - Inside the WO list view select the WO and trigger 'Done'.
      The user error will not show up even though the quality check is unfinished.
    
    Issue
    =====
    The error only shows up if the quality_mrp_workorder module gets installed.
    
    With this commit
    ================
    User error will occur if the WO with an operation having unfinished quality
    check when triggering 'Done'.
    
    closes odoo/enterprise#44705
    
    Task: 3374990
    X-original-commit: 549bdbe0f31876d8816bcda6b35af7d02dd27c13
    Signed-off-by: Tiffany Chang <tic@odoo.com>

--- mrp_workorder/models/mrp_workorder.py
+++ mrp_workorder/models/mrp_workorder.py
@@ -293,6 +293,18 @@ class MrpProductionWorkcenterLine(models.Model):
 
         return res
 
+    def button_finish(self):
+        """ When using the Done button of the simplified view, validate directly some types of quality checks
+        """
+        for check in self.check_ids:
+            if check.quality_state in ['pass', 'fail']:
+                continue
+            if check.test_type in ['register_consumed_materials', 'register_byproducts', 'instructions']:
+                check.quality_state = 'pass'
+            else:
+                raise UserError(_("You need to complete Quality Checks using the Tablet View before marking Work Order as Done."))
+        return super().button_finish()
+
     def action_propose_change(self, change_type, check_id):
         change_type_to_title = {'update_step': 'Update Instructions', 'remove_step': 'Remove Step', 'set_picture': 'Change Picture'}
         return {

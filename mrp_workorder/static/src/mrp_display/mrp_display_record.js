c27f996540144d5bf8162ef64137ef8034937218
svs-odoo <svs@odoo.com>
2023-07-27 09:43:12 +0000

    [FIX] mrp_workorder: WO validation
    In the Shop Floor app, the validation of a MO or a WO usually take same
    time to let the user cancels the validation if they want.
    But while actually validating a MO, some wizard can happen, so to avoid
    the user waiting the validation delay and then get a wizard, we check if
    a wizard will popup by calling `pre_button_mark_done` before to add the
    record into the validation stack.
    
    That said, before this commit, we don't do that while validating a WO.
    Since validate the last WO will also validate its MO, we call
    `pre_button_mark_done` in this case.
    
    task-3432610
    
    Part-of: odoo/enterprise#45367

--- mrp_workorder/static/src/mrp_display/mrp_display_record.js
+++ mrp_workorder/static/src/mrp_display/mrp_display_record.js
@@ -386,20 +386,7 @@ export class MrpDisplayRecord extends Component {
 
     async validate() {
         const { resModel, resId } = this.props.record;
-        if (resModel === "mrp.production") {
-            const params = {};
-            let methodName = "pre_button_mark_done";
-            if (this.trackingMode === "mass_produce") {
-                methodName = "action_serial_mass_produce_wizard";
-                params.mark_as_done = true;
-            }
-            const action = await this.model.orm.call(resModel, methodName, [resId], params);
-            // If there is a wizard while trying to mark as done the production, confirming the
-            // wizard will straight mark the MO as done without the confirmation delay.
-            if (action && typeof action === "object") {
-                return this._doAction(action);
-            }
-        } else if (resModel === "mrp.workorder") {
+        if (resModel === "mrp.workorder") {
             if (this.record.state === "ready") {
                 this.props.record.update({ qty_producing: this.record.qty_production });
                 await this.props.record.save();
@@ -407,19 +394,39 @@ export class MrpDisplayRecord extends Component {
             await this.model.orm.call(resModel, "end_all", [resId]);
             await this.reload();
             await this.props.updateEmployees();
-            if (this.record.is_last_unfinished_wo){
+            if (this.record.is_last_unfinished_wo) {
                 const params = {};
                 let methodName = "pre_button_mark_done";
                 if (this.trackingMode === "mass_produce") {
                     methodName = "action_serial_mass_produce_wizard";
                     params.mark_as_done = true;
                 }
-                const action = await this.model.orm.call("mrp.production", methodName, [this.props.production.resId], params);
+                const action = await this.model.orm.call(
+                    "mrp.production",
+                    methodName,
+                    [this.props.production.resId],
+                    params
+                );
                 // If there is a wizard while trying to mark as done the production, confirming the
                 // wizard will straight mark the MO as done without the confirmation delay.
                 if (action && typeof action === "object") {
                     return this._doAction(action);
                 }
+            }
+        }
+        if (resModel === "mrp.production" || this.record.is_last_unfinished_wo) {
+            const args = [this.props.production.resId];
+            const params = {};
+            let methodName = "pre_button_mark_done";
+            if (this.trackingMode === "mass_produce") {
+                methodName = "action_serial_mass_produce_wizard";
+                params.mark_as_done = true;
+            }
+            const action = await this.model.orm.call("mrp.production", methodName, args, params);
+            // If there is a wizard while trying to mark as done the production, confirming the
+            // wizard will straight mark the MO as done without the confirmation delay.
+            if (action && typeof action === "object") {
+                return this._doAction(action);
             }
         }
         // Makes the validation taking a little amount of time (see o_fadeout_animation CSS class).

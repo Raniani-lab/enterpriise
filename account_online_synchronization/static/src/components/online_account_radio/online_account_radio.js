d25d457a76884cc52a685969eae18b44f18ee191
Florian(flg) <flg@odoo.com>
2023-07-27 14:39:49 +0000

    [FIX] account_online_synchronization: Fixing online_account_radio widget.
    Since 8723f020, a new way to define items in radio_field.js is used.
    The problem is that items are now defined asynchronous and it causes
    that our widget tries to load balances data using these items when
    we still don't have. It causes a traceback.
    
    This fix load the balances data when the component is mounted and there
    is 2 helpers functions that verifies if we have the data or not before
    displaying it.
    
    A test has been added to avoid this problem next time.
    
    closes odoo/enterprise#44749
    
    Task-id: none
    Signed-off-by: Brice Bartoletti (bib) <bib@odoo.com>

--- account_online_synchronization/static/src/components/online_account_radio/online_account_radio.js
+++ account_online_synchronization/static/src/components/online_account_radio/online_account_radio.js
@@ -1,10 +1,10 @@
 /** @odoo-module **/
 
+import { onMounted, useState } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { RadioField, radioField } from "@web/views/fields/radio/radio_field";
-
-const { onWillStart, onMounted, useState } = owl;
 import { useService } from '@web/core/utils/hooks';
+
 
 class OnlineAccountRadio extends RadioField {
     static template = "account_online_synchronization.OnlineAccountRadio";
@@ -12,18 +12,26 @@ class OnlineAccountRadio extends RadioField {
         super.setup();
         this.orm = useService("orm");
         this.state = useState({balances: {}});
-        onWillStart(async () => {
+
+        onMounted(async () => {
             this.state.balances = await this.loadData();
-        });
-        // Make sure the first option is selected by default.
-        onMounted(() => {
+            // Make sure the first option is selected by default.
             this.onChange(this.items[0]);
         });
     }
 
     async loadData() {
         const ids = this.items.map(i => i[0]);
-        return await this.orm.call('account.online.account', 'get_formatted_balances', [ids]);
+        return await this.orm.call("account.online.account", "get_formatted_balances", [ids]);
+    }
+
+    getBalanceName(itemID) {
+        return this.state.balances?.[itemID]?.[0] ?? "Loading ...";
+    }
+
+    isNegativeAmount(itemID) {
+        // In case of the value is undefined, it will return false as intended.
+        return this.state.balances?.[itemID]?.[1] < 0;
     }
 }
 
---
+++ account_online_synchronization/static/tests/online_account_radio_test.js
@@ -0,0 +1,77 @@
+/** @odoo-module **/
+"use strict";
+
+import { start, startServer } from "@mail/../tests/helpers/test_utils";
+
+QUnit.module("Views", {}, function (hooks) {
+    QUnit.module("AccountOnlineSynchronizationAccountRadio");
+
+    QUnit.test("can be rendered", async (assert) => {
+        const pyEnv = await startServer();
+        const onlineLink = pyEnv["account.online.link"].create([{
+            "state": "connected",
+            "name": "Fake Bank",
+        }]);
+        pyEnv["account.online.account"].create([
+            {
+                "name": "account_1",
+                "online_identifier": "abcd",
+                "balance": 10.0,
+                "account_number": "account_number_1",
+                "account_online_link_id": onlineLink,
+            },
+            {
+                "name": "account_2",
+                "online_identifier": "efgh",
+                "balance": 20.0,
+                "account_number": "account_number_2",
+                "account_online_link_id": onlineLink,
+            },
+        ]);
+        const bankSelection = pyEnv["account.bank.selection"].create([{
+            "account_online_link_id": onlineLink,
+        }]);
+
+        const views = {
+            "account.bank.selection,false,form":
+                `<form>
+                    <div>
+                        <field name="account_online_account_ids" invisible="1"/>
+                        <field name="selected_account" widget="online_account_radio" nolabel="1"/>
+                    </div>
+                </form>`,
+        };
+        const { click, openView } = await start({
+            serverData: { views },
+            mockRPC: function (route, args) {
+                if (route === "/web/dataset/call_kw/account.online.account/get_formatted_balances") {
+                    return {
+                        1: ["$ 10.0", 10.0],
+                        2: ["$ 20.0", 20.0],
+                    };
+                }
+            },
+        });
+
+        await openView({
+            res_id: bankSelection,
+            res_model: "account.bank.selection",
+            views: [[false, "form"]],
+        });
+
+        assert.containsN(document.body, ".o_radio_input", 2, "View contains 2 radio buttons");
+
+        assert.equal(document.querySelectorAll(".o_radio_item p")[0].innerText, "$ 10.0");
+        assert.equal(document.querySelectorAll(".o_radio_item label")[0].innerText, "account_1");
+        assert.equal(document.querySelectorAll(".o_radio_item p")[1].innerText, "$ 20.0");
+        assert.equal(document.querySelectorAll(".o_radio_item label")[1].innerText, "account_2");
+
+        assert.ok(document.querySelectorAll(".o_radio_input")[0].checked, "First radio is already selected");
+        assert.notOk(document.querySelectorAll(".o_radio_input")[1].checked, "Second radio is not selected");
+
+        await click('.o_radio_input:last()'); // Select the other radio button
+
+        assert.notOk(document.querySelectorAll(".o_radio_input")[0].checked, "First radio is no longer selected");
+        assert.ok(document.querySelectorAll(".o_radio_input")[1].checked, "Second radio is now selected");
+    });
+});

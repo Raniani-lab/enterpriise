Raniani-lab/enterpriise

commit 5d6cb27f5098d960b4aa80b536e39366befb5b9e

[IMP] account_accountant: Bank reco: improved exchange diff audit
When reconciling through the bank reco widget where an exchange difference
was necessary, we used to add an aml with that exchange difference to the
statement's related move.
In consequence, the exchange difference wasn't linked to a different move
like we do elsewhere in Odoo, the exchange difference aml was also not
linked to the matching number which made the audit/tracking harder.

task-3415644

closes odoo/enterprise#44274

Signed-off-by: Laurent Smet (las) <las@odoo.com>

account_accountant/models/bank_rec_widget.py
@@ -227,7 +227,22 @@ class BankRecWidget(models.Model):
                     # The statement line is already reconciled. We just need to preview the existing amls.
                     _liquidity_lines, _suspense_lines, other_lines = wizard.st_line_id._seek_for_lines()
                     for aml in other_lines:
-                        line_ids_commands.append(Command.create(wizard._lines_widget_prepare_aml_line(aml)))
+                        if wizard.state == 'reconciled':
+                            exchange_diff_aml = (aml.matched_debit_ids + aml.matched_debit_ids)\
+                                .exchange_move_id.line_ids.filtered(lambda l: l.account_id != aml.account_id)
+                            if exchange_diff_aml:
+                                line_ids_commands.append(
+                                    Command.create(wizard._lines_widget_prepare_aml_line(
+                                        aml,  # Create the aml line with un-squashed amounts (aml - exchange diff)
+                                        balance=aml.balance - exchange_diff_aml.balance,
+                                        amount_currency=aml.amount_currency - exchange_diff_aml.amount_currency
+                                    ))
+                                )
+                                line_ids_commands.append(
+                                    Command.create(wizard._lines_widget_prepare_aml_line(exchange_diff_aml))
+                                )
+                        else:
+                            line_ids_commands.append(Command.create(wizard._lines_widget_prepare_aml_line(aml)))
                 wizard.line_ids = line_ids_commands
 
                 wizard._lines_widget_add_auto_balance_line()
@@ -1623,16 +1638,35 @@ class BankRecWidget(models.Model):
         # Prepare the lines to be created.
         to_reconcile = []
         line_ids_create_command_list = []
+        aml_to_exchange_diff_vals = {}
 
         for i, line in enumerate(self.line_ids):
+            if line.flag == 'exchange_diff':
+                continue
+
+            amount_currency = line.amount_currency
+            balance = line.balance
+            if line.flag == 'new_aml':
+                to_reconcile.append((i, line.source_aml_id.id))
+                exchange_diff = self.line_ids \
+                    .filtered(lambda x: x.flag == 'exchange_diff' and x.source_aml_id == line.source_aml_id)
+                if exchange_diff:
+                    aml_to_exchange_diff_vals[i] = {
+                        'amount_residual': exchange_diff.balance,
+                        'amount_residual_currency': exchange_diff.amount_currency
+                    }
+                    # Squash amounts of exchange diff into corresponding new_aml
+                    amount_currency += exchange_diff.amount_currency
+                    balance += exchange_diff.balance
+
             line_ids_create_command_list.append(Command.create({
                 'name': line.name,
                 'sequence': i,
                 'account_id': line.account_id.id,
                 'partner_id': partner_id_to_set if line.flag in ('liquidity', 'auto_balance') else line.partner_id.id,
                 'currency_id': line.currency_id.id,
-                'amount_currency': line.amount_currency,
-                'balance': line.debit - line.credit,
+                'amount_currency': amount_currency,
+                'balance': balance,
                 'reconcile_model_id': line.reconcile_model_id.id,
                 'analytic_distribution': line.analytic_distribution,
                 'tax_repartition_line_id': line.tax_repartition_line_id.id,
@@ -1641,14 +1675,12 @@ class BankRecWidget(models.Model):
                 'group_tax_id': line.group_tax_id.id,
             }))
 
-            if line.flag == 'new_aml':
-                to_reconcile.append((i, line.source_aml_id.id))
-
         self.js_action_reconcile_st_line(
             self.st_line_id.id,
             {
                 'command_list': line_ids_create_command_list,
                 'to_reconcile': to_reconcile,
+                'exchange_diff': aml_to_exchange_diff_vals,
                 'partner_id': partner_id_to_set,
             },
         )
@@ -1759,8 +1791,6 @@ class BankRecWidget(models.Model):
     @api.model
     def js_action_reconcile_st_line(self, st_line_id, params):
         st_line = self.env['account.bank.statement.line'].browse(st_line_id)
-
-        # Remove the existing lines.
         move = st_line.move_id
 
         # Update the move.
@@ -1777,7 +1807,16 @@ class BankRecWidget(models.Model):
         # Perform the reconciliation.
         for index, counterpart_aml_id in params['to_reconcile']:
             counterpart_aml = self.env['account.move.line'].browse(counterpart_aml_id)
-            (move_ctx.line_ids.filtered(lambda x: x.sequence == index) + counterpart_aml).reconcile()
+            line_aml = move_ctx.line_ids.filtered(lambda x: x.sequence == index)
+            exchange_diff_move = False
+            # Create exchange difference move
+            exchange_amounts = params['exchange_diff'].get(index)
+            if exchange_amounts:
+                exch_diff_related_aml = line_aml if exchange_amounts['amount_residual'] * line_aml.amount_residual > 0 else counterpart_aml
+                exchange_vals = exch_diff_related_aml._prepare_exchange_difference_move_vals([exchange_amounts])
+                exchange_diff_move = self.env['account.move.line']._create_exchange_difference_move(exchange_vals)
+            rec_res = (line_aml + counterpart_aml).with_context(no_exchange_difference=True).reconcile()
+            rec_res['partials'].exchange_move_id = exchange_diff_move
 
         # Fill missing partner.
         st_line.with_context(skip_account_move_synchronization=True).partner_id = params['partner_id']

commit c34d95d60b5aeb3cc41fedb894b4ccaadfbe724e

[FIX] account_account: fix bank rec widget displayed lines
Fix displayed lines in bank reconciliation widget that we broke in
commit https://github.com/odoo/enterprise/commit/5d6cb27f5098d960b4aa80b536e39366befb5b9e.

closes odoo/enterprise#45169

Task-id: 3452105
Signed-off-by: Laurent Smet (las) <las@odoo.com>

account_accountant/models/bank_rec_widget.py
@@ -227,20 +227,19 @@ class BankRecWidget(models.Model):
                     # The statement line is already reconciled. We just need to preview the existing amls.
                     _liquidity_lines, _suspense_lines, other_lines = wizard.st_line_id._seek_for_lines()
                     for aml in other_lines:
-                        if wizard.state == 'reconciled':
-                            exchange_diff_aml = (aml.matched_debit_ids + aml.matched_debit_ids)\
-                                .exchange_move_id.line_ids.filtered(lambda l: l.account_id != aml.account_id)
-                            if exchange_diff_aml:
-                                line_ids_commands.append(
-                                    Command.create(wizard._lines_widget_prepare_aml_line(
-                                        aml,  # Create the aml line with un-squashed amounts (aml - exchange diff)
-                                        balance=aml.balance - exchange_diff_aml.balance,
-                                        amount_currency=aml.amount_currency - exchange_diff_aml.amount_currency
-                                    ))
-                                )
-                                line_ids_commands.append(
-                                    Command.create(wizard._lines_widget_prepare_aml_line(exchange_diff_aml))
-                                )
+                        exchange_diff_aml = (aml.matched_debit_ids + aml.matched_debit_ids)\
+                            .exchange_move_id.line_ids.filtered(lambda l: l.account_id != aml.account_id)
+                        if wizard.state == 'reconciled' and exchange_diff_aml:
+                            line_ids_commands.append(
+                                Command.create(wizard._lines_widget_prepare_aml_line(
+                                    aml,  # Create the aml line with un-squashed amounts (aml - exchange diff)
+                                    balance=aml.balance - exchange_diff_aml.balance,
+                                    amount_currency=aml.amount_currency - exchange_diff_aml.amount_currency
+                                ))
+                            )
+                            line_ids_commands.append(
+                                Command.create(wizard._lines_widget_prepare_aml_line(exchange_diff_aml))
+                            )
                         else:
                             line_ids_commands.append(Command.create(wizard._lines_widget_prepare_aml_line(aml)))
                 wizard.line_ids = line_ids_commands

account_accountant/tests/test_bank_rec_widget.py
@@ -147,6 +147,41 @@ class TestBankRecWidget(TestBankRecWidgetCommon):
         # Should not trigger the error.
         self.env['res.partner.bank'].flush_model()
 
+    def test_validation_base_case(self):
+        st_line = self._create_st_line(
+            1000.0,
+            date='2017-01-01',
+        )
+
+        wizard = self.env['bank.rec.widget'].with_context(default_st_line_id=st_line.id).new({})
+        line = wizard.line_ids.filtered(lambda x: x.flag == 'auto_balance')
+        form = WizardForm(wizard)
+        form.todo_command = f'mount_line_in_edit,{line.index}'
+        form.form_account_id = self.account_revenue1
+        wizard = form.save()
+        self.assertRecordValues(wizard.line_ids, [
+            # pylint: disable=C0326
+            {'flag': 'liquidity',     'amount_currency': 1000.0,  'currency_id': self.company_data['currency'].id,    'balance': 1000.0},
+            {'flag': 'manual',        'amount_currency': -1000.0, 'currency_id': self.company_data['currency'].id,    'balance': -1000.0},
+        ])
+        self.assertRecordValues(wizard, [{'state': 'valid'}])
+
+        # The amount is the same, no message under the 'amount' field.
+        self.assert_form_extra_text_value(wizard.form_extra_text, False)
+
+        wizard.button_validate()
+        self.assertRecordValues(st_line.line_ids, [
+            # pylint: disable=C0326
+            {'account_id': st_line.journal_id.default_account_id.id,    'amount_currency': 1000.0,      'currency_id': self.company_data['currency'].id,    'balance': 1000.0,  'reconciled': False},
+            {'account_id': self.account_revenue1.id,                    'amount_currency': -1000.0,     'currency_id': self.company_data['currency'].id,    'balance': -1000.0, 'reconciled': False},
+        ])
+
+        self.assertRecordValues(wizard.line_ids, [
+            # pylint: disable=C0326
+            {'flag': 'liquidity', 'account_id': st_line.journal_id.default_account_id.id,  'amount_currency': 1000.0,  'currency_id': self.company_data['currency'].id,    'balance': 1000.0},
+            {'flag': 'aml',       'account_id': self.account_revenue1.id,                  'amount_currency': -1000.0, 'currency_id': self.company_data['currency'].id,    'balance': -1000.0},
+        ])
+
     def test_validation_new_aml_same_foreign_currency(self):
         income_exchange_account = self.env.company.income_currency_exchange_account_id
 
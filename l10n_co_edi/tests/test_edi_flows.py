2fe5bfeab65e401a1a8ca17cebc00ca414528b53
Yolann Sabaux <yosa@odoo.com>
2023-08-09 11:53:51 +0000

    [FIX] l10n_co_edi, l10n_mx_edi_40: modifies test after change of button_draft's flow
    In the new flow, `button_draft` unnlinks the documents that are not sent.
    
    linked pr: https://github.com/odoo/odoo/pull/130383
    
    note fw-port: button_cancel calls enventuallly button draft when the edi is not sent -> needs to change the test
    opw-3281007
    
    closes odoo/enterprise#46112
    
    X-original-commit: ae2ab577bdd7e97220b2251daccdd7cbc59594db
    Related: odoo/odoo#132752
    Signed-off-by: Laurent Smet (las) <las@odoo.com>
    Signed-off-by: Yolann Sabaux (yosa) <yosa@odoo.com>

--- l10n_co_edi/tests/test_edi_flows.py
+++ l10n_co_edi/tests/test_edi_flows.py
@@ -19,16 +19,9 @@ class TestEdiFlows(TestCoEdiCommon):
             self.assertRecordValues(self.invoice, [{'edi_state': 'to_send'}])
             self.assertRecordValues(document, [{'state': 'to_send'}])
 
-            self.invoice.button_draft()
-
-            self.assertRecordValues(self.invoice, [{'edi_state': 'to_send'}])
-            self.assertRecordValues(document, [{'state': 'to_send'}])
-
             self.invoice.button_cancel()
 
-            self.assertRecordValues(self.invoice, [{'edi_state': 'cancelled'}])
-            self.assertRecordValues(document, [{'state': 'cancelled'}])
-            self.assertFalse(document.attachment_id)
+            self.assertFalse(self.invoice.edi_state)
 
     def test_invoice_flow_sent(self):
         with self.mock_carvajal():

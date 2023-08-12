a35f16f927424c91cf697c9cf0a3ef2feed18a76
Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
2023-08-12 08:59:21 +0200

    [IMP] l10n_at_reports: add custom balance sheet handler
    in order to set line name based on equity sign
    
    closes odoo/enterprise#47198
    
    Related: odoo/odoo#134959
    Signed-off-by: Josse Colpaert <jco@odoo.com>

--- l10n_at_reports/models/__init__.py
+++ l10n_at_reports/models/__init__.py
@@ -3,4 +3,6 @@
 
 # Copyright (c) 2022 WT-IO-IT GmbH (https://www.wt-io-it.at)
 #                    Mag. Wolfgang Taferner <wolfgang.taferner@wt-io-it.at>
+
+from . import account_report_l10n_at_balance_custom_handler
 from . import account_sales_report

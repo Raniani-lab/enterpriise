fe9032eaf9fefba13764850ab8d8c28dcb0bbf05
Rob Coekaerts <roco@odoo.com>
2023-08-17 22:07:51 +0200

    [FIX] hr_work_entry_contract: reload gantt view
    When regenerating work entries in the gantt view, the work entries were
    not reloaded, meaning that the effect of the regeneration could not be
    observed unless you manually reloaded the view.
    
    With this commit, the gantt view is automatically reloaded.
    
    related PR that did the same for calendar views: odoo#129647
    
    task-3382376
    
    closes odoo/enterprise#45916
    
    Signed-off-by: Sofie Gvaladze (sgv) <sgv@odoo.com>

--- hr_work_entry_contract_enterprise/static/src/work_entries_gantt_controller.js
+++ hr_work_entry_contract_enterprise/static/src/work_entries_gantt_controller.js
@@ -19,6 +19,7 @@ export class WorkEntriesGanttController extends GanttController {
                 return [];
             },
             getRange: () => this.model.getRange(),
+            onClose: () => this.model.fetchData({}),
         });
         this.onRegenerateWorkEntries = onRegenerateWorkEntries;
     }

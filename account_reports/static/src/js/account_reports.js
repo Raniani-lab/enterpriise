011867354f01bb9bec3b43df067a1329c17c6249
Thomas Becquevort (thbe) <thbe@odoo.com>
2023-08-22 11:22:51 +0200

    [FIX] account_reports: l10n_fi profit and loss sections expansion
    Description of the issue/feature this commit addresses:
    
    When using the finland localization, some lines in the profit and loss report
    are shown as unfoldable while they should not either because they have no
    children or because they simply don't have an expand function. This means that
    when the user tries to unfold them, an error message shows up telling him
    that it is not possible.
    
    Desired behavior after the commit is merged :
    
    This commit makes it now possible to unfold lines which do not have an
    expansion function. In such a case, clicking on the line to unfold it
    will simply do nothing. This way, it is still possible to have lines
    without an expansion function be unfoldable.
    
    Adding this fix, no error will be raised when unfolding a line without
    an expansion function.
    
    task-3462024
    
    Part-of: odoo/enterprise#46417

--- account_reports/static/src/js/account_reports.js
+++ account_reports/static/src/js/account_reports.js
@@ -1111,8 +1111,7 @@ var accountReportsWidget = AbstractAction.extend({
             line[0].dataset.unfolded = 'True';
             this._add_line_classes();
             return true;
-        }
-        else {
+        } else if ( line.data('expandFunction') ) {
             // Display the total lines (for 'totals below section' option)
             if ($total_lines.length > 0) {
                 $lines_in_dom.removeClass('o_account_reports_filtered_lines');
@@ -1141,6 +1140,8 @@ var accountReportsWidget = AbstractAction.extend({
 
                     self._add_line_classes();
                 });
+        } else {
+            line[0].dataset.unfolded = 'True';
         }
     },
     load_more: function (ev) {

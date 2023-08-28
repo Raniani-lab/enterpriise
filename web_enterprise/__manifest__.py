d4ee882f73eb3dc99fc6951adb11bf03b4b94b83
Pierre Paridans <app@odoo.com>
2023-08-28 11:21:51 +0200

    [IMP] web_enterprise: cleanup promote_studio scss
    task-3439226
    
    Part-of: odoo/enterprise#46381

--- web_enterprise/__manifest__.py
+++ web_enterprise/__manifest__.py
@@ -36,7 +36,6 @@ This module modifies the web addon to provide Enterprise design and responsivene
         'web.assets_backend': [
             'web_enterprise/static/src/legacy/scss/dropdown.scss',
             'web_enterprise/static/src/legacy/scss/modal_mobile.scss',
-            'web_enterprise/static/src/legacy/scss/promote_studio.scss',
             'web_enterprise/static/src/webclient/**/*.scss',
             'web_enterprise/static/src/views/**/*.scss',
 

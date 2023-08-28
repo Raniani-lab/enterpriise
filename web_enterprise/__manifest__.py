5ada800eba0871cb16022f9c5c6be3667b957eef
Pierre Paridans <app@odoo.com>
2023-08-28 10:21:43 +0200

    [IMP] web_enterprise: remove unused control_panel_mobile.scss
    task-3439226
    
    Part-of: odoo/enterprise#46381

--- web_enterprise/__manifest__.py
+++ web_enterprise/__manifest__.py
@@ -35,7 +35,6 @@ This module modifies the web addon to provide Enterprise design and responsivene
         ],
         'web.assets_backend': [
             'web_enterprise/static/src/legacy/scss/dropdown.scss',
-            'web_enterprise/static/src/legacy/scss/control_panel_mobile.scss',
             'web_enterprise/static/src/legacy/scss/modal_mobile.scss',
             'web_enterprise/static/src/legacy/scss/promote_studio.scss',
             'web_enterprise/static/src/webclient/**/*.scss',

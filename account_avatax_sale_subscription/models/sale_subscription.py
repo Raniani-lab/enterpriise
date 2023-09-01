bf7107a60246239de323c791f22341f881338dbf
Joren Van Onder <jov@odoo.com>
2023-09-01 10:25:33 -0700

    [FIX] account_avatax_sale_subscription: correct tax for manual payment
    In previous versions the `/my/subscription/transaction/<int:order_id>`
    controller set `order_sudo.amount_total` as the amount on the
    payment.transaction. Now the amount is being set from the invoice
    created by `_create_invoices()`. This ensures taxes are computed on it
    before it is used.
    
    opw-3490180
    
    closes odoo/enterprise#47039
    
    X-original-commit: 46507e690d76e6f8d641107a68e60196dadef6b8
    Signed-off-by: William André (wan) <wan@odoo.com>
    Signed-off-by: Joren Van Onder (jov) <jov@odoo.com>

--- account_avatax_sale_subscription/models/sale_subscription.py
+++ account_avatax_sale_subscription/models/sale_subscription.py
@@ -13,3 +13,8 @@ class SaleSubscription(models.Model):
     def _do_payment(self, payment_token, invoice, auto_commit=False):
         invoice.button_update_avatax()
         return super()._do_payment(payment_token, invoice, auto_commit=False)
+
+    def _create_invoices(self, grouped=False, final=False, date=None):
+        moves = super()._create_invoices(grouped=grouped, final=final, date=date)
+        moves.filtered(lambda m: m.state == 'draft').button_update_avatax()
+        return moves

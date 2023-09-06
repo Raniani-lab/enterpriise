a462ed068d494a50c7985dace4f24888fb25e7fb
Ahmad Khanalizadeh <khah@odoo.com>
2023-09-06 09:17:32 +0200

    [FIX] delivery_fedex: add `FEDEX_REGIONAL_ECONOMY` service type
    The service type `FEDEX_REGIONAL_ECONOMY` is a new service type in
    FedEx, to facilitate shipments inside Europe and UK. This commit
    adds support for this service type.
    
    opw-3487855
    
    closes odoo/enterprise#47320
    
    X-original-commit: 0fd44a4405ab6d39b502a07963e4f8e90e1578fc
    Signed-off-by: Tiffany Chang (tic) <tic@odoo.com>

--- delivery_fedex/models/delivery_fedex.py
+++ delivery_fedex/models/delivery_fedex.py
@@ -103,6 +103,7 @@ class ProviderFedex(models.Model):
                                            ('FEDEX_NEXT_DAY_AFTERNOON', 'FEDEX_NEXT_DAY_AFTERNOON'),
                                            ('FEDEX_NEXT_DAY_END_OF_DAY', 'FEDEX_NEXT_DAY_END_OF_DAY'),
                                            ('FEDEX_EXPRESS_SAVER', 'FEDEX_EXPRESS_SAVER'),
+                                           ('FEDEX_REGIONAL_ECONOMY', 'FEDEX_REGIONAL_ECONOMY'),
                                            ],
                                           default='FEDEX_INTERNATIONAL_PRIORITY')
     fedex_duty_payment = fields.Selection([('SENDER', 'Sender'), ('RECIPIENT', 'Recipient')], required=True, default="SENDER")

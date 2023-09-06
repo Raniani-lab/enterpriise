fbea23806dbe4ed39f7421da0f9026cc8b019985
Maximilien (malb) <malb@odoo.com>
2023-09-06 12:28:59 +0200

    [IMP] l10n_cl_edi,l10n_cl_edi_stock: UI clean up
    This commit aims at changing some tooltips.
    
    Task: 3270458
    Part-of: odoo/enterprise#47043

--- l10n_cl_edi/models/res_partner.py
+++ l10n_cl_edi/models/res_partner.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    l10n_cl_dte_email = fields.Char(string='DTE Email')
+    l10n_cl_dte_email = fields.Char(string='DTE Email', help="Chile: Email used to send and receive electronic documents.")
 
     def _l10n_cl_is_foreign(self):
         return self.country_id.code != "CL"

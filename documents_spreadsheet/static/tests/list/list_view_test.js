Raniani-lab/enterpriise

commit bee96cb08342669f9233c4422f941ace3b8e67ca

[REF] spreadsheet_edition: remove legacy dialog
This commit replaces the legacy dialog by the new one.

Part of task-id 3439226

Part-of: odoo/enterprise#44634

spreadsheet_edition/static/src/bundle/actions/spreadsheet_dialog/spreadsheet_dialog.xml
@@ -1,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<templates>
+
+    <t t-name="spreadsheet_edition.SpreadsheetDialog" owl="1">
+        <Dialog title="props.title || 'Odoo Spreadsheet'" size="'md'">
+            <div t-if="props.content" t-esc="props.content"/>
+            <t t-if="props.edit">
+                <input t-att-type="props.inputType" class="form-control" t-model="state.inputContent" />
+                <span t-if="props.errorText" class="o_dialog_error_text text-danger" t-esc="props.errorText" />
+            </t>
+            <t t-set-slot="footer">
+                <t t-if="props.edit">
+                    <button class="btn btn-primary" t-on-click="confirm">Confirm</button>
+                    <button class="btn btn-secondary" t-on-click="close">Cancel</button>
+                </t>
+                <t t-else="1">
+                    <button class="btn btn-primary" t-on-click="confirm">Ok</button>
+                </t>
+            </t>
+        </Dialog>
+    </t>
+
+</templates>

spreadsheet_edition/static/src/bundle/actions/spreadsheet.xml
@@ -3,32 +3,7 @@
 
     <div t-name="spreadsheet_edition.SpreadsheetComponent" class="o_spreadsheet_container o_field_highlight" owl="1">
         <Spreadsheet model="model"/>
-        <Dialog t-if="state.dialog.isDisplayed"
-            title="state.dialog.title || 'Odoo Spreadsheet'"
-            size="'medium'"
-            onClosed.bind="closeDialog"
-            >
-            <div t-if="dialogContent" t-esc="dialogContent" />
-            <t t-if="state.dialog.isEditText">
-                <input type="text" class="form-control" t-model="state.inputContent" />
-                <span t-if="state.dialog.errorText" class="o_dialog_error_text text-danger" t-esc="state.dialog.errorText" />
-            </t>
-            <t t-if="state.dialog.isEditInteger">
-                <input t-if="state.dialog.isEditInteger" type="number" class="form-control" t-model="state.dialog.inputIntegerContent" />
-            </t>
-            <t t-set-slot="buttons">
-                <t t-if="state.dialog.isEditText or state.dialog.isEditInteger">
-                    <button class="btn btn-primary" t-on-click="confirmDialog">Confirm</button>
-                    <button class="btn btn-secondary" t-on-click="closeDialog">Cancel</button>
-                </t>
-                <t t-else="1">
-                    <button class="btn btn-primary" t-on-click="confirmDialog">Ok</button>
-                </t>
-            </t>
-        </Dialog>
     </div>
-
-
 
     <div t-name="spreadsheet_edition.AutofillTooltip" owl="1">
         <t t-foreach="props.content" t-as="line" t-key="line.value+line_index">

spreadsheet_edition/static/src/bundle/actions/spreadsheet_component.js
@@ -1,14 +1,14 @@
 /** @odoo-module */
 
 import { _t } from "@web/core/l10n/translation";
-import Dialog from "@web/legacy/js/core/owl_dialog";
 import { useService } from "@web/core/utils/hooks";
 
 import { DEFAULT_LINES_NUMBER } from "@spreadsheet/helpers/constants";
+import { SpreadsheetDialog } from "@spreadsheet_edition/bundle/actions/spreadsheet_dialog/spreadsheet_dialog";
 
 import { Spreadsheet, Model } from "@odoo/o-spreadsheet";
 
-import { useState, useSubEnv, Component } from "@odoo/owl";
+import { useSubEnv, Component } from "@odoo/owl";
 
 const tags = new Set();
 
@@ -31,6 +31,7 @@ export default class SpreadsheetComponent extends Component {
         this.orm = useService("orm");
         this.action = useService("action");
         this.notifications = useService("notification");
+        this.dialog = useService("dialog");
 
         useSubEnv({
             getLinesNumber: this._getLinesNumber.bind(this),
@@ -40,19 +41,6 @@ export default class SpreadsheetComponent extends Component {
             askConfirmation: this.askConfirmation.bind(this),
         });
 
-        this.state = useState({
-            dialog: {
-                isDisplayed: false,
-                title: undefined,
-                isEditText: false,
-                errorText: undefined,
-                inputContent: undefined,
-                isEditInteger: false,
-                inputIntegerContent: undefined,
-            },
-        });
-
-        this.dialogContent = undefined;
         this.pivot = undefined;
         this.confirmDialog = () => true;
     }
@@ -64,12 +52,11 @@ export default class SpreadsheetComponent extends Component {
      * @param {Function} confirm Callback if the user press 'Confirm'
      */
     askConfirmation(content, confirm) {
-        this.dialogContent = content;
-        this.confirmDialog = () => {
-            confirm();
-            this.closeDialog();
-        };
-        this.state.dialog.isDisplayed = true;
+        this.dialog.add(
+            SpreadsheetDialog,
+            { content, confirm },
+            { onClose: this.closeDialog.bind(this) }
+        );
     }
 
     /**
@@ -80,41 +67,39 @@ export default class SpreadsheetComponent extends Component {
      * @param {Object} options Options of the dialog. Can contain a placeholder and an error message.
      */
     editText(title, callback, options = {}) {
-        this.dialogContent = undefined;
-        this.state.dialog.title = title && title.toString();
-        this.state.dialog.errorText = options.error && options.error.toString();
-        this.state.dialog.isEditText = true;
-        this.state.inputContent = options.placeholder;
-        this.confirmDialog = () => {
-            this.closeDialog();
-            callback(this.state.inputContent);
-        };
-        this.state.dialog.isDisplayed = true;
+        this.dialog.add(
+            SpreadsheetDialog,
+            {
+                confirm: callback,
+                title: title && title.toString(),
+                errorText: options.error && options.error.toString(),
+                edit: true,
+                inputContent: options.placeholder,
+                inputType: "text",
+            },
+            { onClose: this.closeDialog.bind(this) }
+        );
     }
 
     _getLinesNumber(callback) {
-        this.dialogContent = _t("Select the number of records to insert");
-        this.state.dialog.title = _t("Re-insert list");
-        this.state.dialog.isEditInteger = true;
-        this.state.dialog.inputIntegerContent = DEFAULT_LINES_NUMBER;
-        this.confirmDialog = () => {
-            this.closeDialog();
-            callback(this.state.dialog.inputIntegerContent);
-        };
-        this.state.dialog.isDisplayed = true;
+        this.dialog.add(
+            SpreadsheetDialog,
+            {
+                content: _t("Select the number of records to insert"),
+                confirm: callback,
+                title: _t("Re-insert list"),
+                edit: true,
+                inputContent: DEFAULT_LINES_NUMBER,
+                inputType: "number",
+            },
+            { onClose: this.closeDialog.bind(this) }
+        );
     }
 
     /**
      * Close the dialog.
      */
     closeDialog() {
-        this.dialogContent = undefined;
-        this.confirmDialog = () => true;
-        this.state.dialog.title = undefined;
-        this.state.dialog.errorText = undefined;
-        this.state.dialog.isDisplayed = false;
-        this.state.dialog.isEditText = false;
-        this.state.dialog.isEditInteger = false;
         document.querySelector(".o-grid>input").focus();
     }
 
@@ -140,17 +125,19 @@ export default class SpreadsheetComponent extends Component {
      * @param {string} content Content to display
      */
     raiseError(content, callback) {
-        this.dialogContent = content;
-        this.confirmDialog = () => {
-            this.closeDialog();
-            callback?.();
-        };
-        this.state.dialog.isDisplayed = true;
+        this.dialog.add(
+            SpreadsheetDialog,
+            {
+                content,
+                confirm: callback,
+            },
+            { onClose: this.closeDialog.bind(this) }
+        );
     }
 }
 
 SpreadsheetComponent.template = "spreadsheet_edition.SpreadsheetComponent";
-SpreadsheetComponent.components = { Spreadsheet, Dialog };
+SpreadsheetComponent.components = { Spreadsheet };
 Spreadsheet._t = _t;
 SpreadsheetComponent.props = {
     model: Model,

spreadsheet_edition/static/src/bundle/actions/spreadsheet_dialog/spreadsheet_dialog.js
@@ -1,0 +1,31 @@
+/** @odoo-module **/
+
+import { Component } from "@odoo/owl";
+import { Dialog } from "@web/core/dialog/dialog";
+
+export class SpreadsheetDialog extends Component {
+    setup() {
+        this.state = {
+            inputContent: this.props.inputContent,
+        };
+    }
+
+    confirm() {
+        this.props.close();
+        this.props.confirm?.(this.state.inputContent);
+    }
+}
+
+SpreadsheetDialog.components = { Dialog };
+
+SpreadsheetDialog.props = {
+    close: Function,
+    content: { type: String, optional: true },
+    edit: { type: Boolean, optional: true },
+    inputType: { type: String, optional: true },
+    inputContent: { type: [String, Number], optional: true },
+    errorText: { type: String, optional: true },
+    confirm: { type: Function, optional: true },
+    title: { type: String, optional: true },
+};
+SpreadsheetDialog.template = "spreadsheet_edition.SpreadsheetDialog";

commit 3b991e8c4fb454607997b7e6e7525499f3251e9f

[FIX] spreadsheet_edition: cancel list reinsertion
Steps to reproduce:
- insert a list view in a spreadsheet
- Open menu "Data > Re-insretlist"
- click on the list to re-insert it
(a dialog opens to ask the number of lines)
- hit the "Cancel" button
=> boom

Bug introduced with bee96cb

Part-of: odoo/enterprise#46697

spreadsheet_edition/static/src/bundle/actions/spreadsheet_dialog/spreadsheet_dialog.xml
@@ -11,7 +11,7 @@
             <t t-set-slot="footer">
                 <t t-if="props.edit">
                     <button class="btn btn-primary" t-on-click="confirm">Confirm</button>
-                    <button class="btn btn-secondary" t-on-click="close">Cancel</button>
+                    <button class="btn btn-secondary" t-on-click="props.close">Cancel</button>
                 </t>
                 <t t-else="1">
                     <button class="btn btn-primary" t-on-click="confirm">Ok</button>

documents_spreadsheet/static/tests/list/list_view_test.js
@@ -185,6 +185,18 @@ QUnit.module("document_spreadsheet > list view", {}, () => {
         const input = document.body.querySelector(".modal-body input");
         assert.ok(input);
         assert.strictEqual(input.type, "number");
+
+        await click(document, ".o_dialog .btn-secondary"); // cancel
+        assert.strictEqual(getCellFormula(model, "Z26"), "", "the list is not re-inserted");
+
+        await doMenuAction(topbarMenuRegistry, ["data", "reinsert_list", "reinsert_list_1"], env);
+        await nextTick();
+        await click(document, ".o_dialog .btn-primary"); // confirm
+        assert.strictEqual(
+            getCellFormula(model, "Z26"),
+            '=ODOO.LIST.HEADER(1,"foo")',
+            "the list is re-inserted"
+        );
     });
 
     QUnit.test("user related context is not saved in the spreadsheet", async function (assert) {
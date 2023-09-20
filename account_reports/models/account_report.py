8359cdc5abdc74774ea8bf34882edb700bfa0a36
Andrea Grazioso (agr-odoo) <agr@odoo.com>
2023-09-20 14:37:46 +0000

    [FIX] account_reports: filter search reset after date change
    Open General Ledger
    Write in search bar 'tax'
    Quick switch period to 'quarter' or something else
    Export XLSX
    
    Issue: lines will be unfiltered
    
    From saas-16.3 we don't clear the search bar filter after the date change
    so we should always keep the `filter_search_bar` option
    
    opw-3509427
    
    closes odoo/enterprise#48012
    
    X-original-commit: a96900a0b0bff85d67d421900177770555bebf50
    Signed-off-by: John Laterre (jol) <jol@odoo.com>
    Signed-off-by: Andrea Grazioso (agr) <agr@odoo.com>

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -1206,8 +1206,8 @@ class AccountReport(models.Model):
     def _init_options_search_bar(self, options, previous_options=None):
         if self.search_bar:
             options['search_bar'] = True
-            if options['print_mode']:
-                options['filter_search_bar'] = previous_options.get('filter_search_bar')
+            if previous_options and 'filter_search_bar' in previous_options:
+                options['filter_search_bar'] = previous_options['filter_search_bar']
 
     ####################################################
     # OPTIONS: COLUMN HEADERS

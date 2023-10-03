b819dfcc832d4c57688b8feee82c96182dbbc861
FrancoisGe <fge@odoo.com>
2023-10-03 14:04:59 +0200

    [FIX] appointment: enable AppointmentInviteCopyClose
    Since the new Relational Model, if the <a> in AppointmentInviteCopyClose
    is disabled, it is not possible to enable it when you resolve alerts.
    
    Why:
    ---
    Before the new Relational Mode, the entire view was re-rendered each
    time the record was updated. AppointmentInviteCopyClose was therefore
    rendered and the callback in useEffect was called. This is no longer the case,
    only the Components that observe the data modified on the record are rendered.
    
    Solution:
    ---------
    We're going to use the dynamicProps.readonly which will be recalculated
    each time a change occurs in its condition. So we'll pass the readonly
    attribute to the <widget> with the condition the combination of all the
    conditions that can display an alert and cause the <a> to be disabled.
    
    closes odoo/enterprise#48309
    
    Related: odoo/odoo#137414
    Signed-off-by: Aaron Bohy (aab) <aab@odoo.com>

--- appointment/static/src/components/appointment_invite/appointment_invite_copy_close.js
+++ appointment/static/src/components/appointment_invite/appointment_invite_copy_close.js
@@ -6,7 +6,7 @@ import { registry } from '@web/core/registry';
 import { standardWidgetProps } from "@web/views/widgets/standard_widget_props";
 import { useService } from "@web/core/utils/hooks";
 
-const { Component, useEffect } = owl;
+const { Component } = owl;
 
 export class AppointmentInviteCopyClose extends Component {
     /**
@@ -14,21 +14,16 @@ export class AppointmentInviteCopyClose extends Component {
      * result to have an incorrect/empty link.
      */
     setup() {
-        super.setup();
         this.notification = useService("notification");
-        useEffect((saveButton, warning) => {
-            if (saveButton) {
-                saveButton.classList.toggle('disabled', !!warning);
-            }
-        }, () => [document.querySelector('.o_appointment_invite_copy_save'), document.querySelector('.alert.alert-warning')]);
     }
     /**
      * Save the invitation and copy the url in the clipboard
      * @param ev
      */
-     async onSaveAndCopy (ev) {
-        ev.preventDefault();
-        ev.stopImmediatePropagation();
+    async onSaveAndCopy(ev) {
+        if (this.props.readonly) {
+            return;
+        }
         if (await this.props.record.save()) {
             const bookUrl = this.props.record.data.book_url;
             setTimeout(async () => {
@@ -49,5 +44,10 @@ AppointmentInviteCopyClose.template = 'appointment.AppointmentInviteCopyClose';
 
 export const appointmentInviteCopyClose = {
     component: AppointmentInviteCopyClose,
+    extractProps(widgetInfo, dynamicInfo) {
+        return {
+            readonly: dynamicInfo.readonly,
+        };
+    },
 };
 registry.category("view_widgets").add("appointment_invite_copy_close", appointmentInviteCopyClose);

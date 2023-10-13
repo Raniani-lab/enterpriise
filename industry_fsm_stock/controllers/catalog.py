47a6e50fa6e000548ef0485c964c8ac184ad6bea
Bastien (bvdn) <bvdn@odoo.com>
2023-10-13 16:24:36 +0200

    [FIX] industry_fsm_sale: prevent removal of delivered Service product
    - steps: open fsm app -> select a task -> add products -> select service product -> add some qty -> try to remove qty -> inconsistent behavior and error message.
    
    - Issue: The qty of these products should not be decreasable
    
    - Cause: Service product with the *qty_delivered_method* set as 'manual' are considered as directly delivered so their quantities shouldn't be decreasable once you added quantity.
    This logic is applied in the __inverse_fsm_quantity_ method in industry_fsm_sale module
    
    - Fix: adding a min_quantity key in the catalog _sale_product_catalog_update_sale_order_line_info_ method return dictionary,
    the value is setted as the sol.qty_delivered. Once this value has been fetched, the kanban_record will include it in the productCatalogData.minimumQuantityOnProduct
    
    I'm open to discussion to implement this in the sale_stock module but i don't think the quantity delivered are updated as soon as
    the quantity is added to the sale order anywhere else than in the industry_fsm_stock module.
    
    Task-3553140
    
    closes odoo/enterprise#48940
    
    Signed-off-by: Xavier Bol (xbo) <xbo@odoo.com>

--- industry_fsm_stock/controllers/catalog.py
+++ industry_fsm_stock/controllers/catalog.py
@@ -1,16 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo.http import request, route
-from odoo.addons.product.controllers.catalog import ProductCatalogController
+from odoo.addons.industry_fsm_sale.controllers.catalog import CatalogControllerFSM
 
-class CatalogControllerFSM(ProductCatalogController):
-
-    @route()
-    def product_catalog_get_order_lines_info(self, res_model, order_id, product_ids, **kwargs):
-        task_id = kwargs.get('task_id')
-        if task_id:
-            request.update_context(fsm_task_id=task_id)
-        return super().product_catalog_get_order_lines_info(res_model, order_id, product_ids, **kwargs)
+class CatalogControllerFSMStock(CatalogControllerFSM):
 
     @route()
     def product_catalog_update_order_line_info(self, res_model, order_id, product_id, quantity=0, **kwargs):
@@ -22,17 +15,16 @@ class CatalogControllerFSM(ProductCatalogController):
         :param float quantity: The quantity selected in the product catalog.
         :param list context: the context comming from the view, used only to propagate the 'fsm_task_id' for the action_assign_serial on the product.
         :return: The unit price of the product, based on the pricelist of the sale order and
-                 the quantity selected.
+                 the quantity selected. Plus the new minimum quantity for the product
         :rtype: A dictionary containing the SN action and the SOL price_unit
         """
         task_id = kwargs.get('task_id')
+        super_dict = super().product_catalog_update_order_line_info(res_model, order_id, product_id, quantity, **kwargs)
         if not task_id:
-            return super().product_catalog_update_order_line_info(res_model, order_id, product_id, quantity, **kwargs)
-        request.update_context(fsm_task_id=task_id)
+            return super_dict
         task = request.env['project.task'].browse(task_id)
-        product = request.env['product.product'].browse(product_id)
-        SN_wizard = product.set_fsm_quantity(quantity)
         sol = request.env['sale.order.line'].search([
             ('order_id', '=', task.sale_order_id.id), ('product_id', '=', product_id),
         ], limit=1)
-        return {"action": SN_wizard, "price": sol.price_unit if sol else False}
+        super_dict["min_quantity"] = sol.product_id.fsm_quantity - sol.product_id.quantity_decreasable_sum
+        return super_dict

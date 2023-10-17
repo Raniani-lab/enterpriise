57c0ea04cc6c08c52f27172793d24f639d5a76b1
Youssef Bashandy <yoba@odoo.com>
2023-10-17 15:42:44 +0300

    [FIX] sale_subscription: Sales smart button in customer's contact pag…
    …e is not updating upon creating subscription quote
    
    Steps to reproduce:
    1- Install Contacts and Subscriptions modules
    2- Check number of sales in a customer's contact page from the smart button
    3- Create a new subscription quote for a customer
    4- Go to the customer's contact page
    5- Check the sales smart button which will remain the same number as step 2
    
    Current behavior before PR:
    The sales smart button is not counting subscription count while when click on it you will find the last of sales quotations along with the subscription quotations
    
    Desired behavior after PR is merged:
    The smart button now is counting the number of both sales and subscription quotations
    
    opw-3525414
    
    closes odoo/enterprise#49051
    
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sale_subscription/models/res_partner.py
+++ sale_subscription/models/res_partner.py
@@ -25,11 +25,6 @@ class ResPartner(models.Model):
                 raise ValidationError(_("You can't archive the partner as it is used in the following recurring orders: %s", contract_str))
         return res
 
-    @api.model
-    def _get_sale_order_domain_count(self):
-        domain = super()._get_sale_order_domain_count()
-        return expression.AND([domain, [('is_subscription', '=', False)]])
-
     def _subscription_count(self):
         # retrieve all children partners and prefetch 'parent_id' on them
         all_partners = self.with_context(active_test=False).search([('id', 'child_of', self.ids)])

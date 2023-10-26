2461a898f496368d2f10f591c80c4e2d9ed38ee3
Damien Bouvy <dbo@odoo.com>
2023-10-26 12:12:14 +0200

    [FIX] web_studio: fix test
    closes odoo/enterprise#48987
    
    Related: odoo/odoo#138804
    Signed-off-by: Bouvy Damien (dbo) <dbo@odoo.com>

--- web_studio/tests/test_ui.py
+++ web_studio/tests/test_ui.py
@@ -75,7 +75,7 @@ class TestUi(odoo.tests.HttpCase):
         self.assertEqual(fields_of_interest[1].get("invisible"), None)
 
         self.assertEqual(fields_of_interest[0].get("column_invisible"), None)
-        self.assertEqual(fields_of_interest[1].get("column_invisible"), "1")
+        self.assertEqual(fields_of_interest[1].get("column_invisible"), "True")
 
     def test_add_field_into_empty_group_by(self):
         self.start_tour("/web?debug=tests", 'web_studio_add_field_into_empty_group_by', login="admin")

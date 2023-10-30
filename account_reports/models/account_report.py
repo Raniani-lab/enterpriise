da536a097cd21635135e378a20310094421b5827
Anh Thao Pham (pta) <pta@odoo.com>
2023-10-30 15:52:22 +0100

    [FIX] account_reports: fix archived analytic search in General Ledger
    Steps to reproduce:
    - Activate "Analytic Accounting" in Accounting settings
    - Create an invoice with an analytic account (e.g. Administrative)
    - Archive the used analytic account (i.e. Administrative)
    - Go "Accounting / Reporting / Audit Reports / General Ledger"
    - Click on "Analytic" button
    - When selecting Accounts, click on "Search More..."
    - Filter "Archived" accounts
    - Select an archived account (e.g. Administrative)
    
    => The filter is selected but it is not taken into account.
    The displayed data stay unchanged.
    
    There is the same issue with partner filter in "Partner Ledger" report
    and accounts field of "Analytic Group By" in other reports.
    
    opw-3514165
    
    closes odoo/enterprise#50276
    
    X-original-commit: e54db62b241a6ad2aedf8c332d7ca9bf9d8fcdb1
    Signed-off-by: Olivier Colson (oco) <oco@odoo.com>
    Signed-off-by: Anh Thao Pham (pta) <pta@odoo.com>

--- account_reports/models/account_report.py
+++ account_reports/models/account_report.py
@@ -680,7 +680,7 @@ class AccountReport(models.Model):
         if enable_analytic_accounts:
             previous_analytic_accounts = (previous_options or {}).get('analytic_accounts', [])
             analytic_account_ids = [int(x) for x in previous_analytic_accounts]
-            selected_analytic_accounts = self.env['account.analytic.account'].search([('id', 'in', analytic_account_ids)])
+            selected_analytic_accounts = self.env['account.analytic.account'].with_context(active_test=False).search([('id', 'in', analytic_account_ids)])
             options['analytic_accounts'] = selected_analytic_accounts.ids
             options['selected_analytic_account_names'] = selected_analytic_accounts.mapped('name')
 
@@ -698,7 +698,7 @@ class AccountReport(models.Model):
 
         selected_partner_ids = [int(partner) for partner in previous_partner_ids]
         # search instead of browse so that record rules apply and filter out the ones the user does not have access to
-        selected_partners = selected_partner_ids and self.env['res.partner'].search([('id', 'in', selected_partner_ids)]) or self.env['res.partner']
+        selected_partners = selected_partner_ids and self.env['res.partner'].with_context(active_test=False).search([('id', 'in', selected_partner_ids)]) or self.env['res.partner']
         options['selected_partner_ids'] = selected_partners.mapped('name')
         options['partner_ids'] = selected_partners.ids
 

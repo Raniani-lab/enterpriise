Raniani-lab/enterpriise

commit 0d7d1a2994f14b80cd8f3644871f87a472821741

[FIX] account_{accountant,online_synchronization}: auto_reconcile after import of statements
Before this commit, when we import transactions we don't directly try to auto-reconcile,
it is done through a cron run asynchronously.
This PR calls the auto-reconciliation synchronously after fetching transactions (either from
different imports or bank sync). It will try to reconcile as many records as possible
within the time allocated.
Also adds a button on reconciliation models to trigger manually synchronous reconciliation.
It will handle as many transactions as possible within allocated time.

closes odoo/enterprise#39143

Task: 3259126
Signed-off-by: Laurent Smet <las@odoo.com>

account_accountant/models/account_bank_statement.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from lxml import etree
 from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 from dateutil.relativedelta import relativedelta
 
@@ -111,14 +112,40 @@ class AccountBankStatementLine(models.Model):
             },
         )
 
-    @api.model
-    def _cron_try_auto_reconcile_statement_lines(self, batch_size=None):
+    def _cron_try_auto_reconcile_statement_lines(self, batch_size=None, limit_time=0):
         """ Method called by the CRON to reconcile the statement lines automatically.
 
-        :param batch_size:  The maximum number of statement lines that could be processed at once by the CRON to avoid
+        :param  batch_size:  The maximum number of statement lines that could be processed at once by the CRON to avoid
                             a timeout. If specified, the CRON will be trigger again asap using a CRON trigger in case
                             there is still some statement lines to process.
+                limit_time: Maximum time allowed to run in seconds. 0 if the Cron is allowed to run without time limit.
         """
+        def _compute_st_lines_to_reconcile(configured_company_ids):
+            # Find the bank statement lines that are not reconciled and try to reconcile them automatically.
+            # The ones that are never be processed by the CRON before are processed first.
+            remaining_line_id = None
+            limit = batch_size + 1 if batch_size else None
+            companies = self.env['res.company'].browse(configured_company_ids)
+            lock_dates = companies.filtered('fiscalyear_lock_date').mapped('fiscalyear_lock_date')
+            st_date_from_limit = max([start_time.date() - relativedelta(months=3)] + lock_dates)
+            domain = [
+                ('is_reconciled', '=', False),
+                ('date', '>', st_date_from_limit),
+                ('company_id', 'in', configured_company_ids),
+            ]
+            query_obj = self._search(domain, limit=limit)
+            query_obj.order = '"account_bank_statement_line"."cron_last_check" ASC NULLS FIRST,"account_bank_statement_line"."id"'
+            query_str, query_params = query_obj.select('account_bank_statement_line.id')
+            self._cr.execute(query_str, query_params)
+            st_line_ids = [r[0] for r in self._cr.fetchall()]
+            if batch_size and len(st_line_ids) > batch_size:
+                remaining_line_id = st_line_ids[batch_size]
+                st_line_ids = st_line_ids[:batch_size]
+            st_lines = self.env['account.bank.statement.line'].browse(st_line_ids)
+            return st_lines, remaining_line_id
+
+        start_time = fields.Datetime.now()
+
         self.env['account.reconcile.model'].flush_model()
 
         # Check the companies having at least one reconcile model using the 'auto_reconcile' feature.
@@ -133,45 +160,32 @@ class AccountBankStatementLine(models.Model):
         if not configured_company_ids:
             return
 
-        # Find the bank statement lines that are not reconciled and try to reconcile them automatically.
-        # The ones that are never be processed by the CRON before are processed first.
-        limit = batch_size + 1 if batch_size else None
-        datetime_now = fields.Datetime.now()
-        companies = self.env['res.company'].browse(configured_company_ids)
-        lock_dates = companies.filtered('fiscalyear_lock_date').mapped('fiscalyear_lock_date')
-        st_date_from_limit = max([datetime_now.date() - relativedelta(months=3)] + lock_dates)
-
         self.env['account.bank.statement.line'].flush_model()
-        domain = [
-            ('is_reconciled', '=', False),
-            ('date', '>', st_date_from_limit),
-            ('company_id', 'in', configured_company_ids),
-        ]
-        query_obj = self._search(domain, limit=limit)
-        query_obj.order = '"account_bank_statement_line"."cron_last_check" ASC NULLS FIRST,"account_bank_statement_line"."id"'
-        query_str, query_params = query_obj.select('account_bank_statement_line.id')
-        self._cr.execute(query_str, query_params)
-        st_line_ids = [r[0] for r in self._cr.fetchall()]
-        remaining_line_id = None
-        if batch_size and len(st_line_ids) > batch_size:
-            remaining_line_id = st_line_ids[batch_size]
-            st_line_ids = st_line_ids[:batch_size]
+        # we either already have statement lines to reconcile or compute them
+        st_lines, remaining_line_id = (self, None) if self else _compute_st_lines_to_reconcile(configured_company_ids)
 
-        st_lines = self.env['account.bank.statement.line'].browse(st_line_ids)
         nb_auto_reconciled_lines = 0
-        for st_line in st_lines:
+        for index, st_line in enumerate(st_lines):
+            # we want the cron to run only for limit_time seconds
+            if limit_time and fields.Datetime.now().timestamp() - start_time.timestamp() > limit_time:
+                remaining_line_id = st_line.id
+                st_lines = st_lines[:index]
+                break
             wizard = self.env['bank.rec.widget'].with_context(default_st_line_id=st_line.id).new({})
             wizard._action_trigger_matching_rules()
             if wizard.state == 'valid' and wizard.matching_rules_allow_auto_reconcile:
-                wizard.button_validate(async_action=False)
+                try:
+                    wizard.button_validate(async_action=False)
 
-                st_line.move_id.message_post(body=_(
-                    "This bank transaction has been automatically validated using the reconciliation model '%s'.",
-                    ', '.join(st_line.move_id.line_ids.reconcile_model_id.mapped('name')),
-                ))
+                    st_line.move_id.message_post(body=_(
+                        "This bank transaction has been automatically validated using the reconciliation model '%s'.",
+                        ', '.join(st_line.move_id.line_ids.reconcile_model_id.mapped('name')),
+                    ))
+                except UserError:
+                    continue
 
                 nb_auto_reconciled_lines += 1
-        st_lines.write({'cron_last_check': datetime_now})
+        st_lines.write({'cron_last_check': start_time})
 
         # The configuration seems effective since some lines has been automatically reconciled right now and there is
         # some statement lines left.

account_online_synchronization/models/account_bank_statement.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import threading
+import time
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models, tools, _
 from odoo.tools import date_utils
 
 
@@ -34,6 +36,7 @@ class AccountBankStatementLine(models.Model):
          :param online_account: The online account for this statement
          Return: The number of imported transaction for the journal
         """
+        start_time = time.time()
         line_to_reconcile = self.env['account.bank.statement.line']
         amount_sign = -1 if online_account.inverse_transaction_sign else 1
         for journal in online_account.journal_ids:
@@ -99,4 +102,15 @@ class AccountBankStatementLine(models.Model):
                 line_to_reconcile += self.env['account.bank.statement.line'].create(st_line_vals_list)
             # Set last sync date as the last transaction date
             journal.account_online_account_id.sudo().write({'last_sync': sorted_transactions[-1]['date']})
+
+            # Commit except in testing mode
+            do_commit = not (hasattr(threading.current_thread(), 'testing') and threading.current_thread().testing)
+            if do_commit:
+                self.env.cr.commit()  # if something occurs during auto reconciliation we don't want to rollback everything
+            if line_to_reconcile:
+                cron_limit_time = tools.config['limit_time_real_cron']  # default is -1
+                limit_time = (cron_limit_time if cron_limit_time > 0 else 180) - (time.time() - start_time)
+                if limit_time > 0:
+                    line_to_reconcile._cron_try_auto_reconcile_statement_lines(limit_time=limit_time)
+
         return line_to_reconcile

account_accountant/__manifest__.py
@@ -28,6 +28,7 @@ It assigns manager and user access rights to the Administrator for the accountin
         'views/account_journal_dashboard_views.xml',
         'views/account_move_views.xml',
         'views/account_payment_views.xml',
+        'views/account_reconcile_model_views.xml',
         'views/account_accountant_menuitems.xml',
         'views/digest_views.xml',
         'views/res_config_settings_views.xml',

account_accountant/i18n/account_accountant.pot
@@ -127,6 +127,13 @@ msgid "<i class=\"fa fa-check\"/> Matched"
 msgstr ""
 
 #. module: account_accountant
+#: model_terms:ir.ui.view,arch_db:account_accountant.view_account_reconcile_model_form_inherit
+msgid ""
+"<i title=\"Run manually\" role=\"img\" aria-label=\"Run manually\" class=\"fa fa-refresh\"/>\n"
+"                                Run manually"
+msgstr ""
+
+#. module: account_accountant
 #: model_terms:ir.ui.view,arch_db:account_accountant.view_account_change_lock_date
 msgid "<i>Lock all journal entries</i>"
 msgstr ""

account_accountant/models/__init__.py
@@ -7,6 +7,7 @@ from . import account_fiscal_year
 from . import account_journal_dashboard
 from . import account_move
 from . import account_payment
+from . import account_reconcile_model
 from . import digest
 from . import res_config_settings
 from . import reconciliation_widget

account_accountant/models/account_reconcile_model.py
@@ -1,0 +1,13 @@
+from odoo import models, tools
+
+
+class AccountReconcileModel(models.Model):
+    _inherit = 'account.reconcile.model'
+
+    def run_auto_reconciliation(self):
+        """ Tries to auto-reconcile as many statements as possible within time limit
+        arbitrary set to 3 minutes (the rest will be reconciled asynchronously with the regular cron).
+        """
+        cron_limit_time = tools.config['limit_time_real_cron']  # default is -1
+        limit_time = cron_limit_time if 0 < cron_limit_time < 180 else 180
+        self.env['account.bank.statement.line']._cron_try_auto_reconcile_statement_lines(limit_time=limit_time)

account_accountant/tests/test_bank_rec_widget.py
@@ -6,8 +6,8 @@ from odoo.tools import html2plaintext
 from odoo import fields, Command
 
 from freezegun import freeze_time
+from unittest.mock import patch
 import re
-
 
 @tagged('post_install', '-at_install')
 class TestBankRecWidget(TestBankRecWidgetCommon):
@@ -1925,3 +1925,70 @@ class TestBankRecWidget(TestBankRecWidgetCommon):
             {'flag': 'new_aml',         'amount_currency': 1200.0,  'balance': 400.0},
             {'flag': 'exchange_diff',   'amount_currency': 0.0,     'balance': 200.0},
         ])
+
+    def test_auto_reconcile_cron_with_time_limit(self):
+        self.env['account.reconcile.model'].search([('company_id', '=', self.company_data['company'].id)]).unlink()
+        cron = self.env.ref('account_accountant.auto_reconcile_bank_statement_line')
+        self.env['ir.cron.trigger'].search([('cron_id', '=', cron.id)]).unlink()
+
+        st_line1 = self._create_st_line(1234.0, partner_id=self.partner_a.id, date='2017-01-01')
+        self.assertEqual(len(self.env['ir.cron.trigger'].search([('cron_id', '=', cron.id)])), 1)
+        st_line2 = self._create_st_line(5678.0, partner_id=self.partner_a.id, date='2017-01-02')
+        self.assertEqual(len(self.env['ir.cron.trigger'].search([('cron_id', '=', cron.id)])), 2)
+
+        self._create_invoice_line(
+            'out_invoice',
+            invoice_date='2017-01-01',
+            invoice_line_ids=[{'price_unit': 1234.0}],
+        )
+        self._create_invoice_line(
+            'out_invoice',
+            invoice_date='2017-01-01',
+            invoice_line_ids=[{'price_unit': 5678.0}],
+        )
+        self.env['account.reconcile.model'].create({
+            'name': "test_auto_reconcile_cron_with_time_limit",
+            'rule_type': 'writeoff_suggestion',
+            'auto_reconcile': True,
+            'line_ids': [Command.create({'account_id': self.account_revenue1.id})],
+        })
+
+        with freeze_time('2017-01-01 00:00:00') as frozen_time:
+            def datetime_now_override():
+                frozen_time.tick()
+                return frozen_time()
+            with patch('odoo.fields.Datetime.now', side_effect=datetime_now_override):
+                # we simulate that the time limit is reached after first loop
+                self.env['account.bank.statement.line']._cron_try_auto_reconcile_statement_lines(limit_time=1)
+        # after first loop, only one statement should be reconciled
+        self.assertRecordValues(st_line1, [{'is_reconciled': True, 'cron_last_check': fields.Datetime.from_string('2017-01-01 00:00:01')}])
+        # the other one should be in queue for regular cron tigger
+        self.assertRecordValues(st_line2, [{'is_reconciled': False, 'cron_last_check': False}])
+        self.assertEqual(len(self.env['ir.cron.trigger'].search([('cron_id', '=', cron.id)])), 3)
+
+    def test_auto_reconcile_cron_with_provided_statements_lines(self):
+        self.env['account.reconcile.model'].search([('company_id', '=', self.company_data['company'].id)]).unlink()
+
+        st_line1 = self._create_st_line(1234.0, partner_id=self.partner_a.id, date='2017-01-01')
+        st_line2 = self._create_st_line(5678.0, partner_id=self.partner_a.id, date='2017-01-02')
+        self._create_invoice_line(
+            'out_invoice',
+            invoice_date='2017-01-01',
+            invoice_line_ids=[{'price_unit': 1234.0}],
+        )
+        self._create_invoice_line(
+            'out_invoice',
+            invoice_date='2017-01-01',
+            invoice_line_ids=[{'price_unit': 5678.0}],
+        )
+        self.env['account.reconcile.model'].create({
+            'name': "test_auto_reconcile_cron_with_time_limit",
+            'rule_type': 'writeoff_suggestion',
+            'auto_reconcile': True,
+            'line_ids': [Command.create({'account_id': self.account_revenue1.id})],
+        })
+        with freeze_time('2017-01-01 00:00:00'):
+            # we call auto reconcile on st_lines1 **only**
+            st_line1._cron_try_auto_reconcile_statement_lines()
+        self.assertRecordValues(st_line1, [{'is_reconciled': True, 'cron_last_check': fields.Datetime.from_string('2017-01-01 00:00:00')}])
+        self.assertRecordValues(st_line2, [{'is_reconciled': False, 'cron_last_check': False}])

account_accountant/views/account_reconcile_model_views.xml
@@ -1,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<odoo>
+    <data>
+        <record id="account_reconcile_model_form_inherit_account_accountant" model="ir.ui.view">
+            <field name="name">account.reconcile.model.form.inherit.account_accountant</field>
+            <field name="model">account.reconcile.model</field>
+            <field name="inherit_id" ref="account.view_account_reconcile_model_form"/>
+            <field name="arch" type="xml">
+                <xpath expr="//field[@name='auto_reconcile']" position="replace">
+                    <label for="auto_reconcile" attrs="{'invisible': [('rule_type', '=', 'writeoff_button')]}"/>
+                    <div class="o_row" attrs="{'invisible': [('rule_type', '=', 'writeoff_button')]}">
+                        <field name="auto_reconcile"/>
+                        <button name="run_auto_reconciliation" attrs="{'invisible': [('auto_reconcile', '=', False)]}" type="object" class="btn-link">
+                            <i title="Run manually" role="img" aria-label="Run manually" class="fa fa-refresh"/>
+                            Run manually
+                        </button>
+                    </div>
+                </xpath>
+            </field>
+        </record>
+    </data>
+</odoo>

commit 7cdd9c0797f5bf5ebac1cda94794c793b289f03f

[FIX] account_accountant: avoid infinite execution of the cron
Issue:
When no statement lines were auto reconciled, the false increment of counter variable
to update the count of number of reconciled lines causes the cron to execute infinite
time as it triggers the same cron by checking the same counter variable.

This regression is mainly caused by the commit 0d7d1a2 where some statements were moved
inside try...catch statement but forgot to move the counter variable along with it. As
a result, it could falsely increment the counter variable when the UserError exeception
raises.

Fix:
Updated the reconciled line count variable when some lines were actually reconciled.

closes odoo/enterprise#50268

Signed-off-by: Laurent Smet (las) <las@odoo.com>

account_accountant/models/account_bank_statement.py
@@ -178,10 +178,10 @@ class AccountBankStatementLine(models.Model):
                             "This bank transaction has been automatically validated using the reconciliation model '%s'.",
                             ', '.join(st_line.move_id.line_ids.reconcile_model_id.mapped('name')),
                         ))
+                        nb_auto_reconciled_lines += 1
                 except UserError:
                     continue
 
-                nb_auto_reconciled_lines += 1
         st_lines.write({'cron_last_check': start_time})
 
         # The configuration seems effective since some lines has been automatically reconciled right now and there is
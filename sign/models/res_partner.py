6b20590f34148c74dd0ca611facb09f59c0a93c8
Thomas Lefebvre (thle) <thle@odoo.com>
2023-11-17 14:28:10 +0100

    [FIX] sign: ignore archived signature requests
    Issue:
    ------
    The purpose of the `signature_count` field is to count the number of signature requests.
    However, it counts signatures that have been archived.
    For example, in the recruitment application, when we want to see the number of requests
    sent to a job application.
    
    Solution:
    ---------
    Modify the domain to take status into account and display only signatures that are `sent` and `completed`.
    
    opw-3550893
    
    closes odoo/enterprise#50907
    
    Signed-off-by: Arnaud Joset (arj) <arj@odoo.com>

--- sign/models/res_partner.py
+++ sign/models/res_partner.py
@@ -10,7 +10,7 @@ class ResPartner(models.Model):
     signature_count = fields.Integer(compute='_compute_signature_count', string="# Signatures")
 
     def _compute_signature_count(self):
-        signature_data = self.env['sign.request.item'].sudo().read_group([('partner_id', 'in', self.ids)], ['partner_id'], ['partner_id'])
+        signature_data = self.env['sign.request.item'].sudo().read_group([('partner_id', 'in', self.ids), ('state', 'in', ['sent', 'completed'])], ['partner_id'], ['partner_id'])
         signature_data_mapped = dict((data['partner_id'][0], data['partner_id_count']) for data in signature_data)
         for partner in self:
             partner.signature_count = signature_data_mapped.get(partner.id, 0)

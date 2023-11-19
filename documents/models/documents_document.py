Raniani-lab/enterpriise

commit 80db35688eddb1b817c47af2b89a0409a5cc83be

[IMP] documents: set contact on incoming document by email
If not explicitly defined on the documents.share, when receiving a document by
email, we set the contact to the partner corresponding to the email sender. If
no partner exists with that email, a new one is created.

Based on preliminary work of

Task-2478266

closes odoo/enterprise#39292

Signed-off-by: Stéphane Debauche (std) <std@odoo.com>
Co-authored-by: ThanhDodeurOdoo <tso@odoo.com>

documents/models/documents_document.py
@@ -313,15 +313,16 @@ class Document(models.Model):
         share = self.create_share_id
         if share:
             attachments = self.env['ir.attachment'].browse([x[1] for x in m2m_commands])
-            for attachment in attachments:
-                document = self.env['documents.document'].create({
-                    'name': attachment.name,
-                    'attachment_id': attachment.id,
-                    'folder_id': share.folder_id.id,
-                    'owner_id': share.owner_id.id if share.owner_id else share.create_uid.id,
-                    'partner_id': share.partner_id.id if share.partner_id else False,
-                    'tag_ids': [(6, 0, share.tag_ids.ids if share.tag_ids else [])],
-                })
+            partner = share.partner_id.id or self.env['res.partner'].find_or_create(msg_vals['email_from']).id
+            documents = self.env['documents.document'].create([{
+                'name': attachment.name,
+                'attachment_id': attachment.id,
+                'folder_id': share.folder_id.id,
+                'owner_id': share.owner_id.id or share.create_uid.id,
+                'partner_id': partner,
+                'tag_ids': [(6, 0, share.tag_ids.ids or [])],
+            } for attachment in attachments])
+            for (attachment, document) in zip(attachments, documents):
                 attachment.write({
                     'res_model': 'documents.document',
                     'res_id': document.id,

documents/tests/test_mail_gateway.py
@@ -1,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from odoo import tools
+from odoo.addons.mail.tests.common import MailCommon
+from odoo.addons.test_mail.data.test_mail_data import MAIL_EML_ATTACHMENT
+
+
+class TestMailGateway(MailCommon):
+    """ Test document creation on incoming mail.
+
+    Especially that the partner_id is correctly set on the created document.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls._init_mail_gateway()
+        cls.folder = cls.env['documents.folder'].create({
+            'name': 'folder',
+        })
+        cls.share_link = cls.env['documents.share'].create({
+            'folder_id': cls.folder.id,
+            'name': 'share_link_folder',
+            'email_drop': True,
+            'alias_name': 'shareFolder',
+        })
+        cls.alias = cls.env['mail.alias'].create({
+            'alias_name': 'inbox-test',
+            'alias_user_id': False,
+            'alias_model_id': cls.env['ir.model']._get('documents.document').id,
+            'alias_parent_model_id': cls.env['ir.model']._get('documents.share').id,
+            'alias_parent_thread_id': cls.share_link.id,
+            'alias_defaults': f"{{'folder_id': {cls.folder.id}, 'create_share_id': {cls.share_link.id}}}",
+            'alias_contact': 'everyone',
+        })
+        cls.email_with_no_partner = tools.email_normalize('non-existing@test.com')
+        cls.pre_existing_partner = cls.env['res.partner'].find_or_create('existing@test.com')
+        cls.default_partner = cls.env['res.partner'].find_or_create('default@test.com')
+        cls.email_filenames = ['attachment', 'original_msg.eml']
+
+    def send_test_mail_with_attachment(self, email_from):
+        with self.mock_mail_gateway():
+            self.format_and_process(
+                MAIL_EML_ATTACHMENT,
+                email_from,
+                'inbox-test@test.com',
+                subject='Test document creation on incoming mail',
+                target_model='documents.document',
+            )
+        documents = self.env['documents.document'].search([('name', 'in', self.email_filenames)])
+        self.assertEqual(len(documents), len(self.email_filenames))
+        return documents
+
+    def test_initial_values(self):
+        self.assertFalse(self.env['res.partner'].search([('email', '=', self.email_with_no_partner)]))
+        self.assertFalse(self.env['documents.document'].search([('name', 'in', self.email_filenames)]))
+
+    def test_set_contact_non_existing_partner(self):
+        for document in self.send_test_mail_with_attachment(self.email_with_no_partner):
+            self.assertTrue(document.partner_id)
+            self.assertEqual(document.partner_id.email, self.email_with_no_partner)
+
+    def test_set_contact_existing_partner(self):
+        for document in self.send_test_mail_with_attachment(self.pre_existing_partner.email):
+            self.assertEqual(document.partner_id, self.pre_existing_partner)
+
+    def test_set_contact_default_partner_non_existing_partner(self):
+        self.share_link.partner_id = self.default_partner
+        for document in self.send_test_mail_with_attachment(self.email_with_no_partner):
+            self.assertEqual(document.partner_id, self.default_partner)
+
+    def test_set_contact_default_partner_existing_partner(self):
+        self.share_link.partner_id = self.default_partner
+        for document in self.send_test_mail_with_attachment(self.pre_existing_partner.email):
+            self.assertEqual(document.partner_id, self.default_partner)

documents/tests/__init__.py
@@ -7,4 +7,5 @@ from . import test_documents_folder
 from . import test_documents_multipage
 from . import test_documents_tag
 from . import test_mail_activity
+from . import test_mail_gateway
 from . import test_security

commit 640ecb06a9f792da2ce18664fbdc7ce48bc6f958

[FIX] documents: avoid partner creation when receiving documents
When receiving a document from an unknown contact, we no longer create a new
partner. However, if the contact is known, we assign the already existing
partner to the document.

We remove that feature because it was creating duplicate partners for documents
sent to accounting which uses VAT number as primary matching criterion. Contacts
created in Documents were messing up with the ones associated with the VAT
numbers.

Issue introduced in the commit: 80db35688eddb1b817c47af2b89a0409a5cc83be.

Task-3579869

closes odoo/enterprise#50810

X-original-commit: 0354c64c6070e376e1c7faca48cddb11aaa56ad9
Signed-off-by: Stéphane Debauche (std) <std@odoo.com>
Signed-off-by: Pierre-Yves Dufays (pydu) <pydu@odoo.com>

documents/models/documents_document.py
@@ -364,13 +364,12 @@ class Document(models.Model):
         share = self.create_share_id
         if share and not self.env.context.get("no_document") or message.message_type == 'email':
             attachments = self.env['ir.attachment'].browse([x[1] for x in m2m_commands])
-            partner = share.partner_id.id or self.env['res.partner'].find_or_create(msg_vals['email_from']).id
             documents = self.env['documents.document'].create([{
                 'name': attachment.name,
                 'attachment_id': attachment.id,
                 'folder_id': share.folder_id.id,
                 'owner_id': share.owner_id.user_ids[0].id if share.owner_id.user_ids else share.create_uid.id,
-                'partner_id': partner,
+                'partner_id': share.partner_id.id or msg_vals.get('author_id'),
                 'tag_ids': [(6, 0, share.tag_ids.ids or [])],
             } for attachment in attachments])
             for (attachment, document) in zip(attachments, documents):

documents/tests/test_mail_gateway.py
@@ -55,8 +55,7 @@ class TestMailGateway(MailCommon):
 
     def test_set_contact_non_existing_partner(self):
         for document in self.send_test_mail_with_attachment(self.email_with_no_partner):
-            self.assertTrue(document.partner_id)
-            self.assertEqual(document.partner_id.email, self.email_with_no_partner)
+            self.assertFalse(document.partner_id)
 
     def test_set_contact_existing_partner(self):
         for document in self.send_test_mail_with_attachment(self.pre_existing_partner.email):